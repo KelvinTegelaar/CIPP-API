e0f91a2cd24b594795d0b7f7189e96a4a0f58976
Kelvin Tegelaar <kelvin@limenetworks.nl>
2021-10-16 20:05:32 +0200

    textual changes

--- AddUser/run.ps1
+++ AddUser/run.ps1
@@ -8,7 +8,7 @@ $user = $request.headers.'x-ms-client-principal'
 # Write to the Azure Functions log stream.
 Write-Host "PowerShell HTTP trigger function processed a request."
 try {
-    $licenses = ($userobj | select-object "License_*").psobject.properties.value
+    $licenses = ($userobj | Select-Object "License_*").psobject.properties.value
     $aliasses = ($userobj.AddedAliasses).Split([Environment]::NewLine)
     $password = if ($userobj.password) { $userobj.password } else { -join ('abcdefghkmnrstuvwxyzABCDEFGHKLMNPRSTUVWXYZ23456789$%&*#'.ToCharArray() | Get-Random -Count 12) }
     $UserprincipalName = "$($UserObj.username)@$($UserObj.domain)"
@@ -24,9 +24,9 @@ try {
             "forceChangePasswordNextSignIn" = [bool]$UserObj.mustchangepass
             "password"                      = $password
         }
-    } | convertto-json
+    } | ConvertTo-Json
     $GraphRequest = New-GraphPostRequest -uri "https://graph.microsoft.com/beta/users" -tenantid $Userobj.tenantid-type POST -body $BodyToship   -verbose
-        Log-Request -user $request.headers.'x-ms-client-principal'   -message "Created user $($userobj.displayname) with id $($GraphRequest.id) for $($UserObj.tenantid)" -Sev "Info"
+    Log-Request -user $request.headers.'x-ms-client-principal'   -message "Created user $($userobj.displayname) with id $($GraphRequest.id) for $($UserObj.tenantid)" -Sev "Info"
 
 }
 catch {
@@ -46,28 +46,28 @@ try {
         $LicRequest = New-GraphPostRequest -uri "https://graph.microsoft.com/beta/users/$($GraphRequest.id)/assignlicense" -tenantid $Userobj.tenantid -type POST -body $LicenseBody -verbose
         Log-Request -user $request.headers.'x-ms-client-principal'   -message "Assigned user $($userobj.displayname) license $($licences)" -Sev "Info"
     }
-    $body = [pscustomobject]@{"Results" = "Success. User has been created. The password is $password" }
+    $body = [pscustomobject]@{"Results" = "Success.  <br> Username: $($UserprincipalName) <br>Password: $password" }
 }
 catch {
     Log-Request -user $request.headers.'x-ms-client-principal'   -message "License assign API failed. $($_.Exception.Message)" -Sev "Error"
-    $body = [pscustomobject]@{"Results" = "Succesfully created user. The password is $password. We've failed to assign the license. $($_.Exception.Message)" }
+    $body = [pscustomobject]@{"Results" = "Succesfully created user.  <br> Username: $($UserprincipalName) <br>Password: $password <br> We've failed to assign the license. $($_.Exception.Message)" }
 }
 
 try {
     if ($aliasses) {
         foreach ($Alias in $aliasses) {
-            write-host $Alias
+            Write-Host $Alias
             New-GraphPostRequest -uri "https://graph.microsoft.com/beta/users/$($GraphRequest.id)" -tenantid $Userobj.tenantid -type "patch" -body "{`"mail`": `"$Alias`"}" -verbose
         }
         New-GraphPostRequest -uri "https://graph.microsoft.com/beta/users/$($GraphRequest.id)" -tenantid $Userobj.tenantid -type "patch" -body "{`"mail`": `"$UserprincipalName`"}" -verbose
 
     }
     Log-Request -user $request.headers.'x-ms-client-principal'   -message "Added aliasses to $($userobj.displayname) license $($licences)" -Sev "Info"
-    $body = [pscustomobject]@{"Results" = "Success. Uses has been created. The password is $password" }
+    $body = [pscustomobject]@{"Results" = "Success. User has been created. <br> Username: $($UserprincipalName) <br>Password: $password" }
 }
 catch {
     Log-Request -user $request.headers.'x-ms-client-principal'   -message "Alias API failed. $($_.Exception.Message)" -Sev "Error"
-    $body = [pscustomobject]@{"Results" = "Succesfully created user. The password is $password. We've failed to create the aliasses: $($_.Exception.Message)" }
+    $body = [pscustomobject]@{"Results" = "Succesfully created user. <br> Username: $($UserprincipalName) <br>Password: $password <br> We've failed to create the aliasses: $($_.Exception.Message)" }
 }
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
