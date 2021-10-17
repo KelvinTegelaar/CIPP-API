b3c65c97319da66967340533b6715e044b017bd0
msp4msps <msp4msps@tminus365.com>
2021-10-17 14:45:04 -0600

    Add ListMFA report Function

---
+++ ListMFAStats/run.ps1
@@ -0,0 +1,35 @@
+using namespace System.Net
+
+# Input bindings are passed in via param block.
+param($Request, $TriggerMetadata)
+
+# Write to the Azure Functions log stream.
+Write-Host "PowerShell HTTP trigger function processed a request."
+
+# Interact with query parameters or the body of the request.
+$TenantFilter = $Request.Query.TenantFilter
+$PerUserMFA = Get-MsolUser -all -TenantId $TenantFilter | Where-Object {$_.isLicensed -eq ”TRUE”} | Select-Object DisplayName,isLicensed,UserPrincipalName,@{N="MFA Status"; E={if( $_.StrongAuthenticationRequirements.State -ne $null) {$_.StrongAuthenticationRequirements.State} else { "Disabled"}}}
+
+
+try{
+  $GraphRequest = New-GraphGetRequest -uri "https://graph.microsoft.com/beta/reports/credentialUserRegistrationDetails" -tenantid $TenantFilter | Select-Object userPrincipalName, isMfaRegistered
+}catch {
+ $GraphRequest = "null"
+}
+
+
+$response = foreach ($user in $PerUserMFA ) {
+  [PSCustomObject]@{
+      'Display Name'                                          = $user.DisplayName
+      'UserPrincipalName'                                     = $user.userPrincipalName
+      "Legacy MFA Enabled"                                    = $user.'MFA Status'
+      "MFA Registered through Security Defaults or CA Policy" = ($GraphRequest | Where-Object { $_.UserPrincipalName -eq $user.userPrincipalName}).isMfaRegistered
+      "isLicensed"                                            = $user.isLicensed
+  }
+
+}
+# Associate values to output bindings by calling 'Push-OutputBinding'.
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+        StatusCode = [HttpStatusCode]::OK
+        Body       = @($response)
+    })
