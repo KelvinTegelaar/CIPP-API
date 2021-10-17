a45942818dab8fb7f14f4f3fb06168b5ea25b2ec
Kelvin Tegelaar <kelvin@limenetworks.nl>
2021-10-17 12:24:20 +0200

    added clear tenant cache option

--- ListTenants/run.ps1
+++ ListTenants/run.ps1
@@ -2,18 +2,28 @@ using namespace System.Net
 
 # Input bindings are passed in via param block.
 param($Request, $TriggerMetadata)
+
 $Skiplist = (Get-Content ExcludedTenants -ErrorAction SilentlyContinue | ConvertFrom-Csv -Delimiter "|" -Header "name", "date", "user").name
 # Write to the Azure Functions log stream.
 $cachefile = 'tenants.cache.json'
 $Testfile = Get-Item  $cachefile  -ErrorAction SilentlyContinue | Where-Object -Property LastWriteTime -GT (Get-Date).Addhours(-24)
 if ($Testfile) {
-    Write-Host "using cache"
-    $GraphRequest = Get-Content $cachefile | ConvertFrom-Json
-    Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-            StatusCode = [HttpStatusCode]::OK
-            Body       = $GraphRequest | Where-Object -Property DefaultdomainName -NotIn $Skiplist
-        })
-    exit
+    if ($request.Query.ClearCache -eq "true") {
+        Remove-Item $cachefile -Force 
+        $GraphRequest = [pscustomobject]@{"Results" = "Successfully completed request." }
+        Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+                StatusCode = [HttpStatusCode]::OK
+                Body       = $GraphRequest
+            })
+    }
+    else {
+        $GraphRequest = Get-Content $cachefile | ConvertFrom-Json
+        Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+                StatusCode = [HttpStatusCode]::OK
+                Body       = $GraphRequest | Where-Object -Property DefaultdomainName -NotIn $Skiplist
+            })
+        exit
+    }
 }
 else {
     Write-Host "Grabbing all tenants via Graph API" -ForegroundColor Green
