9a1972ac3c549a5b101d62d0a41736ac618a8c5b
Kelvin Tegelaar <kelvin@limenetworks.nl>
2021-10-19 12:15:46 +0200

    updated log retreive api

--- Logs/run.ps1
+++ Logs/run.ps1
@@ -5,12 +5,12 @@ param($Request, $TriggerMetadata)
 
 $APIName = $TriggerMetadata.FunctionName
 Log-Request -user $request.headers.'x-ms-client-principal' -API $APINAME  -message "Accessed this API" -Sev "Debug"
-
+$LogLevel = if ($Request.Query.LogLevel) { ($Request.query.Loglevel).split(',') } else { "Info", "Warn", "Error", "Critical" }
 # Write to the Azure Functions log stream.
 Write-Host "PowerShell HTTP trigger function processed a request."
 # Interact with query parameters or the body of the request.
 $date = if ($Request.Query.DateFilter) { $Request.query.DateFilter } else { (Get-Date).ToString('MMyyyy') }
-$ReturnedLog = Get-Content "$($date).log" | ConvertFrom-Csv -Header "DateTime", "Tenant", "API", "Message", "User", "Severity" -Delimiter "|"
+$ReturnedLog = Get-Content "$($date).log" | ConvertFrom-Csv -Header "DateTime", "Tenant", "API", "Message", "User", "Severity" -Delimiter "|" | Where-Object -Property Severity -In $LogLevel
 
 if ($request.query.last) {
     $ReturnedLog = $ReturnedLog | Select-Object -Last $request.query.last
