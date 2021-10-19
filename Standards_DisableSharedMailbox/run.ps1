487764ff3ae735f8013c3517f8e3f4a66f5f070b
Kelvin Tegelaar <kelvin@limenetworks.nl>
2021-10-19 22:16:55 +0200

    added disableSharedMailbox standard

--- Standards_DisableSharedMailbox/run.ps1
+++ Standards_DisableSharedMailbox/run.ps1
@@ -5,7 +5,6 @@ try {
         New-GraphPOSTRequest -uri "https://graph.microsoft.com/v1.0/users/$($_.ObjectKey)" -type "PATCH" -body '{"accountEnabled":"false"}' -tenantid $tenant
     }
     Log-request -API "Standards" -tenant $tenant -message "Unified Audit Log Enabled." -sev Info
-
 }
 catch {
     Log-request -API "Standards" -tenant $tenant -message "Failed to apply Unified Audit Log. Error: $($_.exception.message)" -sev Error
