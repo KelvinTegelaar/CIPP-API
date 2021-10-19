633cf7372bf1740c0a462d6c8411d10a3c50cefb
Gavsto <gavsto@gmail.com>
2021-10-19 23:27:30 +0100

    FEATURE: Added Version Tracking
    Added version tracking on Dashboard

---
+++ GetVersion/run.ps1
@@ -0,0 +1,18 @@
+using namespace System.Net
+
+# Input bindings are passed in via param block.
+param($Request, $TriggerMetadata)
+
+$APIName = $TriggerMetadata.FunctionName
+Log-Request -user $request.headers.'x-ms-client-principal' -API $APINAME  -message "Accessed this API" -Sev "Debug"
+
+$Version = Get-Content "version_latest.txt"
+
+# Write to the Azure Functions log stream.
+Write-Host "Version is $($Version)"
+
+# Associate values to output bindings by calling 'Push-OutputBinding'.
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+        StatusCode = [HttpStatusCode]::OK
+        Body       = $Version
+    })
