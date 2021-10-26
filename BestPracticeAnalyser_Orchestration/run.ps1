4db0790dda661f1c64fc26a49a36361a40550362
Gavsto <gavsto@gmail.com>
2021-10-26 20:28:57 +0000

    NEW FEATURE: Best Practice Analyser
    NEW FEATURE: Best Practice Analyser

--- BestPracticeAnalyser_Orchestration/run.ps1
+++ BestPracticeAnalyser_Orchestration/run.ps1
@@ -1,25 +1,18 @@
 param($Context)
 
-
+Log-request  -API "BestPracticeAnalyser" -tenant $tenant -message "Best Practice Analyser has Started" -sev Info
 
 $Batch = (Invoke-ActivityFunction -FunctionName 'BestPracticeAnalyser_GetQueue' -Input 'LetsGo')
 $ParallelTasks = foreach ($Item in $Batch) {
   Invoke-DurableActivity -FunctionName "BestPracticeAnalyser_All" -Input $item -NoWait
-  #Invoke-ActivityFunction -FunctionName "BestPracticeAnalyser_All" -Input $Item
 }
 
 $Outputs = Wait-ActivityFunction -Task $ParallelTasks
-#Write-Host $Outputs
 
-#Write-Host "FINAL THING $($ParallelTasks | Out-String)"
-#foreach ($item in $ParallelTasks) {
   foreach ($item in $Outputs) {
   write-host $Item | Out-String
   $Object = $Item | ConvertTo-Json
   Set-Content "Cache_BestPracticeAnalyser\$($item.tenant).BestPracticeAnalysis.json" -Value $Object -Force
 }
 
-
-
-
-Log-request  -API "Standards" -tenant $tenant -message "$($Outputs)" -sev Info
+Log-request  -API "BestPracticeAnalyser" -tenant $tenant -message "Best Practice Analyser has Finished" -sev Info
