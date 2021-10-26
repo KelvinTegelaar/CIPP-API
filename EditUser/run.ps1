c9581205039f464439995a378c872410a92cdb2c
Kelvin Tegelaar <kelvin@limenetworks.nl>
2021-10-26 12:45:18 +0200

    cleaned up error handling and reporting. added copy groups

--- EditUser/run.ps1
+++ EditUser/run.ps1
@@ -6,12 +6,14 @@ param($Request, $TriggerMetadata)
 $APIName = $TriggerMetadata.FunctionName
 Log-Request -user $request.headers.'x-ms-client-principal' -API $APINAME  -message "Accessed this API" -Sev "Debug"
 $userobj = $Request.body
+$Results = [System.Collections.ArrayList]@()
 $licenses = ($userobj | Select-Object "License_*").psobject.properties.value
 # Write to the Azure Functions log stream.
 Write-Host "PowerShell HTTP trigger function processed a request."
 #Edit the user
 try {
     $Email = "$($UserObj.username)@$($UserObj.domain)"
+    $UserprincipalName = "$($UserObj.username)@$($UserObj.domain)"
     $BodyToship = [pscustomobject] @{
         "givenName"         = $userobj.firstname
         "surname"           = $userobj.lastname
@@ -35,16 +37,16 @@ try {
         $_ | Select-Object -Property $NonEmptyProperties | ConvertTo-Json
     }
     $GraphRequest = New-GraphPostRequest -uri "https://graph.microsoft.com/beta/users/$($userobj.Userid)" -tenantid $Userobj.tenantid -type PATCH -body $BodyToship  -verbose
-    $body = [pscustomobject]@{"Results" = "Success. The user has been edited." }
+    $results.add( "Success. The user has been edited." )
     if ($userobj.password) {
         $passwordProfile = [pscustomobject] @{"passwordProfile" = @{ "password" = $userobj.password } } | ConvertTo-Json
         $GraphRequest = New-GraphPostRequest -uri "https://graph.microsoft.com/beta/users/$($userobj.Userid)" -tenantid $Userobj.tenantid -type PATCH -body $PasswordProfile  -verbose
-        $body = [pscustomobject]@{"Results" = "Success. The password has been set to $($userobj.password)" }
+        $results.add("Success. The password has been set to $($userobj.password)")
     }
 }
 catch {
     Log-Request -API $APINAME -tenant ($UserObj.tenantid) -user $request.headers.'x-ms-client-principal' -message "User edit API failed. $($_.Exception.Message)" -Sev "Error"
-    $body = [pscustomobject]@{"Results" = "Failed to create user. $($_.Exception.Message)" }
+    $results.add( "Failed to edit user. $($_.Exception.Message)")
     Log-Request -API $APINAME -tenant ($UserObj.tenantid) -user $request.headers.'x-ms-client-principal' -message "Edited user $($userobj.displayname) with id $($userobj.Userid)" -Sev "Info"
 }
 
@@ -63,13 +65,13 @@ try {
         $LicRequest = New-GraphPostRequest -uri "https://graph.microsoft.com/beta/users/$($userobj.Userid)/assignlicense" -tenantid $Userobj.tenantid -type POST -body $LicenseBody -verbose
 
         Log-Request -API $APINAME -tenant ($UserObj.tenantid) -user $request.headers.'x-ms-client-principal' -message "Changed user $($userobj.displayname) license. Sent info: $licensebody" -Sev "Info"
-        $body = [pscustomobject]@{"Results" = "Success. User license has been edited." }
+        $results.add( "Success. User license has been edited." )
     }
 
 }
 catch {
     Log-Request -API $APINAME -tenant ($UserObj.tenantid) -user $request.headers.'x-ms-client-principal' -message "License assign API failed. $($_.Exception.Message)" -Sev "Error"
-    $body = [pscustomobject]@{"Results" = "Succesfully edit user. The password is $password. We've failed to assign the license. $($_.Exception.Message)" }
+    $results.add( "Succesfully edit user. The password is $password. We've failed to assign the license. $($_.Exception.Message)")
 }
 
 #Add aliasses, removal currently not supported.
@@ -80,15 +82,34 @@ try {
         }
         New-GraphPostRequest -uri "https://graph.microsoft.com/beta/users/$($userobj.Userid)" -tenantid $Userobj.tenantid -type "patch" -body "{`"mail`": `"$UserprincipalName`"}" -verbose
         Log-Request -API $APINAME -tenant ($UserObj.tenantid) -user $request.headers.'x-ms-client-principal'   -message "Added aliasses to $($userobj.displayname) license $($licences)" -Sev "Info"
-        $body = [pscustomobject]@{"Results" = "Success. User has been edited" }
+        $results.add( "Success. User has been edited")
     }
 
 }
 catch {
     Log-Request -API $APINAME -tenant ($UserObj.tenantid) -user $request.headers.'x-ms-client-principal'   -message "Alias API failed. $($_.Exception.Message)" -Sev "Error"
-    $body = [pscustomobject]@{"Results" = "Succesfully edited user. The password is $password. We've failed to create the aliasses: $($_.Exception.Message)" }
+    $results.add( "Succesfully edited user. The password is $password. We've failed to create the aliasses: $($_.Exception.Message)")
 }
 
+if ($Request.body.CopyFrom -ne "") {
+    $MemberIDs = "https://graph.microsoft.com/v1.0/directoryObjects/" + (New-GraphGetRequest -uri "https://graph.microsoft.com/beta/users/$($userobj.Userid)" -tenantid $Userobj.tenantid).id 
+    $addmemberbody = "{ `"members@odata.bind`": $(ConvertTo-Json @($MemberIDs)) }"
+        (New-GraphPostRequest -uri "https://graph.microsoft.com/beta/users/$($Request.body.CopyFrom)/GetMemberGroups" -tenantid $Userobj.tenantid -type POST -body  '{"securityEnabledOnly": false}').value | ForEach-Object {
+        try {
+            $groupname = (New-GraphGetRequest -tenantid $Userobj.tenantid -asApp $true -uri "https://graph.microsoft.com/beta/groups/$($_)").displayName
+            Write-Host "name: $groupname"
+            $GroupResult = New-GraphPostRequest -AsApp $true -uri "https://graph.microsoft.com/beta/groups/$($_)" -tenantid $Userobj.tenantid -type patch -body $addmemberbody -Verbose
+            Log-Request -user $request.headers.'x-ms-client-principal' -API $APINAME  -message "Added $($UserprincipalName) to group $groupresult.displayName)" -Sev "Info"  -tenant $TenantFilter
+            $body = $results.add("Added group: $($groupname)")
+        }
+        catch {
+            $body = $results.add("We've failed to add the group $($groupname): $($_.Exception.Message)")
+            Log-Request -user $request.headers.'x-ms-client-principal' -API $APINAME -tenant $($userobj.tenantid) -message "Alias API failed. $($_.Exception.Message)" -Sev "Error"
+        }
+    }
+
+}
+$body = @{"Results" = ($results -join "<br>") }
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
         StatusCode = [HttpStatusCode]::OK
