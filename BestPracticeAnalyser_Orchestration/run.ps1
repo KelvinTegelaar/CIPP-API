bf549f9de72e611ba83ede840eebb91da3e340b5
Kelvin Tegelaar <kelvin@limenetworks.nl>
2021-10-27 20:05:26 +0200

    prevention of running multiple instances of the BPA

--- BestPracticeAnalyser_Orchestration/run.ps1
+++ BestPracticeAnalyser_Orchestration/run.ps1
@@ -2,7 +2,7 @@ param($Context)
 
 Log-request  -API "BestPracticeAnalyser" -tenant $tenant -message "Best Practice Analyser has Started" -sev Info
 New-Item "Cache_BestPracticeAnalyser" -ItemType Directory -ErrorAction SilentlyContinue
-
+New-Item "Cache_BestPracticeAnalyser\CurrentlyRunning.txt" -ItemType File -Force
 $Batch = (Invoke-ActivityFunction -FunctionName 'BestPracticeAnalyser_GetQueue' -Input 'LetsGo')
 $ParallelTasks = foreach ($Item in $Batch) {
   Invoke-DurableActivity -FunctionName "BestPracticeAnalyser_All" -Input $item -NoWait
@@ -10,11 +10,12 @@ $ParallelTasks = foreach ($Item in $Batch) {
 
 $Outputs = Wait-ActivityFunction -Task $ParallelTasks
 
-  foreach ($item in $Outputs) {
-  write-host $Item | Out-String
+foreach ($item in $Outputs) {
+  Write-Host $Item | Out-String
   $Object = $Item | ConvertTo-Json
 
   Set-Content "Cache_BestPracticeAnalyser\$($item.tenant).BestPracticeAnalysis.json" -Value $Object -Force
 }
 
 Log-request  -API "BestPracticeAnalyser" -tenant $tenant -message "Best Practice Analyser has Finished" -sev Info
+Remove-Item "Cache_BestPracticeAnalyser\CurrentlyRunning.txt" -Force
--- BestPracticeAnalyser_OrchestrationStarter/run.ps1
+++ BestPracticeAnalyser_OrchestrationStarter/run.ps1
@@ -2,11 +2,23 @@ using namespace System.Net
 
 param($Request, $TriggerMetadata)
 
-$InstanceId = Start-NewOrchestration -FunctionName 'BestPracticeAnalyser_Orchestration'
-Write-Host "Started orchestration with ID = '$InstanceId'"
+$CurrentlyRunning = Test-Path "Cache_BestPracticeAnalyser\CurrentlyRunning.txt" | Where-Object -Property LastWriteTime -GT (Get-Date).addhours(-24)
+if ($CurrentlyRunning -eq $false) {
+    $InstanceId = Start-NewOrchestration -FunctionName 'BestPracticeAnalyser_Orchestration'
+    Write-Host "Started orchestration with ID = '$InstanceId'"
+    $Orchestrator = New-OrchestrationCheckStatusResponse -Request $Request -InstanceId $InstanceId
+    Log-request  -API "BestPracticeAnalyser" -message "Started retrieving best practice information" -sev Info
+    $Results = [pscustomobject]@{"Results" = "Started running analysis" }
+}
+else {
+    $Results = [pscustomobject]@{"Results" = "Already running. Please wait for the current instance to finish" }
+    Log-request  -API "BestPracticeAnalyser" -message "Attempted to start analysis but an instance was already running." -sev Info
 
-$Response = New-OrchestrationCheckStatusResponse -Request $Request -InstanceId $InstanceId
+}
+Write-Host ($Orchestrator | ConvertTo-Json)
 
-Write-Host ($Response | ConvertTo-Json)
 
-Log-request  -API "BestPracticeAnalyser" -message "Started retrieving best practice information" -sev Info
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+        StatusCode = [HttpStatusCode]::OK
+        Body       = $results
+    })
