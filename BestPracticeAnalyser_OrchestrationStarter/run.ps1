55de985184055c4ba8e6621d0aa6a33845a31400
Kelvin Tegelaar <kelvin@limenetworks.nl>
2021-10-27 20:53:47 +0200

    improvements to current running detection

--- BestPracticeAnalyser_OrchestrationStarter/run.ps1
+++ BestPracticeAnalyser_OrchestrationStarter/run.ps1
@@ -1,19 +1,17 @@
 using namespace System.Net
 
 param($Request, $TriggerMetadata)
-
-$CurrentlyRunning = Test-Path "Cache_BestPracticeAnalyser\CurrentlyRunning.txt" | Where-Object -Property LastWriteTime -GT (Get-Date).addhours(-24)
-if ($CurrentlyRunning -eq $false) {
+$CurrentlyRunning = Get-Item "Cache_BestPracticeAnalyser\CurrentlyRunning.txt" -ErrorAction SilentlyContinue | Where-Object -Property LastWriteTime -GT (Get-Date).AddHours(-24)
+if ($CurrentlyRunning) {
+    $Results = [pscustomobject]@{"Results" = "Already running. Please wait for the current instance to finish" }
+    Log-request  -API "BestPracticeAnalyser" -message "Attempted to start analysis but an instance was already running." -sev Info
+}
+else {
     $InstanceId = Start-NewOrchestration -FunctionName 'BestPracticeAnalyser_Orchestration'
     Write-Host "Started orchestration with ID = '$InstanceId'"
     $Orchestrator = New-OrchestrationCheckStatusResponse -Request $Request -InstanceId $InstanceId
     Log-request  -API "BestPracticeAnalyser" -message "Started retrieving best practice information" -sev Info
     $Results = [pscustomobject]@{"Results" = "Started running analysis" }
-}
-else {
-    $Results = [pscustomobject]@{"Results" = "Already running. Please wait for the current instance to finish" }
-    Log-request  -API "BestPracticeAnalyser" -message "Attempted to start analysis but an instance was already running." -sev Info
-
 }
 Write-Host ($Orchestrator | ConvertTo-Json)
 
