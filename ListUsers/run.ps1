749a869b56362d5fefcf585c204cf8822e4cc245
Kelvin Tegelaar <kelvin@limenetworks.nl>
2021-10-29 19:05:43 +0200

    added last signin info

--- ListUsers/run.ps1
+++ ListUsers/run.ps1
@@ -22,6 +22,19 @@ $GraphRequest = New-GraphGetRequest -uri "https://graph.microsoft.com/beta/users
     $_
 }
 
+if ($userid) {
+    $uri = "https://login.microsoftonline.com/$($Tenant)/oauth2/token"
+    $body = "resource=https://admin.microsoft.com&grant_type=refresh_token&refresh_token=$($ENV:ExchangeRefreshToken)"
+    $token = Invoke-RestMethod $uri -Body $body -ContentType "application/x-www-form-urlencoded" -ErrorAction SilentlyContinue -Method post
+    $LastSignIn = Invoke-RestMethod -ContentType "application/json;charset=UTF-8" -Uri "https://admin.microsoft.com/admin/api/users/$($userid)/lastSignInInfo" -Method GET -Headers @{
+        Authorization            = "Bearer $($token.access_token)";
+        "x-ms-client-request-id" = [guid]::NewGuid().ToString();
+        "x-ms-client-session-id" = [guid]::NewGuid().ToString()
+        'x-ms-correlation-id'    = [guid]::NewGuid()
+        'X-Requested-With'       = 'XMLHttpRequest' 
+    }
+    $GraphRequest | Select-Object *, @{ Name = 'LastSignInfo'; Expression = { $_.LastSignIn } }
+}
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
