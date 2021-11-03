90447c362c03f90f20d4d778816d5ecdb59b8bf1
Kelvin Tegelaar <kelvin@limenetworks.nl>
2021-11-03 23:11:05 +0100

    make urls correct

--- ExecBackendURLs/run.ps1
+++ ExecBackendURLs/run.ps1
@@ -7,7 +7,7 @@ $APIName = $TriggerMetadata.FunctionName
 Log-Request -user $request.headers.'x-ms-client-principal' -API $APINAME  -message "Accessed this API" -Sev "Debug"
 
 $Subscription = ($ENV:WEBSITE_OWNER_NAME).split('+') | Select-Object -First 1
-$SWAName = $ENV:Website_Resource_Group -replace "cipp", "CIPP-SWA-"
+$SWAName = $ENV:Website_SITE_NAME -replace "cipp", "CIPP-SWA-"
 # Write to the Azure Functions log stream.
 Write-Host "PowerShell HTTP trigger function processed a request."
 
