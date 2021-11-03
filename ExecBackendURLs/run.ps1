73a90565926c84cb3f264a14f163e39af46ebd31
Kelvin Tegelaar <kelvin@limenetworks.nl>
2021-11-03 22:55:07 +0100

    prettier urls

--- ExecBackendURLs/run.ps1
+++ ExecBackendURLs/run.ps1
@@ -14,15 +14,15 @@ Write-Host "PowerShell HTTP trigger function processed a request."
 $results = @"
 <h1>Resource Group </h1><br>
 
-Resource group: https://portal.azure.com/#/resource/subscriptions/$Subscription/resourceGroups/$ENV:Website_Resource_Group/overview <br>
-Keyvault (Password storage): https://portal.azure.com/#@limenetworks.nl/resource/subscriptions/$ENV:Website_Resource_Group/resourceGroups/$ENV:Website_Resource_Group/providers/Microsoft.KeyVault/vaults/$($ENV:WEBSITE_SITE_NAME)/secrets <br>
+<a href="https://portal.azure.com/#@Go/resource/subscriptions/$Subscription/resourceGroups/$ENV:Website_Resource_Group/overview">Resource group</a><br>
+<a href="https://portal.azure.com/#@Go/resource/subscriptions/$Subscription/resourceGroups/$ENV:Website_Resource_Group/providers/Microsoft.KeyVault/vaults/$($ENV:WEBSITE_SITE_NAME)/secrets">Keyvault (Password storage)</a><br>
 <h1>Function app </h1><br>
-Function Application (Overview): https://portal.azure.com/#@limenetworks.nl/resource/subscriptions/$Subscription/resourceGroups/$ENV:Website_Resource_Group/providers/Microsoft.Web/sites/$($ENV:WEBSITE_SITE_NAME)/appServices <br>
-Function Application (Configuration): https://portal.azure.com/#@limenetworks.nl/resource/subscriptions/$Subscription/resourceGroups/$ENV:Website_Resource_Group/providers/Microsoft.Web/sites/$($ENV:WEBSITE_SITE_NAME)/configuration <br>
-Function Application (Deployment Center): https://portal.azure.com/#@limenetworks.nl/resource/subscriptions/$Subscription/resourceGroups/$ENV:Website_Resource_Group/providers/Microsoft.Web/sites/$($ENV:WEBSITE_SITE_NAME)/vstscd <br>
+<a href="https://portal.azure.com/#@Go/resource/subscriptions/$Subscription/resourceGroups/$ENV:Website_Resource_Group/providers/Microsoft.Web/sites/$($ENV:WEBSITE_SITE_NAME)/appServices">Function Application (Overview) </a><br>
+<a href="https://portal.azure.com/#@Go/resource/subscriptions/$Subscription/resourceGroups/$ENV:Website_Resource_Group/providers/Microsoft.Web/sites/$($ENV:WEBSITE_SITE_NAME)/configuration">Function Application (Configuration) </a><br>
+<a href="https://portal.azure.com/#@Go/resource/subscriptions/$Subscription/resourceGroups/$ENV:Website_Resource_Group/providers/Microsoft.Web/sites/$($ENV:WEBSITE_SITE_NAME)/vstscd">Function Application (Deployment Center)</a><br>
 <h1>Static Web App </h1><br>
-Static Web App (Custom Domains): https://portal.azure.com/#@limenetworks.nl/resource/subscriptions/$Subscription/resourceGroups/$ENV:Website_Resource_Group/providers/Microsoft.Web/staticSites/$SWAName/customDomains <br>
-Static Web App (Role Management): https://portal.azure.com/#@limenetworks.nl/resource/subscriptions/$Subscription/resourceGroups/$ENV:Website_Resource_Group/providers/Microsoft.Web/staticSites/$SWAName/customDomains <br>
+<a href="https://portal.azure.com/#@Go/resource/subscriptions/$Subscription/resourceGroups/$ENV:Website_Resource_Group/providers/Microsoft.Web/staticSites/$SWAName/customDomains">Static Web App (Custom Domains)</a><br>
+<a href="https://portal.azure.com/#@Go/resource/subscriptions/$Subscription/resourceGroups/$ENV:Website_Resource_Group/providers/Microsoft.Web/staticSites/$SWAName/customDomains">Static Web App (Role Management)</a><br>
 
 
 "@
@@ -32,6 +32,6 @@ $body = @{Results = $Results }
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-        StatusCode = [HttpStatusCode]::OK
+        StatusCode = [httpstatusCode]::OK
         Body       = $body
     })
