5332e299a9f08673f4eef2619159df3284b986ea
Kelvin Tegelaar <kelvin@limenetworks.nl>
2021-11-04 08:45:29 +0100

    added some spacing

--- ExecBackendURLs/run.ps1
+++ ExecBackendURLs/run.ps1
@@ -12,15 +12,15 @@ $SWAName = $ENV:Website_SITE_NAME -replace "cipp", "CIPP-SWA-"
 Write-Host "PowerShell HTTP trigger function processed a request."
 
 $results = @"
-<h1>Resource Group </h1><br>
+<h1>Resource Group </h1><br><br>
 
 <a href="https://portal.azure.com/#@Go/resource/subscriptions/$Subscription/resourceGroups/$ENV:Website_Resource_Group/overview">Resource group</a><br>
 <a href="https://portal.azure.com/#@Go/resource/subscriptions/$Subscription/resourceGroups/$ENV:Website_Resource_Group/providers/Microsoft.KeyVault/vaults/$($ENV:WEBSITE_SITE_NAME)/secrets">Keyvault (Password storage)</a><br>
-<h1>Function app </h1><br>
+<h1>Function app </h1><br><br>
 <a href="https://portal.azure.com/#@Go/resource/subscriptions/$Subscription/resourceGroups/$ENV:Website_Resource_Group/providers/Microsoft.Web/sites/$($ENV:WEBSITE_SITE_NAME)/appServices">Function Application (Overview) </a><br>
 <a href="https://portal.azure.com/#@Go/resource/subscriptions/$Subscription/resourceGroups/$ENV:Website_Resource_Group/providers/Microsoft.Web/sites/$($ENV:WEBSITE_SITE_NAME)/configuration">Function Application (Configuration) </a><br>
 <a href="https://portal.azure.com/#@Go/resource/subscriptions/$Subscription/resourceGroups/$ENV:Website_Resource_Group/providers/Microsoft.Web/sites/$($ENV:WEBSITE_SITE_NAME)/vstscd">Function Application (Deployment Center)</a><br>
-<h1>Static Web App </h1><br>
+<h1>Static Web App </h1><br><br>
 <a href="https://portal.azure.com/#@Go/resource/subscriptions/$Subscription/resourceGroups/$ENV:Website_Resource_Group/providers/Microsoft.Web/staticSites/$SWAName/customDomains">Static Web App (Custom Domains)</a><br>
 <a href="https://portal.azure.com/#@Go/resource/subscriptions/$Subscription/resourceGroups/$ENV:Website_Resource_Group/providers/Microsoft.Web/staticSites/$SWAName/customDomains">Static Web App (Role Management)</a><br>
 
