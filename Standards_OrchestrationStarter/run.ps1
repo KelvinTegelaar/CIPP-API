0bfbf906109d3f92ca6025e6c6fd3ceaa8654272
Kelvin Tegelaar <kelvin@limenetworks.nl>
2021-11-06 20:47:22 +0100

    response check added

--- Standards_OrchestrationStarter/run.ps1
+++ Standards_OrchestrationStarter/run.ps1
@@ -9,7 +9,7 @@ if ($CurrentlyRunning) {
 else {
     $InstanceId = Start-NewOrchestration -FunctionName 'Standards_Orchestration'
     Write-Host "Started orchestration with ID = '$InstanceId'"
-    $Response = New-OrchestrationCheckStatusResponse -Request $timer -InstanceId $InstanceId
+    $Response = New-OrchestrationCheckStatusResponse -Request $Request -InstanceId $InstanceId
     Write-Host ($Response | ConvertTo-Json)
     Log-request  -API "Standards" -tenant $tenant -message "Started applying the standard templates to tenants." -sev Info
     $Results = [pscustomobject]@{"Results" = "Started Applying Standards" }
