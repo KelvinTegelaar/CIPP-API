126ad93b2c353fc40e42f675be4bf1cf8ea1454f
Kelvin Tegelaar <kelvin@limenetworks.nl>
2021-11-06 20:22:24 +0100

    added https endpoint for starting standards

--- Standards_Orchestration/run.ps1
+++ Standards_Orchestration/run.ps1
@@ -1,6 +1,7 @@
 param($Context)
 
-
+New-Item "Cache_Standards" -ItemType Directory -ErrorAction SilentlyContinue
+New-Item "Cache_Standards\CurrentlyRunning.txt" -ItemType File -Force
 
 $Batch = (Invoke-DurableActivity -FunctionName 'Standards_GetQueue' -Input 'LetsGo')
 $ParallelTasks = foreach ($Item in $Batch) {
@@ -9,5 +10,5 @@ $ParallelTasks = foreach ($Item in $Batch) {
 
 $Outputs = Wait-ActivityFunction -Task $ParallelTasks
 Write-Host $Outputs
-
+Remove-Item "Cache_Standards\CurrentlyRunning.txt" -Force
 Log-request  -API "Standards" -tenant $tenant -message "Deployment finished." -sev Info
