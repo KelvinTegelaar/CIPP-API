61566ea2d05ed0cfbed7d03d69a4b4c11520ad86
Kelvin Tegelaar <kelvin@limenetworks.nl>
2021-11-15 19:31:07 +0100

    added undos for standards

--- Standards_SSPR/run.ps1
+++ Standards_SSPR/run.ps1
@@ -2,17 +2,18 @@ param($tenant)
 
 try {
     $uri = "https://login.microsoftonline.com/$($Tenant)/oauth2/token"
-    $body = "resource=https://admin.microsoft.com&grant_type=refresh_token&refresh_token=$($ENV:ExchangeRefreshToken)"
-    $token = Invoke-RestMethod $uri -Body $body -ContentType "application/x-www-form-urlencoded" -ErrorAction SilentlyContinue -method post
-    $AnonReports = Invoke-RestMethod -contenttype "application/json;charset=UTF-8" -uri 'https://admin.microsoft.com/admin/api/reports/config/SetTenantConfiguration' -body '{"PrivacyEnabled":false,"PowerBiEnabled":true}' -method POST -Headers @{
-        Authorization            = "Bearer $($token.access_token)";
+    $bodypasswordresetpol = "resource=74658136-14ec-4630-ad9b-26e160ff0fc6&grant_type=refresh_token&refresh_token=$($ENV:ExchangeRefreshToken)"
+    $tokensspr = Invoke-RestMethod $uri -Body $bodypasswordresetpol -ContentType "application/x-www-form-urlencoded" -ErrorAction SilentlyContinue -Method post
+    $bodysspr = '{"objectId":"default","enablementType":2,"numberOfAuthenticationMethodsRequired":2,"emailOptionEnabled":true,"mobilePhoneOptionEnabled":true,"officePhoneOptionEnabled":false,"securityQuestionsOptionEnabled":false,"mobileAppNotificationEnabled":true,"mobileAppCodeEnabled":true,"numberOfQuestionsToRegister":5,"numberOfQuestionsToReset":3,"registrationRequiredOnSignIn":true,"registrationReconfirmIntevalInDays":180,"skipRegistrationAllowed":true,"skipRegistrationMaxAllowedDays":7,"customizeHelpdeskLink":false,"customHelpdeskEmailOrUrl":"","notifyUsersOnPasswordReset":true,"notifyOnAdminPasswordReset":true,"passwordResetEnabledGroupIds":[],"passwordResetEnabledGroupName":"","securityQuestions":[],"registrationConditionalAccessPolicies":[],"emailOptionAllowed":true,"mobilePhoneOptionAllowed":true,"officePhoneOptionAllowed":true,"securityQuestionsOptionAllowed":true,"mobileAppNotificationOptionAllowed":true,"mobileAppCodeOptionAllowed":true}'
+    $SSPRGraph = Invoke-RestMethod -ContentType "application/json;charset=UTF-8" -Uri 'https://main.iam.ad.ext.azure.com/api/PasswordReset/PasswordResetPolicies' -Method PUT -Body $bodysspr -Headers @{
+        Authorization            = "Bearer $($tokensspr.access_token)";
         "x-ms-client-request-id" = [guid]::NewGuid().ToString();
         "x-ms-client-session-id" = [guid]::NewGuid().ToString()
         'x-ms-correlation-id'    = [guid]::NewGuid()
         'X-Requested-With'       = 'XMLHttpRequest' 
     }
-    "Standards API succesfully changed anonymous reports"
+  Log-request -API "Standards" -tenant $tenant -message "SSPR enabled" -sev Info
 }
 catch {
-    "Standards API: Could not connect to disable anonymous reports. Error: $($exception.message)"
+  Log-request -API "Standards" -tenant $tenant -message  "Failed to enable SSPR $($_.exception.message)"
 }
---
+++ Standards_UndoOauth/run.ps1
@@ -0,0 +1,18 @@
+﻿param($tenant)
+
+try {
+    $uri = "https://login.microsoftonline.com/$($Tenant)/oauth2/token"
+    $body = "resource=https://admin.microsoft.com&grant_type=refresh_token&refresh_token=$($ENV:ExchangeRefreshToken)"
+    $token = Invoke-RestMethod $uri -Body $body -ContentType "application/x-www-form-urlencoded" -ErrorAction SilentlyContinue -Method post
+    $oAuth = Invoke-RestMethod -ContentType "application/json;charset=UTF-8" -Uri 'https://admin.microsoft.com/admin/api/settings/apps/IntegratedApps' -Body '{"Enabled":true}' -Method POST -Headers @{
+        Authorization            = "Bearer $($token.access_token)";
+        "x-ms-client-request-id" = [guid]::NewGuid().ToString();
+        "x-ms-client-session-id" = [guid]::NewGuid().ToString()
+        'x-ms-correlation-id'    = [guid]::NewGuid()
+        'X-Requested-With'       = 'XMLHttpRequest' 
+    }
+    Log-request -API "Standards" -tenant $tenant -message  "Application Consent Mode has been disabled." -sev Info
+}
+catch {
+    Log-request -API "Standards" -tenant $tenant -message  "Failed to set Application Consent Mode to disabled Error: $($_.exception.message)" -sev Error
+}
