893cc33cd80e50a584938912d0b3c3c79b199fd5
lwhitelock <79275328+lwhitelock@users.noreply.github.com>
2021-11-18 21:21:48 +0000

    Added Restricted User Check

--- ListUserMailboxDetails/run.ps1
+++ ListUserMailboxDetails/run.ps1
@@ -17,15 +17,21 @@ $UserID = $Request.Query.UserID
 
 $TenantFilter = $Request.Query.TenantFilter
 try {
-    $FetchParam = @{
-        anr = $UserUPN
-    }
     $CASRequest = New-GraphGetRequest -uri "https://outlook.office365.com/adminapi/beta/$($tenantfilter)/CasMailbox('$UserID')" -Tenantid $tenantfilter -scope ExchangeOnline -noPagination $true
     $MailRequest = New-GraphGetRequest -uri "https://outlook.office365.com/adminapi/beta/$($tenantfilter)/Mailbox('$UserID')" -Tenantid $tenantfilter -scope ExchangeOnline -noPagination $true
     $FetchParam = @{
         anr = $MailRequest.PrimarySmtpAddress
     }
     $MailboxDetailedRequest = New-ExoRequest -TenantID $TenantFilter -cmdlet 'Get-Mailbox' -cmdParams $FetchParam
+    $FetchParam = @{
+        SenderAddress = $MailRequest.PrimarySmtpAddress
+    }
+    $BlockedSender = New-ExoRequest -TenantID $TenantFilter -cmdlet 'Get-BlockedSenderAddress' -cmdParams $FetchParam
+    if ($BlockedSender) {
+        $BlockedForSpam = $True
+    } else {
+        $BlockedForSpam = $False
+    }
     $StatsRequest = New-GraphGetRequest -uri "https://outlook.office365.com/adminapi/beta/$($tenantfilter)/Mailbox('$($MailRequest.PrimarySmtpAddress)')/Exchange.GetMailboxStatistics()" -Tenantid $tenantfilter -scope ExchangeOnline -noPagination $true
     $PermsRequest = New-GraphGetRequest -uri "https://outlook.office365.com/adminapi/beta/$($tenantfilter)/Mailbox('$($MailRequest.PrimarySmtpAddress)')/MailboxPermission" -Tenantid $tenantfilter -scope ExchangeOnline -noPagination $true
 } catch {
@@ -43,21 +49,22 @@ $ParsedPerms = foreach ($Perm in $PermsRequest) {
 
 
 $GraphRequest = [ordered]@{
-    ForwardAndDeliver                     = $MailboxDetailedRequest.DeliverToMailboxAndForward
-    ForwardingAddress                     = $MailboxDetailedRequest.ForwardingAddress + ' ' + $MailboxDetailedRequest.ForwardingSmtpAddress
-    LitiationHold                         = $MailboxDetailedRequest.LitigationHoldEnabled
-    HiddenFromAddressLists                = $MailboxDetailedRequest.HiddenFromAddressListsEnabled
-    EWSEnabled                            = $CASRequest.EwsEnabled
-    MailboxMAPIEnabled                    = $CASRequest.MAPIEnabled
-    MailboxOWAEnabled                     = $CASRequest.OWAEnabled
-    MailboxImapEnabled                    = $CASRequest.ImapEnabled
-    MailboxPopEnabled                     = $CASRequest.PopEnabled
-    MailboxActiveSyncEnabled              = $CASRequest.ActiveSyncEnabled
-    Permissions                           = $ParsedPerms
-    ProhibitSendQuota                     = [math]::Round([float]($MailboxDetailedRequest.ProhibitSendQuota -split ' GB')[0], 2)
-    ProhibitSendReceiveQuota              = [math]::Round([float]($MailboxDetailedRequest.ProhibitSendReceiveQuota -split ' GB')[0], 2)
-    ItemCount                             = [math]::Round($StatsRequest.ItemCount, 2)
-    TotalItemSize                         = [math]::Round($StatsRequest.TotalItemSize / 1Gb, 2)
+    ForwardAndDeliver        = $MailboxDetailedRequest.DeliverToMailboxAndForward
+    ForwardingAddress        = $MailboxDetailedRequest.ForwardingAddress + ' ' + $MailboxDetailedRequest.ForwardingSmtpAddress
+    LitiationHold            = $MailboxDetailedRequest.LitigationHoldEnabled
+    HiddenFromAddressLists   = $MailboxDetailedRequest.HiddenFromAddressListsEnabled
+    EWSEnabled               = $CASRequest.EwsEnabled
+    MailboxMAPIEnabled       = $CASRequest.MAPIEnabled
+    MailboxOWAEnabled        = $CASRequest.OWAEnabled
+    MailboxImapEnabled       = $CASRequest.ImapEnabled
+    MailboxPopEnabled        = $CASRequest.PopEnabled
+    MailboxActiveSyncEnabled = $CASRequest.ActiveSyncEnabled
+    Permissions              = $ParsedPerms
+    ProhibitSendQuota        = [math]::Round([float]($MailboxDetailedRequest.ProhibitSendQuota -split ' GB')[0], 2)
+    ProhibitSendReceiveQuota = [math]::Round([float]($MailboxDetailedRequest.ProhibitSendReceiveQuota -split ' GB')[0], 2)
+    ItemCount                = [math]::Round($StatsRequest.ItemCount, 2)
+    TotalItemSize            = [math]::Round($StatsRequest.TotalItemSize / 1Gb, 2)
+    BlockedForSpam           = $BlockedForSpam
 }
 
 #$GraphRequest = [ordered]@{
