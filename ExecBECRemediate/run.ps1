13039e17b0054b9d299bca6c1b53bcd364b1aac4
Kelvin Tegelaar <kelvin@limenetworks.nl>
2021-11-19 11:58:50 +0100

    prettier tenantfilter

--- ExecBECRemediate/run.ps1
+++ ExecBECRemediate/run.ps1
@@ -27,7 +27,7 @@ try {
     Import-PSSession $session -ea Silentlycontinue -AllowClobber -CommandName "Get-inboxRule", "Disable-InboxRule"
     Get-InboxRule | Disable-InboxRule 
     Get-PSSession | Remove-PSSession
-    $results = [pscustomobject]@{"Results" = "Executed Remediation for $SuspectUser. The temporary is $password and must be changed at next logon." }
+    $results = [pscustomobject]@{"Results" = "Executed Remediation for $SuspectUser and tenant $($TenantFilter). The temporary is $password and must be changed at next logon." }
 
 }
 catch {
