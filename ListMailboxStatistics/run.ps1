8b0dd6f1ede7ebaaf10ad73dcca9b46c976dcf7f
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2022-01-29 15:46:30 +0100

    fixes #688

--- ListMailboxStatistics/run.ps1
+++ ListMailboxStatistics/run.ps1
@@ -12,10 +12,11 @@ Write-Host "PowerShell HTTP trigger function processed a request."
 
 # Interact with query parameters or the body of the request.
 $TenantFilter = $Request.Query.TenantFilter
-$GraphRequest = New-GraphGetRequest -uri "https://graph.microsoft.com/beta/reports/getMailboxUsageDetail(period='D7')" -tenantid $TenantFilter | convertfrom-csv | select-object @{ Name = 'UPN'; Expression = { $_.'User Principal Name' } },
+$GraphRequest = New-GraphGetRequest -uri "https://graph.microsoft.com/beta/reports/getMailboxUsageDetail(period='D7')" -tenantid $TenantFilter | ConvertFrom-Csv | Select-Object @{ Name = 'UPN'; Expression = { $_.'User Principal Name' } },
 @{ Name = 'displayName'; Expression = { $_.'Display Name' } },
 @{ Name = 'LastActive'; Expression = { $_.'Last Activity Date' } },
 @{ Name = 'UsedGB'; Expression = { [math]::round($_.'Storage Used (Byte)' / 1GB, 0) } },
+@{ Name = 'QuotaGB'; Expression = { [math]::round($_.'Prohibit Send/Receive Quota (Byte)' / 1GB, 0) } },
 @{ Name = 'ItemCount'; Expression = { $_.'Item Count' } },
 @{ Name = 'HasArchive'; Expression = { $_.'Has Archive' } }
 
