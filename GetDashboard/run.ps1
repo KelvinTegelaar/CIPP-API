83e68d2fd9945048664221248a6cdf2361c21d54
Kelvin Tegelaar <kelvin@cyberdrain.com>
2022-03-21 09:42:48 +0100

    API returns crontab, conversion done in frontend.

--- GetDashboard/run.ps1
+++ GetDashboard/run.ps1
@@ -188,14 +188,14 @@ Function Get-CronNextExecutionTime {
 $APIName = $TriggerMetadata.FunctionName
 Log-Request -user $request.headers.'x-ms-client-principal' -API $APINAME  -message "Accessed this API" -Sev "Debug"
 $dash = [PSCustomObject]@{
-    NextStandardsRun  = (Get-CronNextExecutionTime -Expression '0 */3 * * *').tostring('s')
-    NextBPARun        = (Get-CronNextExecutionTime -Expression '0 3 * * *').tostring('s')
+    NextStandardsRun  = '0 */3 * * *'
+    NextBPARun        = '0 3 * * *'
     queuedApps        = [int64](Get-ChildItem '.\ChocoApps.Cache' -ErrorAction SilentlyContinue).count
     queuedStandards   = [int64](Get-ChildItem '.\Cache_Standards' -ErrorAction SilentlyContinue).count
     tenantCount       = [int64](get-tenants -ErrorAction SilentlyContinue).count
-    RefreshTokenDate  = (Get-CronNextExecutionTime -Expression '0 0 * * 0').AddDays('-7').tostring('s') -split "T" | Select-Object -First 1
-    ExchangeTokenDate = (Get-CronNextExecutionTime -Expression '0 0 * * 0').AddDays('-7').tostring('s') -split "T" | Select-Object -First 1
-    LastLog           = @()
+    RefreshTokenDate  = '0 0 * * 0'
+    ExchangeTokenDate = '0 0 * * 0'
+    LastLog           = @(Get-Content "Logs\$((Get-Date).ToString('ddMMyyyy')).log" | ConvertFrom-Csv -Header "DateTime", "Tenant", "API", "Message", "User", "Severity" -Delimiter "|" | Select-Object -Last 10)
 }
 # Write to the Azure Functions log stream.
 
