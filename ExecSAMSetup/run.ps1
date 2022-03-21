d48c544c8cfae183f5ddf4d59a2a26d739c697e2
Kelvin Tegelaar <kelvin@cyberdrain.com>
2022-03-21 20:25:19 +0100

    correct item in keyvault

--- ExecSAMSetup/run.ps1
+++ ExecSAMSetup/run.ps1
@@ -20,8 +20,7 @@ try {
                   $URL = ($Request.headers.'x-ms-original-url').split('?') | Select-Object -First 1
                   $clientsecret = Get-AzKeyVaultSecret -VaultName $kv.vaultname -Name 'applicationsecret' -AsPlainText
                   $RefreshToken = Invoke-RestMethod -Method POST -Body "client_id=$appid&scope=https://graph.microsoft.com/.default+offline_access+openid+profile&code=$($request.query.code)&grant_type=authorization_code&redirect_uri=$($url)&client_secret=$clientsecret" -Uri "https://login.microsoftonline.com/$TenantId/oauth2/v2.0/token"
-                  Write-Host ($RefreshToken | ConvertTo-Json)
-                  Set-AzKeyVaultSecret -VaultName $kv.vaultname -Name 'RefreshToken' -SecretValue (ConvertTo-SecureString -String $Request.query.code -AsPlainText -Force)
+                  Set-AzKeyVaultSecret -VaultName $kv.vaultname -Name 'RefreshToken' -SecretValue (ConvertTo-SecureString -String $RefreshToken.refresh_token -AsPlainText -Force)
                   $Results = "Authentication is now complete. You may now close this window."
                   New-Item ".\Cache_SAMSetup\Validated.json" -Value "true" -Force
             }
