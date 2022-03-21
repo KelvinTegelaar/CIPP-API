483f33c319a8d6c52738b2837346b17444b384ba
Kelvin Tegelaar <kelvin@cyberdrain.com>
2022-03-21 19:14:38 +0100

    added failsafes and prettier urls

--- ExecSAMSetup/run.ps1
+++ ExecSAMSetup/run.ps1
@@ -15,7 +15,7 @@ $KV = Get-AzKeyVault -SubscriptionID $Subscription -ResourceGroupName $ResourceG
 try {
       if ($request.query.code) {
             try {
-                  #Set-AzKeyVaultSecret -VaultName $kv.vaultname -Name 'RefreshToken' -SecretValue (ConvertTo-SecureString -String $Request.query.code -AsPlainText -Force)
+                  Set-AzKeyVaultSecret -VaultName $kv.vaultname -Name 'RefreshToken' -SecretValue (ConvertTo-SecureString -String $Request.query.code -AsPlainText -Force)
                   $Results = "Authentication is now complete. You may now close this window."
                   New-Item ".\Cache_SAMSetup\Validated.json" -Value "true"
             }
@@ -33,7 +33,7 @@ try {
             $step = 1
             $DeviceLogon = New-DeviceLogin -clientid "1b730954-1685-4b74-9bfd-dac224a7b894" -Scope 'https://graph.microsoft.com/.default' -FirstLogon
             New-Item '.\Cache_SAMSetup\SamSetup.json' -Value ($DeviceLogon | ConvertTo-Json) -Force
-            $Results = @{ message = $DeviceLogon.message  ; step = $step }
+            $Results = @{ message = "Your code is $($DeviceLogon.user_code). Enter the code"  ; step = $step; url = $DeviceLogon.verification_uri }
       }
       if ($Request.query.CheckSetupProcess -and $request.query.step -eq 1) {
             $SAMSetup = Get-Content '.\Cache_SAMSetup\SamSetup.json' | ConvertFrom-Json
@@ -50,7 +50,23 @@ try {
                         $app = $app | ConvertTo-Json -Depth 15
                         $AppId = (Invoke-RestMethod "https://graph.microsoft.com/v1.0/applications" -Headers @{ authorization = "Bearer $($Token.Access_Token)" } -Method POST -Body $app -ContentType 'application/json')
                         $AppId.appId | Out-File '.\Cache_SAMSetup\cache.appid'
-
+                        $attempt = 0
+                        do {
+                              try {
+                                    Write-Host "{ `"appId`": `"$($AppId.appId)`" }" 
+                                    $SPN = (Invoke-RestMethod "https://graph.microsoft.com/v1.0/servicePrincipals" -Headers @{ authorization = "Bearer $($Token.Access_Token)" } -Method POST -Body "{ `"appId`": `"$($AppId.appId)`" }" -ContentType 'application/json')
+                                    Write-Host "SPN"
+                                    Start-Sleep 3
+                                    $GroupID = (Invoke-RestMethod "https://graph.microsoft.com/v1.0/groups?`$filter=startswith(displayName,'AdminAgents')" -Headers @{ authorization = "Bearer $($Token.Access_Token)" } -Method Get -ContentType 'application/json').value.id
+                                    Write-Host "Id is $GroupID"
+                                    $AddingToAdminAgent = (Invoke-RestMethod "https://graph.microsoft.com/v1.0/groups/$($GroupID)/members/`$ref" -Headers @{ authorization = "Bearer $($Token.Access_Token)" } -Method POST -Body "{ `"@odata.id`": `"https://graph.microsoft.com/v1.0/directoryObjects/$($SPN.id)`"}" -ContentType 'application/json')
+                                    Write-Host "Added to adminagents"
+                                    $attempt ++
+                              }
+                              catch {
+                                    $attempt ++
+                              }
+                        } until ($attempt -gt 5)
                   }
                   else {
                         $app = Get-Content '.\Cache_SAMSetup\SAMManifestNoPartner.json'
@@ -58,14 +74,14 @@ try {
                         $AppId.appId | Out-File '.\Cache_SAMSetup\cache.appid'
                   }
                   $AppPassword = (Invoke-RestMethod "https://graph.microsoft.com/v1.0/applications/$($AppID.id)/addPassword" -Headers @{ authorization = "Bearer $($Token.Access_Token)" } -Method POST -Body '{"passwordCredential":{"displayName":"CIPPInstall"}}' -ContentType 'application/json').secretText
-                  #Set-AzKeyVaultSecret -VaultName $kv.vaultname -Name 'tenantid' -SecretValue (ConvertTo-SecureString -String $TenantId -AsPlainText -Force)
-                  #Set-AzKeyVaultSecret -VaultName $kv.vaultname -Name 'applicationid' -SecretValue (ConvertTo-SecureString -String $Appid.appid -AsPlainText -Force)
-                  #Set-AzKeyVaultSecret -VaultName $kv.vaultname -Name 'applicationsecret' -SecretValue (ConvertTo-SecureString -String $AppPassword -AsPlainText -Force)
+                  Set-AzKeyVaultSecret -VaultName $kv.vaultname -Name 'tenantid' -SecretValue (ConvertTo-SecureString -String $TenantId -AsPlainText -Force)
+                  Set-AzKeyVaultSecret -VaultName $kv.vaultname -Name 'applicationid' -SecretValue (ConvertTo-SecureString -String $Appid.appid -AsPlainText -Force)
+                  Set-AzKeyVaultSecret -VaultName $kv.vaultname -Name 'applicationsecret' -SecretValue (ConvertTo-SecureString -String $AppPassword -AsPlainText -Force)
                   $Results = @{"message" = "Created application. Waiting 30 seconds for Azure propagation"; step = $step }
             }
             else {
                   $step = 1
-                  $Results = @{"message" = $SAMSetup.message ; step = $step }
+                  $Results = @{ message = "Your code is $($SAMSetup.user_code). Enter the code "  ; step = $step; url = $SAMSetup.verification_uri }            
             }
            
       }
@@ -79,28 +95,12 @@ try {
                   $URL = ($Request.url).split('?') | Select-Object -First 1
                   $Validated = Get-Content ".\Cache_SAMSetup\Validated.json" -ErrorAction SilentlyContinue
                   if ($Validated) { $step = 3 }
-                  $Results = @{ message = "Please visit https://login.microsoftonline.com/$TenantId/oauth2/v2.0/authorize?scope=https://graph.microsoft.com/.default+offline_access+openid+profile&response_type=code&client_id=$($appid)&redirect_uri=$($url)&client-request-id=eef306a9-fa85-4c1f-bb9a-a340d11de748&x-client-SKU=CIPP-AUTH&x-client-Ver=4.8.1.0&prompt=select_account"  ; step = $step }
+                  $Results = @{ message = "Give the next approval by clicking "  ; step = $step; url = "https://login.microsoftonline.com/$TenantId/oauth2/v2.0/authorize?scope=https://graph.microsoft.com/.default+offline_access+openid+profile&response_type=code&client_id=$($appid)&redirect_uri=$($url)&client-request-id=eef306a9-fa85-4c1f-bb9a-a340d11de748&x-client-SKU=CIPP-AUTH&x-client-Ver=4.8.1.0&prompt=select_account" }
             }
             3 {
-                  $RefreshToken = Get-AzKeyVaultSecret -VaultName $kv.vaultname -Name 'refreshtoken' -AsPlainText
-                  $attempt = 0
-                  do {
-                        try {
-                              Start-Sleep 3
-                              $GroupID = (Invoke-RestMethod "https://graph.microsoft.com/v1.0/groups?`$filter=startswith(displayName,'AdminAgents')" -Headers @{ authorization = "Bearer $($RefreshToken.Access_Token)" } -Method Get -ContentType 'application/json').value.id
-                              Write-Host "Id is $GroupID"
-                              $SPN = (Invoke-RestMethod "https://graph.microsoft.com/v1.0/servicePrincipals?`$filter=appId eq '$($Appid)'" -Headers @{ authorization = "Bearer $($RefreshToken.Access_Token)" } -Method Get -ContentType 'application/json').value.id
-                              Write-Host "SPN is $SPN"
-                              $AddingToAdminAgent = (Invoke-RestMethod "https://graph.microsoft.com/v1.0/groups/$($GroupID)/members/`$ref" -Headers @{ authorization = "Bearer $($RefreshToken.Access_Token)" } -Method POST -Body "{ `"@odata.id`": `"https://graph.microsoft.com/v1.0/directoryObjects/$($SPN)`"}" -ContentType 'application/json')
-                              Write-Host "Added to adminagents"
-                              $attempt ++
-                        }
-                        catch {
-                              $attempt ++
-                        }
-                  } until ($attempt -gt 5)
+
                   $step = 4
-                  $Results = @{"message" = "Received token, Add application to AdminAgents if required"; step = $step }
+                  $Results = @{"message" = "Received token."; step = $step }
                   
  
             }
@@ -110,7 +110,7 @@ try {
                   $FirstExchangeLogonRefreshtoken = New-DeviceLogin -clientid 'a0c73c16-a7e3-4564-9a95-2bdf47383716' -Scope 'https://outlook.office365.com/.default' -FirstLogon  -TenantId $TenantId
                   New-Item '.\Cache_SAMSetup\SamSetup.json' -Value ($FirstExchangeLogonRefreshtoken | ConvertTo-Json) -Force
                   $step = 5
-                  $Results = @{ message = $FirstExchangeLogonRefreshtoken.message  ; step = $step }
+                  $Results = @{ message = "Your code is $($FirstExchangeLogonRefreshtoken.user_code). Enter the code "  ; step = $step; url = $FirstExchangeLogonRefreshtoken.verification_uri }            
             }
             5 {
                   $step = 5
@@ -123,7 +123,7 @@ try {
                         $Results = @{"message" = "Retrieved refresh token and saving to Keyvault."; step = $step }
                   }
                   else {
-                        $Results = @{"message" = $SAMSetup.message ; step = $step }
+                        $Results = @{ message = "Your code is $($SAMSetup.user_code). Enter the code "  ; step = $step; url = $SAMSetup.verification_uri }            
                   }
             }
             6 {
