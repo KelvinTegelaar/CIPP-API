66b69f42a2e262886009b5170cbcbe0294c40d99
Kelvin Tegelaar <kelvin@cyberdrain.com>
2022-03-29 16:00:09 +0200

    added functionality to add transport rule

---
+++ AddTransportRule/run.ps1
@@ -0,0 +1,29 @@
+using namespace System.Net
+
+# Input bindings are passed in via param block.
+param($Request, $TriggerMetadata)
+
+$APIName = $TriggerMetadata.FunctionName
+Log-Request -user $request.headers.'x-ms-client-principal' -API $APINAME  -message "Accessed this API" -Sev "Debug"
+
+$RequestParams = $Request.Body.PowerShellCommand | ConvertFrom-Json | Select-Object -Property * -ExcludeProperty GUID
+Write-Host $RequestParams
+
+$Tenants = ($Request.body | Select-Object Select_*).psobject.properties.value
+$Result = foreach ($Tenantfilter in $tenants) {
+    try {
+        $GraphRequest = New-ExoRequest -tenantid $Tenantfilter -cmdlet "New-TransportRule" -cmdParams $RequestParams
+        "Succesfully created transport rule for $tenantfilter."
+        Log-request -API $APINAME -tenant $tenantfilter -message "Created transport rule for $($tenantfilter)" -sev Debug
+    }
+    catch {
+        "Could not create created transport rule for $($tenantfilter): $($_.Exception.message)"
+    }
+}
+ 
+
+# Associate values to output bindings by calling 'Push-OutputBinding'.
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+        StatusCode = [HttpStatusCode]::OK
+        Body       = @{Results = @($Result) }
+    })
