94bcb11d18185a06ddeb82ddd66e4795232c0047
Kelvin Tegelaar <kelvin@cyberdrain.com>
2022-03-29 12:43:04 +0200

    added list transport rule templates

--- AddTransportTemplate/run.ps1
+++ AddTransportTemplate/run.ps1
@@ -7,28 +7,23 @@ $APIName = $TriggerMetadata.FunctionName
 Log-Request -user $request.headers.'x-ms-client-principal' -API $APINAME  -message "Accessed this API" -Sev "Debug"
 
 
-
 try {        
-    if (!$Request.body.displayname) { throw "You must enter a displayname" }
-    if (!$Request.body.rawjson) { throw "You must fill in the RAW json" }
-    if ($null -eq ($Request.body.Rawjson | ConvertFrom-Json)) { throw "the JSON is invalid" }
     $GUID = New-Guid
 
-    $object = [PSCustomObject]@{
-        Displayname = $request.body.displayname
-        Description = $request.body.description
-        RAWJson     = $request.body.RawJSON
-        Type        = $request.body.TemplateType
-        GUID        = $GUID
-    } | ConvertTo-Json
+    $object = [PSCustomObject]@{}
+    $request.body.PowerShellCommand | ConvertFrom-Csv -Delimiter " " -Header "name", "value" | ForEach-Object {
+        $object | Add-Member -NotePropertyName ($_.name -replace '-', '') -NotePropertyValue $_.value
+    }
+
+
     New-Item Config -ItemType Directory -ErrorAction SilentlyContinue
-    Set-Content "Config\$($GUID).IntuneTemplate.json" -Value $Object -Force
-    Log-Request -user $request.headers.'x-ms-client-principal' -API $APINAME  -message "Created intune policy template named $($Request.body.displayname) with GUID $GUID" -Sev "Debug"
+    Set-Content "Config\$($GUID).TransportRuleTemplate.json" -Value ($Object | ConvertTo-Json -Depth 10) -Force
+    Log-Request -user $request.headers.'x-ms-client-principal' -API $APINAME  -message "Created Transport Rule Template $($Request.body.displayname) with GUID $GUID" -Sev "Debug"
 
     $body = [pscustomobject]@{"Results" = "Successfully added template" }
 }
 catch {
-    Log-Request -user $request.headers.'x-ms-client-principal'  -API $APINAME -message "Intune Template Deployment failed: $($_.Exception.Message)" -Sev "Error"
+    Log-Request -user $request.headers.'x-ms-client-principal'  -API $APINAME -message "Failed to create Transport Rule Template: $($_.Exception.Message)" -Sev "Error"
     $body = [pscustomobject]@{"Results" = "Intune Template Deployment failed: $($_.Exception.Message)" }
 }
 
