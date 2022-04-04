816a7fadf31a0823c2006f5724a58d840ae66a3f
Kelvin Tegelaar <kelvin@cyberdrain.com>
2022-04-04 14:09:35 +0200

    removed exchange dep

--- Standards_DelegateSentItems/run.ps1
+++ Standards_DelegateSentItems/run.ps1
@@ -1,13 +1,15 @@
 param($tenant)
 
 try {
-    $upn = "notRequired@required.com"
-    $tokenvalue = ConvertTo-SecureString (Get-GraphToken -AppID 'a0c73c16-a7e3-4564-9a95-2bdf47383716' -RefreshToken $ENV:ExchangeRefreshToken -Scope 'https://outlook.office365.com/.default' -Tenantid $($Tenant)).Authorization -AsPlainText -Force
-    $credential = New-Object System.Management.Automation.PSCredential($upn, $tokenValue)
-    $session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://ps.outlook.com/powershell-liveid?DelegatedOrg=$($Tenant)&BasicAuthToOAuthConversion=true" -Credential $credential -Authentication Basic -AllowRedirection -ErrorAction Continue
-    Import-PSSession $session -ea Silentlycontinue -AllowClobber -CommandName "Get-Mailbox", "Set-mailbox"
-    Get-Mailbox -ResultSize Unlimited -RecipientTypeDetails UserMailbox, SharedMailbox | Where-Object { $_.MessageCopyForSendOnBehalfEnabled -eq $false -or $_.MessageCopyForSentAsEnabled -eq $false } | set-mailbox -erroraction SilentlyContinue -MessageCopyForSentAsEnabled $true -MessageCopyForSendOnBehalfEnabled $true 
-    Get-PSSession | Remove-PSSession
+    $Mailboxes = New-ExoRequest -tenantid $Tenant -cmdlet "Get-Mailbox" -cmdParams @{ RecipientTypeDetails = @("UserMailbox", "SharedMailbox") } | Where-Object { $_.MessageCopyForSendOnBehalfEnabled -eq $false -or $_.MessageCopyForSentAsEnabled -eq $false } | ForEach-Object {
+        try {
+            $username = $_.UserPrincipalName
+            New-ExoRequest -tenantid $Tenant -cmdlet "set-mailbox" -cmdParams @{Identity = $_.GUID ; MessageCopyForSendOnBehalfEnabled = $True; MessageCopyForSentAsEnabled = $True }
+        }
+        catch {
+            Log-request  -API "Standards" -tenant $tenant -message "Could not enable delegate sent item style for $($username): $($_.Exception.message)" -sev Warn
+        }
+    }   
     Log-request  -API "Standards" -tenant $tenant -message "Delegate Sent Items Style enabled." -sev Info
 }
 catch {
