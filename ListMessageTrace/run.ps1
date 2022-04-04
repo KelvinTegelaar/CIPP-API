c28c5a1adc85115759fea56bb53562708fec1534
Kelvin Tegelaar <kelvin@cyberdrain.com>
2022-04-04 17:29:43 +0200

    remove exchange deps

--- ListMessageTrace/run.ps1
+++ ListMessageTrace/run.ps1
@@ -8,34 +8,28 @@ Log-Request -user $request.headers.'x-ms-client-principal' -API $APINAME  -messa
 
 
 try {
-    $Tenant = $request.query.TenantFilter
+    $TenantFilter = $request.query.TenantFilter
     $SearchParams = @{
-        StartDate = (Get-Date).AddDays( - $($request.query.days))
-        EndDate   = Get-Date
+        StartDate = (Get-Date).AddDays( - $($request.query.days)).ToString('s')
+        EndDate   = (Get-Date).ToString('s')
     }
     
     if ($null -ne $request.query.recipient) { $Searchparams.Add('RecipientAddress', $($request.query.recipient)) }
     if ($null -ne $request.query.sender) { $Searchparams.Add('SenderAddress', $($request.query.sender)) }
     $type = $request.query.Tracedetail    
-    $upn = "notRequired@required.com"
-    $tokenvalue = ConvertTo-SecureString (Get-GraphToken -AppID 'a0c73c16-a7e3-4564-9a95-2bdf47383716' -RefreshToken $ENV:ExchangeRefreshToken -Scope 'https://outlook.office365.com/.default' -Tenantid $tenant).Authorization -AsPlainText -Force
-    $credential = New-Object System.Management.Automation.PSCredential($upn, $tokenValue)
-    $session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://ps.outlook.com/powershell-liveid?DelegatedOrg=$($tenant)&BasicAuthToOAuthConversion=true" -Credential $credential -Authentication Basic -AllowRedirection -ErrorAction Continue
-    Import-PSSession $session -ea Silentlycontinue -AllowClobber -CommandName "Get-messagetrace$type"
     $trace = if ($Request.Query.Tracedetail) {
+        New-ExoRequest -tenantid $Tenantfilter -cmdlet "Get-MessageTraceDetail" -cmdParams $Searchparams
         Get-MessageTraceDetail -MessageTraceId $Request.Query.ID -RecipientAddress $request.query.recipient -erroraction stop | Select-Object Event, Action, Detail, @{ Name = 'Date'; Expression = { $_.Date.Tostring('s') } }
     }
     else {
-        Get-MessageTrace @SearchParams -erroraction stop | Select-Object MessageTraceId, Status, Subject, RecipientAddress, SenderAddress, @{ Name = 'Date'; Expression = { $_.Received.tostring('s') } }
-        Log-Request -user $request.headers.'x-ms-client-principal' -API $APINAME -tenant $($Tenant)  -message "Executed message trace" -Sev "Info"
+        New-ExoRequest -tenantid $Tenantfilter -cmdlet "Get-MessageTrace" -cmdParams $Searchparams | Select-Object MessageTraceId, Status, Subject, RecipientAddress, SenderAddress, @{ Name = 'Date'; Expression = { $_.Received.tostring('s') } }
+        Log-Request -user $request.headers.'x-ms-client-principal' -API $APINAME -tenant $($tenantfilter)  -message "Executed message trace" -Sev "Info"
 
     }
-    Get-PSSession | Remove-PSSession
 }
 catch {
-    Log-Request -user $request.headers.'x-ms-client-principal' -API $APINAME -tenant $($Tenant) -message "Failed executing messagetrace. Error: $($_.Exception.Message)" -Sev "Error"
+    Log-Request -user $request.headers.'x-ms-client-principal' -API $APINAME -tenant $($tenantfilter) -message "Failed executing messagetrace. Error: $($_.Exception.Message)" -Sev "Error"
     $trace = @{Status = "Failed to retrieve message trace $($_.Exception.Message)" }
-    Get-PSSession | Remove-PSSession
 }
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
