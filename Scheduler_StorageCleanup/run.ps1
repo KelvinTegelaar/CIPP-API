f9e94916a248f1d86d4ae5100a64992eb065fd04
Kelvin Tegelaar <kelvin@cyberdrain.com>
2022-04-08 15:41:56 +0200

    decided to remove entire blob daily, let it be recreated automatically.

--- Scheduler_StorageCleanup/run.ps1
+++ Scheduler_StorageCleanup/run.ps1
@@ -3,7 +3,7 @@ $ConnectionString = ($ENV:AzureWebJobsStorage).split(';') | ConvertFrom-StringDa
 $Date = Get-Date
 if ($date.hour -eq 23 -and $date.minute -le '9') {
     $context = New-AzStorageContext -StorageAccountName $($connectionstring.accountname | Select-Object -Last 1) -StorageAccountKey ($connectionstring.accountkey  | Select-Object -Last 1)
-    Get-AzStorageBlob -Context $context -Container "$($ENV:WEBSITE_CONTENTSHARE)-largemessages" | Where-Object -Property LastModified -LT (Get-Date).addhours(-24) | Remove-AzStorageBlob
+    Get-AzStorageBlob -Context $context -Container "$($ENV:WEBSITE_CONTENTSHARE)-largemessages" | Remove-AzStorageBlob -Force
     $InstancesTable = (Get-AzStorageTable -Context $context -Name "*instances").cloudTable
     $HistoryTable = (Get-AzStorageTable -Context $context -Name "*history").cloudTable
     Get-AzTableRow -Table $InstancesTable | Where-Object -Property RunTimeStatus -NE "Running" | Remove-AzTableRow -Table $InstancesTable
