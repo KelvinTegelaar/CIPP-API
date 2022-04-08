76052644ee020eb4d3727d648a0244644e3a090c
Kelvin Tegelaar <kelvin@cyberdrain.com>
2022-04-08 14:22:28 +0200

    fixing typo

--- Scheduler_StorageCleanup/run.ps1
+++ Scheduler_StorageCleanup/run.ps1
@@ -3,11 +3,11 @@ $ConnectionString = ($ENV:AzureWebJobsStorage).split(';') | ConvertFrom-StringDa
 $Date = Get-Date
 if ($date.hour -eq 23 -and $date.minute -le '9') {
     $context = New-AzStorageContext -StorageAccountName $($connectionstring.accountname | Select-Object -Last 1) -StorageAccountKey ($connectionstring.accountkey  | Select-Object -Last 1)
-    Get-AzStorageBlob -Context $context -Container "$($ENV:Website_Content_Share)-largemessages" | Where-Object -Property LastModified -LT (Get-Date).addhours(-24) | Remove-AzStorageBlob
+    Get-AzStorageBlob -Context $context -Container "$($ENV:WEBSITE_CONTENTSHARE)-largemessages" | Where-Object -Property LastModified -LT (Get-Date).addhours(-24) | Remove-AzStorageBlob
     $InstancesTable = (Get-AzStorageTable -Context $context -Name "*instances").cloudTable
     $HistoryTable = (Get-AzStorageTable -Context $context -Name "*history").cloudTable
-    Get-AzTableRow -table $InstancesTable | Where-Object -Property RunTimeStatus -NE "Running" | Remove-AzTableRow -Table $InstancesTable
-    Get-AzTableRow -table $HistoryTable | Where-Object -Property TimeStamp -LT (Get-Date).addhours(-24) | Remove-AzTableRow -Table $HistoryTable
+    Get-AzTableRow -Table $InstancesTable | Where-Object -Property RunTimeStatus -NE "Running" | Remove-AzTableRow -Table $InstancesTable
+    Get-AzTableRow -Table $HistoryTable | Where-Object -Property TimeStamp -LT (Get-Date).addhours(-24) | Remove-AzTableRow -Table $HistoryTable
     Get-AzStorageTable -Context $context -Name "AzureWebJobsHostLogs*" | Remove-AzStorageTable -Force
 }
 else {
