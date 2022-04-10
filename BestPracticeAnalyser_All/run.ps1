036b4dd9aa74f1cb1f63b52a47938719763ea094
Kelvin Tegelaar <kelvin@cyberdrain.com>
2022-04-10 19:24:27 +0200

    added revoke sessions

--- BestPracticeAnalyser_All/run.ps1
+++ BestPracticeAnalyser_All/run.ps1
@@ -86,7 +86,7 @@ try {
         ResultSize           = 'Unlimited'
         RecipientTypeDetails = 'UserMailbox, SharedMailbox'
     }
-    $MailboxBPA = New-ExoRequest -tenantid $Tenant -cmdlet "Get-Mailbox"
+    $MailboxBPA = New-ExoRequest -tenantid $Tenant -cmdlet "Get-Mailbox" -cmdparams $MailboxBPAParams
     $TotalMailboxes = $MailboxBPA | Measure-Object | Select-Object -ExpandProperty Count
     $TotalMessageCopyForSentAsEnabled = $MailboxBPA | Where-Object { $_.MessageCopyForSentAsEnabled -eq $true } | Measure-Object | Select-Object -ExpandProperty Count
     $TotalMessageCopyForSendOnBehalfEnabled = $MailboxBPA | Where-Object { $_.MessageCopyForSendOnBehalfEnabled -eq $true } | Measure-Object | Select-Object -ExpandProperty Count
