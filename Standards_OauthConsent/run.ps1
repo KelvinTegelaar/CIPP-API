9dacdb3ae2cbba0ccb4d72ab05b85fc387c19612
Kelvin Tegelaar <kelvin@cyberdrain.com>
2022-04-13 14:25:18 +0200

    Add standard for activitybasedtimeout

--- Standards_OauthConsent/run.ps1
+++ Standards_OauthConsent/run.ps1
@@ -1,16 +1,35 @@
 ﻿param($tenant)
-
+if ((Test-Path ".\Cache_Standards\$($Tenant).Standards.json")) {
+    $AllowedAppIdsForTenant = (Get-Content ".\Cache_Standards\$($Tenant).Standards.json" -ErrorAction SilentlyContinue | ConvertFrom-Json -ErrorAction SilentlyContinue).Standards.OauthConsent.AllowedApps -split ','
+}
+else {
+    $AllowedAppIdsForTenant = (Get-Content ".\Cache_Standards\AllTenants.Standards.json" -ErrorAction SilentlyContinue | ConvertFrom-Json -ErrorAction SilentlyContinue).Standards.OauthConsent.AllowedApps -split ','
+}
 try {
-    $uri = "https://login.microsoftonline.com/$($Tenant)/oauth2/token"
-    $body = "resource=https://admin.microsoft.com&grant_type=refresh_token&refresh_token=$($ENV:ExchangeRefreshToken)"
-    $token = Invoke-RestMethod $uri -Body $body -ContentType "application/x-www-form-urlencoded" -ErrorAction SilentlyContinue -Method post
-    $oAuth = Invoke-RestMethod -ContentType "application/json;charset=UTF-8" -Uri 'https://admin.microsoft.com/admin/api/settings/apps/IntegratedApps' -Body '{"Enabled":false}' -Method POST -Headers @{
-        Authorization            = "Bearer $($token.access_token)";
-        "x-ms-client-request-id" = [guid]::NewGuid().ToString();
-        "x-ms-client-session-id" = [guid]::NewGuid().ToString()
-        'x-ms-correlation-id'    = [guid]::NewGuid()
-        'X-Requested-With'       = 'XMLHttpRequest' 
+    $State = (New-GraphGetRequest -Uri "https://graph.microsoft.com/beta/policies/authorizationPolicy/authorizationPolicy" -tenantid $tenant)
+    if ($State.permissionGrantPolicyIdsAssignedToDefaultUserRole -notin @("ManagePermissionGrantsForSelf.cipp-1sent-policy")) {
+        Write-Host "Going to set"
+        
+        $Existing = (New-GraphGetRequest -Uri "https://graph.microsoft.com/beta/policies/permissionGrantPolicies/" -tenantid $tenant) | Where-Object -Property id -EQ "cipp-consent-policy"
+        if (!$Existing) {
+            New-GraphPostRequest -tenantid $tenant -Uri "https://graph.microsoft.com/beta/policies/permissionGrantPolicies" -Type POST -Body '{ "id":"cipp-consent-policy", "displayName":"Application Consent Policy", "description":"This policy controls the current application consent policies."}' -ContentType "application/json" 
+            New-GraphPostRequest -tenantid $tenant -Uri "https://graph.microsoft.com/beta/policies/permissionGrantPolicies/cipp-consent-policy/includes" -Type POST -Body '{"permissionClassification":"all","permissionType":"delegated","clientApplicationIds":["d414ee2d-73e5-4e5b-bb16-03ef55fea597"]}'  -ContentType "application/json"
+        }
+        try {
+            foreach ($AllowedApp in $AllowedAppIdsForTenant) {
+                Write-Host "$AllowedApp"
+                New-GraphPostRequest -tenantid $tenant -Uri "https://graph.microsoft.com/beta/policies/permissionGrantPolicies/cipp-consent-policy/includes" -Type POST -Body ('{"permissionType": "delegated","clientApplicationIds": ["' + $AllowedApp + '"]}')  -ContentType "application/json"
+                New-GraphPostRequest -tenantid $tenant -Uri "https://graph.microsoft.com/beta/policies/permissionGrantPolicies/cipp-consent-policy/includes" -Type POST -Body ('{ "permissionType": "Application", "clientApplicationIds": ["' + $AllowedApp + '"] }') -ContentType "application/json"
+            }
+        }
+        catch {
+            "Could not add exclusions, probably already exist: $($_)"
+        }
+        New-GraphPostRequest -tenantid $tenant -Uri "https://graph.microsoft.com/beta/policies/authorizationPolicy/authorizationPolicy" -Type PATCH -Body '{"permissionGrantPolicyIdsAssignedToDefaultUserRole":["managePermissionGrantsForSelf.cipp-consent-policy"]}' -ContentType "application/json"
     }
+    if ($AllowedAppIdsForTenant) {
+    }
+
     Log-request -API "Standards" -tenant $tenant -message  "Application Consent Mode has been enabled." -sev Info
 }
 catch {
