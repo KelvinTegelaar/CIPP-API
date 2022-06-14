ffe6027fad30a9cd967a665022b5a67096e8e217
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2022-06-14 09:49:32 +0200

    removed currentlyrunning file as scheduler cannot be influenced.

--- Standards_Orchestration/run.ps1
+++ Standards_Orchestration/run.ps1
@@ -3,7 +3,6 @@ param($Context)
 New-Item "Cache_Standards" -ItemType Directory -ErrorAction SilentlyContinue
 New-Item "Cache_Standards\CurrentlyRunning.txt" -ItemType File -Force
 
-
 $Batch = (Invoke-DurableActivity -FunctionName 'Standards_GetQueue' -Input 'LetsGo')
 $ParallelTasks = foreach ($Item in $Batch) {
   Invoke-DurableActivity -FunctionName "Standards_$($item['Standard'])"-Input $item['Tenant'] -NoWait
