916e664fb6ac17501a05c79e72524a6a5fd61c9c
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2022-06-15 15:08:09 +0200

    fix Ms's booboos

--- Standards_DeletedUserRentention/run.ps1
+++ Standards_DeletedUserRentention/run.ps1
@@ -2,7 +2,8 @@
 
 try {
     $body = '{"deletedUserPersonalSiteRetentionPeriodInDays": 365}'
-    New-GraphPostRequest -tenantid $tenant -Uri "https://graph.microsoft.com/beta/admin/sharepoint/settings" -AsApp $true -Type patch -Body $body -ContentType "application/json"
+    New-GraphPostRequest -tenantid $tenant -Uri "https://graph.microsoft.com/beta/admin/sharepoint/settings" -AsApp $true -Type PATCH -Body $body -ContentType "application/json"
+
     Log-request -API "Standards" -tenant $tenant -message  "Set deleted user rentention of OneDrive to 1 year" -sev Info
 }
 catch {
--- Standards_DisableReshare/run.ps1
+++ Standards_DisableReshare/run.ps1
@@ -1,8 +1,11 @@
 ﻿param($tenant)
 
 try {
-    $body = '{"isResharingByExternalUsersEnabled": false}'
-    New-GraphPostRequest -tenantid $tenant -Uri "https://graph.microsoft.com/beta/admin/sharepoint/settings" -AsApp $true -Type patch -Body $body -ContentType "application/json"
+    #Seems like MS made a mistake here, notified them of the flipped boolean. will change when they fix this.
+    $body = '{"isResharingByExternalUsersEnabled": "True"}'
+    $Request = New-GraphPostRequest -tenantid $tenant -Uri "https://graph.microsoft.com/beta/admin/sharepoint/settings" -AsApp $true -Type patch -Body $body -ContentType "application/json"
+    Write-Host "Here's Johnny"
+    Write-Host ($Request | ConvertTo-Json)
     Log-request -API "Standards" -tenant $tenant -message  "Disabled guests from resharing files" -sev Info
 }
 catch {
