194094f86ddfaafc49a75b48d92c4b057fbc7a79
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2022-06-15 14:28:34 +0200

    new apis

---
+++ Standards_DisableReshare/run.ps1
@@ -0,0 +1,10 @@
+﻿param($tenant)
+
+try {
+    $body = '{"isResharingByExternalUsersEnabled": false}'
+    New-GraphPostRequest -tenantid $tenant -Uri "https://graph.microsoft.com/beta/admin/sharepoint/settings" -AsApp $true -Type patch -Body $body -ContentType "application/json"
+    Log-request -API "Standards" -tenant $tenant -message  "Disabled guests from resharing files" -sev Info
+}
+catch {
+    Log-request -API "Standards" -tenant $tenant -message  "Failed to disable guests from resharing files: $($_.exception.message)" -sev Error
+}
