d6ae558ea3d42f1bbe3536aa13695f6898b63b87
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2022-07-17 15:27:56 +0200

    new standards

---
+++ Standards_DisableM365GroupUsers/run.ps1
@@ -0,0 +1,12 @@
+param($tenant)
+
+try {
+    $CurrentState = (New-GraphGetRequest -Uri "https://graph.microsoft.com/beta/settings" -tenantid $tenant) | Where-Object -Property displayname -EQ 'Group.unified'
+    ($CurrentState.values | Where-Object { $_.name -eq 'EnableGroupCreation' }).value = "false"
+    $body = "{values : $($CurrentState.values | ConvertTo-Json -Compress)}"
+    (New-GraphPostRequest -tenantid $tenant -Uri "https://graph.microsoft.com/beta/settings/$($CurrentState.id)" -Type patch -Body $body -ContentType "application/json")
+    Log-request -API "Standards" -tenant $tenant -message "Standards API: Disabled users from creating Security Groups." -sev Info
+}
+catch {
+    Log-request -API "Standards" -tenant $tenant -message  "Failed to disable users from creating Security Groups: $($_.exception.message)"
+}
