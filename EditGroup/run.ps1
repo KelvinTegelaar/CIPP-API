f530c68a5b146455fd7514f0f245ea722bc3834a
Kelvin Tegelaar <you@example.com>
2022-07-18 14:35:58 +0200

    added DL editing

--- EditGroup/run.ps1
+++ EditGroup/run.ps1
@@ -20,7 +20,13 @@ if ($AddMembers) {
             $member = $_
             $MemberIDs = "https://graph.microsoft.com/v1.0/directoryObjects/" + (New-GraphGetRequest -uri "https://graph.microsoft.com/beta/users/$($_)" -tenantid $Userobj.tenantid).id 
             $addmemberbody = "{ `"members@odata.bind`": $(ConvertTo-Json @($MemberIDs)) }"
-            New-GraphPostRequest -uri "https://graph.microsoft.com/beta/groups/$($userobj.groupid)" -tenantid $Userobj.tenantid -type patch -body $addmemberbody -Verbose
+            if ($userobj.groupType -eq "Distribution list" -or "Mail-Enabled Security") {
+                $Params = @{ Identity = $userobj.groupid; Member = $member }
+                New-ExoRequest -tenantid $Userobj.tenantid -cmdlet "Add-DistributionGroupMember" -cmdParams $params
+            }
+            else {
+                New-GraphPostRequest -uri "https://graph.microsoft.com/beta/groups/$($userobj.groupid)" -tenantid $Userobj.tenantid -type patch -body $addmemberbody -Verbose
+            }
             Log-Request -API $APINAME -tenant $Userobj.tenantid -user $request.headers.'x-ms-client-principal' -message "Added member to $($userobj.displayname) group" -Sev "Info"
             $body = $results.add("Success. $member has been added")
         }
@@ -37,7 +43,13 @@ try {
     if ($RemoveMembers) {
         $RemoveMembers | ForEach-Object { 
             $MemberInfo = (New-GraphGetRequest -uri "https://graph.microsoft.com/beta/users/$($_)" -tenantid $Userobj.tenantid)
-            New-GraphPostRequest -uri "https://graph.microsoft.com/beta/groups/$($userobj.groupid)/members/$($MemberInfo.id)/`$ref" -tenantid $Userobj.tenantid -type DELETE 
+            if ($userobj.groupType -eq "Distribution list" -or "Mail-Enabled Security") {
+                $Params = @{ Identity = $userobj.groupid; Member = $_ }
+                New-ExoRequest -tenantid $Userobj.tenantid -cmdlet "Remove-DistributionGroupMember" -cmdParams $params
+            }
+            else {
+                New-GraphPostRequest -uri "https://graph.microsoft.com/beta/groups/$($userobj.groupid)/members/$($MemberInfo.id)/`$ref" -tenantid $Userobj.tenantid -type DELETE 
+            }
             Log-Request -API $APINAME -tenant $Userobj.tenantid -user $request.headers.'x-ms-client-principal'  -message "Removed $($MemberInfo.UserPrincipalname) from $($userobj.displayname) group" -Sev "Info"
             $body = $results.add("Success. Member $_ has been removed from $($userobj.Groupid)")
         }  
