1e73003944df733712a50ebf974ddc9b9f691e62
Kelvin Tegelaar <you@example.com>
2022-07-18 13:10:47 +0200

    fixed typo

--- AddGroup/run.ps1
+++ AddGroup/run.ps1
@@ -7,7 +7,6 @@ $APIName = $TriggerMetadata.FunctionName
 Log-Request -user $request.headers.'x-ms-client-principal' -API $APINAME  -message "Accessed this API" -Sev "Debug"
 
 $groupobj = $Request.body
-$user = $request.headers.'x-ms-client-principal'
 
 # Write to the Azure Functions log stream.
 Write-Host "PowerShell HTTP trigger function processed a request."
@@ -24,7 +23,7 @@ try {
     } | ConvertTo-Json
     $GraphRequest = New-GraphPostRequest -AsApp $true -uri "https://graph.microsoft.com/beta/groups" -tenantid $groupobj.tenantid -type POST -body $BodyToship   -verbose
     $body = [pscustomobject]@{"Results" = "Succesfully created group. $($_.Exception.Message)" }
-    Log-Request -user $request.headers.'x-ms-client-principal' -API $APINAME -tenant $($groupobj.tenantid) -message "Created group $($groupobj.displayname) with id $($GraphRequest.id) for " -Sev "Info"
+    Log-Request -user $request.headers.'x-ms-client-principal' -API $APINAME -tenant $($groupobj.tenantid) -message "Created group $($groupobj.displayname) with id $($GraphRequest.id) " -Sev "Info"
 
 }
 catch {
