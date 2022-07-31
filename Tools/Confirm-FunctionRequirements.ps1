81443c711ff6f7ecc17ac5c95acbe95e44e860a2
John Duprey <jwd@johnwduprey.com>
2022-07-31 14:56:49 -0400

    Create Confirm-FunctionRequirements.ps1
    New script to check for unused modules in the requirements.psd1 file

---
+++ Tools/Confirm-FunctionRequirements.ps1
@@ -0,0 +1,67 @@
+# Set CippRoot directory and process requirements.psd1
+$CippRoot = (Get-Item $PSScriptRoot).Parent.FullName
+$Requirements = & { Get-Content $CippRoot\requirements.psd1 -Raw | Invoke-Expression }
+$Modules = $Requirements.Keys
+
+# Exclude top level Modules and Tools directories
+$Exclude = @(
+    'Modules'
+    'Tools'
+)
+$Files = Get-ChildItem $CippRoot -Exclude $Exclude | Get-ChildItem -Recurse -Include @('*.ps1', '*.psm1')
+
+# Process each module in requirements
+$ModuleTests = $Modules | ForEach-Object -Parallel {
+    $ModuleRefs = 0
+    $Success = $true
+    $Module = $_
+
+    try { 
+        # Check for module
+        if (-not (Get-Module -ListAvailable $Module)) {
+            Install-Module $Module -Force -ErrorAction Stop
+        }
+
+        # Get list of module commands
+        $Commands = (Get-Command -Module $Module -ErrorAction Stop).Name
+    
+        # Review all powershell files and search for module commands
+        $Files = foreach ($File in $using:Files) {
+            $References = 0
+            $Content = Get-Content -Raw $File
+            $MatchedCommands = foreach ($Command in $Commands) {
+                if ($Content | Select-String -Pattern $Command) {
+                    $References++
+                    $ModuleRefs++
+                    $Command
+                }
+            }
+            if ($References -gt 0) {
+                [pscustomobject]@{
+                    File       = $File | Resolve-Path -Relative
+                    References = $References
+                    Commands   = $MatchedCommands
+                }
+            }
+        }
+        if ($ModuleRefs -eq 0) {
+            $ErrorMsg = 'No references found'
+            $Success = $false
+        }
+    }
+    catch {
+        $ErrorMsg = $_.Exception.Message
+        $Success = $false
+    }
+
+    # Return processed module object
+    [pscustomobject]@{
+        Module     = $Module
+        References = $ModuleRefs
+        Files      = $Files
+        ErrorMsg   = $ErrorMsg
+        Success    = $Success
+    }
+}
+
+$ModuleTests
