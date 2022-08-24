675537acee1a67ae34ad120f7a0a7a078175f31c
Kelvin Tegelaar <you@example.com>
2022-08-24 22:16:33 +0200

    scriptroot

--- GraphHelper.psm1
+++ GraphHelper.psm1
@@ -194,7 +194,7 @@ function New-GraphPOSTRequest ($uri, $tenantid, $body, $type, $scope, $AsApp) {
 }
 
 function convert-skuname($skuname, $skuID) {
-    Set-Location $ENV:CippRoot
+    Set-Location (Get-Item $PSScriptRoot).Parent.FullName
     $ConvertTable = Import-Csv Conversiontable.csv
     if ($skuname) { $ReturnedName = ($ConvertTable | Where-Object { $_.String_Id -eq $skuname } | Select-Object -Last 1).'Product_Display_Name' }
     if ($skuID) { $ReturnedName = ($ConvertTable | Where-Object { $_.guid -eq $skuid } | Select-Object -Last 1).'Product_Display_Name' }
