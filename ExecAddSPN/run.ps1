dc5245ae2cc2461c32daf31d165e0f55cdd6146f
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2022-09-29 09:53:21 +0200

    link to correct app

--- ExecAddSPN/run.ps1
+++ ExecAddSPN/run.ps1
@@ -10,7 +10,7 @@ Write-LogMessage -user $request.headers.'x-ms-client-principal' -API $APINAME  -
 $Body = if ($Request.Query.Enable) { '{"accountEnabled":"true"}' } else { '{"accountEnabled":"false"}' }
 try {
       $GraphRequest = New-GraphPostRequest -uri "https://graph.microsoft.com/v1.0/servicePrincipals" -tenantid $ENV:TenantID -type POST -Body "{ `"appId`": `"2832473f-ec63-45fb-976f-5d45a7d4bb91`" }" -NoAuthCheck $true
-      $Results = [pscustomobject]@{"Results" = "Successfully completed request. See the documentation on how to add your GDAP migration permissions to your SAM application here: " }
+      $Results = [pscustomobject]@{"Results" = "Successfully completed request. Add your GDAP migration permissions to your SAM application here: https://portal.azure.com/#view/Microsoft_AAD_RegisteredApps/ApplicationMenuBlade/~/CallAnAPI/appId/$($ENV:ApplicationID)/isMSAApp/ " }
 }
 catch {
       $Results = [pscustomobject]@{"Results" = "Failed to add SPN. Please manually execute 'New-AzureADServicePrincipal -AppId 2832473f-ec63-45fb-976f-5d45a7d4bb91' The error was $($_.Exception.Message)" }
