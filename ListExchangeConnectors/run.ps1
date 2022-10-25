0332d08607de168f19bd51691d0e4ae88cb380f4
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2022-10-25 18:42:25 +0200

    create list exchange connectors

---
+++ ListExchangeConnectors/run.ps1
@@ -0,0 +1,25 @@
+using namespace System.Net
+
+# Input bindings are passed in via param block.
+param($Request, $TriggerMetadata)
+
+$APIName = $TriggerMetadata.FunctionName
+Write-LogMessage -user $request.headers.'x-ms-client-principal' -API $APINAME  -message "Accessed this API" -Sev "Debug"
+$Tenantfilter = $request.Query.tenantfilter
+
+try {
+    $Outbound = New-ExoRequest -tenantid $Tenantfilter -cmdlet "Get-OutboundConnector"
+    $Inbound = New-ExoRequest -tenantid $Tenantfilter -cmdlet "Get-InboundConnector"
+    $StatusCode = [HttpStatusCode]::OK
+}
+catch {
+    $ErrorMessage = Get-NormalizedError -Message $_.Exception.Message
+    $StatusCode = [HttpStatusCode]::Forbidden
+    $GraphRequest = $ErrorMessage
+}
+
+# Associate values to output bindings by calling 'Push-OutputBinding'.
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+        StatusCode = $StatusCode
+        Body       = @($outbound, $Inbound)
+    })
