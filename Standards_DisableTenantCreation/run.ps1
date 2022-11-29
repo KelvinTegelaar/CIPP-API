aaaa33465bceb678e942597830574dffed05e023
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2022-11-29 16:26:43 +0100

    disable tenant creation added

--- Standards_DisableTenantCreation/run.ps1
+++ Standards_DisableTenantCreation/run.ps1
@@ -3,7 +3,6 @@ param($tenant)
 try {
     $body = '{"defaultUserRolePermissions":{"allowedToCreateTenants":false}}'
     (New-GraphPostRequest -tenantid $tenant -Uri "https://graph.microsoft.com/beta/policies/authorizationPolicy/authorizationPolicy" -Type patch -Body $body -ContentType "application/json")
-
     Write-LogMessage -API "Standards" -tenant $tenant -message "Standards API: Disabled users from creating tenants." -sev Info
 }
 catch {
