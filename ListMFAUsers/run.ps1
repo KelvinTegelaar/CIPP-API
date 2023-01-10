e848f698ef39a3802e7ed23c951480d235f15b12
Mark <85716589+markdavidoffosit@users.noreply.github.com>
2023-01-10 22:03:51 +0800

    Added more data export to ListMFAUsers

--- ListMFAUsers/run.ps1
+++ ListMFAUsers/run.ps1
@@ -73,7 +73,9 @@ if ($Request.query.TenantFilter -ne 'AllTenants') {
 
         $MFARegUser = if (($MFARegistration | Where-Object -Property UserPrincipalName -EQ $_.UserPrincipalName).IsMFARegistered -eq $null) { $false } else { ($MFARegistration | Where-Object -Property UserPrincipalName -EQ $_.UserPrincipalName).IsMFARegistered }
         [PSCustomObject]@{
+            ID              = $_.Id
             UPN             = $_.UserPrincipalName
+            DisplayName     = $_.UserDisplayName
             AccountEnabled  = $AccountState
             PerUser         = $PerUser
             isLicensed      = $_.isLicensed
