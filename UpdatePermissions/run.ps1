13d24bc53cee575608daf343ce40892b20175ec4
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2023-01-18 14:13:24 +0100

    fixed

--- UpdatePermissions/run.ps1
+++ UpdatePermissions/run.ps1
@@ -5,17 +5,23 @@ param($Timer)
 $currentUTCtime = (Get-Date).ToUniversalTime()
 
 $Table = Get-CIPPTable -TableName cpvtenants
-$Rows = Get-AzDataTableEntity @Table
+$CPVRows = Get-AzDataTableEntity @Table
 
 $Tenants = get-tenants
 
 foreach ($Row in $Tenants ) {
     Write-Host "Processing tenants"
-    if (!$rows) {
+    if (!($CPVRows)) {
+        "No list available"
         Push-OutputBinding -Name Msg -Value $row.customerId
     }
 
-    if ($rows | Where-Object { $_.customerId -eq $row.customerId } | Where-Object { $_.LastApply -EQ $null -or $_.LastApply -lt (Get-Date).AddSeconds(-14) }) {
+    if (!($CPVRows | Where-Object { $row.customerId -In $CPVRows.Tenant })) {
+        "Not in the list: $($row.customerId)"
+        Push-OutputBinding -Name Msg -Value $row.customerId
+    }
+
+    if ($CPVRows | Where-Object { $_.Tenant -eq $row.customerId } | Where-Object { $_.LastApply -EQ $null -or $_.LastApply -lt (Get-Date).AddSeconds(-14) }) {
         Write-Host "In list, Old age."
         Push-OutputBinding -Name Msg -Value $row.customerId
     }
