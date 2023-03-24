0c19b72b1d9fc11489436c18e113c03feeeb7382
BNWEIN <bweinberg@prime-networks.co.uk>
2023-03-24 15:33:34 +0000

    Update run.ps1

--- ListUserMailboxRules/run.ps1
+++ ListUserMailboxRules/run.ps1
@@ -16,7 +16,11 @@ $UserID = $Request.Query.UserID
             $GraphRequest = New-ExoRequest -tenantid $TenantFilter -cmdlet "Get-InboxRule" -cmdParams @{mailbox = $UserID} | Select-Object
             @{ Name = 'DisplayName'; Expression = { $_.displayName} },
             @{ Name = 'Description'; Expression = { $_.Description} },
-            @{ Name = 'Forwards To'; Expression = { $_.ForwardTo} }
+            @{ Name = 'Redirect To'; Expression = { $_.RedirectTo} },
+            @{ Name = 'Copy To Folder'; Expression = { $_.CopyToFolder} },
+            @{ Name = 'Move To Folder'; Expression = { $_.MoveToFolder} },
+            @{ Name = 'Soft Delete Message'; Expression = { $_.SoftDeleteMessage} },
+            @{ Name = 'Delete Message'; Expression = { $_.DeleteMessage} }
 
     catch {
         Write-LogMessage -user $request.headers.'x-ms-client-principal' -API $APINAME -message "Failed to retrieve mailbox rules $($request.query.id): $($_.Exception.message) " -Sev 'Error' -tenant $TenantFilter
