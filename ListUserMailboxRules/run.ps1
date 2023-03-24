24e8eef61bb41abff5b533988ad0a44aaf608613
BNWEIN <bweinberg@prime-networks.co.uk>
2023-03-24 14:43:34 +0000

    Added User Mailbox Rules to the Users Page
    Added User Mailbox Rules to the Users Page

---
+++ ListUserMailboxRules/run.ps1
@@ -0,0 +1,34 @@
+using namespace System.Net
+
+# Input bindings are passed in via param block.
+param($Request, $TriggerMetadata)
+
+$APIName = $TriggerMetadata.FunctionName
+Write-LogMessage -user $request.headers.'x-ms-client-principal' -API $APINAME  -message "Accessed this API" -Sev "Debug"
+
+
+# Write to the Azure Functions log stream.
+Write-Host "PowerShell HTTP trigger function processed a request."
+
+# Interact with query parameters or the body of the request.
+$TenantFilter = $Request.Query.TenantFilter
+$UserID = $Request.Query.UserID
+            $GraphRequest = New-ExoRequest -tenantid $TenantFilter -cmdlet "Get-InboxRule" -cmdParams @{mailbox = $UserID} | Select-Object
+            @{ Name = 'DisplayName'; Expression = { $_.displayName} },
+            @{ Name = 'Description'; Expression = { $_.Description} },
+            @{ Name = 'Forwards To'; Expression = { $_.ForwardTo} }
+
+    catch {
+        Write-LogMessage -user $request.headers.'x-ms-client-principal' -API $APINAME -message "Failed to retrieve mailbox rules $($request.query.id): $($_.Exception.message) " -Sev 'Error' -tenant $TenantFilter
+        # Associate values to output bindings by calling 'Push-OutputBinding'.
+        Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+                StatusCode = '500'
+                Body       = $(Get-NormalizedError -message $_.Exception.message)
+            })
+    }
+
+# Associate values to output bindings by calling 'Push-OutputBinding'.
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+    StatusCode = [HttpStatusCode]::OK
+    Body       = @($GraphRequest)
+})
