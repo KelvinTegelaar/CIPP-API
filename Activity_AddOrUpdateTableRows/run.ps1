84de215febd5bb792d0156ef9c4b2bf8fff42024
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2023-03-28 15:02:10 +0200

    better handling for adding entities.

--- Activity_AddOrUpdateTableRows/run.ps1
+++ Activity_AddOrUpdateTableRows/run.ps1
@@ -1,12 +1,15 @@
 param($TableParams)
+$TableName = ($TableParams.Context['TableName'])
+$Table = Get-CippTable -tablename $TableName
+Write-Host "The table is $TableName"
+
 foreach ($param in $TableParams.Entity) {
   try {
-    $tableparams.entity = $param
     #Sending each item indivually, if it fails, log an error.
-    Add-AzDataTableEntity @tableparams
+    Add-AzDataTableEntity @Table -Entity $param -Force
   }
   catch {
-    Write-Host ($TableParams | ConvertTo-Json)
+    Write-Host $($_.Exception.Message)
     Write-LogMessage -API 'Activity_AddOrUpdateTableRows' -message "Unable to write to '$($TableParams.TableName)' Using RowKey $($param.RowKey) table: $($_.Exception.Message)" -sev error
   }
 }
