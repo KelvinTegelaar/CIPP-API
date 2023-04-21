32138086afc91a7340f8c716c9a44cb331aebb45
jrandRTCC <66025354+jrandRTCC@users.noreply.github.com>
2023-04-21 11:27:09 -0400

    Added Audit Log Extra

---
+++ Standards_AuditLogExtra/run.ps1
@@ -0,0 +1,18 @@
+param($tenant)
+
+try {
+    $DehydratedTenant = (New-ExoRequest -tenantid $Tenant -cmdlet "Get-OrganizationConfig").IsDehydrated
+    if ($DehydratedTenant) {
+        New-ExoRequest -tenantid $Tenant -cmdlet "Enable-OrganizationCustomization"
+    }
+    $AdminAuditLogParams = @{
+        UnifiedAuditLogIngestionEnabled = $true
+    }
+    New-ExoRequest -tenantid $Tenant -cmdlet "Set-AdminAuditLogConfig" -cmdParams $AdminAuditLogParams
+    Write-LogMessage -API "Standards" -tenant $tenant -message "Unified Audit Log Enabled Extra." -sev Info
+
+}
+catch {
+    $ErrorMessage = Get-NormalizedError -Message $_.Exception.Message
+    Write-LogMessage -API "Standards" -tenant $tenant -message "Failed to apply Unified Audit Log Extra. Error: $ErrorMessage" -sev Error
+}
