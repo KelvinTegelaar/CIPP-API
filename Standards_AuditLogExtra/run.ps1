d4b2e633666563c1c9b3682c6b47333efce2f4f8
jrandRTCC <66025354+jrandRTCC@users.noreply.github.com>
2023-04-21 14:37:19 -0400

    Made Audit Extra set user mailbox accounts

--- Standards_AuditLogExtra/run.ps1
+++ Standards_AuditLogExtra/run.ps1
@@ -5,12 +5,12 @@ try {
     if ($DehydratedTenant) {
         New-ExoRequest -tenantid $Tenant -cmdlet "Enable-OrganizationCustomization"
     }
-    $AdminAuditLogParams = @{
-        UnifiedAuditLogIngestionEnabled = $true
-    }
-    New-ExoRequest -tenantid $Tenant -cmdlet "Set-AdminAuditLogConfig" -cmdParams $AdminAuditLogParams
-    Write-LogMessage -API "Standards" -tenant $tenant -message "Unified Audit Log Enabled Extra." -sev Info
-
+    $users = New-GraphGetRequest -uri "https://graph.microsoft.com/beta/users/?`$top=999&`$select=id,userPrincipalName,assignedLicenses" -Tenantid $tenantfilter
+    $AuditLogAgeLimit = 365
+    (New-ExoRequest -tenantid $TenantFilter -cmdlet "Get-mailbox") |Select UserPrincipalName|%{  (New-ExoRequest -tenantid $TenantFilter -cmdlet "Set-Mailbox" -cmdParams @{Identity = $_.userprincipalname; AuditEnabled = $true; AuditLogAgeLimit = $AuditLogAgeLimit; `
+    AuditAdmin = @{Add="Copy","Create","FolderBind","HardDelete","Move","MoveToDeletedItems","SendAs","SendOnBehalf","SoftDelete","Update","UpdateFolderPermissions","UpdateInboxRules","UpdateCalendarDelegation"}; `
+    AuditDelegate = @{Add="Create","FolderBind","HardDelete","Move","MoveToDeletedItems","SendAs","SendOnBehalf","SoftDelete","Update","UpdateFolderPermissions","UpdateInboxRules"};  `
+    AuditOwner = @{Add="Create","HardDelete","Move","Mailboxlogin","MoveToDeletedItems","SoftDelete","Update","UpdateFolderPermissions","UpdateInboxRules","UpdateCalendarDelegation"}}})
 }
 catch {
     $ErrorMessage = Get-NormalizedError -Message $_.Exception.Message
