da77900b91f4849641764e2327a2848bde164f6a
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2023-04-25 10:37:53 +0200

    added laps standard

---
+++ Standards_laps/run.ps1
@@ -0,0 +1,8 @@
+﻿param($tenant)
+try {
+    New-GraphPostRequest -tenantid $tenant -Uri "https://graph.microsoft.com/beta/policies/deviceRegistrationPolicy" -Type PUT -Body '{ "localAdminPassword":{"isEnabled": true }}' -ContentType "application/json"
+    Write-LogMessage -API "Standards" -tenant $tenant -message  "LAPS has been enabled." -sev Info
+}
+catch {
+    Write-LogMessage -API "Standards" -tenant $tenant -message  "Failed to set LAPS: $($_.exception.message)" -sev Error
+}
