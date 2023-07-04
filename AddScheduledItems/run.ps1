a6c5c4f8758a36e0f2b6f36cf430c5eb87dba214
Ashley Cooper <122837620+ashley-mspgeek@users.noreply.github.com>
2023-07-04 17:04:38 -0400

    added function for storing the users scheduled tasks and updated add function to include additional properties.

--- AddScheduledItems/run.ps1
+++ AddScheduledItems/run.ps1
@@ -6,10 +6,12 @@ $task = $Request.Body | ConvertFrom-Json
 $Table = Get-CIPPTable -TableName 'ScheduledTasks'
 Add-AzDataTableEntity @Table -Entity @{
     PartitionKey = 'ScheduledTask'
+    TaskState = 'Scheduled'
     RowKey = $task.TaskID
     Command = $task.Command
     Parameters = $task.Parameters
     ScheduledTime = $task.ScheduledTime
+    Results = 'Not Executed'
     # add more properties here based on what properties your tasks have
 }
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
