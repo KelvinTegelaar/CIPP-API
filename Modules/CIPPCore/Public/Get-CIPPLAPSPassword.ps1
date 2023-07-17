2530d98f1d9c01eb618b0b473306825d31697851
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2023-07-17 14:45:50 +0200

    BETTER RETURNS

--- Modules/CIPPCore/Public/Get-CIPPLAPSPassword.ps1
+++ Modules/CIPPCore/Public/Get-CIPPLAPSPassword.ps1
@@ -12,8 +12,9 @@ function Get-CIPPLapsPassword {
         $GraphRequest = (New-GraphGetRequest -noauthcheck $true -uri "https://graph.microsoft.com/beta/deviceLocalCredentials/$($device)?`$select=credentials" -tenantid $TenantFilter).credentials | Select-Object -First 1 | ForEach-Object {
             $PlainText = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($_.passwordBase64))
             $date = $_.BackupDateTime
-            return  "The password for $($_.AccountName) is $($PlainText) generated at $($date)"
+            "The password for $($_.AccountName) is $($PlainText) generated at $($date)"
         }
+        if ($GraphRequest) { return $GraphRequest } else { return "No LAPS password found for $device" }
     }
     catch {
         Write-LogMessage -user $ExecutingUser -API $APIName -message "Could not add OOO for $($userid)" -Sev "Error" -tenant $TenantFilter
