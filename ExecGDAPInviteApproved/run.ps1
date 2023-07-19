4fee9e391a5d5b52037f2be6d0b016eb15cfcb85
John Duprey <jwd@johnwduprey.com>
2023-07-19 19:50:32 -0400

    Add HTTP output binding

--- ExecGDAPInviteApproved/run.ps1
+++ ExecGDAPInviteApproved/run.ps1
@@ -3,3 +3,10 @@ using namespace System.Net
 param($Request, $TriggerMetadata)
 
 Set-CIPPGDAPInviteGroups
+
+$body = @{Results = @('Processing recently activated GDAP relationships') }
+
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+        StatusCode = [HttpStatusCode]::OK
+        Body       = $body
+    })
