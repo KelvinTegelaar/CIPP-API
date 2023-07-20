dcf9e36079deb980529995112041a1d1357f531d
John Duprey <jwd@johnwduprey.com>
2023-07-19 20:44:10 -0400

    GDAP invite tweaks

--- ExecGDAPInviteApproved/run.ps1
+++ ExecGDAPInviteApproved/run.ps1
@@ -2,6 +2,9 @@ using namespace System.Net
 
 param($Request, $TriggerMetadata)
 
+$APIName = $TriggerMetadata.FunctionName
+Write-LogMessage -user $request.headers.'x-ms-client-principal' -API $APINAME -message 'Accessed this API' -Sev 'Debug'
+
 Set-CIPPGDAPInviteGroups
 
 $body = @{Results = @('Processing recently activated GDAP relationships') }
