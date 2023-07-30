e494767c76e1ee476eced4240271e1e3a790cc2a
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2023-07-30 23:46:35 +0200

    added webhook subscription management

---
+++ Modules/CIPPCore/Public/New-CIPPGraphSubscription.ps1
@@ -0,0 +1,46 @@
+function New-CIPPGraphSubscription {
+    [CmdletBinding()]
+    param (
+        $TenantFilter,
+        $TypeofSubscription,
+        $Resource,
+        $EventType,
+        $APIName = "Create Webhook",
+        $ExecutingUser
+    )
+    $CIPPID = (New-Guid).GUID
+    $expiredate = (Get-Date).AddDays(1).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ")
+    $params = @{
+        changeType         = $TypeofSubscription
+        notificationUrl    = "https://webhook.site/9650bc4a-0120-41de-8ffd-6616e71244e1?EventType=$EventType&CIPPID=$CIPPID"
+        resource           = $Resource
+        expirationDateTime = $expiredate
+    } | ConvertTo-Json
+
+    try {
+        $GraphRequest = New-GraphPostRequest -uri "https://graph.microsoft.com/beta/subscriptions" -tenantid $TenantFilter -type POST -body $params -verbose
+        #If creation is succesfull, we store the GUID in the storage table webhookTable to make sure we can check against this later on. 
+        #We store the GUID as rowkey, the event type, the resource, and the expiration date as properties, we also add the Tenant name so we can easily find this later on.
+        #We don't store the return, because Ms decided that a renewal or re-authenticate does not change the url, but does change the id...
+        $WebhookTable = Get-CIPPTable -TableName webhookTable
+        $WebhookRow = @{
+            PartitionKey           = [string]$TenantFilter
+            RowKey                 = [string]$CIPPID
+            EventType              = [string]$EventType
+            Resource               = [string]$Resource
+            Expiration             = [string]$expiredate
+            WebhookNotificationUrl = [string]$GraphRequest.notificationUrl
+        }
+        $null = Add-AzDataTableEntity @WebhookTable -Entity $WebhookRow
+        #todo: add remove webhook function, add check webhook function, add list webhooks function
+        #add refresh webhook function based on table. 
+        Write-LogMessage -user $ExecutingUser -API $APIName -message "Created Webhook subscription for $($TenantFilter)" -Sev "Info" -tenant $TenantFilter
+        return "Created Webhook subscription for $($TenantFilter)"
+    }
+    catch {
+        Write-LogMessage -user $ExecutingUser -API $APIName -message "Failed to create Webhook Subscription: $($_.Exception.Message)" -Sev "Error" -tenant $TenantFilter
+        Return "Failed to create Webhook Subscription: $($_.Exception.Message)" 
+    }
+
+}
+
