6c296d90ca4612c42a5027d44cbb88ed162f61fb
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2023-07-31 00:52:07 +0200

    add classic auditlog api.

--- Modules/CIPPCore/Public/New-CIPPGraphSubscription.ps1
+++ Modules/CIPPCore/Public/New-CIPPGraphSubscription.ps1
@@ -2,6 +2,7 @@ function New-CIPPGraphSubscription {
     [CmdletBinding()]
     param (
         $TenantFilter,
+        [bool]$auditLogAPI = $false,
         $TypeofSubscription,
         $Resource,
         $EventType,
@@ -16,24 +17,43 @@ function New-CIPPGraphSubscription {
         resource           = $Resource
         expirationDateTime = $expiredate
     } | ConvertTo-Json
+    $WebhookTable = Get-CIPPTable -TableName webhookTable
 
     try {
-        $GraphRequest = New-GraphPostRequest -uri "https://graph.microsoft.com/beta/subscriptions" -tenantid $TenantFilter -type POST -body $params -verbose
-        #If creation is succesfull, we store the GUID in the storage table webhookTable to make sure we can check against this later on. 
-        #We store the GUID as rowkey, the event type, the resource, and the expiration date as properties, we also add the Tenant name so we can easily find this later on.
-        #We don't store the return, because Ms decided that a renewal or re-authenticate does not change the url, but does change the id...
-        $WebhookTable = Get-CIPPTable -TableName webhookTable
-        $WebhookRow = @{
-            PartitionKey           = [string]$TenantFilter
-            RowKey                 = [string]$CIPPID
-            EventType              = [string]$EventType
-            Resource               = [string]$Resource
-            Expiration             = [string]$expiredate
-            WebhookNotificationUrl = [string]$GraphRequest.notificationUrl
+        if ($auditLogAPI) {
+            $AuditLogParams = @{
+                webhook = @{
+                    "address" = "https://webhook.site/9650bc4a-0120-41de-8ffd-6616e71244e1/"
+                }
+            } | ConvertTo-Json
+            $AuditLog = New-GraphPOSTRequest -uri "https://manage.office.com/api/v1.0/$($TenantFilter)/activity/feed/subscriptions/start?contentType=$EventType&PublisherIdentifier=$($TenantFilter)" -tenantid $TenantFilter -type POST -body $AuditLogparams -verbose
+            $WebhookRow = @{
+                PartitionKey           = [string]$TenantFilter
+                RowKey                 = [string]$CIPPID
+                EventType              = [string]$EventType
+                Resource               = "M365AuditLogs"
+                Expiration             = "None"
+                WebhookNotificationUrl = [string]$Auditlog.webhook.address
+            }
+            $null = Add-AzDataTableEntity @WebhookTable -Entity $WebhookRow
         }
-        $null = Add-AzDataTableEntity @WebhookTable -Entity $WebhookRow
-        #todo: add remove webhook function, add check webhook function, add list webhooks function
-        #add refresh webhook function based on table. 
+        else {
+            $GraphRequest = New-GraphPostRequest -uri "https://graph.microsoft.com/beta/subscriptions" -tenantid $TenantFilter -type POST -body $params -verbose
+            #If creation is succesfull, we store the GUID in the storage table webhookTable to make sure we can check against this later on. 
+            #We store the GUID as rowkey, the event type, the resource, and the expiration date as properties, we also add the Tenant name so we can easily find this later on.
+            #We don't store the return, because Ms decided that a renewal or re-authenticate does not change the url, but does change the id...
+            $WebhookRow = @{
+                PartitionKey           = [string]$TenantFilter
+                RowKey                 = [string]$CIPPID
+                EventType              = [string]$EventType
+                Resource               = [string]$Resource
+                Expiration             = [string]$expiredate
+                WebhookNotificationUrl = [string]$GraphRequest.notificationUrl
+            }
+            $null = Add-AzDataTableEntity @WebhookTable -Entity $WebhookRow
+            #todo: add remove webhook function, add check webhook function, add list webhooks function
+            #add refresh webhook function based on table. 
+        }
         Write-LogMessage -user $ExecutingUser -API $APIName -message "Created Webhook subscription for $($TenantFilter)" -Sev "Info" -tenant $TenantFilter
         return "Created Webhook subscription for $($TenantFilter)"
     }
