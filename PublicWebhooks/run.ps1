2d619a28e298f7a533f02a84a51b19eea762bbbd
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2023-08-07 12:09:49 +0200

    added logging

--- PublicWebhooks/run.ps1
+++ PublicWebhooks/run.ps1
@@ -4,16 +4,21 @@ using namespace System.Net
 param($Request, $TriggerMetadata)
 $WebhookTable = Get-CIPPTable -TableName webhookTable
 $Webhooks = Get-AzDataTableEntity @WebhookTable
-
+Write-Host "Received request"
+Write-Host "CIPPID: $($request.CIPPID)"
 $url = ($request.headers.'x-ms-original-url').split('/api') | Select-Object -First 1
   
 if ($Request.CIPPID -in $Webhooks.CIPPID) {
+    Write-Host "Found matching CIPPID"
+
     $Webhookinfo = $Webhooks | Where-Object -Property CIPPID -EQ $Request.CIPPID
     if ($Request.query.ValidationToken -or $Request.body.validationCode) {
+        Write-Host "Validation token received"
         $body = $request.query.ValidationToken
     }
 
     if ($Request.body.ContentUri) {
+        Write-Host "ContentUri received"
         if ($Request.body.ContentUri -notlike "https://manage.office.com/api/v1.0/*") { exit }
         $TenantFilter = (Get-Tenants | Where-Object -Property customerId -EQ $Request.body.TenantID).defaultDomainName
         $Data = New-GraphPostRequest -type GET -uri "$($request.body.contenturi)" -tenantid $TenantFilter -scope "https://manage.office.com/.default"
@@ -24,7 +29,10 @@ if ($Request.CIPPID -in $Webhooks.CIPPID) {
     }
 
     foreach ($Item in $Data) {
+        Write-Host "Data to process found."
+
         if ($item.Operation -in ($Webhooks.Operation -split ',')) {
+            Write-Host "Working on $($item.operation)."
             Invoke-CippWebhookProcessing -TenantFilter $TenantFilter -Data $Data -CIPPPURL $url -allowedlocations $Webhookinfo.AllowedLocations
         }
     }
