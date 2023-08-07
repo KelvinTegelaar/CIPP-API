af8ab3efab70f4f8976d0e3d714a933bd03103e2
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2023-08-07 16:18:01 +0200

    force typecasting?

--- PublicWebhooks/run.ps1
+++ PublicWebhooks/run.ps1
@@ -18,7 +18,7 @@ if ($Request.CIPPID -in $Webhooks.CIPPID) {
         $body = $request.query.ValidationToken
     }
     Write-Host "Request body: $($request.body | ConvertTo-Json -Depth 10)"
-    foreach ($ReceivedItem in $Request.body) {
+    foreach ($ReceivedItem in ([pscustomobject]$Request.body)) {
         Write-Host "ContentUri received: $($ReceivedItem.ContentUri)"
         if ($ReceivedItem.ContentUri -notlike "https://manage.office.com/api/v1.0/*") {
             Write-Host "Potential url forgery detected. Quitting to not send headers."
