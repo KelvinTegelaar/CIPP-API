7fa37d6c0b333ceabe3dd6ae894fbad0b779f9d8
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2023-08-09 20:53:01 +0200

    improved geoip lookup

--- ExecGeoIPLookup/run.ps1
+++ ExecGeoIPLookup/run.ps1
@@ -5,11 +5,20 @@ param($Request, $TriggerMetadata)
 
 $APIName = $TriggerMetadata.FunctionName
 Write-LogMessage -user $request.headers.'x-ms-client-principal' -API $APINAME -message 'Accessed this API' -Sev 'Debug'
-
-$location = Get-CIPPGeoIPLocation -IP $Request.Query.IP
+Write-Host $Request.Query.IP
+$location = Get-CIPPGeoIPLocation -IP $Request.query.IP
+$ARINInfo = Invoke-RestMethod -Uri "https://whois.arin.net/rest/ip/$($Request.Query.IP)" -Method Get -ContentType "application/json" -Headers @{Accept = "application/json" }
+$LocationInfo = [pscustomobject]@{
+    location     = $location
+    arin         = $ARINInfo
+    startaddress = $arininfo.net.startaddress.'$'  
+    endAddress   = $arininfo.net.endAddress.'$'
+    OrgRef       = $arininfo.net.orgRef.'@NAME'
+    SubnetName   = $arininfo.net.name.'$'
+}
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
         StatusCode = [HttpStatusCode]::OK
-        Body       = @($location)
+        Body       = $LocationInfo
     })
--- Modules/CIPPCore/Public/Get-CIPPGeoIPLocation.ps1
+++ Modules/CIPPCore/Public/Get-CIPPGeoIPLocation.ps1
@@ -1,5 +1,3 @@
-
-
 function Get-CIPPGeoIPLocation {
     [CmdletBinding()]
     param (
@@ -8,12 +6,15 @@ function Get-CIPPGeoIPLocation {
     if ($IP -like '*:*') { $IP = $IP.split(':')[0] }
     $CTX = new-azdatatablecontext -TableName geoipdb -ConnectionString 'TableEndpoint=https://cyberdraingeoipdb.table.core.windows.net/;SharedAccessSignature=sv=2022-11-02&ss=t&srt=o&sp=rl&se=2025-08-08T21:05:23Z&st=2023-08-08T13:05:23Z&spr=https&sig=89Bmk2Un89xqNzZPLkryFnLRCjHs9rCWGUJjhvf5mso%3D'
     $GeoTable = @{ Context = $CTX }
-    $IP.Split(".") | ForEach-Object { $IPAsint = 0; $IPAddressByte = 0 } {
+
+    $IPAsint = 0
+    $IP.Split(".") | ForEach-Object {
+        $IPAddressByte = 0
         [int]::TryParse($_, [ref] $IPAddressByte) | Out-Null
-        $IPAsint = $IPAsint -shl 8 -bor $IPAddressByte
+        $IPAsint = ([long]($IPAsint -shl 8)) -bor [byte]$_
     }
+
     $location = (Get-AzDataTableEntity @GeoTable -Filter "PartitionKey eq 'GeoIP' and RowKey le '$IPAsint' and ipTo ge '$IPAsint'") | Select-Object -Last 1
 
     return $location
-     
 }
