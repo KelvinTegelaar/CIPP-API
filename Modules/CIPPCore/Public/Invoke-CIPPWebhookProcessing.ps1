77db727f164bf1daf25e4bb400adb87110debea5
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2023-08-10 13:11:04 +0200

    d

--- Modules/CIPPCore/Public/Invoke-CIPPWebhookProcessing.ps1
+++ Modules/CIPPCore/Public/Invoke-CIPPWebhookProcessing.ps1
@@ -22,7 +22,8 @@ function Invoke-CippWebhookProcessing {
     #Database for knownlocations
     $LocationTable = Get-CIPPTable -TableName 'knownlocationdb'
 
-    Write-Host "Result status $($data.ResultStatus))"
+    Write-Host "Result status $($data.ResultStatus)"
+    Write-Host "Result status detail $($data.ExtendedProperties.resultstatusdetail)"
     switch ($data.operation) {
         { "UserLoggedIn" -eq $data.operation -and $Country -notin $AllowedLocations -and $data.ResultStatus -eq "Success" -and $data.ExtendedProperties.resultstatusdetail -eq "Success" } { $data.operation = "UserLoggedInFromUnknownLocation"; break }
         { "UserloggedIn" -eq $data.operation -and $data.UserType -eq 2 -and $data.ResultStatus -eq "Success" -and $data.ExtendedProperties.resultstatusdetail -eq "Success" } { $data.operation = "AdminLoggedIn"; break }
