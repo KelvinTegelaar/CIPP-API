b6362019deb2c7eb024bcc07dfdbc3199d0cd7be
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2023-08-10 23:50:29 +0200

    fixes issuers with returned results

--- ExecCPVPermissions/run.ps1
+++ ExecCPVPermissions/run.ps1
@@ -91,5 +91,5 @@ $StatusCode = [HttpStatusCode]::OK
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
         StatusCode = $StatusCode
-        Body       = @($GraphRequest)
+        Body       = @(@{Results = $GraphRequest })
     })
