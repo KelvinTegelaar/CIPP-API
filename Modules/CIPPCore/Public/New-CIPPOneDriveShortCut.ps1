3da9c57dce858d33695dd02700e50a80e79655cb
KelvinTegelaar <Kelvin@limenetworks.nl>
2023-09-08 01:06:35 +0200

    sharepoint url stuff

--- Modules/CIPPCore/Public/New-CIPPOneDriveShortCut.ps1
+++ Modules/CIPPCore/Public/New-CIPPOneDriveShortCut.ps1
@@ -9,7 +9,7 @@ function New-CIPPOneDriveShortCut {
         $APIName = "Create OneDrive shortcut",
         $ExecutingUser
     )
-
+    Write-Host "Received $username and $userid. We're using $url and $TenantFilter"
     try {
         $SiteInfo = New-GraphGetRequest -uri "https://graph.microsoft.com/beta/sites/" -tenantid $TenantFilter -asapp $true | Where-Object -Property weburl -EQ $url
         $ListItemUniqueId = (New-GraphGetRequest -uri "https://graph.microsoft.com/beta/sites/$($siteInfo.id)/drive?`$select=SharepointIds" -tenantid $TenantFilter -asapp $true).SharePointIds
@@ -26,12 +26,13 @@ function New-CIPPOneDriveShortCut {
             }
             '@microsoft.graph.conflictBehavior' = "rename"
         } | ConvertTo-Json -Depth 10
-        New-GraphPOSTRequest -method POST "https://graph.microsoft.com/beta/users/$userid/drive/root/children" -body $body -tenantid $TenantFilter -asapp $true
-        return "Succesfully created Shortcut in OneDrive for $username using $url"
+        New-GraphPOSTRequest -method POST "https://graph.microsoft.com/beta/users/$username/drive/root/children" -body $body -tenantid $TenantFilter -asapp $true
+        Write-LogMessage -message "Created OneDrive shortcut called $($SiteInfo.displayName) for $($username)" -Sev 'info' -API $APIName -user $ExecutingUser
+        return "Created OneDrive Shortcut for $username called $($SiteInfo.displayName) "
     }
     catch {
         Write-LogMessage -message "Could not add Onedrive shortcut to $username : $($_.Exception.Message)" -Sev 'error' -API $APIName -user $ExecutingUser
-        return $false
+        return "Could not add Onedrive shortcut to $username : $($_.Exception.Message)"
     }
 }
 
