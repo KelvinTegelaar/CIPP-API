ccbc544b4ae37ce0bc58ff4b8495d0c6eb0bbb68
lwhitelock <79275328+lwhitelock@users.noreply.github.com>
2023-09-18 09:37:39 +0100

    Fixed not configured handling

--- Modules/CippExtensions/NinjaOne/Get-NinjaOneFieldMapping.ps1
+++ Modules/CippExtensions/NinjaOne/Get-NinjaOneFieldMapping.ps1
@@ -6,10 +6,6 @@ function Get-NinjaOneFieldMapping {
     try {
         #Get available mappings
         $Mappings = [pscustomobject]@{}
-        $Filter = "PartitionKey eq 'NinjaFieldMapping'"
-        Get-AzDataTableEntity @CIPPMapping -Filter $Filter | ForEach-Object {
-            $Mappings | Add-Member -NotePropertyName $_.RowKey -NotePropertyValue @{ label = "$($_.NinjaOneName)"; value = "$($_.NinjaOne)" }
-        }
 
         [System.Collections.Generic.List[PSCustomObject]]$CIPPFields = @(
             [PSCustomObject]@{
@@ -38,6 +34,11 @@ function Get-NinjaOneFieldMapping {
             }
         )
 
+        $Filter = "PartitionKey eq 'NinjaFieldMapping'"
+        Get-AzDataTableEntity @CIPPMapping -Filter $Filter | ForEach-Object {
+            $Mappings | Add-Member -NotePropertyName $_.RowKey -NotePropertyValue @{ label = "$($_.NinjaOneName)"; value = "$($_.NinjaOne)" }
+        }
+
 
         $Table = Get-CIPPTable -TableName Extensionsconfig
         $Configuration = ((Get-AzDataTableEntity @Table).config | ConvertFrom-Json -ea stop).NinjaOne
@@ -52,14 +53,16 @@ function Get-NinjaOneFieldMapping {
         $NinjaCustomFieldsOrgRaw = (Invoke-WebRequest -uri "https://$($Configuration.Instance)/api/v2/device-custom-fields?scopes=organization" -Method GET -Headers @{Authorization = "Bearer $($token.access_token)" } -ContentType 'application/json').content | ConvertFrom-Json -depth 100
         [System.Collections.Generic.List[PSCustomObject]]$NinjaCustomFieldsOrg = $NinjaCustomFieldsOrgRaw | Where-Object { $_.technicianPermission -eq 'READ_ONLY' -and $_.type -in $CIPPFields.Type } | Select-Object @{n = 'name'; e = { $_.label } }, @{n = 'value'; e = { $_.name } }, type
 
-        $DoNotSync = [PSCustomObject]@{
-            name  = '--- Do not synchronize ---'
-            value = $null
-            type  = 'unset'
-        }
+        
     } catch {
         [System.Collections.Generic.List[PSCustomObject]]$NinjaCustomFieldsNode = @()
         [System.Collections.Generic.List[PSCustomObject]]$NinjaCustomFieldsOrg = @()
+    }
+
+    $DoNotSync = [PSCustomObject]@{
+        name  = '--- Do not synchronize ---'
+        value = $null
+        type  = 'unset'
     }
 
     $NinjaCustomFieldsOrg.Insert(0, $DoNotSync)
