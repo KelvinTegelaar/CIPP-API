c72f34c088dbfc55b584105b63e73fd495253bdb
BNWEIN <bweinberg@prime-networks.co.uk>
2023-09-26 13:08:20 +0100

    Update run.ps1

--- EditUser/run.ps1
+++ EditUser/run.ps1
@@ -18,7 +18,6 @@ try {
     $Email = "$($UserObj.username)@$($UserObj.domain)"
     $UserprincipalName = "$($UserObj.username)@$($UserObj.domain)"
     $BodyToship = [pscustomobject] @{
-        "officeLocation"    = $userobj.officeLocation
         "givenName"         = $userobj.firstname
         "surname"           = $userobj.lastname
         "city"              = $userobj.city
