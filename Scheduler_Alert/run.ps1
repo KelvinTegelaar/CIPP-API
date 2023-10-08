364bdbd8922d18902a325fce0b1f82709a27beac
Yoshify <xyoshify@gmail.com>
2023-10-09 07:48:51 +1000

    Pass $Tenant.tenantid to $ShippedAlerts in Scheduler_Alert/run.ps1

--- Scheduler_Alert/run.ps1
+++ Scheduler_Alert/run.ps1
@@ -379,7 +379,7 @@ try {
 
     $ShippedAlerts | ForEach-Object {
         if ($_ -notin $currentlog.Message) {
-            Write-LogMessage -message $_ -API 'Alerts' -tenant $tenant.tenant -sev Alert
+            Write-LogMessage -message $_ -API 'Alerts' -tenant $tenant.tenant -sev Alert -tenantid $Tenant.tenantid
         }
     }
     [PSCustomObject]@{
