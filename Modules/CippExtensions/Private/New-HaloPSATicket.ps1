88ad58cbdc4c28d18893738d99f4060a8229b040
KelvinTegelaar <Kelvin@limenetworks.nl>
2023-10-12 01:43:23 +0200

    allow adding of the ticket type

--- Modules/CippExtensions/Private/New-HaloPSATicket.ps1
+++ Modules/CippExtensions/Private/New-HaloPSATicket.ps1
@@ -10,25 +10,29 @@ function New-HaloPSATicket {
   $Configuration = ((Get-AzDataTableEntity @Table).config | ConvertFrom-Json).HaloPSA
 
   $token = Get-HaloToken -configuration $Configuration
-  #use the token to create a new ticket in HaloPSA
-  $body = ConvertTo-Json -Compress -Depth 10 -InputObject @(
-    [PSCustomObject]@{
-      files                      = $null
-      usertype                   = 1
-      userlookup                 = @{
-        id            = -1
-        lookupdisplay = "Enter Details Manually"
-      }
-      client_id                  = $client
-      site_id                    = $null
-      user_name                  = $null
-      reportedby                 = $null
-      summary                    = $title
-      details_html               = $description
-      donotapplytemplateintheapi = $true
-      attachments                = @()
+  $Object = [PSCustomObject]@{
+    files                      = $null
+    usertype                   = 1
+    userlookup                 = @{
+      id            = -1
+      lookupdisplay = "Enter Details Manually"
     }
-  )
+    client_id                  = $client
+    site_id                    = $null
+    user_name                  = $null
+    reportedby                 = $null
+    summary                    = $title
+    details_html               = $description
+    donotapplytemplateintheapi = $true
+    attachments                = @()
+  }
+
+  if ($Configuration.TicketType) {
+    $object | Add-Member -MemberType NoteProperty -Name "tickettype_id" -Value $Configuration.TicketType
+  }
+  #use the token to create a new ticket in HaloPSA
+  $body = ConvertTo-Json -Compress -Depth 10 -InputObject @($Object)
+
 
   Write-Host "Sending ticket to HaloPSA"
   Write-Host $body
