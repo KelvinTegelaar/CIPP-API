57e11a0d25d53e86f92bdc135a9a2dbacb2a8bb5
KelvinTegelaar <Kelvin@limenetworks.nl>
2023-10-18 14:11:14 +0200

    return data

--- Modules/CIPPCore/Public/Set-CIPPGDAPAutoExtend.ps1
+++ Modules/CIPPCore/Public/Set-CIPPGDAPAutoExtend.ps1
@@ -7,17 +7,20 @@ function Set-CIPPGDAPAutoExtend {
         $ExecutingUser
     )
 
-    if ($All -eq $true) {
+    $ReturnedData = if ($All -eq $true) {
         $Relationships = New-GraphGetRequest -uri "https://graph.microsoft.com/beta/tenantRelationships/delegatedAdminRelationships" -tenantid $env:tenantid -NoAuthCheck $true
         foreach ($Relation in $Relationships) {
             $GraphRequest = New-GraphPostRequest -uri "https://graph.microsoft.com/beta/tenantRelationships/delegatedAdminRelationships/$($Relation.id)" -tenantid $env:tenantid -type PATCH -body '{"autoExtendDuration":"P180D"}' -Verbose -NoAuthCheck $true
             write-LogMessage -user $ExecutingUser -API $APIName -message "Successfully set auto renew for $($Relation.id)" -Sev "Info"
-
+            "Successfully set auto renew for $($RelationShipid)" 
         }
     }
     else {
         $GraphRequest = New-GraphPostRequest -uri "https://graph.microsoft.com/beta/tenantRelationships/delegatedAdminRelationships/$($RelationShipid)" -tenantid $env:tenantid -type PATCH -body '{"autoExtendDuration":"P180D"}' -Verbose -NoAuthCheck $true
         write-LogMessage -user $ExecutingUser -API $APIName -message "Successfully set auto renew for $($RelationShipid)" -Sev "Info"
+        @("Successfully set auto renew for $($RelationShipid)" )
     }
+
+    return $ReturnedData
 
 }
