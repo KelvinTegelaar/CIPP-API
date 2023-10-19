0536e8d18cc09ac598b11046c44ad2268f3b6d63
KelvinTegelaar <Kelvin@limenetworks.nl>
2023-10-19 13:26:43 +0200

    fixes to autoextend

--- Modules/CIPPCore/Public/Set-CIPPGDAPAutoExtend.ps1
+++ Modules/CIPPCore/Public/Set-CIPPGDAPAutoExtend.ps1
@@ -8,13 +8,13 @@ function Set-CIPPGDAPAutoExtend {
     )
 
     $ReturnedData = if ($All -eq $true) {
-        $Relationships = New-GraphGetRequest -uri "https://graph.microsoft.com/beta/tenantRelationships/delegatedAdminRelationships" -tenantid $env:tenantid -NoAuthCheck $true
+        $Relationships = New-GraphGetRequest -uri "https://graph.microsoft.com/beta/tenantRelationships/delegatedAdminRelationships" -tenantid $env:tenantid -NoAuthCheck $true | Where-Object -Property autoExtendDuration -eq "PT0S"
         foreach ($Relation in $Relationships) {
             try {
                 $AddedHeader = @{"If-Match" = $Relation."@odata.etag" }
                 $GraphRequest = New-GraphPostRequest -uri "https://graph.microsoft.com/beta/tenantRelationships/delegatedAdminRelationships/$($Relation.id)" -tenantid $env:tenantid -type PATCH -body '{"autoExtendDuration":"P180D"}' -Verbose -NoAuthCheck $true -AddedHeaders $AddedHeader
-                write-LogMessage -user $ExecutingUser -API $APIName -message "Successfully set auto renew for $($Relation.id)" -Sev "Info"
-                "Successfully set auto renew for $($Relation.id)" 
+                write-LogMessage -user $ExecutingUser -API $APIName -message "Successfully set auto renew for tenant $($Relation.customer.displayName) with ID $($RelationShipid)" -Sev "Info"
+                @("Successfully set auto renew for tenant $($Relation.customer.displayName) with ID $($Relation.id)" )
     
             }
             catch {
@@ -27,9 +27,11 @@ function Set-CIPPGDAPAutoExtend {
     }
     else {
         try {
+            $Relationship = New-GraphGetRequest -uri "https://graph.microsoft.com/beta/tenantRelationships/delegatedAdminRelationships" -tenantid $env:tenantid -NoAuthCheck $true | Where-Object -Property id -EQ $RelationShipid
+
             $GraphRequest = New-GraphPostRequest -uri "https://graph.microsoft.com/beta/tenantRelationships/delegatedAdminRelationships/$($RelationShipid)" -tenantid $env:tenantid -type PATCH -body '{"autoExtendDuration":"P180D"}' -Verbose -NoAuthCheck $true
-            write-LogMessage -user $ExecutingUser -API $APIName -message "Successfully set auto renew for $($RelationShipid)" -Sev "Info"
-            @("Successfully set auto renew for $($RelationShipid)" )
+            write-LogMessage -user $ExecutingUser -API $APIName -message "Successfully set auto renew for tenant $($Relationship.customer.displayName) with ID $($RelationShipid)" -Sev "Info"
+            @("Successfully set auto renew for tenant $($Relationship.customer.displayName) with ID $($RelationShipid)" )
         }
         catch {
             $ErrorMessage = $_.Exception.Message
