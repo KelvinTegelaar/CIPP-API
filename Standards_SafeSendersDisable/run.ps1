5b82885f8b358d45059eed112a0c7e65782bd836
KelvinTegelaar <Kelvin@limenetworks.nl>
2023-10-26 20:41:41 +0200

    added safsender standard

---
+++ Standards_SafeSendersDisable/run.ps1
@@ -0,0 +1,17 @@
+param($tenant)
+
+try {
+    $Mailboxes = New-ExoRequest -tenantid $Tenant -cmdlet "Get-Mailbox" | ForEach-Object {
+        try {
+            $username = $_.UserPrincipalName
+            New-ExoRequest -tenantid $Tenant -cmdlet "Set-MailboxJunkEmailConfiguration" -cmdParams @{Identity = $_.GUID ; TrustedRecipientsAndDomains = $null } -anchor $username
+        }
+        catch {
+            Write-LogMessage  -API "Standards" -tenant $tenant -message "Could not disbale SafeSenders list for $($username): $($_.Exception.message)" -sev Warn
+        }
+    }   
+    Write-LogMessage  -API "Standards" -tenant $tenant -message "Safe Senders disabled" -sev Info
+}
+catch {
+    Write-LogMessage  -API "Standards" -tenant $tenant -message "Failed to disable SafeSenders. Error: $($_.exception.message)" -sev Error
+}
