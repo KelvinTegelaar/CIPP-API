9f90ad69d1313e8a4d0f53208b7ee59c311ce77e
KelvinTegelaar <Kelvin@limenetworks.nl>
2023-11-24 17:12:52 +0100

    typo

--- Modules/CippExtensions/NinjaOne/Invoke-NinjaOneDocumentTemplate.ps1
+++ Modules/CippExtensions/NinjaOne/Invoke-NinjaOneDocumentTemplate.ps1
@@ -13,23 +13,26 @@ function Invoke-NinjaOneDocumentTemplate {
     }
 
     if (!$ID) {
-        $DocumentTemplates = (Invoke-WebRequest -uri "https://$($Configuration.Instance)/api/v2/document-templates/" -Method GET -Headers @{Authorization = "Bearer $($token.access_token)" } -ContentType 'application/json').content | ConvertFrom-Json -depth 100
+        $DocumentTemplates = (Invoke-WebRequest -Uri "https://$($Configuration.Instance)/api/v2/document-templates/" -Method GET -Headers @{Authorization = "Bearer $($token.access_token)" } -ContentType 'application/json').content | ConvertFrom-Json -Depth 100
         $DocumentTemplate = $DocumentTemplates | Where-Object { $_.name -eq $Template.name }
-    } else {
-        $DocumentTemplate = (Invoke-WebRequest -uri "https://$($Configuration.Instance)/api/v2/document-templates/$($ID)" -Method GET -Headers @{Authorization = "Bearer $($token.access_token)" } -ContentType 'application/json').content | ConvertFrom-Json -depth 100
+    }
+    else {
+        $DocumentTemplate = (Invoke-WebRequest -Uri "https://$($Configuration.Instance)/api/v2/document-templates/$($ID)" -Method GET -Headers @{Authorization = "Bearer $($token.access_token)" } -ContentType 'application/json').content | ConvertFrom-Json -Depth 100
     }
     
     $MatchedCount = ($DocumentTemplate | Measure-Object).count
     if ($MatchedCount -eq 1) {
         # Matched a single document template
         $NinjaDocumentTemplate = $DocumentTemplate
-    } elseif ($MatchedCount -eq 0) {
+    }
+    elseif ($MatchedCount -eq 0) {
         # Create a new Document Template
-        $Body = $Template | ConvertTo-Json -depth 100
-        $NinjaDocumentTemplate = (Invoke-WebRequest -uri "https://$($Configuration.Instance)/api/v2/document-templates/" -Method POST -Headers @{Authorization = "Bearer $($token.access_token)" } -ContentType 'application/json'  -Body $Body).content | ConvertFrom-Json -depth 100
-    } else {
+        $Body = $Template | ConvertTo-Json -Depth 100
+        $NinjaDocumentTemplate = (Invoke-WebRequest -Uri "https://$($Configuration.Instance)/api/v2/document-templates/" -Method POST -Headers @{Authorization = "Bearer $($token.access_token)" } -ContentType 'application/json'  -Body $Body).content | ConvertFrom-Json -Depth 100
+    }
+    else {
         # Matched multiple templates. Should be impossible but lets check anyway :D
-        Throw "Multiiple Documents Matched the Provided Criteria"
+        Throw "Multiple Documents Matched the Provided Criteria"
     }
 
     return $NinjaDocumentTemplate
