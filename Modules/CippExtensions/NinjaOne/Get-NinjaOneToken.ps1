a212da32d8ae7e9cce4ec7b35d58eb3cc7392c2f
lwhitelock <79275328+lwhitelock@users.noreply.github.com>
2023-11-24 13:34:06 +0000

    Fixed scope

--- Modules/CippExtensions/NinjaOne/Get-NinjaOneToken.ps1
+++ Modules/CippExtensions/NinjaOne/Get-NinjaOneToken.ps1
@@ -3,13 +3,13 @@ function Get-NinjaOneToken {
     param (
         $Configuration 
     )
-
     $null = Connect-AzAccount -Identity
+
     $body = @{
         grant_type    = 'client_credentials'
         client_id     = $Configuration.ClientId
         client_secret = (Get-AzKeyVaultSecret -VaultName $ENV:WEBSITE_DEPLOYMENT_ID -Name "NinjaOne" -AsPlainText)
-        scope         = 'monioring management'
+        scope         = 'monitoring management'
     }
 
     $token = Invoke-RestMethod -Uri "https://$($Configuration.Instance -replace '/ws','')/ws/oauth/token" -Method Post -Body $body -ContentType 'application/x-www-form-urlencoded'
