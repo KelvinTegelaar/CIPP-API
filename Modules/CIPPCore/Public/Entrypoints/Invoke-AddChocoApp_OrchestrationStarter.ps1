5a0d44b1612dbf5426661157fd60c44f2e96bf08
KelvinTegelaar <Kelvin@limenetworks.nl>
2023-11-26 00:36:17 +0100

    fixes orch start

--- Modules/CIPPCore/Public/Entrypoints/Invoke-AddChocoApp_OrchestrationStarter.ps1
+++ Modules/CIPPCore/Public/Entrypoints/Invoke-AddChocoApp_OrchestrationStarter.ps1
@@ -1,6 +1,6 @@
-    using namespace System.Net
+using namespace System.Net
 
-    Function Invoke-AddChocoApp_OrchestrationStarter {
+Function Invoke-AddChocoApp_OrchestrationStarter {
     <#
     .FUNCTIONALITY
     Entrypoint
@@ -8,21 +8,19 @@
     [CmdletBinding()]
     param($Request, $TriggerMetadata)
 
-            Write-LogMessage  -API "ChocoApps" -message "Attempted to start upload but an instance was already running." -sev Info
-}
-else {
+    Write-LogMessage -API 'ChocoApps' -message 'Attempted to start upload but an instance was already running.' -sev Info
     $InstanceId = Start-NewOrchestration -FunctionName 'Applications_Orchestrator'
     Write-Host "Started orchestration with ID = '$InstanceId'"
     $Orchestrator = New-OrchestrationCheckStatusResponse -Request $Request -InstanceId $InstanceId
-    Write-LogMessage  -API "ChocoApps" -message "Started uploading applications to tenants" -sev Info
-    $Results = [pscustomobject]@{"Results" = "Started application queue" }
+    Write-LogMessage -API 'ChocoApps' -message 'Started uploading applications to tenants' -sev Info
+    $Results = [pscustomobject]@{'Results' = 'Started application queue' }
+
+    Write-Host ($Orchestrator | ConvertTo-Json)
+
+
+    Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+            StatusCode = [HttpStatusCode]::OK
+            Body       = $results
+        })
+
 }
-Write-Host ($Orchestrator | ConvertTo-Json)
-
-
-Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-        StatusCode = [HttpStatusCode]::OK
-        Body       = $results
-    })
-
-    }
