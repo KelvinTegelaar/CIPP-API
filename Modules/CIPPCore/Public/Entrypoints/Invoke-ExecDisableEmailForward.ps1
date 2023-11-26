bdf65d7d663f6f702dabf182305a7eff59731720
KelvinTegelaar <Kelvin@limenetworks.nl>
2023-11-26 20:57:31 +0100

    Fix issues with imports

--- Modules/CIPPCore/Public/Entrypoints/Invoke-ExecDisableEmailForward.ps1
+++ Modules/CIPPCore/Public/Entrypoints/Invoke-ExecDisableEmailForward.ps1
@@ -7,26 +7,26 @@ Function Invoke-ExecDisableEmailForward {
     #>
     [CmdletBinding()]
     param($Request, $TriggerMetadata)
+    try {
+        $APIName = $TriggerMetadata.FunctionName
+        Write-LogMessage -user $request.headers.'x-ms-client-principal' -API $APINAME -message 'Accessed this API' -Sev 'Debug'
+        $Username = $request.body.user
+        $Tenantfilter = $request.body.tenantfilter
+        $Results = try {
+            Set-CIPPForwarding -userid $Request.body.user -tenantFilter $TenantFilter -APIName $APINAME -ExecutingUser $request.headers.'x-ms-client-principal' -Forward $null -keepCopy $false -ForwardingSMTPAddress $null -Disable $true
+        } catch {
+            "Could not disable forwarding message for $($username). Error: $($_.Exception.Message)"
+        }
 
-    $APIName = $TriggerMetadata.FunctionName
-    Write-LogMessage -user $request.headers.'x-ms-client-principal' -API $APINAME -message 'Accessed this API' -Sev 'Debug'
-    $Username = $request.body.user
-    $Tenantfilter = $request.body.tenantfilter
-    $Results = try {
-        Set-CIPPForwarding -userid $Request.body.user -tenantFilter $TenantFilter -APIName $APINAME -ExecutingUser $request.headers.'x-ms-client-principal' -Forward $null -keepCopy $false -ForwardingSMTPAddress $null -Disable $true
+        $body = [pscustomobject]@{'Results' = @($results) }
     } catch {
-        "Could not disable forwarding message for $($username). Error: $($_.Exception.Message)"
+        $body = [pscustomobject]@{'Results' = @("Could not disable forwarding user: $($_.Exception.message)") }
     }
 
-    $body = [pscustomobject]@{'Results' = @($results) }
-} catch {
-    $body = [pscustomobject]@{'Results' = @("Could not disable forwarding user: $($_.Exception.message)") }
-}
-
-# Associate values to output bindings by calling 'Push-OutputBinding'.
-Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-        StatusCode = [HttpStatusCode]::OK
-        Body       = $Body
-    })
+    # Associate values to output bindings by calling 'Push-OutputBinding'.
+    Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+            StatusCode = [HttpStatusCode]::OK
+            Body       = $Body
+        })
 
 }
--- Modules/CIPPCore/Public/Entrypoints/Invoke-ExecSchedulerBillingRun.ps1
+++ Modules/CIPPCore/Public/Entrypoints/Invoke-ExecSchedulerBillingRun.ps1
@@ -7,22 +7,22 @@ Function Invoke-ExecSchedulerBillingRun {
     #>
     [CmdletBinding()]
     param($Request, $TriggerMetadata)
+    try {
+        Write-LogMessage -API 'Scheduler_Billing' -tenant 'none' -message 'Starting billing processing.' -sev Info
 
-    Write-LogMessage -API 'Scheduler_Billing' -tenant 'none' -message 'Starting billing processing.' -sev Info
-
-    $Table = Get-CIPPTable -TableName Extensionsconfig
-    $Configuration = (Get-CIPPAzDataTableEntity @Table).config | ConvertFrom-Json -Depth 10
-    foreach ($ConfigItem in $Configuration.psobject.properties.name) {
-        switch ($ConfigItem) {
-            'Gradient' {
-                If ($Configuration.Gradient.enabled -and $Configuration.Gradient.BillingEnabled) {
-                    New-GradientServiceSyncRun
+        $Table = Get-CIPPTable -TableName Extensionsconfig
+        $Configuration = (Get-CIPPAzDataTableEntity @Table).config | ConvertFrom-Json -Depth 10
+        foreach ($ConfigItem in $Configuration.psobject.properties.name) {
+            switch ($ConfigItem) {
+                'Gradient' {
+                    If ($Configuration.Gradient.enabled -and $Configuration.Gradient.BillingEnabled) {
+                        New-GradientServiceSyncRun
+                    }
                 }
             }
         }
+    } catch {
+        Write-LogMessage -API 'Scheduler_Billing' -tenant 'none' -message "Could not start billing processing $($_.Exception.Message)" -sev Error
     }
-} catch {
-    Write-LogMessage -API 'Scheduler_Billing' -tenant 'none' -message "Could not start billing processing $($_.Exception.Message)" -sev Error
-}
 
 }
