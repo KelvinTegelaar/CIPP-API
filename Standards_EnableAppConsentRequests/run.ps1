9814813c5f25130d1d0e4626499cbdf238ff2d3c
John Duprey <jwd@johnwduprey.com>
2023-11-28 09:14:50 -0500

    Fix app consent standard

--- Standards_EnableAppConsentRequests/run.ps1
+++ Standards_EnableAppConsentRequests/run.ps1
@@ -3,30 +3,50 @@ param($tenant)
 try {
     # Get current state
     $CurrentInfo = New-GraphGetRequest -uri 'https://graph.microsoft.com/beta/policies/adminConsentRequestPolicy' -tenantid $Tenant
-    
+
     # Change state to enabled with default settings
     $CurrentInfo.isEnabled = 'true'
     $CurrentInfo.notifyReviewers = 'true'
     $CurrentInfo.remindersEnabled = 'true'
     $CurrentInfo.requestDurationInDays = 30
 
-    # Get Global Admin role ID TODO: change to be able to chose role
-    $Role = New-GraphGetRequest -uri "https://graph.microsoft.com/beta/roleManagement/directory/roleDefinitions?`$filter=(displayName eq 'Global Administrator')&`$select=displayName,id" -tenantid $Tenant
-    $RoleReviewers = @(@{
-            query     = "/beta/roleManagement/directory/roleAssignments?`$filter=roleDefinitionId eq `'$($Role.id)`'"
+    # Currently GA role - TODO: Add role selection from standards
+    $RolesToAdd = @('62e90394-69f5-4237-9190-012177145e10')
+
+    $NewReviewers = foreach ($Role in $RolesToAdd) {
+        @{
+            query     = "/beta/roleManagement/directory/roleAssignments?`$filter=roleDefinitionId eq '$Role'"
             queryType = 'MicrosoftGraph'
             queryRoot = 'null'
-        })
-    # Set reviewers to Global Admins if not already set, this avoids overwriting existing reviewers and duplication of reviewers objects
-    $CurrentInfo.reviewers = if ($CurrentInfo.reviewers.query -notlike "*$($Role.id)*") {
-        $RoleReviewers
+        }
     }
-    $body = (ConvertTo-Json -Depth 10 -InputObject $CurrentInfo)
-    (New-GraphPostRequest -tenantid $tenant -Uri 'https://graph.microsoft.com/beta/policies/adminConsentRequestPolicy' -Type put -Body $body -ContentType 'application/json')
 
+    # Add existing reviewers
+    $Reviewers = [System.Collections.Generic.List[object]]::new()
+    foreach ($Reviewer in $CurrentInfo.reviewers) {
+        $RoleFound = $false
+        foreach ($Role in $RolesToAdd) {
+            if ($Reviewer.query -match $RolesToAdd) {
+                $RoleFound = $true
+            }
+        }
+        if (!$RoleFound) {
+            $Reviewers.add($Reviewer)
+        }
+    }
 
+    # Add new reviewer roles
+    foreach ($NewReviewer in $NewReviewers) {
+        $Reviewers.add($NewReviewer)
+    }
+
+    # Update reviewer list
+    $CurrentInfo.reviewers = @($Reviewers)
+    $body = (ConvertTo-Json -Compress -Depth 10 -InputObject $CurrentInfo)
+
+    New-GraphPostRequest -tenantid $tenant -Uri 'https://graph.microsoft.com/beta/policies/adminConsentRequestPolicy' -Type put -Body $body -ContentType 'application/json'
     Write-LogMessage -API 'Standards' -tenant $tenant -message 'Enabled App consent admin requests' -sev Info
-    
+
 } catch {
     Write-LogMessage -API 'Standards' -tenant $tenant -message "Failed to enable App consent admin requests. Error: $($_.exception.message)" -sev Error
 }
