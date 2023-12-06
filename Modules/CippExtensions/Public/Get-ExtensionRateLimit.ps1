c6af60a8d83cbd3ac0ab7a78826da7e90b7308d8
lwhitelock <79275328+lwhitelock@users.noreply.github.com>
2023-12-06 08:42:56 +0000

    More rate limit fixes

--- Modules/CippExtensions/Public/Get-ExtensionRateLimit.ps1
+++ Modules/CippExtensions/Public/Get-ExtensionRateLimit.ps1
@@ -5,13 +5,13 @@ function Get-ExtensionRateLimit($ExtensionName, $ExtensionPartitionKey, $RateLim
 
     # Check Global Rate Limiting
     try {
-    $ActiveJobs = $CurrentMap | Where-Object { ($Null -ne $_.lastStartTime -or $_.lastStartTime -gt (Get-Date).AddMinutes(-10)) -and ($Null -eq $_.lastEndTime -or $_.lastStartTime -gt $_.lastEndTime) }
+    $ActiveJobs = $CurrentMap | Where-Object { ($Null -ne $_.lastStartTime) -and ($_.lastStartTime -gt (Get-Date).AddMinutes(-10)) -and ($Null -eq $_.lastEndTime -or $_.lastStartTime -gt $_.lastEndTime) }
     } catch {
         $ActiveJobs = 'FirstRun'
     }
     if (($ActiveJobs | Measure-Object).count -ge $RateLimit) {
-        Write-LogMessage -API 'ExtensionRateLimiting' -user 'CIPP' -message "$ExtensionName Rate Limited" -Sev 'Info'
-        Write-Host "Rate Limiting"
+        Write-LogMessage -API 'ExtensionRateLimiting' -user 'CIPP' -message "$ExtensionName Rate Limited $($ActiveJobs.count) active jobs" -Sev 'Info'
+        Write-Host "Rate Limiting. Currently $($ActiveJobs.count) Active Jobs"
         Start-Sleep -Seconds $WaitTime
         $CurrentMap = Get-ExtensionRateLimit -ExtensionName $ExtensionName -ExtensionPartitionKey $ExtensionPartitionKey -RateLimit $RateLimit -WaitTime $WaitTime
     }
