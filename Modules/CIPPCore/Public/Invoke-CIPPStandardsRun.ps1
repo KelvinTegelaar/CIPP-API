8e43edf09ca6402a21f9739aaeda4e80c09f2122
KelvinTegelaar <Kelvin@limenetworks.nl>
2023-12-11 15:51:18 +0100

    Hotfix

--- Modules/CIPPCore/Public/Invoke-CIPPStandardsRun.ps1
+++ Modules/CIPPCore/Public/Invoke-CIPPStandardsRun.ps1
@@ -16,7 +16,7 @@ function Invoke-CIPPStandardsRun {
     $Tenants = (Get-CIPPAzDataTableEntity @Table -Filter $Filter).JSON | ConvertFrom-Json
 
     #Migrate from old standards to new standards.
-    $Tenants | Where-Object -Property 'v2' -NE $true | ForEach-Object {
+    $Tenants | Where-Object -Property 'v2.1' -NE $true | ForEach-Object {
         $OldStd = $_
         $OldStd.standards.psobject.properties.name | ForEach-Object {
             if ($_ -eq 'MailContacts') {
@@ -28,10 +28,15 @@ function Invoke-CIPPStandardsRun {
                     remediate        = $true
                 }
             } else {
-                $OldStd.Standards.$_ | Add-Member -NotePropertyName 'remediate' -NotePropertyValue $true 
+                if ($OldStd.Standards.$_ -eq $true) { 
+                    $OldStd.Standards.$_ = @{ remediate = $true } 
+                } else { 
+                    $OldStd.Standards.$_ | Add-Member -NotePropertyName 'remediate' -NotePropertyValue $true -Force 
+                }
+                
             }
         }
-        $OldStd | Add-Member -NotePropertyName 'v2' -NotePropertyValue $true -PassThru -Force
+        $OldStd | Add-Member -NotePropertyName 'v2.1' -NotePropertyValue $true -PassThru -Force
         $Entity = @{ 
             PartitionKey = 'standards'
             RowKey       = "$($OldStd.Tenant)"
