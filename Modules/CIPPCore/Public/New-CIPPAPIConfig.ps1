5d8eef271753de28d2a9be837c0b2cfede229e33
Stefano <crema.stefano@enneuno.it>
2023-12-18 15:30:37 +0100

    Update New-CIPPAPIConfig.ps1

--- Modules/CIPPCore/Public/New-CIPPAPIConfig.ps1
+++ Modules/CIPPCore/Public/New-CIPPAPIConfig.ps1
@@ -61,9 +61,9 @@ function New-CIPPAPIConfig {
 
     }
     catch {
-        Write-LogMessage -user $ExecutingUser -API $APINAME -tenant 'None' -message "Failed to setup CIPP-API Access: $($_.Exception.Message) Linenumber: $($_.InvocationInfo.ScriptLineNumber)" -Sev "Error"
+        Write-LogMessage -user $ExecutingUser -API $APINAME -tenant 'None' -message "TEST Failed to setup CIPP-API Access: $($_.Exception.Message) Linenumber: $($_.InvocationInfo.ScriptLineNumber)" -Sev "Error"
         return @{
-            Results = " but could not set API configuration MIO: $($_.Exception.Message)"
+            Results = " but could not set API configuration: $($_.Exception.Message)"
         }
 
     }
