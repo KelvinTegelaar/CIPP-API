cc0595f100c19def86f85396ce3dc8a3fd237d51
Kristian Kjærgård <kris6673@hotmail.com>
2023-12-21 23:28:18 +0100

    Less parallel and fix try catch placement

--- Modules/CIPPCore/Public/Standards/Invoke-CIPPStandardcalDefault.ps1
+++ Modules/CIPPCore/Public/Standards/Invoke-CIPPStandardcalDefault.ps1
@@ -7,22 +7,25 @@ function Invoke-CIPPStandardcalDefault {
     If ($Settings.remediate) {
 
         $Mailboxes = New-ExoRequest -tenantid $Tenant -cmdlet 'Get-Mailbox'
-        try {
-            # BRRRRRRRRRR
-            $Mailboxes | ForEach-Object -ThrottleLimit 25 -Parallel {
-                Import-Module CIPPcore
-                $Tenant = $Using:Tenant
-                $Settings = $Using:Settings
-                $Mailbox = $_
-                
-                New-ExoRequest -tenantid $Tenant -cmdlet 'Get-MailboxFolderStatistics' -cmdParams @{identity = $Mailbox.UserPrincipalName; FolderScope = 'Calendar' } -Anchor $Mailbox.UserPrincipalName | Where-Object { $_.FolderType -eq 'Calendar' } | ForEach-Object {
-                    New-ExoRequest -tenantid $Tenant -cmdlet 'Set-MailboxFolderPermission' -cmdparams @{Identity = "$($Mailbox.UserPrincipalName):$($_.FolderId)"; User = 'Default'; AccessRights = $Settings.permissionlevel } -Anchor $Mailbox.UserPrincipalName 
-                    Write-LogMessage -API 'Standards' -tenant $tenant -message "Set default folder permission for $($Mailbox.UserPrincipalName):\$($_.Name) to $($Settings.permissionlevel)" -sev Info }
-            }
-        } catch {
-            Write-LogMessage -API 'Standards' -tenant $tenant -message "Could not set default calendar permissions for $($Mailbox.UserPrincipalName). Error: $($_.exception.message)" -sev Error
-        }        
-        Write-LogMessage -API 'Standards' -tenant $tenant -message 'Done setting default calendar permissions.' -sev Info
-    }
 
-}
+        # BRRRRRRRRRR
+        $Mailboxes | ForEach-Object -ThrottleLimit 15 -Parallel {
+            Import-Module CIPPcore
+            $Tenant = $Using:Tenant
+            $Settings = $Using:Settings
+            $Mailbox = $_
+            try {
+                New-ExoRequest -tenantid $Tenant -cmdlet 'Get-MailboxFolderStatistics' -cmdParams @{identity = $Mailbox.UserPrincipalName; FolderScope = 'Calendar' } -Anchor $Mailbox.UserPrincipalName | 
+                    # Set permissions for each calendar found
+                    Where-Object { $_.FolderType -eq 'Calendar' } | ForEach-Object {
+                        New-ExoRequest -tenantid $Tenant -cmdlet 'Set-MailboxFolderPermission' -cmdparams @{Identity = "$($Mailbox.UserPrincipalName):$($_.FolderId)"; User = 'Default'; AccessRights = $Settings.permissionlevel } -Anchor $Mailbox.UserPrincipalName 
+                        Write-LogMessage -API 'Standards' -tenant $tenant -message "Set default folder permission for $($Mailbox.UserPrincipalName):\$($_.Name) to $($Settings.permissionlevel)" -sev Info 
+                    }
+                } catch {
+                    Write-LogMessage -API 'Standards' -tenant $tenant -message "Could not set default calendar permissions for $($Mailbox.UserPrincipalName). Error: $($_.exception.message)" -sev Error
+                }        
+            }
+            Write-LogMessage -API 'Standards' -tenant $tenant -message 'Done setting default calendar permissions.' -sev Info
+
+        }
+    }
