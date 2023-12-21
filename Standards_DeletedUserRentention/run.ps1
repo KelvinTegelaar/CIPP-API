34dd17359f2b6bcf319457d7d40fe9bcbc11f10c
smood922 <48338396+smood922@users.noreply.github.com>
2023-12-21 09:48:18 -0800

    Revert "Update run.ps1"
    This reverts commit 413907ac64a1e8e29ad2606fd484e52f7c829875.

--- Standards_DeletedUserRentention/run.ps1
+++ Standards_DeletedUserRentention/run.ps1
@@ -1,11 +1,11 @@
 ﻿param($tenant)
 
 try {
-    $body = '{"deletedUserPersonalSiteRetentionPeriodInDays": 3650}'
+    $body = '{"deletedUserPersonalSiteRetentionPeriodInDays": 365}'
     New-GraphPostRequest -tenantid $tenant -Uri "https://graph.microsoft.com/beta/admin/sharepoint/settings" -AsApp $true -Type PATCH -Body $body -ContentType "application/json"
 
-    Write-LogMessage -API "Standards" -tenant $tenant -message  "Set deleted user rentention of OneDrive to 10 years" -sev Info
+    Write-LogMessage -API "Standards" -tenant $tenant -message  "Set deleted user rentention of OneDrive to 1 year" -sev Info
 }
 catch {
-    Write-LogMessage -API "Standards" -tenant $tenant -message  "Failed to set deleted user rentention of OneDrive to 10 years: $($_.exception.message)" -sev Error
+    Write-LogMessage -API "Standards" -tenant $tenant -message  "Failed to set deleted user rentention of OneDrive to 1 year: $($_.exception.message)" -sev Error
 }
