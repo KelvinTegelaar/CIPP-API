e3476a83a9bdba52f991609bf383a4c220090060
Kristian Kjærgård <kris6673@hotmail.com>
2023-12-22 01:48:46 +0100

    Add alternate bool success variable

--- Modules/CIPPCore/Public/Standards/Invoke-CIPPStandardcalDefault.ps1
+++ Modules/CIPPCore/Public/Standards/Invoke-CIPPStandardcalDefault.ps1
@@ -5,10 +5,11 @@ function Invoke-CIPPStandardcalDefault {
     #>
     param($Tenant, $Settings)
     If ($Settings.remediate) {
-
         $Mailboxes = New-ExoRequest -tenantid $Tenant -cmdlet 'Get-Mailbox'
+        Write-LogMessage -API 'Standards' -tenant $Tenant -message "Started setting default calendar permissions for $($Mailboxes.Count) mailboxes." -sev Info
 
         # BRRRRRRRRRR
+        $global:UserSuccesses = 0
         $Mailboxes | ForEach-Object -ThrottleLimit 25 -Parallel {
             Import-Module CIPPcore
             $Tenant = $Using:Tenant
@@ -27,28 +28,29 @@ function Invoke-CIPPStandardcalDefault {
                                 do {
                                     try {
                                         New-ExoRequest -tenantid $Tenant -cmdlet 'Set-MailboxFolderPermission' -cmdparams @{Identity = "$($Mailbox.UserPrincipalName):$($_.FolderId)"; User = 'Default'; AccessRights = $Settings.permissionlevel } -Anchor $Mailbox.UserPrincipalName 
-                                        Write-LogMessage -API 'Standards' -tenant $tenant -message "Set default folder permission for $($Mailbox.UserPrincipalName):\$($_.Name) to $($Settings.permissionlevel)" -sev Info 
-                                        $SetRetryCount = 3
+                                        Write-LogMessage -API 'Standards' -tenant $Tenant -message "Set default folder permission for $($Mailbox.UserPrincipalName):\$($_.Name) to $($Settings.permissionlevel)" -sev Debug 
+                                        $Success = $true
                                     } catch {
                                         # Set part
                                         Start-Sleep -Milliseconds 250
                                         $SetRetryCount++
                                     }
-                                } Until ($SetRetryCount -ge 3 )
+                                } Until ($SetRetryCount -ge 3 -or $Success -eq $true)
                             }
-                            $GetRetryCount = 3
+                            $Success = $true
+                            $global:UserSuccesses++
                         } catch {
                             # Get part
                             Start-Sleep -Milliseconds 250
                             $GetRetryCount++
                         }
 
-                    } until ($GetRetryCount -ge 3)
+                    } until ($GetRetryCount -ge 3 -or $Success -eq $true)
                 } catch {
-                    Write-LogMessage -API 'Standards' -tenant $tenant -message "Could not set default calendar permissions for $($Mailbox.UserPrincipalName). Error: $($_.exception.message)" -sev Error
+                    Write-LogMessage -API 'Standards' -tenant $Tenant -message "Could not set default calendar permissions for $($Mailbox.UserPrincipalName). Error: $($_.exception.message)" -sev Error
                 }        
             }
-            Write-LogMessage -API 'Standards' -tenant $tenant -message 'Done setting default calendar permissions.' -sev Info
+            Write-LogMessage -API 'Standards' -tenant $Tenant -message "Done setting default calendar permissions for $global:UserSuccesses out of $($Mailboxes.Count) mailboxes." -sev Info
 
         }
     }
