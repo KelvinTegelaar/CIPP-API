c641e6d6198f12da468fc46de285d6a492c57319
KelvinTegelaar <Kelvin@limenetworks.nl>
2024-01-02 18:47:48 +0100

    one more edit pre release

--- Modules/CIPPCore/Public/New-CIPPAlertTemplate.ps1
+++ Modules/CIPPCore/Public/New-CIPPAlertTemplate.ps1
@@ -224,7 +224,7 @@ function New-CIPPAlertTemplate {
 
         default {
             $Title = 'A custom alert has occured'
-            $Table = ($data | ConvertTo-Html -Fragment | Out-String).Replace('<table>', ' <table class="table-modern">')
+            $Table = ($data | ConvertTo-Html -Fragment -As List | Out-String).Replace('<table>', ' <table class="table-modern">')
             $IntroText = "<p>You have setup CIPP to send you a custom alert for the event $($Data.operation)</p>$Table"
             if ($ActionResults) { $IntroText = $IntroText + "<p>Based on the rule, the following actions have been taken: $($ActionResults -join '<br/>' )</p>" }
             if ($LocationInfo) {
