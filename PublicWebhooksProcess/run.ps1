84347fc465279ddad1b67f31f2e04223155ec9c0
John Duprey <jwd@johnwduprey.com>
2024-01-10 12:36:17 -0500

    Update run.ps1

--- PublicWebhooksProcess/run.ps1
+++ PublicWebhooksProcess/run.ps1
@@ -27,12 +27,16 @@ if ($Request.query.CIPPID -in $Webhooks.RowKey) {
             $ReceivedItem = [pscustomobject]$ReceivedItem
             $TenantFilter = (Get-Tenants | Where-Object -Property customerId -EQ $ReceivedItem.TenantId).defaultDomainName
             Write-Host "TenantFilter: $TenantFilter"
-            $Data = New-GraphPostRequest -type GET -uri "https://manage.office.com/api/v1.0/$($ReceivedItem.tenantId)/activity/feed/audit/$($ReceivedItem.contentid)" -tenantid $TenantFilter -scope 'https://manage.office.com/.default'
-            Write-Host "Data to process found: $(($ReceivedItem.operation).count) items"
-            Write-Host "Operations to process for this client: $($Webhookinfo.Operations)"
-            foreach ($Item in $Data) {
-                Write-Host "Processing $($item.operation)"
-                Invoke-CippWebhookProcessing -TenantFilter $TenantFilter -Data $Item -CIPPPURL $url
+            try {
+                $Data = New-GraphPostRequest -type GET -uri "https://manage.office.com/api/v1.0/$($ReceivedItem.tenantId)/activity/feed/audit/$($ReceivedItem.contentid)" -tenantid $TenantFilter -scope 'https://manage.office.com/.default'
+                Write-Host "Data to process found: $(($ReceivedItem.operation).count) items"
+                Write-Host "Operations to process for this client: $($Webhookinfo.Operations)"
+                foreach ($Item in $Data) {
+                    Write-Host "Processing $($item.operation)"
+                    Invoke-CippWebhookProcessing -TenantFilter $TenantFilter -Data $Item -CIPPPURL $url
+                }
+            } catch {
+                Write-Host "Exception getting webhook data $($_.Exception.Message)"
             }
         }
     }
