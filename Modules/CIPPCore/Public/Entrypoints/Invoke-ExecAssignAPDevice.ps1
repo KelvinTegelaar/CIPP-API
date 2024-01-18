cc6a08cf6fc7c63aa57c58b2c6002e9226e06b0f
KelvinTegelaar <Kelvin@limenetworks.nl>
2024-01-18 14:58:49 +0100

    minor updates interface

--- Modules/CIPPCore/Public/Entrypoints/Invoke-ExecAssignAPDevice.ps1
+++ Modules/CIPPCore/Public/Entrypoints/Invoke-ExecAssignAPDevice.ps1
@@ -9,7 +9,7 @@ Function Invoke-ExecAssignAPDevice {
     param($Request, $TriggerMetadata)
     $APIName = $TriggerMetadata.FunctionName
     Write-LogMessage -user $request.headers.'x-ms-client-principal' -API $APINAME -message 'Accessed this API' -Sev 'Debug'
-    $tenantfilter = $Request.Query.TenantFilter
+    $tenantfilter = $Request.Body.TenantFilter
     try {
         $body = @{
             UserPrincipalName   = $Request.body.UserPrincipalName
@@ -18,7 +18,7 @@ Function Invoke-ExecAssignAPDevice {
         New-GraphPOSTRequest -uri "https://graph.microsoft.com/beta/deviceManagement/windowsAutopilotDeviceIdentities/$($request.body.Device)/UpdateDeviceProperties" -tenantid $TenantFilter -body $body -method POST 
         $Results = "Successfully assigned device to $($Request.body.UserPrincipalName) for $($tenantfilter)"
     } catch {
-        $Results = "Could not $($Request.body.UserPrincipalName) to $($Request.query.device) for $($tenantfilter) Error: $($_.Exception.Message)"
+        $Results = "Could not $($Request.body.UserPrincipalName) to $($Request.body.device) for $($tenantfilter) Error: $($_.Exception.Message)"
     }
 
     $Results = [pscustomobject]@{'Results' = "$results" }
