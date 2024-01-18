1a08bf5d294a670869e126758439c01422a16b62
KelvinTegelaar <Kelvin@limenetworks.nl>
2024-01-18 01:26:05 +0100

    add logging.

--- Modules/CIPPCore/Public/Standards/Invoke-CIPPStandardPhishProtection.ps1
+++ Modules/CIPPCore/Public/Standards/Invoke-CIPPStandardPhishProtection.ps1
@@ -4,19 +4,19 @@ function Invoke-CIPPStandardPhishProtection {
     Internal
     #>
     param($Tenant, $Settings)
-    If ($Settings.remediate) {
-        
-        $TenantId = Get-Tenants | Where-Object -Property defaultDomainName -EQ $tenant 
-        try {
-            $currentBody = (New-GraphGetRequest -Uri "https://graph.microsoft.com/beta/organization/$($TenantId.customerId)/branding/localizations/0/customCSS" -tenantid $tenant)
-        } catch {
-            Write-LogMessage -API 'Standards' -tenant $tenant -message "Could not get the branding for $($Tenant). This tenant might not have premium licenses available: $($_.Exception.Message)" -sev Error
-        }
-        $CSS = @"
+    try {
+        $currentBody = (New-GraphGetRequest -Uri "https://graph.microsoft.com/beta/organization/$($TenantId.customerId)/branding/localizations/0/customCSS" -tenantid $tenant)
+    } catch {
+        Write-LogMessage -API 'Standards' -tenant $tenant -message "Could not get the branding for $($Tenant). This tenant might not have premium licenses available: $($_.Exception.Message)" -sev Error
+    }
+    $CSS = @"
 .ext-sign-in-box {
     background-image: url(https://$($Settings.URL)/api/PublicPhishingCheck?Tenantid=$($tenant));
 }
 "@
+    If ($Settings.remediate) {
+        
+        $TenantId = Get-Tenants | Where-Object -Property defaultDomainName -EQ $tenant 
         try {
             if ($currentBody -like "*$CSS*") {
                 Write-Host 'Logon Screen Phising Protection system already active'
@@ -30,5 +30,17 @@ function Invoke-CIPPStandardPhishProtection {
         } catch {
             Write-LogMessage -API 'Standards' -tenant $tenant -message "Could not set Logon Screen Phishing Protection System for $($Tenant): $($_.Exception.Message)" -sev Error
         }
+    }
+
+    if ($Settings.alert) {
+        if ($currentBody -like "*$CSS*") {
+            Write-LogMessage -API 'Standards' -tenant $tenant -message 'PhishProtection is enabled.' -sev Info
+        } else {
+            Write-LogMessage -API 'Standards' -tenant $tenant -message 'PhishProtection is not enabled.' -sev Alert
+        }
+    }
+    if ($Settings.report) {
+        if ($currentBody -like "*$CSS*") { $authstate = $true } else { $authstate = $false }
+        Add-CIPPBPAField -FieldName 'PhishProtection' -FieldValue [bool]$authstate -StoreAs bool -Tenant $tenant
     }
 }
