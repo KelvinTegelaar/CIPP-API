b5bde28ee00a71871b84ed8158be36fdd6b2e775
KelvinTegelaar <Kelvin@limenetworks.nl>
2024-02-05 11:15:00 +0100

    add setting of onedrive permission

---
+++ Modules/CIPPCore/Public/Entrypoints/Invoke-ExecOneDrivePermission.ps1
@@ -0,0 +1,26 @@
+using namespace System.Net
+
+Function Invoke-ExecOneDrivePermission {
+      <#
+    .FUNCTIONALITY
+    Entrypoint
+    #>
+      [CmdletBinding()]
+      param($Request, $TriggerMetadata)
+
+      $APIName = $TriggerMetadata.FunctionName
+      $tenantFilter = $Request.Body.TenantFilter
+      try {
+            $State = Set-CIPPOnedriveAccess -tenantFilter $tenantFilter -userid $request.body.UPN -OnedriveAccessUser $request.body.input -ExecutingUser $ExecutingUser -APIName $APIName
+            $Results = [pscustomobject]@{'Results' = "$State" }
+      } catch {
+            $Results = [pscustomobject]@{'Results' = "Failed. $($_.Exception.Message)" }
+      }
+
+      # Associate values to output bindings by calling 'Push-OutputBinding'.
+      Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+                  StatusCode = [HttpStatusCode]::OK
+                  Body       = $Results
+            })
+
+}
