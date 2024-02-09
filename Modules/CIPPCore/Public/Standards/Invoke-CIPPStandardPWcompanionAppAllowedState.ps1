ee1bda04ff4402f6c17a5b3f3fb0c486e75fcd1a
Kristian Kjærgård <kris6673@hotmail.com>
2024-02-09 01:18:46 +0100

    More logging changes and a few bug fixes

--- Modules/CIPPCore/Public/Standards/Invoke-CIPPStandardPWcompanionAppAllowedState.ps1
+++ Modules/CIPPCore/Public/Standards/Invoke-CIPPStandardPWcompanionAppAllowedState.ps1
@@ -5,42 +5,48 @@ function Invoke-CIPPStandardPWcompanionAppAllowedState {
     #>
     param($Tenant, $Settings)
     $authenticatorFeaturesState = (New-GraphGetRequest -tenantid $tenant -Uri 'https://graph.microsoft.com/beta/policies/authenticationMethodsPolicy/authenticationMethodConfigurations/microsoftAuthenticator' -Type GET)
+    $authstate = if ($authenticatorFeaturesState.featureSettings.companionAppAllowedState.state -eq 'enabled') { $true } else { $false }
+    
     If ($Settings.remediate) {
-        $Setting = $Settings
-        try {
-            # Get current state of microsoftAuthenticator policy
-            # Remove number matching from featureSettings because this is now Microsoft enforced and shipping it returns an error
-            $authenticatorFeaturesState.featureSettings.PSObject.Properties.Remove('numberMatchingRequiredState')
-            # Define feature body
-            $featureBody = @{
-                state         = $Setting.state
-                includeTarget = [PSCustomObject]@{
-                    targetType = 'group'
-                    id         = 'all_users'
+
+        if ($authenticatorFeaturesState.featureSettings.companionAppAllowedState.state -eq $Settings.state) {
+            Write-LogMessage -API 'Standards' -tenant $tenant -message "companionAppAllowedState is already set to the desired state of $($Settings.state)." -sev Info
+        } else {
+            try {
+                # Remove number matching from featureSettings because this is now Microsoft enforced and shipping it returns an error
+                $authenticatorFeaturesState.featureSettings.PSObject.Properties.Remove('numberMatchingRequiredState')
+                # Define feature body
+                $featureBody = @{
+                    state         = $Settings.state
+                    includeTarget = [PSCustomObject]@{
+                        targetType = 'group'
+                        id         = 'all_users'
+                    }
+                    excludeTarget = [PSCustomObject]@{
+                        targetType = 'group'
+                        id         = '00000000-0000-0000-0000-000000000000'
+                    }
                 }
-                excludeTarget = [PSCustomObject]@{
-                    targetType = 'group'
-                    id         = '00000000-0000-0000-0000-000000000000'
-                }
+                $authenticatorFeaturesState.featureSettings.companionAppAllowedState = $featureBody
+                $body = ConvertTo-Json -Depth 3 -Compress -InputObject $authenticatorFeaturesState
+                (New-GraphPostRequest -tenantid $tenant -Uri 'https://graph.microsoft.com/beta/policies/authenticationMethodsPolicy/authenticationMethodConfigurations/microsoftAuthenticator' -Type patch -Body $body -ContentType 'application/json')
+                Write-LogMessage -API 'Standards' -tenant $tenant -message "Set companionAppAllowedState to $($Settings.state)." -sev Info
+            } catch {
+                Write-LogMessage -API 'Standards' -tenant $tenant -message "Failed to set companionAppAllowedState to $($Settings.state). Error: $($_.exception.message)" -sev Error
             }
-            $authenticatorFeaturesState.featureSettings.companionAppAllowedState = $featureBody
-            $body = $authenticatorFeaturesState | ConvertTo-Json -Depth 3
-            (New-GraphPostRequest -tenantid $tenant -Uri 'https://graph.microsoft.com/beta/policies/authenticationMethodsPolicy/authenticationMethodConfigurations/microsoftAuthenticator' -Type patch -Body $body -ContentType 'application/json')
-            Write-LogMessage -API 'Standards' -tenant $tenant -message 'Enabled companionAppAllowedState.' -sev Info
-        } catch {
-            Write-LogMessage -API 'Standards' -tenant $tenant -message "Failed to enable companionAppAllowedState. Error: $($_.exception.message)" -sev Error
         }
     }
+
     if ($Settings.alert) {
 
-        if ($authenticatorFeaturesState.featureSettings.companionAppAllowedState.state -eq 'enabled') {
+        if ($authstate) {
             Write-LogMessage -API 'Standards' -tenant $tenant -message 'companionAppAllowedState is enabled.' -sev Info
         } else {
             Write-LogMessage -API 'Standards' -tenant $tenant -message 'companionAppAllowedState is not enabled.' -sev Alert
         }
     }
+
     if ($Settings.report) {
-        if ($authenticatorFeaturesState.featureSettings.companionAppAllowedState.state -eq 'enabled') { $authstate = $true } else { $authstate = $false }
         Add-CIPPBPAField -FieldName 'companionAppAllowedState' -FieldValue [bool]$authstate -StoreAs bool -Tenant $tenant
     }
 }
--- Modules/CIPPCore/Public/Standards/Invoke-CIPPStandardallowOTPTokens.ps1
+++ Modules/CIPPCore/Public/Standards/Invoke-CIPPStandardallowOTPTokens.ps1
@@ -4,25 +4,26 @@ function Invoke-CIPPStandardallowOTPTokens {
     Internal
     #>
     param($Tenant, $Settings)
+    $CurrentInfo = New-GraphGetRequest -uri 'https://graph.microsoft.com/beta/policies/authenticationMethodsPolicy/authenticationMethodConfigurations/microsoftAuthenticator' -tenantid $Tenant
     
     If ($Settings.remediate) {
-        Set-CIPPAuthenticationPolicy -Tenant $tenant -APIName 'Standards' -AuthenticationMethodId 'MicrosoftAuthenticator' -Enabled $true -MicrosoftAuthenticatorSoftwareOathEnabled $true
-    }
-
-    # This is ugly but done to avoid a second call to the Graph API
-    if ($Settings.alert -or $Settings.report) {
-        $CurrentInfo = new-graphgetRequest -uri 'https://graph.microsoft.com/beta/policies/authenticationMethodsPolicy/authenticationMethodConfigurations/microsoftAuthenticator' -tenantid $Tenant
-        if ($Settings.alert) {
-        
-            if ($CurrentInfo.isSoftwareOathEnabled) {
-                Write-LogMessage -API 'Standards' -tenant $tenant -message 'MS authenticator OTP/oAuth tokens is enabled' -sev Info
-            } else {
-                Write-LogMessage -API 'Standards' -tenant $tenant -message 'MS authenticator OTP/oAuth tokens is not enabled' -sev Alert
-            }
-        }
-
-        if ($Settings.report) {
-            Add-CIPPBPAField -FieldName 'MSAuthenticator' -FieldValue [bool]$CurrentInfo.isSoftwareOathEnabled -StoreAs bool -Tenant $tenant
+        if ($CurrentInfo.isSoftwareOathEnabled) {
+            Write-LogMessage -API 'Standards' -tenant $tenant -message 'MS authenticator OTP/oAuth tokens is already enabled.' -sev Info
+        } else {
+            Set-CIPPAuthenticationPolicy -Tenant $tenant -APIName 'Standards' -AuthenticationMethodId 'MicrosoftAuthenticator' -Enabled $true -MicrosoftAuthenticatorSoftwareOathEnabled $true
         }
     }
+
+    if ($Settings.alert) {
+        if ($CurrentInfo.isSoftwareOathEnabled) {
+            Write-LogMessage -API 'Standards' -tenant $tenant -message 'MS authenticator OTP/oAuth tokens is enabled' -sev Info
+        } else {
+            Write-LogMessage -API 'Standards' -tenant $tenant -message 'MS authenticator OTP/oAuth tokens is not enabled' -sev Alert
+        }
+    }
+
+    if ($Settings.report) {
+        Add-CIPPBPAField -FieldName 'MSAuthenticator' -FieldValue [bool]$CurrentInfo.isSoftwareOathEnabled -StoreAs bool -Tenant $tenant
+    }
+    
 }
