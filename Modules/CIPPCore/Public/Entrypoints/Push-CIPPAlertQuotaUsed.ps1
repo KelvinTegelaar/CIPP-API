aadb9b79e5d6a4a80e6c1456737af0b938dc8671
BNWEIN <bweinberg@prime-networks.co.uk>
2024-02-28 14:29:22 +0000

    Added custom thresholds for SharePoint and Mailbox Quota alerts
    Added custom thresholds for SharePoint and Mailbox Quota alerts

--- Modules/CIPPCore/Public/Entrypoints/Push-CIPPAlertQuotaUsed.ps1
+++ Modules/CIPPCore/Public/Entrypoints/Push-CIPPAlertQuotaUsed.ps1
@@ -11,8 +11,13 @@ function Push-CIPPAlertQuotaUsed {
         New-GraphGetRequest -uri "https://graph.microsoft.com/beta/reports/getMailboxUsageDetail(period='D7')?`$format=application/json" -tenantid $QueueItem.tenant | ForEach-Object {
             if ($_.StorageUsedInBytes -eq 0) { continue }
             $PercentLeft = [math]::round($_.StorageUsedInBytes / $_.prohibitSendReceiveQuotaInBytes * 100)
-            if ($PercentLeft -gt 90) { 
-                Write-AlertMessage -tenant $($QueueItem.tenant) -message "$($_.UserPrincipalName): Mailbox has less than 10% space left. Mailbox is $PercentLeft% full" 
+            if ($QueueItem.value -eq $true) {
+                if ($PercentLeft -gt 90) { 
+                    Write-AlertMessage -tenant $($QueueItem.tenant) -message "$($_.UserPrincipalName): Mailbox is more than $($QueueItem.value)% full. Mailbox is $PercentLeft% full" 
+                }
+            }
+            elseif ($PercentLeft -gt $QueueItem.value) { 
+                Write-AlertMessage -tenant $($QueueItem.tenant) -message "$($_.UserPrincipalName): Mailbox is more than $($QueueItem.value)% full. Mailbox is $PercentLeft% full" 
             }
         }
     } catch {
--- Modules/CIPPCore/Public/Entrypoints/Push-CIPPAlertSharepointQuota.ps1
+++ Modules/CIPPCore/Public/Entrypoints/Push-CIPPAlertSharepointQuota.ps1
@@ -13,8 +13,13 @@ function Push-CIPPAlertSharepointQuota {
         $sharepointQuota = (Invoke-RestMethod -Method 'GET' -Headers $sharepointToken -Uri "https://$($tenantName)-admin.sharepoint.com/_api/StorageQuotas()?api-version=1.3.2" -ErrorAction Stop).value
         if ($sharepointQuota) {
             $UsedStoragePercentage = [int](($sharepointQuota.GeoUsedStorageMB / $sharepointQuota.TenantStorageMB) * 100)
-            if ($UsedStoragePercentage -gt 90) {
-                Write-AlertMessage -tenant $($QueueItem.tenant) -message "SharePoint Storage is at $($UsedStoragePercentage)%"
+            if ($QueueItem.value -eq $true){
+                if ($UsedStoragePercentage -gt 90) {
+                    Write-AlertMessage -tenant $($QueueItem.tenant) -message "SharePoint Storage is at $($UsedStoragePercentage)%. Your alert threshold is 90%"
+                }
+            }
+            elseif ($UsedStoragePercentage -gt $QueueItem.value) {
+                Write-AlertMessage -tenant $($QueueItem.tenant) -message "SharePoint Storage is at $($UsedStoragePercentage)%. Your alert threshold is $($QueueItem.value)%"
             }
         }
     } catch {
