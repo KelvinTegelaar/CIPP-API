c7140d873e728bd4d35bedbddce961aee4017cdd
John Duprey <jwd@johnwduprey.com>
2024-03-04 14:25:44 -0500

    Add entrypoints for generic durables

--- Modules/CippEntrypoints/CippEntrypoints.psm1
+++ Modules/CippEntrypoints/CippEntrypoints.psm1
@@ -19,7 +19,7 @@ function Receive-CippHttpTrigger {
 
 function Receive-CippQueueTrigger {
     Param($QueueItem, $TriggerMetadata)
-  
+
     $Start = (Get-Date).ToUniversalTime()
     $APIName = $TriggerMetadata.FunctionName
     Write-Host "#### Running $APINAME"
@@ -48,5 +48,59 @@ function Receive-CippQueueTrigger {
     Write-CippFunctionStats @Stats
 }
 
-Export-ModuleMember -Function @('Receive-CippHttpTrigger', 'Receive-CippQueueTrigger')
+function Receive-CippOrchestrationTrigger {
+    param($Context)
+
+    $DurableRetryOptions = @{
+        FirstRetryInterval  = (New-TimeSpan -Seconds 5)
+        MaxNumberOfAttempts = 3
+        BackoffCoefficient  = 2
+    }
+    $RetryOptions = New-DurableRetryOptions @DurableRetryOptions
+    Write-LogMessage -API $Context.Input.OrchestratorName -tenant $Context.Input.TenantFilter -message "Started $($Context.Input.OrchestratorName)" -sev info
+
+    if (!$Context.Input.Batch -or ($Context.Input.Batch | Measure-Object).Count -eq 0) {
+        $Batch = (Invoke-ActivityFunction -FunctionName 'CIPPActivityFunction' -Input $Context.Input.QueueFunction)
+    } else {
+        $Batch = $Context.Input.Batch
+    }
+
+    foreach ($Item in $Batch) {
+        Invoke-DurableActivity -FunctionName 'CIPPActivityFunction' -Input $Item -NoWait -RetryOptions $RetryOptions
+    }
+
+    Write-LogMessage -API $Context.Input.OrchestratorName -tenant $tenant -message "Finished $($Context.Input.OrchestratorName)" -sev Info
+}
+
+function Receive-CippActivityTrigger {
+    Param($Item)
+
+    $Start = (Get-Date).ToUniversalTime()
+    Set-Location (Get-Item $PSScriptRoot).Parent.Parent.FullName
+
+    if ($Item.FunctionName) {
+        $FunctionName = 'Push-{0}' -f $Item.FunctionName
+        try {
+            & $FunctionName @Item
+        } catch {
+            $ErrorMsg = $_.Exception.Message
+        }
+    } else {
+        $ErrorMsg = 'Function not provided'
+    }
+
+    $End = (Get-Date).ToUniversalTime()
+
+    $Stats = @{
+        FunctionType = 'Durable'
+        Entity       = $Item
+        Start        = $Start
+        End          = $End
+        ErrorMsg     = $ErrorMsg
+    }
+    Write-Information '####### Adding stats'
+    Write-CippFunctionStats @Stats
+}
+
+Export-ModuleMember -Function @('Receive-CippHttpTrigger', 'Receive-CippQueueTrigger', 'Receive-CippOrchestrationTrigger', 'Receive-CippActivityTrigger')
 
