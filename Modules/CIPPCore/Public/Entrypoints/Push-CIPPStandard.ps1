5c87c669c71f068d91f0b85d815d172a99c10d03
John Duprey <jwd@johnwduprey.com>
2024-03-04 15:47:40 -0500

    Standards to durable

--- Modules/CIPPCore/Public/Entrypoints/Push-CIPPStandard.ps1
+++ Modules/CIPPCore/Public/Entrypoints/Push-CIPPStandard.ps1
@@ -1,15 +1,15 @@
 function Push-CIPPStandard {
     param (
-        $QueueItem, $TriggerMetadata
+        $Tenant,
+        $Standard,
+        $Settings
     )
 
-    Write-Host "Received queue item for $($QueueItem.Tenant) and standard $($QueueItem.Standard)."
-    $Tenant = $QueueItem.Tenant
-    $Standard = $QueueItem.Standard
+    Write-Host "Received queue item for $Tenant and standard $Standard."
     $FunctionName = 'Invoke-CIPPStandard{0}' -f $Standard
     Write-Host "We'll be running $FunctionName"
     try {
-        & $FunctionName -Tenant $Tenant -Settings $QueueItem.Settings -ErrorAction Stop
+        & $FunctionName -Tenant $Tenant -Settings $Settings -ErrorAction Stop
     } catch {
         throw $_.Exception.Message
     }
