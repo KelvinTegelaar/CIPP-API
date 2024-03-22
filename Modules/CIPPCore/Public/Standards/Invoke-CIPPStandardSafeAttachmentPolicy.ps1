353e134a8677f61339240136c4d1a8deb25bc55a
Esco <OfficialEsco@users.noreply.github.com>
2024-03-22 09:36:39 +0100

    SafeAttachmentPolicy Standard
    * Update Invoke-CIPPStandardSafeAttachmentPolicy.ps1

---
+++ Modules/CIPPCore/Public/Standards/Invoke-CIPPStandardSafeAttachmentPolicy.ps1
@@ -0,0 +1,62 @@
+function Invoke-CIPPStandardSafeAttachmentPolicy {
+    <#
+    .FUNCTIONALITY
+    Internal
+    #>
+
+    param($Tenant, $Settings)
+    $SafeAttachmentState = New-ExoRequest -tenantid $Tenant -cmdlet 'Get-SafeAttachmentPolicy' | 
+    Where-Object -Property Name -eq $Settings.Name | 
+    Select-Object Name, Enable, Action, QuarantineTag, Redirect, RedirectAddress
+
+    $StateIsCorrect = if (
+        ($SafeAttachmentState.Name -eq $Settings.Name) -and
+        ($SafeAttachmentState.Enable -eq $Settings.Enable) -and
+        ($SafeAttachmentState.QuarantineTag -eq $Settings.QuarantineTag) -and
+        ($SafeAttachmentState.Redirect -eq $Settings.Redirect) -and
+        ($SafeAttachmentState.RedirectAddress -eq $Settings.RedirectAddress)
+    ) { $true } else { $false }
+
+    if ($Settings.remediate) {
+        
+        if ($StateIsCorrect) {
+            Write-LogMessage -API 'Standards' -tenant $Tenant -message 'Safe Attachment Policy already exists.' -sev Info
+        } else {
+            $cmdparams = @{
+                Identity = $Settings.Name
+                Enable = $Settings.Enable
+                QuarantineTag = $Settings.QuarantineTag
+                Redirect = $Settings.Redirect
+                RedirectAddress = $Settings.RedirectAddress
+            }
+
+            try {
+                if ($SafeAttachmentState.Name -eq $Settings.Name) {
+                    $cmdparams.Add("Identity", $Settings.Name)
+                    New-ExoRequest -tenantid $Tenant -cmdlet 'Set-SafeAttachmentPolicy' -cmdparams $cmdparams
+                    Write-LogMessage -API 'Standards' -tenant $Tenant -message 'Updated Safe Attachment Policy' -sev Info
+                } else {
+                    $cmdparams.Add("Name", $Settings.Name)
+                    New-ExoRequest -tenantid $Tenant -cmdlet 'New-SafeAttachmentPolicy' -cmdparams $cmdparams
+                    Write-LogMessage -API 'Standards' -tenant $Tenant -message 'Created Safe Attachment Policy' -sev Info
+                }
+            } catch {
+                Write-LogMessage -API 'Standards' -tenant $Tenant -message "Failed to create Safe Attachment Policy. Error: $($_.exception.message)" -sev Error
+            }
+        }
+    }
+
+    if ($Settings.alert) {
+
+        if ($StateIsCorrect) {
+            Write-LogMessage -API 'Standards' -tenant $Tenant -message 'Safe Attachment Policy is enabled' -sev Info
+        } else {
+            Write-LogMessage -API 'Standards' -tenant $Tenant -message 'Safe Attachment Policy is not enabled' -sev Alert
+        }
+    }
+
+    if ($Settings.report) {
+        Add-CIPPBPAField -FieldName 'SafeAttachmentPolicy' -FieldValue [bool]$StateIsCorrect -StoreAs bool -Tenant $tenant
+    }
+    
+}
