5e044ef19221828267752901ce85ebb713315b26
KelvinTegelaar <Kelvin@limenetworks.nl>
2024-04-11 14:13:28 +0200

    add ability to replace users & groups in policy.

--- Modules/CIPPCore/Public/Set-CIPPSignature.ps1
+++ Modules/CIPPCore/Public/Set-CIPPSignature.ps1
@@ -16,7 +16,7 @@ function Set-CIPPOutOfOffice {
         $SignatureProfile = @'
 [{"name":"Roaming_New_Signature","itemClass":"","id":"","scope":"AdeleV@M365x42953883.OnMicrosoft.com","parentSetting":"","secondaryKey":"","type":"String","timestamp":638296273181532792,"metadata":"","value":"Kelvin","isFirstSync":"true","source":"UserOverride"}]
 '@
-        $GraphRequest = New-GraphPostRequest -uri 'https://substrate.office.com/ows/beta/outlookcloudsettings/settings/global' -tenantid $TenantFilter -type GET -contentType 'application/json' -verbose -scope 'https://outlook.office.com/.default'
+        $GraphRequest = New-GraphPostRequest -uri 'https://substrate.office.com/ows/beta/outlookcloudsettings/settings/global' -tenantid $TenantFilter -type PATCH -contentType 'application/json' -verbose -scope 'https://outlook.office.com/.default'
         Write-LogMessage -user $ExecutingUser -API $APIName -message "Set Out-of-office for $($userid) to $state" -Sev 'Info' -tenant $TenantFilter
         return "Set Out-of-office for $($userid) to $state."
 
