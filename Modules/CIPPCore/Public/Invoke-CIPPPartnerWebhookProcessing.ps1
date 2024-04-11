9ec3b79a91227f4c4c535b1d1a86219fdfaf8ea0
John Duprey <jwd@johnwduprey.com>
2024-04-11 11:49:26 -0400

    Logging improvemnt
    Add Get-CippException
    Add LogData parameter to Write-LogMessage

--- Modules/CIPPCore/Public/Invoke-CIPPPartnerWebhookProcessing.ps1
+++ Modules/CIPPCore/Public/Invoke-CIPPPartnerWebhookProcessing.ps1
@@ -11,9 +11,9 @@ function Invoke-CippPartnerWebhookProcessing {
         default {
             if ($Data.AuditUri) {
                 $AuditLog = New-GraphGetRequest -uri $Data.AuditUri -tenantid $env:TenantID -NoAuthCheck $true -scope 'https://api.partnercenter.microsoft.com/.default'
-                Write-Logessage -API 'Webhooks' -message "Partner Center $($Data.EventName) audit log: $($AuditLog | ConvertTo-Json -Depth 5)" -Sev 'Info'
+                Write-Logessage -API 'Webhooks' -message "Partner Center $($Data.EventName) audit log received" -LogData $AuditLog -Sev 'Info'
             } else {
-                Write-LogMessage -API 'Webhooks' -message "Partner Center webhook received (no audit): $($Data | ConvertTo-Json -Depth 5)" -Sev 'Info'
+                Write-LogMessage -API 'Webhooks' -message 'Partner Center webhook received (no audit)' -LogData $Data -Sev 'Info'
             }
         }
     }
