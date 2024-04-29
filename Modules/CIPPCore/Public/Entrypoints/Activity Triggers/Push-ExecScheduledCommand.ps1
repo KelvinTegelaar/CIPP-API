20acd6c7b13992f1df077e6b7eca2d12bfadfe4a
KelvinTegelaar <Kelvin@limenetworks.nl>
2024-04-29 14:41:12 +0200

    text corrections

--- Modules/CIPPCore/Public/Entrypoints/Activity Triggers/Push-ExecScheduledCommand.ps1
+++ Modules/CIPPCore/Public/Entrypoints/Activity Triggers/Push-ExecScheduledCommand.ps1
@@ -17,7 +17,6 @@ function Push-ExecScheduledCommand {
             $results = & $Item.command @commandParameters
         } catch {
             $results = "Task Failed: $($_.Exception.Message)"
-
         }
 
         Write-Host 'ran the command'
@@ -50,7 +49,7 @@ function Push-ExecScheduledCommand {
 
     $TableDesign = '<style>table.blueTable{border:1px solid #1C6EA4;background-color:#EEE;width:100%;text-align:left;border-collapse:collapse}table.blueTable td,table.blueTable th{border:1px solid #AAA;padding:3px 2px}table.blueTable tbody td{font-size:13px}table.blueTable tr:nth-child(even){background:#D0E4F5}table.blueTable thead{background:#1C6EA4;background:-moz-linear-gradient(top,#5592bb 0,#327cad 66%,#1C6EA4 100%);background:-webkit-linear-gradient(top,#5592bb 0,#327cad 66%,#1C6EA4 100%);background:linear-gradient(to bottom,#5592bb 0,#327cad 66%,#1C6EA4 100%);border-bottom:2px solid #444}table.blueTable thead th{font-size:15px;font-weight:700;color:#FFF;border-left:2px solid #D0E4F5}table.blueTable thead th:first-child{border-left:none}table.blueTable tfoot{font-size:14px;font-weight:700;color:#FFF;background:#D0E4F5;background:-moz-linear-gradient(top,#dcebf7 0,#d4e6f6 66%,#D0E4F5 100%);background:-webkit-linear-gradient(top,#dcebf7 0,#d4e6f6 66%,#D0E4F5 100%);background:linear-gradient(to bottom,#dcebf7 0,#d4e6f6 66%,#D0E4F5 100%);border-top:2px solid #444}table.blueTable tfoot td{font-size:14px}table.blueTable tfoot .links{text-align:right}table.blueTable tfoot .links a{display:inline-block;background:#1C6EA4;color:#FFF;padding:2px 8px;border-radius:5px}</style>'
     $HTML = ($results | Select-Object * -ExcludeProperty RowKey, PartitionKey | ConvertTo-Html -Fragment) -replace '<table>', "$TableDesign<table class=blueTable>" | Out-String
-    $title = "Scheduled Task $($task.Name) - $($task.ExpectedRunTime)"
+    $title = "Scheduled Task $($task.Name)"
     Write-Host $title
     switch -wildcard ($task.PostExecution) {
         '*psa*' { Send-CIPPAlert -Type 'psa' -Title $title -HTMLContent $HTML }
@@ -85,5 +84,5 @@ function Push-ExecScheduledCommand {
             ScheduledTime = "$nextRunUnixTime"
         }
     }
-    Write-LogMessage -API 'Scheduler_UserTasks' -tenant $tenant -message "Successfully executed task: $($task.name)" -sev Info
+    Write-LogMessage -API 'Scheduler_UserTasks' -tenant $tenant -message "Successfully executed task: $($task.Name)" -sev Info
 }
