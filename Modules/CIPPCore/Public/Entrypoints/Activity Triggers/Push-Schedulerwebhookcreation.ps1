178202d35fe69779f0b34735912c17ae0c5bdd02
KelvinTegelaar <Kelvin@limenetworks.nl>
2024-05-12 01:35:07 +0200

    changes

--- Modules/CIPPCore/Public/Entrypoints/Activity Triggers/Push-Schedulerwebhookcreation.ps1
+++ Modules/CIPPCore/Public/Entrypoints/Activity Triggers/Push-Schedulerwebhookcreation.ps1
@@ -4,7 +4,36 @@ function Push-Schedulerwebhookcreation {
         Entrypoint
     #>
     param (
-        $QueueItem, $TriggerMetadata
+        $item
     )
+    $Table = Get-CIPPTable -TableName 'SchedulerConfig'
+    $Row = Get-CIPPAzDataTableEntity @Table -Filter "RowKey eq '$($item.SchedulerRow)'"
+    Write-Host "Working on $($Row.tenant) - $($Row.tenantid)"
+    #Temporary cleanup - Find all audit log webhooks, tell Microsoft we no longer want them. 
 
+
+    #use the queueitem to see if we already have a webhook for this tenant + webhooktype. If we do, delete this row from SchedulerConfig.
+    $WebhookTable = Get-CIPPTable -TableName 'WebhookTable'
+    $Webhook = Get-CIPPAzDataTableEntity @WebhookTable -Filter "PartitionKey eq '$($Row.tenant)' and Version eq '2' and Resource eq '$($Row.webhookType)'"
+    if ($Webhook) {
+        Write-Host "Found existing webhook for $($Row.tenant) - $($Row.webhookType)"
+        Remove-CIPPAzDataTableEntity @Table -Entity $Row
+    } else {
+        Write-Host "No existing webhook for $($Row.tenant) - $($Row.webhookType) - Time to create."
+        try {
+            $NewSub = New-CIPPGraphSubscription -TenantFilter $Row.tenant -Resource $Row.webhookType -BaseURL $Row.CIPPURL
+            if ($NewSub.Success) {
+                Remove-CIPPAzDataTableEntity @Table -Entity $Row
+            } else {
+                Write-Host "Failed to create webhook for $($Row.tenant) - $($Row.webhookType) - $($_.Exception.Message)"
+                Write-LogMessage -message "Failed to create webhook for $($Row.tenant) - $($Row.webhookType)" -Sev 'Error' -LogData $_.Exception
+            }
+        } catch {
+            Write-Host "Failed to create webhook for $($Row.tenant) - $($Row.webhookType): $($_.Exception.Message)"
+            Write-LogMessage -message "Failed to create webhook for $($Row.tenant) - $($Row.webhookType)" -Sev 'Error' -LogData $_.Exception
+ 
+        }
+
+    }
+    
 }
