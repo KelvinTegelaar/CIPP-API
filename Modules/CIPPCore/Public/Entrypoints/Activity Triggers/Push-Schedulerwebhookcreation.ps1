7c21a6b267e4c9e8216bef3e4eef16eaeb39f893
KelvinTegelaar <Kelvin@limenetworks.nl>
2024-05-12 17:07:04 +0200

    5 minute timer for testing webhooks

--- Modules/CIPPCore/Public/Entrypoints/Activity Triggers/Push-Schedulerwebhookcreation.ps1
+++ Modules/CIPPCore/Public/Entrypoints/Activity Triggers/Push-Schedulerwebhookcreation.ps1
@@ -11,7 +11,7 @@ function Push-Schedulerwebhookcreation {
 
     $Row = Get-CIPPAzDataTableEntity @Table -Filter "RowKey eq '$($item.SchedulerRow)'"
     $Tenant = (Get-Tenants | Where-Object { $_.customerId -eq $Row.tenantid }).defaultDomainName
-    Write-Host "Working on  $Tenant - $($Row.tenantid)"
+    Write-Host "Working on $Tenant - $($Row.tenantid)"
     #use the queueitem to see if we already have a webhook for this tenant + webhooktype. If we do, delete this row from SchedulerConfig.
     $Webhook = Get-CIPPAzDataTableEntity @WebhookTable -Filter "PartitionKey eq '$Tenant' and Version eq '2' and Resource eq '$($Row.webhookType)'"
     if ($Webhook) {
@@ -23,7 +23,7 @@ function Push-Schedulerwebhookcreation {
             $NewSub = New-CIPPGraphSubscription -TenantFilter $Tenant -EventType $Row.webhookType -BaseURL $Row.CIPPURL -auditLogAPI $true
             if ($NewSub.Success) {
                 Remove-AzDataTableEntity @Table -Entity $Row
-            } else {
+            } else { 
                 Write-Host "Failed to create webhook for $Tenant - $($Row.webhookType) - $($_.Exception.Message)"
                 Write-LogMessage -message "Failed to create webhook for $Tenant - $($Row.webhookType)" -Sev 'Error' -LogData $_.Exception
             }
