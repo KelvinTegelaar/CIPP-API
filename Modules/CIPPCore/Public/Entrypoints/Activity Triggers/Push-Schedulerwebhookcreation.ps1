fd741c1632bb503b66eab11acb5f59c85a89999f
KelvinTegelaar <Kelvin@limenetworks.nl>
2024-05-13 17:07:13 +0200

    updated for all tenants.

--- Modules/CIPPCore/Public/Entrypoints/Activity Triggers/Push-Schedulerwebhookcreation.ps1
+++ Modules/CIPPCore/Public/Entrypoints/Activity Triggers/Push-Schedulerwebhookcreation.ps1
@@ -14,29 +14,35 @@ function Push-Schedulerwebhookcreation {
         Write-Host "No row found for $($item.SchedulerRow). Full received item was $($item | ConvertTo-Json)"
         return
     } else {
-        $Tenant = (Get-Tenants | Where-Object { $_.customerId -eq $Row.tenantid }).defaultDomainName
-        Write-Host "Working on $Tenant - $($Row.tenantid)"
-        #use the queueitem to see if we already have a webhook for this tenant + webhooktype. If we do, delete this row from SchedulerConfig.
-        $Webhook = Get-CIPPAzDataTableEntity @WebhookTable -Filter "PartitionKey eq '$Tenant' and Version eq '2' and Resource eq '$($Row.webhookType)'"
-        if ($Webhook) {
-            Write-Host "Found existing webhook for $Tenant - $($Row.webhookType)"
-            Remove-AzDataTableEntity @Table -Entity $Row
+        if ($Row.tenantid -eq 'AllTEnants') {
+            $Tenants = (Get-Tenants).defaultDomainName
         } else {
-            Write-Host "No existing webhook for $Tenant - $($Row.webhookType) - Time to create."
-            try {
-                $NewSub = New-CIPPGraphSubscription -TenantFilter $Tenant -EventType $Row.webhookType -BaseURL $Row.CIPPURL -auditLogAPI $true
-                if ($NewSub.Success) {
-                    Remove-AzDataTableEntity @Table -Entity $Row
-                } else { 
-                    Write-Host "Failed to create webhook for $Tenant - $($Row.webhookType) - $($_.Exception.Message)"
+            $Tenants = (Get-Tenants | Where-Object { $_.customerId -eq $Row.tenantid }).defaultDomainName
+        }
+        foreach ($Tenant in $Tenants) {
+            Write-Host "Working on $Tenant - $($Row.tenantid)"
+            #use the queueitem to see if we already have a webhook for this tenant + webhooktype. If we do, delete this row from SchedulerConfig.
+            $Webhook = Get-CIPPAzDataTableEntity @WebhookTable -Filter "PartitionKey eq '$Tenant' and Version eq '2' and Resource eq '$($Row.webhookType)'"
+            if ($Webhook) {
+                Write-Host "Found existing webhook for $Tenant - $($Row.webhookType)"
+                Remove-AzDataTableEntity @Table -Entity $Row
+            } else {
+                Write-Host "No existing webhook for $Tenant - $($Row.webhookType) - Time to create."
+                try {
+                    $NewSub = New-CIPPGraphSubscription -TenantFilter $Tenant -EventType $Row.webhookType -BaseURL $Row.CIPPURL -auditLogAPI $true
+                    if ($NewSub.Success) {
+                        Remove-AzDataTableEntity @Table -Entity $Row
+                    } else { 
+                        Write-Host "Failed to create webhook for $Tenant - $($Row.webhookType) - $($_.Exception.Message)"
+                        Write-LogMessage -message "Failed to create webhook for $Tenant - $($Row.webhookType)" -Sev 'Error' -LogData $_.Exception
+                    }
+                } catch {
+                    Write-Host "Failed to create webhook for $Tenant - $($Row.webhookType): $($_.Exception.Message)"
                     Write-LogMessage -message "Failed to create webhook for $Tenant - $($Row.webhookType)" -Sev 'Error' -LogData $_.Exception
-                }
-            } catch {
-                Write-Host "Failed to create webhook for $Tenant - $($Row.webhookType): $($_.Exception.Message)"
-                Write-LogMessage -message "Failed to create webhook for $Tenant - $($Row.webhookType)" -Sev 'Error' -LogData $_.Exception
  
-            }
+                }
 
+            }
         }
     }
     
