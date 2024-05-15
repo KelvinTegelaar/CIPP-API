ba66a30b893df4e851b726733110afea3df6d525
KelvinTegelaar <Kelvin@limenetworks.nl>
2024-05-15 19:26:06 +0200

    added contents of reuslts for alert email

--- Modules/CIPPCore/Public/Entrypoints/Activity Triggers/Push-ExecScheduledCommand.ps1
+++ Modules/CIPPCore/Public/Entrypoints/Activity Triggers/Push-ExecScheduledCommand.ps1
@@ -21,7 +21,6 @@ function Push-ExecScheduledCommand {
 
         Write-Host 'ran the command. Processing results'
         if ($item.command -like 'Get-CIPPAlert*') {
-            $Results = @{ Results = $results }
             $TaskType = 'Alert'
         } else {
             $TaskType = 'Scheduled Task'
@@ -61,7 +60,8 @@ function Push-ExecScheduledCommand {
         $TableDesign = '<style>table.blueTable{border:1px solid #1C6EA4;background-color:#EEE;width:100%;text-align:left;border-collapse:collapse}table.blueTable td,table.blueTable th{border:1px solid #AAA;padding:3px 2px}table.blueTable tbody td{font-size:13px}table.blueTable tr:nth-child(even){background:#D0E4F5}table.blueTable thead{background:#1C6EA4;background:-moz-linear-gradient(top,#5592bb 0,#327cad 66%,#1C6EA4 100%);background:-webkit-linear-gradient(top,#5592bb 0,#327cad 66%,#1C6EA4 100%);background:linear-gradient(to bottom,#5592bb 0,#327cad 66%,#1C6EA4 100%);border-bottom:2px solid #444}table.blueTable thead th{font-size:15px;font-weight:700;color:#FFF;border-left:2px solid #D0E4F5}table.blueTable thead th:first-child{border-left:none}table.blueTable tfoot{font-size:14px;font-weight:700;color:#FFF;background:#D0E4F5;background:-moz-linear-gradient(top,#dcebf7 0,#d4e6f6 66%,#D0E4F5 100%);background:-webkit-linear-gradient(top,#dcebf7 0,#d4e6f6 66%,#D0E4F5 100%);background:linear-gradient(to bottom,#dcebf7 0,#d4e6f6 66%,#D0E4F5 100%);border-top:2px solid #444}table.blueTable tfoot td{font-size:14px}table.blueTable tfoot .links{text-align:right}table.blueTable tfoot .links a{display:inline-block;background:#1C6EA4;color:#FFF;padding:2px 8px;border-radius:5px}</style>'
         $HTML = ($results | Select-Object * -ExcludeProperty RowKey, PartitionKey | ConvertTo-Html -Fragment) -replace '<table>', "$TableDesign<table class=blueTable>" | Out-String
         $title = "$TaskType - $($task.Name)"
-        Write-Host $title
+        Write-Host 'Scheduler: Sending the results to the target.'
+        Write-Host "The content of results is: $Results"
         switch -wildcard ($task.PostExecution) {
             '*psa*' { Send-CIPPAlert -Type 'psa' -Title $title -HTMLContent $HTML }
             '*email*' { Send-CIPPAlert -Type 'email' -Title $title -HTMLContent $HTML }
