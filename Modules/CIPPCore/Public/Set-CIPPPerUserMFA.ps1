3a5b49f854cc12525e5dd70c6a9424d42cd7bf41
John Duprey <jwd@johnwduprey.com>
2024-06-12 23:53:01 -0400

    Per user mfa tweaks

--- Modules/CIPPCore/Public/Set-CIPPPerUserMFA.ps1
+++ Modules/CIPPCore/Public/Set-CIPPPerUserMFA.ps1
@@ -1,31 +1,69 @@
 function Set-CIPPPerUserMFA {
+    <#
+    .SYNOPSIS
+    Change Per-User MFA State for a User
+
+    .DESCRIPTION
+    Change the Per-User MFA State for a user via the /users/{id}/authentication/requirements endpoint
+
+    .PARAMETER TenantFilter
+    Tenant where the user resides
+
+    .PARAMETER userId
+    One or more User IDs to set the MFA state for (GUID or UserPrincipalName)
+
+    .PARAMETER State
+    State to set the user to (enabled, disabled, enforced)
+
+    .PARAMETER executingUser
+    User executing the command
+
+    .EXAMPLE
+    Set-CIPPPerUserMFA -TenantFilter 'contoso.onmicrosoft.com' -userId user@contoso.onmicrosoft.com -State 'disabled' -executingUser 'mspuser@partner.com'
+    #>
     [CmdletBinding()]
     param(
-        $TenantFilter,
-        $userId,
+        [Parameter(Mandatory = $true)]
+        [string]$TenantFilter,
+        [Parameter(Mandatory = $true)]
+        [string[]]$userId,
         [ValidateSet('enabled', 'disabled', 'enforced')]
         $State = 'enabled',
-        $executingUser
+        [string]$executingUser = 'CIPP'
     )
     try {
         $int = 0
+        $Body = @{
+            state = $State
+        }
         $Requests = foreach ($id in $userId) {
             @{
                 id        = $int++
                 method    = 'PATCH'
                 url       = "users/$id/authentication/requirements"
-                body      = @{ 'perUserMfaState' = "$state" }
+                body      = $Body
                 'headers' = @{
                     'Content-Type' = 'application/json'
                 }
             }
         }
+
         #Split the requests by batches of 20, we can have anywhere from 1 to 1000 batches and execute New-GraphBulkRequest -tenantid $tenantfilter -scope 'https://graph.microsoft.com/.default' -Requests $batch
         $Requests = New-GraphBulkRequest -tenantid $tenantfilter -scope 'https://graph.microsoft.com/.default' -Requests @($Requests) -asapp $true
         "Successfully set Per user MFA State for $userId"
-        Write-LogMessage -user $executingUser -API 'Set-CIPPPerUserMFA' -message "Successfully set Per user MFA State for $id" -Sev 'Info' -tenant $TenantFilter
+
+        $Users = foreach ($id in $userId) {
+            @{
+                userId     = $id
+                Properties = @{
+                    perUserMfaState = $State
+                }
+            }
+        }
+        Set-CIPPUserSchemaProperties -TenantFilter $TenantFilter -Users $Users
+        Write-LogMessage -user $executingUser -API 'Set-CIPPPerUserMFA' -message "Successfully set Per user MFA State to $State for $id" -Sev 'Info' -tenant $TenantFilter
     } catch {
         "Failed to set MFA State for $id : $_"
-        Write-LogMessage -user $executingUser -API 'Set-CIPPPerUserMFA' -message "Failed to set MFA State for $id : $_" -Sev 'Error' -tenant $TenantFilter
+        Write-LogMessage -user $executingUser -API 'Set-CIPPPerUserMFA' -message "Failed to set MFA State to $State for $id : $_" -Sev 'Error' -tenant $TenantFilter
     }
 }
--- Modules/CIPPCore/Public/Set-CIPPUserSchemaProperties.ps1
+++ Modules/CIPPCore/Public/Set-CIPPUserSchemaProperties.ps1
@@ -1,19 +1,46 @@
 function Set-CIPPUserSchemaProperties {
+    <#
+    .SYNOPSIS
+    Set Schema Properties for a user
+
+    .DESCRIPTION
+    Uses scheam extensions to set properties for a user
+
+    .PARAMETER TenantFilter
+    Tenant for user
+
+    .PARAMETER UserId
+    One or more user ids to set properties for
+
+    .PARAMETER Properties
+    Hashtable of properties to set
+
+    #>
     [CmdletBinding(SupportsShouldProcess = $true)]
     Param(
+        [Parameter(Mandatory = $true)]
         [string]$TenantFilter,
-        [string]$UserId,
-        [hashtable]$Properties
+        [Parameter(Mandatory = $true)]
+        [object]$Users
     )
 
     $Schema = Get-CIPPSchemaExtensions | Where-Object { $_.id -match '_cippUser' }
-
-    $Body = [PSCustomObject]@{
-        "$($Schema.id)" = $Properties
+    $int = 0
+    $Requests = foreach ($User in $Users) {
+        @{
+            id        = $int++
+            method    = 'PATCH'
+            url       = "users/$($User.userId)"
+            body      = @{
+                "$($Schema.id)" = $User.Properties
+            }
+            'headers' = @{
+                'Content-Type' = 'application/json'
+            }
+        }
     }
 
-    $Json = ConvertTo-Json -Depth 5 -InputObject $Body
-    if ($PSCmdlet.ShouldProcess("User: $UserId", "Set Schema Properties to $($Properties|ConvertTo-Json -Compress)")) {
-        New-GraphPOSTRequest -type PATCH -Uri "https://graph.microsoft.com/beta/users/$UserId" -Body $Json -tenantid $TenantFilter
+    if ($PSCmdlet.ShouldProcess("User: $($Users.userId -join ', ')", 'Set Schema Properties')) {
+        $Requests = New-GraphBulkRequest -tenantid $tenantfilter -Requests @($Requests)
     }
 }
