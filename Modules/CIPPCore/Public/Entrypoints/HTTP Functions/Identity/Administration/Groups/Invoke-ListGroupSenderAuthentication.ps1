d1a315506145698f95da655a8abd82b8be88d1d5
BNWEIN <bweinberg@prime-networks.co.uk>
2024-06-23 22:22:17 +0100

    Update Invoke-ListGroupSenderAuthentication.ps1

--- Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Identity/Administration/Groups/Invoke-ListGroupSenderAuthentication.ps1
+++ Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Identity/Administration/Groups/Invoke-ListGroupSenderAuthentication.ps1
@@ -29,10 +29,6 @@ Function Invoke-ListGroupSenderAuthentication {
         $Request = $ErrorMessage
     }
 
-    write-host "Group ID is: $($groupid)"
-    write-host "Tenant Filter is: $($TenantFilter)"
-    write-host "Search This New: $($Request.RequireSenderAuthenticationEnabled)"
-
     # Associate values to output bindings by calling 'Push-OutputBinding'.
     Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
             StatusCode = $StatusCode
