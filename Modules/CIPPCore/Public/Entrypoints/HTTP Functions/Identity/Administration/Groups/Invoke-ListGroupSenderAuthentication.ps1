46f9dad45d7a4e52259d8526f0a5dae5a9eb35d2
BNWEIN <bweinberg@prime-networks.co.uk>
2024-06-26 14:05:35 +0100

    Update Invoke-ListGroupSenderAuthentication.ps1

--- Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Identity/Administration/Groups/Invoke-ListGroupSenderAuthentication.ps1
+++ Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Identity/Administration/Groups/Invoke-ListGroupSenderAuthentication.ps1
@@ -20,6 +20,11 @@ Function Invoke-ListGroupSenderAuthentication {
         Identity = $groupid
     }
 
+    Write-Host = "This is the group id $groupid"
+    Write-Host = "This is the tenant filter $TenantFilter"
+    $GroupType = Invoke-ListGroups -tenantFilter $TenantFilter -GroupID $groupid
+    Write-Host = "This is the group type $($GroupType.calculatedGroupType)"
+
     try {
         $Request = New-ExoRequest -tenantid $TenantFilter -cmdlet 'Get-DistributionGroup' -cmdParams $params -UseSystemMailbox $true
         $StatusCode = [HttpStatusCode]::OK 
@@ -32,6 +37,6 @@ Function Invoke-ListGroupSenderAuthentication {
     # Associate values to output bindings by calling 'Push-OutputBinding'.
     Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
             StatusCode = $StatusCode
-            Body       = $($Request.RequireSenderAuthenticationEnabled)
+            Body       = @{ enabled = $Request.RequireSenderAuthenticationEnabled }
         })
 }
