acf6977d6e4675333f1c4499809c7da57e9a6f16
John Duprey <jwd@johnwduprey.com>
2024-07-30 14:21:12 -0400

    PermissionUpdate changes
    - Add logic to check the CPV table for the last update date
    - Compare with SAM manifest modified date or -le 7 days and trigger refresh
    - Also check default domain name and refresh tenant list for that tenant
    - Update Get-Tenants to support -TenantFilter query to shorten refresh time

--- Modules/CIPPCore/Public/Entrypoints/Activity Triggers/Push-UpdatePermissionsQueue.ps1
+++ Modules/CIPPCore/Public/Entrypoints/Activity Triggers/Push-UpdatePermissionsQueue.ps1
@@ -4,21 +4,47 @@ function Push-UpdatePermissionsQueue {
         Entrypoint
     #>
     param($Item)
-    Write-Host "Applying permissions for $($Item.defaulomainName)"
-    $Table = Get-CIPPTable -TableName cpvtenants
-    $CPVRows = Get-CIPPAzDataTableEntity @Table | Where-Object -Property Tenant -EQ $Item.customerId
-    $PermissionUpdateRequired = $false
-    if (!$CPVRows -or $ENV:ApplicationID -notin $CPVRows.applicationId) {
-        Write-LogMessage -tenant $Item.defaultDomainName -tenantId $Item.customerId -message 'A New tenant has been added, or a new CIPP-SAM Application is in use' -Sev 'Warn' -API 'NewTenant'
-        Write-Host 'Adding CPV permissions'
-        Set-CIPPCPVConsent -Tenantfilter $Item.customerId
-        $PermissionUpdateRequired = $true
+
+    try {
+        $DomainRefreshRequired = $false
+
+        if (!$Item.defaultDomainName) {
+            $DomainRefreshRequired = $true
+        }
+
+        Write-Information "Applying permissions for $($Item.displayName)"
+        $Table = Get-CIPPTable -TableName cpvtenants
+        $CPVRows = Get-CIPPAzDataTableEntity @Table | Where-Object -Property Tenant -EQ $Item.customerId
+
+        if (!$CPVRows -or $ENV:ApplicationID -notin $CPVRows.applicationId) {
+            Write-LogMessage -tenant $Item.defaultDomainName -tenantId $Item.customerId -message 'A New tenant has been added, or a new CIPP-SAM Application is in use' -Sev 'Warn' -API 'NewTenant'
+            Write-Information 'Adding CPV permissions'
+            Set-CIPPCPVConsent -Tenantfilter $Item.customerId
+            $DomainRefreshRequired = $true
+        }
+        Write-Information 'Updating permissions'
+        Add-CIPPApplicationPermission -RequiredResourceAccess 'CippDefaults' -ApplicationId $ENV:ApplicationID -tenantfilter $Item.customerId
+        Add-CIPPDelegatedPermission -RequiredResourceAccess 'CippDefaults' -ApplicationId $ENV:ApplicationID -tenantfilter $Item.customerId
+        Write-LogMessage -tenant $Item.defaultDomainName -tenantId $Item.customerId -message "Updated permissions for $($Item.displayName)" -Sev 'Info' -API 'UpdatePermissionsQueue'
+
+        $Table = Get-CIPPTable -TableName cpvtenants
+        $unixtime = [int64](([datetime]::UtcNow) - (Get-Date '1/1/1970')).TotalSeconds
+        $GraphRequest = @{
+            LastApply     = "$unixtime"
+            applicationId = "$($ENV:applicationId)"
+            Tenant        = "$($Item.customerId)"
+            PartitionKey  = 'Tenant'
+            RowKey        = "$($Item.customerId)"
+        }
+        Add-CIPPAzDataTableEntity @Table -Entity $GraphRequest -Force
+
+        if ($DomainRefreshRequired) {
+            $UpdatedTenant = Get-Tenants -TenantFilter $Item.customerId -TriggerRefresh
+            if ($UpdatedTenant.defaultDomainName) {
+                Write-Information "Updated tenant domains $($UpdatedTenant.defaultDomainName)"
+            }
+        }
+    } catch {
+        Write-Information "Error updating permissions for $($Item.displayName)"
     }
-
-    $CippCorePath = (Get-Item $PSScriptRoot).Parent.Parent.FullName
-
-    Add-CIPPApplicationPermission -RequiredResourceAccess 'CippDefaults' -ApplicationId $ENV:ApplicationID -tenantfilter $Item.customerId
-    Add-CIPPDelegatedPermission -RequiredResourceAccess 'CippDefaults' -ApplicationId $ENV:ApplicationID -tenantfilter $Item.customerId
-
-    Write-LogMessage -tenant $Item.defaultDomainName -tenantId $Item.customerId -message "Updated permissions for $($Item.displayName)" -Sev 'Info' -API 'UpdatePermissionsQueue'
 }
