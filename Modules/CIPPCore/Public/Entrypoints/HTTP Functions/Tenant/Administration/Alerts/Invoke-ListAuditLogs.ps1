f01f831ed48f1fdb4677a571953c48b74e462960
John Duprey <jwd@johnwduprey.com>
2024-07-31 20:27:13 -0400

    Add single audit log support

--- Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Tenant/Administration/Alerts/Invoke-ListAuditLogs.ps1
+++ Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Tenant/Administration/Alerts/Invoke-ListAuditLogs.ps1
@@ -13,38 +13,42 @@ function Invoke-ListAuditLogs {
 
     $TenantFilter = $Request.Query.TenantFilter
     $FilterConditions = [System.Collections.Generic.List[string]]::new()
-    if ($TenantFilter) {
-        $FilterConditions.Add("Tenant eq '$TenantFilter'")
-    }
 
-    if ($Request.Query.RelativeTime) {
-        $RelativeTime = $Request.Query.RelativeTime
-
-        if ($RelativeTime -match '(\d+)([dhm])') {
-            $EndDate = (Get-Date).ToUniversalTime().ToString('yyyy-MM-ddTHH:mm:ssZ')
-            switch ($Matches[2]) {
-                'd' { $StartDate = (Get-Date).AddDays(-$Matches[1]).ToUniversalTime().ToString('yyyy-MM-ddTHH:mm:ssZ') }
-                'h' { $StartDate = (Get-Date).AddHours(-$Matches[1]).ToUniversalTime().ToString('yyyy-MM-ddTHH:mm:ssZ') }
-                'm' { $StartDate = (Get-Date).AddMinutes(-$Matches[1]).ToUniversalTime().ToString('yyyy-MM-ddTHH:mm:ssZ') }
-            }
-        }
-        $FilterConditions.Add("Timestamp ge '$StartDate' and Timestamp le '$EndDate'")
+    if ($Request.Query.LogId) {
+        $FilterConditions.Add("RowKey eq '$($Request.Query.LogId)'")
     } else {
-        if ($Request.Query.StartDate) {
-            if ($Request.Query.StartDate -match '^\d+$') {
-                $Request.Query.StartDate = [DateTimeOffset]::FromUnixTimeSeconds($Request.Query.StartDate).DateTime.ToUniversalTime().ToString('yyyy-MM-ddTHH:mm:ssZ')
-            } else {
-                $StartDate = (Get-Date $Request.Query.StartDate).ToString('yyyy-MM-ddTHH:mm:ssZ')
-            }
-            $FilterConditions.Add("Timestamp ge '$StartDate'")
+        if ($TenantFilter) {
+            $FilterConditions.Add("Tenant eq '$TenantFilter'")
+        }
+        if ($Request.Query.RelativeTime) {
+            $RelativeTime = $Request.Query.RelativeTime
 
-            if ($Request.Query.EndDate) {
-                if ($Request.Query.EndDate -match '^\d+$') {
-                    $Request.Query.EndDate = [DateTimeOffset]::FromUnixTimeSeconds($Request.Query.EndDate).DateTime.ToUniversalTime().ToString('yyyy-MM-ddTHH:mm:ssZ')
-                } else {
-                    $EndDate = (Get-Date $Request.Query.EndDate).ToString('yyyy-MM-ddTHH:mm:ssZ')
+            if ($RelativeTime -match '(\d+)([dhm])') {
+                $EndDate = (Get-Date).ToUniversalTime().ToString('yyyy-MM-ddTHH:mm:ssZ')
+                switch ($Matches[2]) {
+                    'd' { $StartDate = (Get-Date).AddDays(-$Matches[1]).ToUniversalTime().ToString('yyyy-MM-ddTHH:mm:ssZ') }
+                    'h' { $StartDate = (Get-Date).AddHours(-$Matches[1]).ToUniversalTime().ToString('yyyy-MM-ddTHH:mm:ssZ') }
+                    'm' { $StartDate = (Get-Date).AddMinutes(-$Matches[1]).ToUniversalTime().ToString('yyyy-MM-ddTHH:mm:ssZ') }
                 }
-                $FilterConditions.Add("Timestamp le '$EndDate'")
+            }
+            $FilterConditions.Add("Timestamp ge '$StartDate' and Timestamp le '$EndDate'")
+        } else {
+            if ($Request.Query.StartDate) {
+                if ($Request.Query.StartDate -match '^\d+$') {
+                    $Request.Query.StartDate = [DateTimeOffset]::FromUnixTimeSeconds($Request.Query.StartDate).DateTime.ToUniversalTime().ToString('yyyy-MM-ddTHH:mm:ssZ')
+                } else {
+                    $StartDate = (Get-Date $Request.Query.StartDate).ToString('yyyy-MM-ddTHH:mm:ssZ')
+                }
+                $FilterConditions.Add("Timestamp ge '$StartDate'")
+
+                if ($Request.Query.EndDate) {
+                    if ($Request.Query.EndDate -match '^\d+$') {
+                        $Request.Query.EndDate = [DateTimeOffset]::FromUnixTimeSeconds($Request.Query.EndDate).DateTime.ToUniversalTime().ToString('yyyy-MM-ddTHH:mm:ssZ')
+                    } else {
+                        $EndDate = (Get-Date $Request.Query.EndDate).ToString('yyyy-MM-ddTHH:mm:ssZ')
+                    }
+                    $FilterConditions.Add("Timestamp le '$EndDate'")
+                }
             }
         }
     }
@@ -59,7 +63,7 @@ function Invoke-ListAuditLogs {
     }
 
     $Body = @{
-        Results  = $AuditLogs
+        Results  = @($AuditLogs)
         Metadata = @{
             Count  = $AuditLogs.Count
             Filter = $Table.Filter ?? ''
