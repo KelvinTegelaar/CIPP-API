6a14681fa41d6fa9288ecbb8515773a4a983e0b6
KelvinTegelaar <Kelvin@limenetworks.nl>
2024-08-09 12:22:12 +0200

    update

--- Modules/CIPPCore/Public/Entrypoints/Invoke-PublicPhishingCheck.ps1
+++ Modules/CIPPCore/Public/Entrypoints/Invoke-PublicPhishingCheck.ps1
@@ -6,6 +6,8 @@ Function Invoke-PublicPhishingCheck {
         Entrypoint
     #>
     [CmdletBinding()]
+
+    #this has been switched to the external free service by cyberdrain at clone.cipp.app due to extreme numbers of executions if selfhosted.
     param($Request, $TriggerMetadata)
     if ($Request.body.Cloned) {
         Write-AlertMessage -message $Request.body.AlertMessage -sev 'Alert' -tenant $Request.body.TenantId
