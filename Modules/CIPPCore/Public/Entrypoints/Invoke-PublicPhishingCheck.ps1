f1f1a1f2e4c8cb452a42da3b21d2f33ba5b561d7
KelvinTegelaar <Kelvin@limenetworks.nl>
2024-08-09 12:03:38 +0200

    changes to phishing check

--- Modules/CIPPCore/Public/Entrypoints/Invoke-PublicPhishingCheck.ps1
+++ Modules/CIPPCore/Public/Entrypoints/Invoke-PublicPhishingCheck.ps1
@@ -8,12 +8,7 @@ Function Invoke-PublicPhishingCheck {
     [CmdletBinding()]
     param($Request, $TriggerMetadata)
     if ($Request.body.Cloned) {
-        $AlertMessage = If ($Request.body.headers.referer) {
-            "Potential Phishing page detected. Detected Information: Hosted at $($Request.headers.referer). Access by IP $($request.headers.'x-forwarded-for')"
-        } else {
-            "Potential Phishing page detected. Detected Information:  Access by IP $($request.headers.'x-forwarded-for')"
-        }
-        Write-AlertMessage -message $AlertMessage -sev 'Alert' -tenant $Request.body.TenantId
+        Write-AlertMessage -message $Request.body.AlertMessage -sev 'Alert' -tenant $Request.body.TenantId
     }
 
     # Associate values to output bindings by calling 'Push-OutputBinding'.
