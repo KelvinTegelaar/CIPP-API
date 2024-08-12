1956e6b0026fa687e556f9c4e128aed7c9b211f6
John Duprey <jwd@johnwduprey.com>
2024-08-12 13:40:26 -0400

    CIPP-API-Processor

--- Tools/Clear-DevEnvironment.ps1
+++
@@ -1,4 +0,0 @@
-$EnvironmentVariables = @('TenantId', 'ApplicationId', 'ApplicationSecret', 'RefreshToken', 'AzureWebJobsStorage', 'PartnerTenantAvailable', 'SetFromProfile')
-ForEach ($Key in $EnvironmentVariables) {
-    [Environment]::SetEnvironmentVariable($Key, $null)
-}
