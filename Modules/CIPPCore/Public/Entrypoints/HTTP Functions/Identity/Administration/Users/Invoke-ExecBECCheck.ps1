5d041bd56f4c9fae193f0d30a3a4cf056aaa7721
John Duprey <jwd@johnwduprey.com>
2024-08-28 15:02:30 -0400

    BEC tweaks

--- Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Identity/Administration/Users/Invoke-ExecBECCheck.ps1
+++ Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Identity/Administration/Users/Invoke-ExecBECCheck.ps1
@@ -1,5 +1,3 @@
-using namespace System.Net
-
 Function Invoke-ExecBECCheck {
     <#
     .FUNCTIONALITY
@@ -14,19 +12,39 @@ Function Invoke-ExecBECCheck {
         $Table = Get-CippTable -tablename 'cachebec'
         $Filter = "PartitionKey eq 'bec' and RowKey eq '$($request.query.GUID)'"
         $JSONOutput = Get-CIPPAzDataTableEntity @Table -Filter $Filter
-        if (!$JSONOutput) {
+        if (!$JSONOutput -or $JSONOutput.Status -eq 'Waiting') {
             @{ Waiting = $true }
         } else {
             $JSONOutput.Results
         }
     } else {
-        $OrchRequest = [PSCustomObject]@{
-            TenantFilter = $request.query.tenantfilter
-            UserID       = $request.query.userid
-            userName     = $request.query.userName
+        $Batch = @{
+            'FunctionName' = 'BECRun'
+            'UserID'       = $Request.Query.userid
+            'TenantFilter' = $Request.Query.tenantfilter
+            'userName'     = $Request.Query.userName
         }
-        $InstanceId = Start-NewOrchestration -FunctionName 'Durable_BECRun' -InputObject $OrchRequest
-        @{ GUID = $request.query.userid }
+
+        $Table = Get-CippTable -tablename 'cachebec'
+
+        $Entity = @{
+            UserId       = $Request.Query.userid
+            Results      = ''
+            RowKey       = $Request.Query.userid
+            Status       = 'Waiting'
+            PartitionKey = 'bec'
+        }
+        Add-CIPPAzDataTableEntity @Table -Entity $Entity -Force
+
+        $InputObject = [PSCustomObject]@{
+            OrchestratorName = 'BECRunOrchestrator'
+            Batch            = @($Batch)
+            SkipLog          = $true
+        }
+        #Write-Host ($InputObject | ConvertTo-Json)
+        $InstanceId = Start-NewOrchestration -FunctionName 'CIPPOrchestrator' -InputObject ($InputObject | ConvertTo-Json -Depth 5 -Compress)
+
+        @{ GUID = $Request.Query.userid }
     }
 
 
