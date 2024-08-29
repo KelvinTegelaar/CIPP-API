adb7e6e591e763d1a52c12896c15dc8a6128aef9
John Duprey <jwd@johnwduprey.com>
2024-08-29 11:05:48 -0400

    Move Application orchestrator functions
    - Move activity/http functions into module
    - Cleanup code

--- AddChocoApp_OrchestrationStarterTimer/run.ps1
+++ AddChocoApp_OrchestrationStarterTimer/run.ps1
@@ -1,19 +1,7 @@
 param($Timer)
 
 try {
-    if ($CurrentlyRunning) {
-        $Results = [pscustomobject]@{'Results' = 'Already running. Please wait for the current instance to finish' }
-        Write-LogMessage -API 'ChocoApps' -message 'Attempted to start upload but an instance was already running.' -sev Info
-    }
-    else {
-        $InstanceId = Start-NewOrchestration -FunctionName 'Applications_Orchestrator'
-        Write-Host "Started orchestration with ID = '$InstanceId'"
-        $Orchestrator = New-OrchestrationCheckStatusResponse -Request $Timer -InstanceId $InstanceId
-        Write-LogMessage -API 'ChocoApps' -message 'Started uploading applications to tenants' -sev Info
-        $Results = [pscustomobject]@{'Results' = 'Started running analysis' }
-    }
-    Write-Host ($Orchestrator | ConvertTo-Json)
-}
-catch {
+    Start-ApplicationOrchestrator
+} catch {
     Write-Host "AddChocoApp_OrchestratorStarterTimer Exception: $($_.Exception.Message)"
 }
