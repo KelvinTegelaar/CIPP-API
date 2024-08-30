2ed900664b0d55636e2d91704894036177bc5fef
John Duprey <jwd@johnwduprey.com>
2024-08-29 22:15:09 -0400

    Improve ListExoRequest

--- Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Email-Exchange/Invoke-ListExoRequest.ps1
+++ Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Email-Exchange/Invoke-ListExoRequest.ps1
@@ -1,64 +1,73 @@
 function Invoke-ListExoRequest {
     param($Request, $TriggerMetadata)
 
-    $AllowedVerbs = @(
-        'Get'
-        'Search'
-    )
+    try {
+        $AllowedVerbs = @(
+            'Get'
+            'Search'
+        )
 
-    $Cmdlet = $Request.Query.Cmdlet
-    $cmdParams = if ($Request.Body) { $Request.Body } else { [PSCustomObject]@{} }
-    $Verb = ($Cmdlet -split '-')[0]
+        Write-Information ($Request.Query | ConvertTo-Json)
+        $Cmdlet = $Request.Query.Cmdlet
+        $cmdParams = if ($Request.Body) { $Request.Body } else { [PSCustomObject]@{} }
+        $Verb = ($Cmdlet -split '-')[0]
 
-    $AllowedTenants = Test-CIPPAccess -Request $Request -TenantList
-    $TenantFilter = $Request.Query.TenantFilter
-    $Tenants = Get-Tenants -IncludeErrors
-    $Tenant = $Tenants | Where-Object { $_.defaultDomainName -eq $TenantFilter -or $_.customerId -eq $TenantFilter }
-    if ($Tenant.customerId -in $AllowedTenants) {
-        if ($AllowedVerbs -notcontains $Verb) {
-            $Body = [pscustomobject]@{
-                Results = "Invalid cmdlet: $Cmdlet"
+        $AllowedTenants = Test-CIPPAccess -Request $Request -TenantList
+        $TenantFilter = $Request.Query.TenantFilter
+        $Tenants = Get-Tenants -IncludeErrors
+        $Tenant = $Tenants | Where-Object { $_.defaultDomainName -eq $TenantFilter -or $_.customerId -eq $TenantFilter }
+        if ($Tenant.customerId -in $AllowedTenants -or $AllowedTenants -eq 'AllTenants') {
+            if ($AllowedVerbs -notcontains $Verb) {
+                $Body = [pscustomobject]@{
+                    Results = "Invalid cmdlet: $Cmdlet"
+                }
+                Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+                        StatusCode = [HttpStatusCode]::BadRequest
+                        Body       = $Body
+                    })
+                return
             }
-            Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-                    StatusCode = [HttpStatusCode]::BadRequest
-                    Body       = $Body
-                })
-            return
-        }
-        $ExoParams = @{
-            Cmdlet    = $Cmdlet
-            cmdParams = $cmdParams
-            tenantid  = $TenantFilter
-        }
+            $ExoParams = @{
+                Cmdlet    = $Cmdlet
+                cmdParams = $cmdParams
+                tenantid  = $TenantFilter
+            }
 
-        if ($Request.Query.Select) {
-            $ExoParams.Select = $Request.Query.Select
-        }
+            if ($Request.Query.Select) {
+                $ExoParams.Select = $Request.Query.Select
+            }
 
-        if ($Request.Query.UseSystemMailbox) {
-            $ExoParams.useSystemMailbox = $true
-        }
+            if ($Request.Query.UseSystemMailbox) {
+                $ExoParams.useSystemMailbox = $true
+            }
 
-        if ($Request.Query.Anchor) {
-            $ExoParams.Anchor = $Request.Query.Anchor
-        }
+            if ($Request.Query.Anchor) {
+                $ExoParams.Anchor = $Request.Query.Anchor
+            }
 
-        if ($Request.Query.Compliance) {
-            $ExoParams.Compliance = $true
-        }
+            if ($Request.Query.Compliance) {
+                $ExoParams.Compliance = $true
+            }
 
-        if ($Request.Query.AsApp) {
-            $ExoParams.AsApp = $true
-        }
+            if ($Request.Query.AsApp) {
+                $ExoParams.AsApp = $true
+            }
 
-        Write-Information ($ExoParams | ConvertTo-Json)
-        $Results = New-ExoRequest @ExoParams
-        $Body = [pscustomobject]@{
-            Results = $Results
+            Write-Information ($ExoParams | ConvertTo-Json)
+            $Results = New-ExoRequest @ExoParams
+            $Body = [pscustomobject]@{
+                Results = $Results
+            }
+        } else {
+            $Body = [pscustomobject]@{
+                Results = "Invalid tenant: $TenantFilter"
+            }
         }
-        Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-                StatusCode = [HttpStatusCode]::OK
-                Body       = ConvertTo-Json -InputObject $Body -Compress
-            })
+    } catch {
+        Write-Information "ExoRequest Error: $($_.Exception.Message)"
     }
+    Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+            StatusCode = [HttpStatusCode]::OK
+            Body       = ConvertTo-Json -InputObject $Body -Compress
+        })
 }
