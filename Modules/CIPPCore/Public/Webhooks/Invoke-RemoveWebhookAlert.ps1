8b80f94418c3f81fd1cbd628bde14b6913c6452a
RunningFreak <156828136+OwenIbarra@users.noreply.github.com>
2024-09-04 19:51:17 -0400

    Error handling and unused variable

--- Modules/CIPPCore/Public/Webhooks/Invoke-RemoveWebhookAlert.ps1
+++ Modules/CIPPCore/Public/Webhooks/Invoke-RemoveWebhookAlert.ps1
@@ -12,7 +12,6 @@ Function Invoke-RemoveWebhookAlert {
 
     $APIName = $TriggerMetadata.FunctionName
     Write-LogMessage -user $request.headers.'x-ms-client-principal' -API $APINAME -message 'Accessed this API' -Sev 'Debug'
-    $Table = get-cipptable -TableName 'SchedulerConfig'
 
     try {
         $WebhookTable = Get-CIPPTable -TableName SchedulerConfig
@@ -34,7 +33,7 @@ Function Invoke-RemoveWebhookAlert {
                     }
                     Remove-AzDataTableEntity @Table -Entity $CompleteObject -ErrorAction SilentlyContinue | Out-Null
                 } catch {
-                    #
+                    Write-LogMessage -user $Request.headers.'x-ms-client-principal' -API $APIName -message "Failed to remove webhook for AllTenants. $($_.Exception.Message)" -Sev 'Error'
                 }
             } else {
                 $Tenants = $Request.query.TenantFilter
@@ -52,7 +51,6 @@ Function Invoke-RemoveWebhookAlert {
         Write-LogMessage -user $request.headers.'x-ms-client-principal' -API $APINAME -message "Failed to remove webhook alert. $($_.Exception.Message)" -Sev 'Error'
         $body = [pscustomobject]@{'Results' = "Failed to remove webhook alert: $($_.Exception.Message)" }
     }
-
 
     # Associate values to output bindings by calling 'Push-OutputBinding'.
     Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
