d8b4ba042ff244285c6ca656adfc8e6e2fccf9d2
John Duprey <jwd@johnwduprey.com>
2024-09-10 15:16:45 -0400

    bugfix: get-cippazdatatable with -property filters

--- Modules/CippEntrypoints/CippEntrypoints.psm1
+++ Modules/CippEntrypoints/CippEntrypoints.psm1
@@ -242,7 +242,7 @@ function Receive-CIPPTimerTrigger {
         if ($FunctionStatus.OrchestratorId) {
             $FunctionName = $env:WEBSITE_SITE_NAME
             $InstancesTable = Get-CippTable -TableName ('{0}Instances' -f ($FunctionName -replace '-', ''))
-            $Instance = Get-CIPPAzDataTableEntity @InstancesTable -Filter "PartitionKey eq '$($FunctionStatus.OrchestratorId)'" -Property RuntimeStatus
+            $Instance = Get-CIPPAzDataTableEntity @InstancesTable -Filter "PartitionKey eq '$($FunctionStatus.OrchestratorId)'" -Property PartitionKey, RowKey, RuntimeStatus
             if ($Instance.RuntimeStatus -eq 'Running') {
                 Write-LogMessage -API 'TimerFunction' -message "$($Function.Command) - $($FunctionStatus.OrchestratorId) is still running" -sev Warn -LogData $FunctionStatus
                 Write-Warning "CIPP Timer: $($Function.Command) - $($FunctionStatus.OrchestratorId) is still running, skipping execution"
