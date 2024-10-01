3c1f85263421c8193bc692ece7d9e99f2ad22fef
John Duprey <jwd@johnwduprey.com>
2024-09-30 22:53:57 -0400

    audit logs
    tweak timing
    
    Update Start-AuditLogOrchestrator.ps1
    
    Create Get-CippLastAuditLogSearch.ps1
    
    Add sharepoint admin url endpoint
    
    Fix issues with graph requests missing data
    
    Only use -ComplexFilter when $filter is present
    
    Update Push-AuditLogTenant.ps1
    
    Update Push-AuditLogTenant.ps1
    
    Update Start-AuditLogOrchestrator.ps1
    
    Fix api role permissions
    
    audit logs fixes
    
    Update Push-AuditLogTenant.ps1
    
    audit logs
    
    Update Push-AuditLogTenant.ps1
    
    Update Push-AuditLogTenant.ps1
    
    audit log tweaks
    
    audit logs
    
    Update Push-AuditLogTenant.ps1
    
    try / catch
    
    New audit logs

--- Modules/CIPPCore/Public/Entrypoints/Orchestrator Functions/Start-AuditLogOrchestrator.ps1
+++ Modules/CIPPCore/Public/Entrypoints/Orchestrator Functions/Start-AuditLogOrchestrator.ps1
@@ -6,29 +6,48 @@ function Start-AuditLogOrchestrator {
     [CmdletBinding(SupportsShouldProcess = $true)]
     param()
     try {
-        $webhookTable = Get-CIPPTable -tablename webhookTable
-        $Webhooks = Get-CIPPAzDataTableEntity @webhookTable -Filter "Version eq '3'" | Where-Object { $_.Resource -match '^Audit' -and $_.Status -ne 'Disabled' }
-        if (($Webhooks | Measure-Object).Count -eq 0) {
-            Write-Information 'No webhook subscriptions found. Exiting.'
-            return
+
+        $AuditLogSearchesTable = Get-CIPPTable -TableName 'AuditLogSearches'
+        $AuditLogSearches = Get-CIPPAzDataTableEntity @AuditLogSearchesTable -Filter "CippStatus eq 'Pending'"
+
+        # Round time down to nearest minute
+        $Now = Get-Date
+        $DefaultStartTime = (Get-Date).AddSeconds(-$Now.Seconds).AddHours(-1)
+        $EndTime = $Now.AddMinutes(-30).AddSeconds(-$Now.Seconds)
+
+        if (($AuditLogSearches | Measure-Object).Count -eq 0) {
+            Write-Information 'No audit log searches available'
+        } else {
+            $TenantList = Get-Tenants -IncludeErrors
+            $Queue = New-CippQueueEntry -Name 'Audit Log Collection' -Reference 'AuditLogCollection' -TotalTasks ($AuditLogSearches).Count
+
+            $Batch = $AuditLogSearches | Sort-Object -Property Tenant -Unique | Select-Object @{Name = 'TenantFilter'; Expression = { $_.Tenant } }, @{Name = 'QueueId'; Expression = { $Queue.RowKey } }, @{Name = 'FunctionName'; Expression = { 'AuditLogTenant' } }
+
+            $InputObject = [PSCustomObject]@{
+                OrchestratorName = 'AuditLogs'
+                Batch            = @($Batch)
+                SkipLog          = $true
+            }
+            if ($PSCmdlet.ShouldProcess('Start-AuditLogOrchestrator', 'Starting Audit Log Polling')) {
+                Start-NewOrchestration -FunctionName 'CIPPOrchestrator' -InputObject ($InputObject | ConvertTo-Json -Depth 5 -Compress)
+            }
         }
 
-        $StartTime = (Get-Date).AddMinutes(-30)
-        $EndTime = Get-Date
-
-        $Queue = New-CippQueueEntry -Name 'Audit Log Collection' -Reference 'AuditLogCollection' -TotalTasks ($Webhooks | Sort-Object -Property PartitionKey -Unique | Measure-Object).Count
-
-        $Batch = $Webhooks | Sort-Object -Property PartitionKey -Unique | Select-Object @{Name = 'TenantFilter'; Expression = { $_.PartitionKey } }, @{Name = 'QueueId'; Expression = { $Queue.RowKey } }, @{Name = 'FunctionName'; Expression = { 'AuditLogTenant' } }, @{Name = 'StartTime'; Expression = { $StartTime } }, @{Name = 'EndTime'; Expression = { $EndTime } }
-        $InputObject = [PSCustomObject]@{
-            OrchestratorName = 'AuditLogs'
-            Batch            = @($Batch)
-            SkipLog          = $true
-        }
-        if ($PSCmdlet.ShouldProcess('Start-AuditLogPolling', 'Starting Audit Log Polling')) {
-            Start-NewOrchestration -FunctionName 'CIPPOrchestrator' -InputObject ($InputObject | ConvertTo-Json -Depth 5 -Compress)
+        Write-Information 'Audit Logs: Creating new searches'
+        foreach ($Tenant in $TenantList) {
+            try {
+                $LastSearch = Get-CippLastAuditLogSearch -TenantFilter $Tenant.defaultDomainName
+                if ($LastSearch -and $StartTime -gt $DefaultStartTime) {
+                    $StartTime = $LastSearch.EndTime
+                } else {
+                    $StartTime = $DefaultStartTime
+                }
+                $null = New-CippAuditLogSearch -TenantFilter $Tenant.defaultDomainName -StartTime $StartTime -EndTime $EndTime -ProcessLogs
+            } catch {
+            }
         }
     } catch {
-        Write-LogMessage -API 'Webhooks' -message 'Error processing webhooks' -sev Error -LogData (Get-CippException -Exception $_)
-        Write-Information ( 'Webhook error {0} line {1} - {2}' -f $_.InvocationInfo.ScriptName, $_.InvocationInfo.ScriptLineNumber, $_.Exception.Message)
+        Write-LogMessage -API 'Audit Logs' -message 'Error processing audit logs' -sev Error -LogData (Get-CippException -Exception $_)
+        Write-Information ( 'Audit logs error {0} line {1} - {2}' -f $_.InvocationInfo.ScriptName, $_.InvocationInfo.ScriptLineNumber, $_.Exception.Message)
     }
 }
