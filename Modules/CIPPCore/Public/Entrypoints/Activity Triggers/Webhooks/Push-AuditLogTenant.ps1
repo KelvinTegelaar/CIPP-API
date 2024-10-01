c4b1f35e21cfe9c65d1ab31bf9ec8d73ed83791f
John Duprey <jwd@johnwduprey.com>
2024-09-30 23:09:36 -0400

    try / catch

--- Modules/CIPPCore/Public/Entrypoints/Activity Triggers/Webhooks/Push-AuditLogTenant.ps1
+++ Modules/CIPPCore/Public/Entrypoints/Activity Triggers/Webhooks/Push-AuditLogTenant.ps1
@@ -14,37 +14,43 @@ function Push-AuditLogTenant {
 
     $Configuration = $ConfigEntries | Where-Object { ($_.Tenants -match $TenantFilter -or $_.Tenants -match 'AllTenants') }
     if ($Configuration) {
-        $LogSearches = Get-CippAuditLogSearches -TenantFilter $Item.defaultDomainName -ReadyToProcess
-        foreach ($Search in $LogSearches) {
-            $SearchEntity = Get-CIPPAzDataTableEntity @LogSearchesTable -Filter "PartitionKey eq '$($Item.defaultDomainName)' and RowKey eq '$($Search.id)'"
-            $SearchEntity.Status = 'Processing'
-            Add-CIPPAzDataTableEntity @LogSearchesTable -Entity $SearchEntity -Force
-            try {
-                # Test the audit log rules against the search results
-                $AuditLogTest = Test-CIPPAuditLogRules -TenantFilter $Item.defaultDomainName -SearchId $Search.id
+        try {
+            Write-Information "Audit Logs: Processing $($Item.defaultDomainName)"
+            $LogSearches = Get-CippAuditLogSearches -TenantFilter $Item.defaultDomainName -ReadyToProcess
+            Write-Information ('Audit Logs: Found {0} searches, begin processing' -f $LogSearches.Count)
+            foreach ($Search in $LogSearches) {
+                $SearchEntity = Get-CIPPAzDataTableEntity @LogSearchesTable -Filter "PartitionKey eq '$($Item.defaultDomainName)' and RowKey eq '$($Search.id)'"
+                $SearchEntity.Status = 'Processing'
+                Add-CIPPAzDataTableEntity @LogSearchesTable -Entity $SearchEntity -Force
+                try {
+                    # Test the audit log rules against the search results
+                    $AuditLogTest = Test-CIPPAuditLogRules -TenantFilter $Item.defaultDomainName -SearchId $Search.id
 
-                $SearchEntity.CippStatus = 'Completed'
-                $SearchEntity | Add-Member -MemberType NoteProperty -Name MatchedRules -Value [string](ConvertTo-Json -Compress -Depth 10 -InputObject $AuditLogTest.MatchedRules)
-                $SearchEntity | Add-Member -MemberType NoteProperty -Name MatchedLogs -Value $AuditLogTest.MatchedLogs
-                $SearchEntity | Add-Member -MemberType NoteProperty -Name TotalLogs -Value $AuditLogTest.TotalLogs
-            } catch {
-                $SearchEntity.CippStatus = 'Failed'
-                $SearchEntity | Add-Member -MemberType NoteProperty -Name Error -Value $_.InvocationInfo.PositionMessage
-            }
-            Add-CIPPAzDataTableEntity @LogSearchesTable -Entity $SearchEntity -Force
-            $DataToProcess = ($AuditLogTest).DataToProcess
-            Write-Information "Audit Logs: Data to process found: $($DataToProcess.count) items"
-            if ($DataToProcess) {
-                foreach ($AuditLog in $DataToProcess) {
-                    Write-Information "Processing $($AuditLog.operation)"
-                    $Webhook = @{
-                        Data         = $AuditLog
-                        CIPPURL      = [string]$CIPPURL
-                        TenantFilter = $Item.defaultDomainName
+                    $SearchEntity.CippStatus = 'Completed'
+                    $SearchEntity | Add-Member -MemberType NoteProperty -Name MatchedRules -Value [string](ConvertTo-Json -Compress -Depth 10 -InputObject $AuditLogTest.MatchedRules)
+                    $SearchEntity | Add-Member -MemberType NoteProperty -Name MatchedLogs -Value $AuditLogTest.MatchedLogs
+                    $SearchEntity | Add-Member -MemberType NoteProperty -Name TotalLogs -Value $AuditLogTest.TotalLogs
+                } catch {
+                    $SearchEntity.CippStatus = 'Failed'
+                    $SearchEntity | Add-Member -MemberType NoteProperty -Name Error -Value $_.InvocationInfo.PositionMessage
+                }
+                Add-CIPPAzDataTableEntity @LogSearchesTable -Entity $SearchEntity -Force
+                $DataToProcess = ($AuditLogTest).DataToProcess
+                Write-Information "Audit Logs: Data to process found: $($DataToProcess.count) items"
+                if ($DataToProcess) {
+                    foreach ($AuditLog in $DataToProcess) {
+                        Write-Information "Processing $($AuditLog.operation)"
+                        $Webhook = @{
+                            Data         = $AuditLog
+                            CIPPURL      = [string]$CIPPURL
+                            TenantFilter = $Item.defaultDomainName
+                        }
+                        Invoke-CippWebhookProcessing @Webhook
                     }
-                    Invoke-CippWebhookProcessing @Webhook
                 }
             }
+        } catch {
+            Write-Information ( 'Audit Logs: Error {0} line {1} - {2}' -f $_.InvocationInfo.ScriptName, $_.InvocationInfo.ScriptLineNumber, $_.Exception.Message)
         }
     }
 }
