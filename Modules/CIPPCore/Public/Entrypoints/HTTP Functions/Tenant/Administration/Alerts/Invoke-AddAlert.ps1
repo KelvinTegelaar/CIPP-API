a21f71c15b08695128ffe2f25c8f6990616daa3e
KelvinTegelaar <Kelvin@limenetworks.nl>
2024-10-26 12:33:27 +0200

    changes

--- Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Tenant/Administration/Alerts/Invoke-AddAlert.ps1
+++ Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Tenant/Administration/Alerts/Invoke-AddAlert.ps1
@@ -11,29 +11,7 @@ Function Invoke-AddAlert {
     param($Request, $TriggerMetadata)
     $APIName = $TriggerMetadata.FunctionName
     Write-LogMessage -user $request.headers.'x-ms-client-principal' -API $APINAME -message 'Accessed this API' -Sev 'Debug'
-    $URL = ($request.headers.'x-ms-original-url').split('/api') | Select-Object -First 1
     $Tenants = $request.body.tenantFilter
-    $Table = get-cipptable -TableName 'SchedulerConfig'
-    $Results = foreach ($Tenant in $Tenants) {
-        try {
-            Write-Host "Working on $($Tenant.value) - $($Tenant.fullValue.displayName)"
-            $CompleteObject = @{
-                tenant       = [string]$($Tenant.value)
-                tenantid     = [string]$($Tenant.value)
-                webhookType  = [string]$request.body.logbook.value
-                type         = 'webhookcreation'
-                RowKey       = "$($Tenant.value)-$($request.body.logbook.value)"
-                PartitionKey = 'webhookcreation'
-                Configured   = $false
-                CIPPURL      = [string]$URL
-            }
-            Add-CIPPAzDataTableEntity @Table -Entity $CompleteObject -Force
-            Write-LogMessage -user $request.headers.'x-ms-client-principal' -API $APINAME -tenant $tenant.fullValue.defaultDomainName -message "Successfully added Audit Log Webhook for $($Tenant.fullValue.displayName) to queue." -Sev 'Info'
-        } catch {
-            Write-LogMessage -user $request.headers.'x-ms-client-principal' -API $APINAME -tenant $tenant.fullValue.defaultDomainName -message "Failed to add Audit Log Webhook for $($Tenant.fullValue.displayName) to queue" -Sev 'Error'
-            "Failed to add Alert for for $($Tenant) to queue $($_.Exception.message)"
-        }
-    }
     $Conditions = $request.body.conditions | ConvertTo-Json -Compress -Depth 10 | Out-String
     $TenantsJson = $Tenants | ConvertTo-Json -Compress -Depth 10 | Out-String
     $Actions = $request.body.actions | ConvertTo-Json -Compress -Depth 10 | Out-String
