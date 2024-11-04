dc3658f232d2b656ebe7efa605c5dd17a8b286b9
Kristian Kjærgård <kris6673@hotmail.com>
2024-11-04 19:31:40 +0100

    Refactor block signin and alias addition logic in Invoke-AddSharedMailbox function

--- Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Email-Exchange/Invoke-AddSharedMailbox.ps1
+++ Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Email-Exchange/Invoke-AddSharedMailbox.ps1
@@ -43,35 +43,37 @@ Function Invoke-AddSharedMailbox {
             $Body = $Results.add("Failed to block sign-in for shared mailbox $Email. Error: $ErrorMessage")
         }
 
+        # Add aliases to the mailbox if any are provided
+        if ($Aliases) {
+            try {
+                Start-Sleep 3 # Sleep since there is apparently a race condition with the mailbox creation if we don't delay for a lil bit
+                $AliasBodyToShip = [pscustomobject] @{
+                    'Identity'       = $AddSharedRequest.Guid
+                    'EmailAddresses' = @{'@odata.type' = '#Exchange.GenericHashTable'; Add = $Aliases }
+                }
+                $null = New-ExoRequest -tenantid $MailboxObject.tenantid -cmdlet 'Set-Mailbox' -cmdparams $AliasBodyToShip -UseSystemMailbox $true
+                Write-LogMessage -user $User -API $APINAME -tenant $($MailboxObject.tenantid) -message "Added aliases to $Email : $($Aliases -join ',')" -Sev 'Info'
+                $Body = $results.add("Added Aliases to $Email : $($Aliases -join ',')")
+
+            } catch {
+                $ErrorMessage = Get-NormalizedError -Message $_.Exception.Message
+                Write-LogMessage -user $User -API $APINAME -tenant $($MailboxObject.tenantid) -message "Failed to add aliases to $Email : $ErrorMessage" -Sev 'Error'
+                $Body = $results.add("ERROR: Failed to add aliases to $Email : $ErrorMessage")
+            }
+        }
+        $StatusCode = [HttpStatusCode]::OK
     } catch {
         $ErrorMessage = Get-NormalizedError -Message $_.Exception.Message
         Write-LogMessage -user $User -API $APINAME -tenant $($MailboxObject.tenantid) -message "Failed to create shared mailbox. Error: $ErrorMessage" -Sev 'Error'
         $Body = $Results.add("Failed to create Shared Mailbox. $ErrorMessage")
+        $StatusCode = [HttpStatusCode]::Forbidden
     }
 
-    # Add aliases to the mailbox if any are provided
-    if ($Aliases) {
-        try {
-            Start-Sleep 3 # Sleep since there is apparently a race condition with the mailbox creation if we don't delay for a lil bit
-            $AliasBodyToShip = [pscustomobject] @{
-                'Identity'       = $AddSharedRequest.Guid
-                'EmailAddresses' = @{'@odata.type' = '#Exchange.GenericHashTable'; Add = $Aliases }
-            }
-            $null = New-ExoRequest -tenantid $MailboxObject.tenantid -cmdlet 'Set-Mailbox' -cmdparams $AliasBodyToShip -UseSystemMailbox $true
-            Write-LogMessage -user $User -API $APINAME -tenant $($MailboxObject.tenantid) -message "Added aliases to $Email : $($Aliases -join ',')" -Sev 'Info'
-            $Body = $results.add("Added Aliases to $Email : $($Aliases -join ',')")
-
-        } catch {
-            $ErrorMessage = Get-NormalizedError -Message $_.Exception.Message
-            Write-LogMessage -user $User -API $APINAME -tenant $($MailboxObject.tenantid) -message "Failed to add aliases to $Email : $ErrorMessage" -Sev 'Error'
-            $Body = $results.add("ERROR: Failed to add aliases to $Email : $ErrorMessage")
-        }
-    }
 
     $Body = [pscustomobject] @{ 'Results' = @($results) }
     # Associate values to output bindings by calling 'Push-OutputBinding'.
     Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-            StatusCode = [HttpStatusCode]::OK
+            StatusCode = $StatusCode
             Body       = $Body
         })
 
