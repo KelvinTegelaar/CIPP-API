d44ab8975a9aedd80bf6a961908d408679f398ce
John Duprey <jwd@johnwduprey.com>
2024-11-15 16:27:52 -0500

    switch to bulk request to get image data properly

--- Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Identity/Administration/Users/Invoke-ListUserPhoto.ps1
+++ Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Identity/Administration/Users/Invoke-ListUserPhoto.ps1
@@ -22,15 +22,28 @@ Function Invoke-ListUserPhoto {
     $userId = $Request.Query.UserID
 
 
-    $URI = "https://graph.microsoft.com/v1.0/users/$userId/photos/240x240/`$value"
+    $URI = "/users/$userId/photo/`$value"
     Write-Host $URI
-    $graphRequest = New-GraphGetRequest -uri $URI -tenantid $tenantFilter
+    #$ImageData = New-GraphGetRequest -uri $URI -tenantid $tenantFilter -noPagination $true
+    #Write-Host $ImageData
 
+    $Requests = @(
+        @{
+            id     = 'photo'
+            url    = $URI
+            method = 'GET'
+        }
+    )
+
+    $ImageData = New-GraphBulkRequest -Requests $Requests -tenantid $tenantFilter
+    #convert body from base64 to byte array
+    $Body = [Convert]::FromBase64String($ImageData.body)
 
     # Associate values to output bindings by calling 'Push-OutputBinding'.
     Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-            StatusCode = [HttpStatusCode]::OK
-            Body       = @($graphRequest)
+            StatusCode  = [HttpStatusCode]::OK
+            ContentType = $ImageData.headers.'Content-Type'
+            Body        = $Body
         })
 
 }
