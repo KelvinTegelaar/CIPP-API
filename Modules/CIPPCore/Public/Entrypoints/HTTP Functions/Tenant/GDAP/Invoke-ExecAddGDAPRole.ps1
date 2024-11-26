b9d06e79aaa0b5bf2dc2604aa35c2198558631fb
John Duprey <jwd@johnwduprey.com>
2024-11-25 21:52:47 -0500

    Refactor GDAP Role group
    bulk requests
    correct bug when suffix with spaces is used

--- Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Tenant/GDAP/Invoke-ExecAddGDAPRole.ps1
+++ Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Tenant/GDAP/Invoke-ExecAddGDAPRole.ps1
@@ -13,46 +13,72 @@ Function Invoke-ExecAddGDAPRole {
     $APIName = $TriggerMetadata.FunctionName
     Write-LogMessage -user $request.headers.'x-ms-client-principal' -API $APINAME -message 'Accessed this API' -Sev 'Debug'
     $Groups = $Request.body.gdapRoles
+
     $CustomSuffix = $Request.body.customSuffix
     $Table = Get-CIPPTable -TableName 'GDAPRoles'
 
     $Results = [System.Collections.Generic.List[string]]::new()
+    $Requests = [System.Collections.Generic.List[object]]::new()
     $ExistingGroups = New-GraphGetRequest -NoAuthCheck $True -uri 'https://graph.microsoft.com/beta/groups' -tenantid $env:TenantID -AsApp $true
 
-    $RoleMappings = foreach ($group in $Groups) {
+    $RoleMappings = foreach ($Group in $Groups) {
+        $RoleName = $Group.label ?? $Group.Name
+        $Value = $Group.value ?? $Group.ObjectId
+
         if ($CustomSuffix) {
-            $GroupName = "M365 GDAP $($Group.Name) - $CustomSuffix"
-            $MailNickname = "M365GDAP$(($Group.Name).replace(' ',''))$($CustomSuffix)"
+            $GroupName = "M365 GDAP $($RoleName) - $CustomSuffix"
+            $MailNickname = "M365GDAP$(($RoleName).replace(' ',''))$($CustomSuffix.replace(' ',''))"
         } else {
-            $GroupName = "M365 GDAP $($Group.Name)"
-            $MailNickname = "M365GDAP$(($Group.Name).replace(' ',''))"
+            $GroupName = "M365 GDAP $($RoleName)"
+            $MailNickname = "M365GDAP$(($RoleName).replace(' ',''))"
         }
-        try {
-            if ($GroupName -in $ExistingGroups.displayName) {
-                @{
-                    PartitionKey     = 'Roles'
-                    RowKey           = ($ExistingGroups | Where-Object -Property displayName -EQ $GroupName).id
-                    RoleName         = $Group.Name
-                    GroupName        = $GroupName
-                    GroupId          = ($ExistingGroups | Where-Object -Property displayName -EQ $GroupName).id
-                    roleDefinitionId = $group.ObjectId
-                }
-                $Results.Add("M365 GDAP $($Group.Name) already exists")
-            } else {
-                $BodyToship = [pscustomobject] @{'displayName' = $GroupName; 'description' = "This group is used to manage M365 partner tenants at the $($group.name) level."; securityEnabled = $true; mailEnabled = $false; mailNickname = $MailNickname } | ConvertTo-Json
-                $GraphRequest = New-GraphPostRequest -NoAuthCheck $True -uri 'https://graph.microsoft.com/beta/groups' -tenantid $env:TenantID -type POST -body $BodyToship -AsApp $true
-                @{
-                    PartitionKey     = 'Roles'
-                    RowKey           = $GraphRequest.Id
-                    RoleName         = $Group.Name
-                    GroupName        = $GroupName
-                    GroupId          = $GraphRequest.Id
-                    roleDefinitionId = $group.ObjectId
-                }
-                $Results.Add("$GroupName added successfully")
+
+        if ($GroupName -in $ExistingGroups.displayName) {
+            @{
+                PartitionKey     = 'Roles'
+                RowKey           = ($ExistingGroups | Where-Object -Property displayName -EQ $GroupName).id
+                RoleName         = $RoleName
+                GroupName        = $GroupName
+                GroupId          = ($ExistingGroups | Where-Object -Property displayName -EQ $GroupName).id
+                roleDefinitionId = $Value
             }
-        } catch {
-            $Results.Add("Could not create GDAP group $($GroupName): $($_.Exception.Message)")
+            $Results.Add("$GroupName already exists")
+        } else {
+            $Requests.Add(@{
+                    id      = $Value
+                    url     = '/groups'
+                    method  = 'POST'
+                    headers = @{
+                        'Content-Type' = 'application/json'
+                    }
+                    body    = @{
+                        displayName     = $GroupName
+                        description     = "This group is used to manage M365 partner tenants at the $($RoleName) level."
+                        securityEnabled = $true
+                        mailEnabled     = $false
+                        mailNickname    = $MailNickname
+                    }
+                })
+        }
+    }
+
+    if ($Requests) {
+        $ReturnedData = New-GraphBulkRequest -Requests $Requests -tenantid $env:TenantID -NoAuthCheck $True -asapp $true
+        foreach ($Return in $ReturnedData) {
+            if ($Return.body.error) {
+                $Results.Add("Could not create GDAP group: $($Return.body.error.message)")
+            } else {
+                $GroupName = $Return.body.displayName
+                @{
+                    PartitionKey     = 'Roles'
+                    RowKey           = $Return.body.id
+                    RoleName         = $Return.body.displayName -replace '^M365 GDAP ', '' -replace " - $CustomSuffix$", ''
+                    GroupName        = $Return.body.displayName
+                    GroupId          = $Return.body.id
+                    roleDefinitionId = $group.ObjectId
+                }
+                $Results.Add("Created $($GroupName)")
+            }
         }
     }
 
