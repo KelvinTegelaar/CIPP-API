19c53cd42b6ccaa8b2372e887f572addbf0547a2
John Duprey <jwd@johnwduprey.com>
2024-12-16 10:39:09 -0500

    geo ip caching

--- Modules/CIPPCore/Public/Get-CIPPGeoIPLocation.ps1
+++ Modules/CIPPCore/Public/Get-CIPPGeoIPLocation.ps1
@@ -3,7 +3,21 @@ function Get-CIPPGeoIPLocation {
     param (
         [string]$IP
     )
+
+    $CacheGeoIPTable = Get-CippTable -tablename 'cachegeoip'
+    $30DaysAgo = (Get-Date).AddDays(-30).ToString('yyyy-MM-ddTHH:mm:ssZ')
+    $Filter = "RowKey eq '$IP' and Timestamp ge datetime'$30DaysAgo'"
+    $GeoIP = Get-CippAzDataTableEntity @CacheGeoIPTable -Filter $Filter
+    if ($GeoIP) {
+        return ($GeoIP.Data | ConvertFrom-Json)
+    }
     $location = Invoke-RestMethod "https://geoipdb.azurewebsites.net/api/GetIPInfo?IP=$IP"
     if ($location.status -eq 'FAIL') { throw "Could not get location for $IP" }
+    $CacheGeo = @{
+        PartitionKey = 'IP'
+        RowKey       = $IP
+        Data         = [string]($location | ConvertTo-Json -Compress)
+    }
+    Add-AzDataTableEntity @CacheGeoIPTable -Entity $CacheGeo -Force
     return $location
 }
