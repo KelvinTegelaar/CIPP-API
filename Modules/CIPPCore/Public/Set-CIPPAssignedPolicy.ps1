22d419ffda93ad8ecfc098285d6354d19632370e
Esco <OfficialEsco@users.noreply.github.com>
2025-02-19 14:23:15 +0100

    feat: InTune Template exclude

--- Modules/CIPPCore/Public/Set-CIPPAssignedPolicy.ps1
+++ Modules/CIPPCore/Public/Set-CIPPAssignedPolicy.ps1
@@ -2,6 +2,7 @@ function Set-CIPPAssignedPolicy {
     [CmdletBinding(SupportsShouldProcess = $true)]
     param(
         $GroupName,
+        $excludeGroup,
         $PolicyId,
         $Type,
         $TenantFilter,
@@ -11,25 +12,23 @@ function Set-CIPPAssignedPolicy {
     )
     if (!$PlatformType) { $PlatformType = 'deviceManagement' }
     try {
-        $assignmentsObject = switch ($GroupName) {
+        $assignmentsObject = @()
+
+        $assignmentsObject += switch ($GroupName) {
             'allLicensedUsers' {
-                @(
-                    @{
-                        target = @{
-                            '@odata.type' = '#microsoft.graph.allLicensedUsersAssignmentTarget'
-                        }
+                @{
+                    target = @{
+                        '@odata.type' = '#microsoft.graph.allLicensedUsersAssignmentTarget'
                     }
-                )
+                }
                 break
             }
             'AllDevices' {
-                @(
-                    @{
-                        target = @{
-                            '@odata.type' = '#microsoft.graph.allDevicesAssignmentTarget'
-                        }
+                @{
+                    target = @{
+                        '@odata.type' = '#microsoft.graph.allDevicesAssignmentTarget'
                     }
-                )
+                }
                 break
             }
             'AllDevicesAndUsers' {
@@ -45,6 +44,7 @@ function Set-CIPPAssignedPolicy {
                         }
                     }
                 )
+                break
             }
             default {
                 Write-Host "We're supposed to assign a custom group. The group is $GroupName"
@@ -53,22 +53,44 @@ function Set-CIPPAssignedPolicy {
                     $Group = $_
                     foreach ($SingleName in $GroupNames) {
                         if ($_.displayName -like $SingleName) {
-                            $group.id
+                            $_.id
                         }
                     }
                 }
-                foreach ($Group in $GroupIds) {
+                $GroupIds | ForEach-Object {
                     @{
                         target = @{
                             '@odata.type' = '#microsoft.graph.groupAssignmentTarget'
-                            groupId       = $Group
+                            groupId       = $_
                         }
                     }
                 }
             }
         }
+
+        # Handle excludeGroup
+        if ($excludeGroup) {
+            $ExcludeGroupNames = $excludeGroup.Split(',')
+            $ExcludeGroupIds = New-GraphGetRequest -uri 'https://graph.microsoft.com/beta/groups?$select=id,displayName&$top=999' -tenantid $TenantFilter | ForEach-Object {
+                $Group = $_
+                foreach ($SingleName in $ExcludeGroupNames) {
+                    if ($_.displayName -like $SingleName) {
+                        $_.id
+                    }
+                }
+            }
+            $ExcludeGroupIds | ForEach-Object {
+                $assignmentsObject += @{
+                    target = @{
+                        '@odata.type' = '#microsoft.graph.exclusionGroupAssignmentTarget'
+                        groupId       = $_
+                    }
+                }
+            }
+        }
+
         $assignmentsObject = [PSCustomObject]@{
-            assignments = @($assignmentsObject)
+            assignments = $assignmentsObject
         }
 
         $AssignJSON = ($assignmentsObject | ConvertTo-Json -Depth 10 -Compress)
@@ -76,7 +98,7 @@ function Set-CIPPAssignedPolicy {
         if ($PSCmdlet.ShouldProcess($GroupName, "Assigning policy $PolicyId")) {
             Write-Host "https://graph.microsoft.com/beta/$($PlatformType)/$Type('$($PolicyId)')/assign"
             $null = New-GraphPOSTRequest -uri "https://graph.microsoft.com/beta/$($PlatformType)/$Type('$($PolicyId)')/assign" -tenantid $tenantFilter -type POST -body $AssignJSON
-            Write-LogMessage -headers $Headers -API $APIName -message "Assigned $GroupName to Policy $PolicyId" -Sev 'Info' -tenant $TenantFilter
+            Write-LogMessage -headers $Headers -API $APIName -message "Assigned $GroupName and excluded $excludeGroup to Policy $PolicyId" -Sev 'Info' -tenant $TenantFilter
         }
     } catch {
         $ErrorMessage = Get-NormalizedError -Message $_.Exception.Message
