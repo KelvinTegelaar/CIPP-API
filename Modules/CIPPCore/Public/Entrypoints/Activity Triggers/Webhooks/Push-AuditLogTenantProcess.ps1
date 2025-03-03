bc81c51961df69873c339e60ed4a260b618c1426
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2025-03-03 15:21:26 +0100

    added clean exit

--- Modules/CIPPCore/Public/Entrypoints/Activity Triggers/Webhooks/Push-AuditLogTenantProcess.ps1
+++ Modules/CIPPCore/Public/Entrypoints/Activity Triggers/Webhooks/Push-AuditLogTenantProcess.ps1
@@ -20,8 +20,10 @@ function Push-AuditLogTenantProcess {
         if ($Rows.Count -gt 0) {
             Write-Information "Retrieved $($Rows.Count) rows from cache for processing"
             Test-CIPPAuditLogRules -TenantFilter $TenantFilter -Rows $Rows
+            exit 0
         } else {
             Write-Information 'No rows found in cache for the provided row IDs'
+            exit 0
         }
     } catch {
         Write-Information ('Push-AuditLogTenant: Error {0} line {1} - {2}' -f $_.InvocationInfo.ScriptName, $_.InvocationInfo.ScriptLineNumber, $_.Exception.Message)
