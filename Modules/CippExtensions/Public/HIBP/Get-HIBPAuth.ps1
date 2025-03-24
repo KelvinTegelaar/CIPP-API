baf275c08760b2c35a3822f418f207c2091b7cb4
John Duprey <john@cyberdrain.com>
2025-03-24 11:22:47 -0400

    hosted tweaks

--- Modules/CippExtensions/Public/HIBP/Get-HIBPAuth.ps1
+++ Modules/CippExtensions/Public/HIBP/Get-HIBPAuth.ps1
@@ -19,8 +19,11 @@ function Get-HIBPAuth {
                 $Secret = $null
             }
 
-            if ([string]::IsNullOrEmpty($Secret) -and $ENV:WEBSITE_OWNER_NAME -like '3e625d35-bf18-4e55*' -or $ENV:WEBSITE_OWNER_NAME -like '61e84181-ff2a-4ba3*') {
+            if ([string]::IsNullOrEmpty($Secret) -and $ENV:CIPP_HOSTED -eq 'true') {
                 $VaultName = 'hibp-kv'
+                if ($SubscriptionId -ne $ENV:CIPP_HOSTED_KV_SUB -and $ENV:CIPP_HOSTED_KV_SUB -match '^[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}$') {
+                    $null = Set-AzContext -SubscriptionId $ENV:CIPP_HOSTED_KV_SUB
+                }
                 $Secret = Get-AzKeyVaultSecret -VaultName $VaultName -Name 'HIBP' -AsPlainText
             }
         }
