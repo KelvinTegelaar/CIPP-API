c464bb1f7e968ac3f4ac41814e61c3496022c020
John Duprey <john@cyberdrain.com>
2025-03-25 00:07:58 -0400

    switch to app permissions

--- Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Identity/Administration/Users/Invoke-ExecSendPush.ps1
+++ Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Identity/Administration/Users/Invoke-ExecSendPush.ps1
@@ -1,6 +1,6 @@
 using namespace System.Net
 
-Function Invoke-ExecSendPush {
+function Invoke-ExecSendPush {
     <#
     .FUNCTIONALITY
         Entrypoint
@@ -33,7 +33,7 @@ Function Invoke-ExecSendPush {
                 Start-Sleep 1
                 $ClientToken = Get-ClientAccess -uri $uri -body $body -count $count
             } else {
-                Throw "Could not get Client Token: $_"
+                throw "Could not get Client Token: $_"
             }
         }
         return $ClientToken
@@ -41,7 +41,7 @@ Function Invoke-ExecSendPush {
 
 
     # Get all service principals
-    $SPResult = New-GraphGetRequest -uri "https://graph.microsoft.com/beta/servicePrincipals?`$top=999&`$select=id,appId" -tenantid $TenantFilter
+    $SPResult = New-GraphGetRequest -uri "https://graph.microsoft.com/beta/servicePrincipals?`$top=999&`$select=id,appId" -tenantid $TenantFilter -AsApp $true
 
     # Check if we have one for the MFA App
     $SPID = ($SPResult | Where-Object { $_.appId -eq $MFAAppID }).id
@@ -52,7 +52,7 @@ Function Invoke-ExecSendPush {
         $SPBody = [pscustomobject]@{
             appId = $MFAAppID
         }
-        $SPID = (New-GraphPostRequest -uri 'https://graph.microsoft.com/v1.0/servicePrincipals' -tenantid $TenantFilter -type POST -body $SPBody ).id
+        $SPID = (New-GraphPostRequest -uri 'https://graph.microsoft.com/v1.0/servicePrincipals' -tenantid $TenantFilter -type POST -body $SPBody -AsApp $true).id
     }
 
 
