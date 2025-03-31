c6fe54a831bc90d1d70d4de8f4ce193c0fb57bcf
John Duprey <john@cyberdrain.com>
2025-03-31 18:13:34 -0400

    Update Set-CippApiAuth.ps1

--- Modules/CIPPCore/Public/Authentication/Set-CippApiAuth.ps1
+++ Modules/CIPPCore/Public/Authentication/Set-CippApiAuth.ps1
@@ -20,6 +20,8 @@ function Set-CippApiAuth {
     # Get auth settings
     $AuthSettings = Invoke-AzRestMethod -Uri "https://management.azure.com/subscriptions/$SubscriptionId/resourceGroups/$RGName/providers/Microsoft.Web/sites/$($FunctionAppName)/config/authsettingsV2/list?api-version=2020-06-01" | Select-Object -ExpandProperty Content | ConvertFrom-Json
 
+    Write-Information "AuthSettings: $($AuthSettings | ConvertTo-Json -Depth 10)"
+
     # Set allowed audiences
     $AllowedAudiences = foreach ($ClientId in $ClientIds) {
         "api://$ClientId"
