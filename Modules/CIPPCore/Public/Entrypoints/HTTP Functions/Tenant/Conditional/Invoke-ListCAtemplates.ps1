8dbbee12af9f7d82e0c2f354aad0073bacadd97a
John Duprey <john@cyberdrain.com>
2025-04-02 14:20:27 -0400

    update template list query
    fixes ticket 22021573133

--- Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Tenant/Conditional/Invoke-ListCAtemplates.ps1
+++ Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Tenant/Conditional/Invoke-ListCAtemplates.ps1
@@ -3,7 +3,7 @@ using namespace System.Net
 Function Invoke-ListCAtemplates {
     <#
     .FUNCTIONALITY
-        Entrypoint
+        Entrypoint,AnyTenant
     .ROLE
         Tenant.ConditionalAccess.Read
     #>
