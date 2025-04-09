99c841a3ccbecf50a2fc6bb625ce146ed48e3b8c
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2025-04-09 19:12:14 +0200

    text replacements

--- Modules/CIPPCore/Public/GraphHelper/New-GraphPOSTRequest.ps1
+++ Modules/CIPPCore/Public/GraphHelper/New-GraphPOSTRequest.ps1
@@ -20,6 +20,7 @@ function New-GraphPOSTRequest ($uri, $tenantid, $body, $type, $scope, $AsApp, $N
             $contentType = 'application/json; charset=utf-8'
         }
         try {
+            $body = Get-CIPPTextReplacement -TenantFilter $tenantid -Text $body
             $ReturnedData = (Invoke-RestMethod -Uri $($uri) -Method $TYPE -Body $body -Headers $headers -ContentType $contentType -SkipHttpErrorCheck:$IgnoreErrors -ResponseHeadersVariable responseHeaders)
         } catch {
             $Message = if ($_.ErrorDetails.Message) {
