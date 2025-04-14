dc7545c381690a6b15c3336ac8b54c59cbdd17ce
John Duprey <john@cyberdrain.com>
2025-04-14 18:53:56 -0400

    Update Set-CIPPStandardsCompareField.ps1

--- Modules/CIPPCore/Public/Set-CIPPStandardsCompareField.ps1
+++ Modules/CIPPCore/Public/Set-CIPPStandardsCompareField.ps1
@@ -18,24 +18,28 @@ function Set-CIPPStandardsCompareField {
     }
 
     $Existing = Get-CIPPAzDataTableEntity @Table -Filter "PartitionKey eq 'StandardReport' and RowKey eq '$($TenantName.defaultDomainName)'"
-    if ($Existing) {
-        $Existing = $Existing | Select-Object * -ExcludeProperty ETag, TimeStamp | ConvertTo-Json -Depth 10 -Compress | ConvertFrom-Json -AsHashtable
-        $Existing[$FieldName] = $FieldValue
-        $Existing['LastRefresh'] = [string]$(Get-Date (Get-Date).ToUniversalTime() -UFormat '+%Y-%m-%dT%H:%M:%S.000Z')
-        $Existing = [PSCustomObject]$Existing
+    try {
+        if ($Existing) {
+            $Existing = $Existing | Select-Object * -ExcludeProperty ETag, TimeStamp | ConvertTo-Json -Depth 10 -Compress | ConvertFrom-Json -AsHashtable
+            $Existing[$FieldName] = $FieldValue
+            $Existing['LastRefresh'] = [string]$(Get-Date (Get-Date).ToUniversalTime() -UFormat '+%Y-%m-%dT%H:%M:%S.000Z')
+            $Existing = [PSCustomObject]$Existing
 
-        Add-CIPPAzDataTableEntity @Table -Entity $Existing -Force
-    } else {
-        $Result = @{
-            tenantFilter = "$($TenantName.defaultDomainName)"
-            GUID         = "$($TenantName.customerId)"
-            RowKey       = "$($TenantName.defaultDomainName)"
-            PartitionKey = 'StandardReport'
-            LastRefresh  = [string]$(Get-Date (Get-Date).ToUniversalTime() -UFormat '+%Y-%m-%dT%H:%M:%S.000Z')
+            Add-CIPPAzDataTableEntity @Table -Entity $Existing -Force
+        } else {
+            $Result = @{
+                tenantFilter = "$($TenantName.defaultDomainName)"
+                GUID         = "$($TenantName.customerId)"
+                RowKey       = "$($TenantName.defaultDomainName)"
+                PartitionKey = 'StandardReport'
+                LastRefresh  = [string]$(Get-Date (Get-Date).ToUniversalTime() -UFormat '+%Y-%m-%dT%H:%M:%S.000Z')
+            }
+            $Result[$FieldName] = $FieldValue
+            Add-CIPPAzDataTableEntity @Table -Entity $Result -Force
+
         }
-        $Result[$FieldName] = $FieldValue
-        Add-CIPPAzDataTableEntity @Table -Entity $Result -Force
-
+        Write-Information "Adding $FieldName to StandardCompare for $Tenant. content is $FieldValue"
+    } catch {
+        Write-Warning "Failed to add $FieldName to StandardCompare for $Tenant. content is $FieldValue. The error was: $($_.Exception.Message)"
     }
-    Write-Information "Adding $FieldName to StandardCompare for $Tenant. content is $FieldValue"
 }
