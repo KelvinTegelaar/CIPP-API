8e7104147bc66a68ec16be4ef65da76280f9a9b4
Zac Richards <107489668+Zacgoose@users.noreply.github.com>
2025-05-17 02:08:45 +0800

    Add username to TAP response

--- Modules/CIPPCore/Public/New-CIPPTAP.ps1
+++ Modules/CIPPCore/Public/New-CIPPTAP.ps1
@@ -11,7 +11,8 @@ function New-CIPPTAP {
         $GraphRequest = New-GraphPostRequest -uri "https://graph.microsoft.com/beta/users/$($userid)/authentication/temporaryAccessPassMethods" -tenantid $TenantFilter -type POST -body '{}' -verbose
         Write-LogMessage -headers $Headers -API $APIName -message "Created Temporary Access Password (TAP) for $userid" -Sev 'Info' -tenant $TenantFilter
         return @{
-            resultText          = "The TAP for this user is $($GraphRequest.temporaryAccessPass) - This TAP is usable for the next $($GraphRequest.LifetimeInMinutes) minutes"
+            resultText          = "The TAP for $userid is $($GraphRequest.temporaryAccessPass) - This TAP is usable for the next $($GraphRequest.LifetimeInMinutes) minutes"
+            userid              = $userid
             copyField           = $GraphRequest.temporaryAccessPass
             temporaryAccessPass = $GraphRequest.temporaryAccessPass
             lifetimeInMinutes   = $GraphRequest.LifetimeInMinutes
