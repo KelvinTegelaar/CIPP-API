4777f16053cf751bc66707bfc87a53e704621738
Kristian Kjærgård <kris6673@hotmail.com>
2025-05-26 18:45:25 +0200

    casing

--- Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Identity/Administration/Users/Invoke-ListUserGroups.ps1
+++ Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Identity/Administration/Users/Invoke-ListUserGroups.ps1
@@ -14,17 +14,13 @@ Function Invoke-ListUserGroups {
     $Headers = $Request.Headers
     Write-LogMessage -headers $Headers -API $APIName -message 'Accessed this API' -Sev 'Debug'
 
-
-
-
     # Interact with query parameters or the body of the request.
-    $TenantFilter = $Request.Query.TenantFilter
-    $UserID = $Request.Query.UserID
-
-
+    $TenantFilter = $Request.Query.tenantFilter
+    $UserID = $Request.Query.userId
     $URI = "https://graph.microsoft.com/beta/users/$UserID/memberOf/$/microsoft.graph.group?`$select=id,displayName,mailEnabled,securityEnabled,groupTypes,onPremisesSyncEnabled,mail,isAssignableToRole`&`$orderby=displayName asc"
     Write-Host $URI
-    $GraphRequest = New-GraphGetRequest -uri $URI -tenantid $TenantFilter -noPagination $true -verbose | Select-Object id,
+
+    $GraphRequest = New-GraphGetRequest -uri $URI -tenantid $TenantFilter -noPagination $true -Verbose | Select-Object id,
     @{ Name = 'DisplayName'; Expression = { $_.displayName } },
     @{ Name = 'MailEnabled'; Expression = { $_.mailEnabled } },
     @{ Name = 'Mail'; Expression = { $_.mail } },
