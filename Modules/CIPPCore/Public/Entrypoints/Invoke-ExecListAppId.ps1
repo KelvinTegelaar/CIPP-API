a143bbbc6e590292a246567c7985b36309ac0ffe
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2025-05-26 22:32:50 +0200

    keyvault

--- Modules/CIPPCore/Public/Entrypoints/Invoke-ExecListAppId.ps1
+++ Modules/CIPPCore/Public/Entrypoints/Invoke-ExecListAppId.ps1
@@ -21,8 +21,24 @@ Function Invoke-ExecListAppId {
         $env:ApplicationID = $Secret.ApplicationID
         $env:TenantID = $Secret.TenantID
     } else {
-        $env:ApplicationID = (Get-AzKeyVaultSecret -AsPlainText -VaultName $env:WEBSITE_DEPLOYMENT_ID -Name 'ApplicationID').SecretValueText
-        $env:TenantID = (Get-AzKeyVaultSecret -AsPlainText -VaultName $env:WEBSITE_DEPLOYMENT_ID -Name 'TenantID').SecretValueText
+        Write-Information 'Connecting to Azure'
+        Connect-AzAccount -Identity
+        $SubscriptionId = $env:WEBSITE_OWNER_NAME -split '\+' | Select-Object -First 1
+        try {
+            $Context = Get-AzContext
+            if ($Context.Subscription) {
+                #Write-Information "Current context: $($Context | ConvertTo-Json)"
+                if ($Context.Subscription.Id -ne $SubscriptionId) {
+                    Write-Information "Setting context to subscription $SubscriptionId"
+                    $null = Set-AzContext -SubscriptionId $SubscriptionId
+                }
+            }
+        } catch {
+            Write-Information "ERROR: Could not set context to subscription $SubscriptionId."
+        }
+        $keyvaultname = ($env:WEBSITE_DEPLOYMENT_ID -split '-')[0]
+        $env:ApplicationID = (Get-AzKeyVaultSecret -AsPlainText -VaultName $keyvaultname -Name 'ApplicationID').SecretValueText
+        $env:TenantID = (Get-AzKeyVaultSecret -AsPlainText -VaultName $keyvaultname -Name 'TenantID').SecretValueText
     }
     $Results = @{
         applicationId = $env:ApplicationID
