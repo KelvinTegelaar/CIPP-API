ea12858814eae49eecdf703d8d6a39b9e208e98a
ngms-psh <psh@ngms.dk>
2025-05-27 14:39:14 +0200

    Modified to calculate decimalValue from binary all values are now parameters and use parameterSetName

--- Modules/CIPPCore/Private/Convert-QuarantinePermissionsValue.ps1
+++ Modules/CIPPCore/Private/Convert-QuarantinePermissionsValue.ps1
@@ -1,16 +1,30 @@
 function Convert-QuarantinePermissionsValue {
+    [CmdletBinding(DefaultParameterSetName = 'DecimalValue')]
     param (
-        [Parameter(Mandatory = $true, Position=0)]
+        [Parameter(Mandatory, Position = 0, ParameterSetName = "StringValue")]
         [ValidateNotNullOrEmpty()]
-        [ValidateScript(
-            {$_ -is [String] ? $true : $_ -is [Hashtable] ? $true : $false},
-            ErrorMessage = "Input must be a string or hashtable."
-            )]
-        $InputObject
+        [string]$InputObject,
+
+        [Parameter(Position = 0, ParameterSetName = "DecimalValue")]
+        [int]$PermissionToViewHeader = 0,
+        [Parameter(Position = 1, ParameterSetName = "DecimalValue")]
+        [int]$PermissionToDownload = 0,
+        [Parameter(Mandatory, Position = 2, ParameterSetName = "DecimalValue")]
+        [int]$PermissionToAllowSender,
+        [Parameter(Mandatory, Position = 3, ParameterSetName = "DecimalValue")]
+        [int]$PermissionToBlockSender,
+        [Parameter(Mandatory, Position = 4, ParameterSetName = "DecimalValue")]
+        [int]$PermissionToRequestRelease,
+        [Parameter(Mandatory, Position = 5, ParameterSetName = "DecimalValue")]
+        [int]$PermissionToRelease,
+        [Parameter(Mandatory, Position = 6, ParameterSetName = "DecimalValue")]
+        [int]$PermissionToPreview,
+        [Parameter(Mandatory, Position = 7, ParameterSetName = "DecimalValue")]
+        [int]$PermissionToDelete
     )
 
     #Converts string value with EndUserQuarantinePermissions received from Get-QuarantinePolicy
-    if ($InputObject -is [String]) {
+    if (($PSCmdlet.ParameterSetName) -eq "StringValue") {
         try {
             # Remove square brackets and split into lines
             $InputObject = $InputObject.Trim('[', ']')
@@ -22,28 +36,36 @@ function Convert-QuarantinePermissionsValue {
             return $hashtable
         }
         catch {
-            $ErrorMessage = Get-NormalizedError -Message $_.Exception.Message
-            throw "Convert-QuarantinePermissionsValue: Failed to convert string to hashtable. Error: $ErrorMessage"
+            throw "Convert-QuarantinePermissionsValue: Failed to convert string to hashtable."
         }
     }
 
-    #Converts hashtable with selected end user quarantine permissions to decimal value used by EndUserQuarantinePermissionsValue property in New-QuarantinePolicy and Set-QuarantinePolicy
-    elseif ($InputObject -is [Hashtable]) {
+    #Converts selected end user quarantine permissions to decimal value used by EndUserQuarantinePermissionsValue property in New-QuarantinePolicy and Set-QuarantinePolicy
+    elseif (($PSCmdlet.ParameterSetName) -eq "DecimalValue") {
         try {
-            $EndUserQuarantinePermissionsValue = 0
-            $EndUserQuarantinePermissionsValue += ([int]$InputObject.PermissionToViewHeader ?? 0 ) * 128
-            $EndUserQuarantinePermissionsValue += ([int]$InputObject.PermissionToDownload ?? 0 ) * 64
-            $EndUserQuarantinePermissionsValue += [int]$InputObject.PermissionToAllowSender * 32
-            $EndUserQuarantinePermissionsValue += [int]$InputObject.PermissionToBlockSender * 16
-            $EndUserQuarantinePermissionsValue += [int]$InputObject.PermissionToRequestRelease * 8
-            $EndUserQuarantinePermissionsValue += [int]$InputObject.PermissionToRelease * 4
-            $EndUserQuarantinePermissionsValue += [int]$InputObject.PermissionToPreview * 2
-            $EndUserQuarantinePermissionsValue += [int]$InputObject.PermissionToDelete * 1
-            return $EndUserQuarantinePermissionsValue
+            # both PermissionToRequestRelease and PermissionToRelease cannot be set to true at the same time
+            if($PermissionToRequestRelease -eq 1 -and $PermissionToRelease -eq 1) {
+                throw "PermissionToRequestRelease and PermissionToRelease cannot both be set to true."
+            }
+
+            # Convert each permission to a binary string
+            $BinaryValue = [string]@(
+                $PermissionToViewHeader,
+                $PermissionToDownload,
+                $PermissionToAllowSender,
+                $PermissionToBlockSender,
+                $PermissionToRequestRelease,
+                $PermissionToRelease,
+                $PermissionToPreview,
+                $PermissionToDelete
+            ) -replace '\s',''
+
+            # Convert the binary string to an Decimal value
+            return [convert]::ToInt32($BinaryValue,2)
         }
         catch {
             $ErrorMessage = Get-NormalizedError -Message $_.Exception.Message
-            throw "Convert-QuarantinePermissionsValue: Failed to convert hashtable to QuarantinePermissionsValue. Error: $ErrorMessage"
+            throw "Convert-QuarantinePermissionsValue: Failed to convert QuarantinePermissions to QuarantinePermissionsValue. Error: $ErrorMessage"
         }
     }
 }
