7613d60e37ab92c9b0093f9fd47271938f277da8
Kristian Kjærgård <kris6673@hotmail.com>
2025-06-19 18:27:16 +0200

    Refactor Invoke-ExecSetOoO function to improve error handling and logging. Updated header variable usage and streamlined result assignment for out-of-office settings.

--- Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Email-Exchange/Administration/Invoke-ExecSetOoO.ps1
+++ Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Email-Exchange/Administration/Invoke-ExecSetOoO.ps1
@@ -11,9 +11,13 @@ Function Invoke-ExecSetOoO {
     param($Request, $TriggerMetadata)
     try {
         $APIName = $Request.Params.CIPPEndpoint
-        Write-LogMessage -headers $Request.Headers -API $APIName -message 'Accessed this API' -Sev 'Debug'
+        $Headers = $Request.Headers
+        Write-LogMessage -headers $Headers -API $APIName -message 'Accessed this API' -Sev 'Debug'
+
+
         $Username = $Request.Body.userId
         $TenantFilter = $Request.Body.tenantFilter
+
         if ($Request.Body.input) {
             $InternalMessage = $Request.Body.input
             $ExternalMessage = $Request.Body.input
@@ -25,25 +29,25 @@ Function Invoke-ExecSetOoO {
         $StartTime = if ($Request.Body.StartTime -match '^\d+$') { [DateTimeOffset]::FromUnixTimeSeconds([int]$Request.Body.StartTime).DateTime } else { $Request.Body.StartTime }
         $EndTime = if ($Request.Body.EndTime -match '^\d+$') { [DateTimeOffset]::FromUnixTimeSeconds([int]$Request.Body.EndTime).DateTime } else { $Request.Body.EndTime }
 
-        $Results = try {
-            if ($Request.Body.AutoReplyState.value -ne 'Scheduled') {
-                Set-CIPPOutOfOffice -userid $Username -tenantFilter $TenantFilter -APIName $APIName -Headers $Request.Headers -InternalMessage $InternalMessage -ExternalMessage $ExternalMessage -State $Request.Body.AutoReplyState.value
-            } else {
-                Set-CIPPOutOfOffice -userid $Username -tenantFilter $TenantFilter -APIName $APIName -Headers $Request.Headers -InternalMessage $InternalMessage -ExternalMessage $ExternalMessage -StartTime $StartTime -EndTime $EndTime -State $Request.Body.AutoReplyState.value
-            }
-        } catch {
-            "Could not add out of office message for $($Username). Error: $($_.Exception.Message)"
+
+        if ($Request.Body.AutoReplyState.value -ne 'Scheduled') {
+            $Results = Set-CIPPOutOfOffice -userid $Username -tenantFilter $TenantFilter -APIName $APIName -Headers $Headers -InternalMessage $InternalMessage -ExternalMessage $ExternalMessage -State $Request.Body.AutoReplyState.value
+        } else {
+            $Results = Set-CIPPOutOfOffice -userid $Username -tenantFilter $TenantFilter -APIName $APIName -Headers $Headers -InternalMessage $InternalMessage -ExternalMessage $ExternalMessage -StartTime $StartTime -EndTime $EndTime -State $Request.Body.AutoReplyState.value
         }
 
-        $Body = [PSCustomObject]@{'Results' = $($Results) }
+
     } catch {
-        $Body = [PSCustomObject]@{'Results' = "Could not set Out of Office user: $($_.Exception.Message)" }
+        $ErrorMessage = Get-CippException -Exception $_
+        $Results = "Could not set Out of Office for user: $($Username). Error: $($ErrorMessage.NormalizedError)"
+        Write-LogMessage -headers $Headers -API $APIName -message $Results -Sev 'Error' -LogData $ErrorMessage
+        $StatusCode = [HttpStatusCode]::InternalServerError
     }
 
     # Associate values to output bindings by calling 'Push-OutputBinding'.
     Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-            StatusCode = [HttpStatusCode]::OK
-            Body       = $Body
+            StatusCode = $StatusCode
+            Body       = @{'Results' = $($Results) }
         })
 
 }
