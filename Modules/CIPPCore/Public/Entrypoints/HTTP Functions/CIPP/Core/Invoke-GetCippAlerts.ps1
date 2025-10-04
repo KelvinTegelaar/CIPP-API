bb466e3e04252edb5fb3d6b1cb1e2d5e82c5dc35
Zac Richards <107489668+Zacgoose@users.noreply.github.com>
2025-10-04 12:29:43 +0800

    Fix: Logic error with dev settings

--- Modules/CIPPCore/Public/Entrypoints/HTTP Functions/CIPP/Core/Invoke-GetCippAlerts.ps1
+++ Modules/CIPPCore/Public/Entrypoints/HTTP Functions/CIPP/Core/Invoke-GetCippAlerts.ps1
@@ -60,7 +60,7 @@ function Invoke-GetCippAlerts {
                 type  = 'error'
             })
     }
-    if ((!$env:WEBSITE_RUN_FROM_PACKAGE -or [string]::IsNullOrEmpty($env:WEBSITE_RUN_FROM_PACKAGE)) -and $env:AzureWebJobsStorage -ne 'UseDevelopmentStorage=true' -or $env:NonLocalHostAzurite -ne 'true') {
+    if ((!$env:WEBSITE_RUN_FROM_PACKAGE -or [string]::IsNullOrEmpty($env:WEBSITE_RUN_FROM_PACKAGE)) -and $env:AzureWebJobsStorage -ne 'UseDevelopmentStorage=true' -and $env:NonLocalHostAzurite -ne 'true') {
         $Alerts.Add(
             @{
                 title = 'Function App in Write Mode'
