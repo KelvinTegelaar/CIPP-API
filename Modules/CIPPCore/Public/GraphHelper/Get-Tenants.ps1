d4830f6010c5dafcd628e070513ee60b98fabcc4
rvdwegen <rvdwegen@vdwegen.app>
2025-10-28 16:03:14 +0100

    change to v1

--- Modules/CIPPCore/Public/GraphHelper/Get-Tenants.ps1
+++ Modules/CIPPCore/Public/GraphHelper/Get-Tenants.ps1
@@ -94,7 +94,7 @@ function Get-Tenants {
             throw 'RefreshToken not set. Cannot get tenant list.'
         }
         #get the full list of tenants
-        $GDAPRelationships = New-GraphGetRequest -uri "https://graph.microsoft.com/beta/tenantRelationships/delegatedAdminRelationships?`$filter=status eq 'active' and not startsWith(displayName,'MLT_')$RelationshipFilter&`$select=customer,autoExtendDuration,endDateTime" -NoAuthCheck:$true
+        $GDAPRelationships = New-GraphGetRequest -uri "https://graph.microsoft.com/v1.0/tenantRelationships/delegatedAdminRelationships?`$filter=status eq 'active' and not startsWith(displayName,'MLT_')$RelationshipFilter&`$select=customer,autoExtendDuration,endDateTime" -NoAuthCheck:$true
         Write-Host "GDAP relationships found: $($GDAPRelationships.Count)"
         Write-Information "GDAP relationships found: $($GDAPRelationships.Count)"
         $totalTenants = $GDAPRelationships.customer.tenantId | Select-Object -Unique
