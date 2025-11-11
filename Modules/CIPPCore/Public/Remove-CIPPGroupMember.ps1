c0820ee396b5e3ff726784aea7fd08bb0848c2be
John Duprey <john@cyberdrain.com>
2025-11-11 11:23:11 -0500

    resolve username to guid for scheduled tasks

--- Modules/CIPPCore/Public/Remove-CIPPGroupMember.ps1
+++ Modules/CIPPCore/Public/Remove-CIPPGroupMember.ps1
@@ -7,13 +7,22 @@ function Remove-CIPPGroupMember(
     [string]$APIName = 'Remove Group Member'
 ) {
     try {
-        if ($Member -like '*#EXT#*') { $Member = [System.Web.HttpUtility]::UrlEncode($Member) }
-        # $MemberIDs = 'https://graph.microsoft.com/v1.0/directoryObjects/' + (New-GraphGetRequest -uri "https://graph.microsoft.com/beta/users/$($Member)" -tenantid $TenantFilter).id
-        # $AddMemberBody = "{ `"members@odata.bind`": $(ConvertTo-Json @($MemberIDs)) }"
         if ($GroupType -eq 'Distribution list' -or $GroupType -eq 'Mail-Enabled Security') {
             $Params = @{ Identity = $GroupId; Member = $Member; BypassSecurityGroupManagerCheck = $true }
             $null = New-ExoRequest -tenantid $TenantFilter -cmdlet 'Remove-DistributionGroupMember' -cmdParams $Params -UseSystemMailbox $true
         } else {
+            if ($Member -match '^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$') {
+                Write-Information "Member $Member is a GUID, proceeding with removal."
+            } else {
+                Write-Information "Member $Member is not a GUID, attempting to resolve to object ID."
+                if ($Member -like '*#EXT#*') { $Member = [System.Web.HttpUtility]::UrlEncode($Member) }
+                $UserObject = New-GraphGetRequest -uri "https://graph.microsoft.com/v1.0/users/$($Member)?`$select=id" -tenantid $TenantFilter
+                if ($null -eq $UserObject.id) {
+                    throw "Could not resolve user $Member to an object ID."
+                }
+                $Member = $UserObject.id
+                Write-Information "Resolved member to object ID: $Member"
+            }
             $null = New-GraphPostRequest -uri "https://graph.microsoft.com/beta/groups/$($GroupId)/members/$($Member)/`$ref" -tenantid $TenantFilter -type DELETE -body '{}' -Verbose
         }
         $Results = "Successfully removed user $($Member) from $($GroupId)."
