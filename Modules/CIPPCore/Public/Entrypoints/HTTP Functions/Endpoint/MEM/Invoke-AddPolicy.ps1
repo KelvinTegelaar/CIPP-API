8b92f5a7e181cbcce3d8c1427f132c8cc110153f
Kristian Kjærgård <kris6673@hotmail.com>
2025-11-25 21:40:59 +0100

    Fix: Standardize casing for function names ,improve error handling  and add support for app config profiles

--- Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Endpoint/MEM/Invoke-AddPolicy.ps1
+++ Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Endpoint/MEM/Invoke-AddPolicy.ps1
@@ -9,26 +9,38 @@ function Invoke-AddPolicy {
     param($Request, $TriggerMetadata)
 
     $APIName = $Request.Params.CIPPEndpoint
+    $Headers = $Request.Headers
     $Tenants = $Request.Body.tenantFilter.value ? $Request.Body.tenantFilter.value : $Request.Body.tenantFilter
-    if ('AllTenants' -in $Tenants) { $Tetnants = (Get-Tenants).defaultDomainName }
-    $displayname = $Request.Body.displayName
+    if ('AllTenants' -in $Tenants) { $Tenants = (Get-Tenants).defaultDomainName }
+
+    $DisplayName = $Request.Body.displayName
     $description = $Request.Body.Description
     $AssignTo = if ($Request.Body.AssignTo -ne 'on') { $Request.Body.AssignTo }
     $ExcludeGroup = $Request.Body.excludeGroup
-    $Request.body.customGroup ? ($AssignTo = $Request.body.customGroup) : $null
+    $Request.Body.customGroup ? ($AssignTo = $Request.Body.customGroup) : $null
     $RawJSON = $Request.Body.RAWJson
 
-    $results = foreach ($Tenant in $tenants) {
-        if ($Request.Body.replacemap.$tenant) {
-            ([pscustomobject]$Request.Body.replacemap.$tenant).psobject.properties | ForEach-Object { $RawJson = $RawJson -replace $_.name, $_.value }
+    $results = foreach ($Tenant in $Tenants) {
+        if ($Request.Body.replacemap.$Tenant) {
+            ([pscustomobject]$Request.Body.replacemap.$Tenant).PSObject.Properties | ForEach-Object { $RawJSON = $RawJSON -replace $_.name, $_.value }
         }
         try {
             Write-Host 'Calling Adding policy'
-            Set-CIPPIntunePolicy -TemplateType $Request.body.TemplateType -Description $description -DisplayName $displayname -RawJSON $RawJSON -AssignTo $AssignTo -ExcludeGroup $ExcludeGroup -tenantFilter $Tenant -Headers $Request.Headers
-            Write-LogMessage -headers $Request.Headers -API $APINAME -tenant $($Tenant) -message "Added policy $($Displayname)" -Sev 'Info'
+            $params = @{
+                TemplateType = $Request.Body.TemplateType
+                Description  = $description
+                DisplayName  = $DisplayName
+                RawJSON      = $RawJSON
+                AssignTo     = $AssignTo
+                ExcludeGroup = $ExcludeGroup
+                tenantFilter = $Tenant
+                Headers      = $Headers
+            }
+            Set-CIPPIntunePolicy @params
+            Write-LogMessage -headers $Headers -API $APIName -tenant $($Tenant) -message "Added policy $($DisplayName)" -Sev 'Info'
         } catch {
             "$($_.Exception.Message)"
-            Write-LogMessage -headers $Request.Headers -API $APINAME -tenant $($Tenant) -message "Failed adding policy $($Displayname). Error: $($_.Exception.Message)" -Sev 'Error'
+            Write-LogMessage -headers $Headers -API $APIName -tenant $($Tenant) -message "Failed adding policy $($DisplayName). Error: $($_.Exception.Message)" -Sev 'Error'
             continue
         }
 
