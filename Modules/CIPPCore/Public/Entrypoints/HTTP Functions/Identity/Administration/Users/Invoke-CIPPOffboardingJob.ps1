ceac7423bd205cce16d4ae88a50ce904ccd6ac71
Jacob Newman <jacob@renada.co.uk>
2025-11-25 15:59:49 +0000

    Restructure offboarding webhook notifications with descriptive keys and proper result handling
    - Consolidate all results into single object with clean key names
    
    - Handle array vs single-value results appropriately
    
    - Fix loop-based actions to collect all results

--- Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Identity/Administration/Users/Invoke-CIPPOffboardingJob.ps1
+++ Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Identity/Administration/Users/Invoke-CIPPOffboardingJob.ps1
@@ -15,146 +15,234 @@ function Invoke-CIPPOffboardingJob {
     $UserID = $User.id
     $DisplayName = $User.displayName
     Write-Host "Running offboarding job for $Username with options: $($Options | ConvertTo-Json -Depth 10)"
-    $Return = switch ($Options) {
+    $JobResults = [ordered]@{}
+
+    switch ($Options) {
         { $_.ConvertToShared -eq $true } {
             try {
-                Set-CIPPMailboxType -Headers $Headers -tenantFilter $TenantFilter -userid $UserID -username $Username -MailboxType 'Shared' -APIName $APIName
-            } catch {
-                $_.Exception.Message
+                $Result = Set-CIPPMailboxType -Headers $Headers -tenantFilter $TenantFilter -userid $UserID -username $Username -MailboxType 'Shared' -APIName $APIName
+                $JobResults['ConvertToSharedMailbox'] = $Result
+            }
+            catch {
+                $JobResults['ConvertToSharedMailbox'] = $_.Exception.Message
             }
         }
         { $_.RevokeSessions -eq $true } {
             try {
-                Revoke-CIPPSessions -tenantFilter $TenantFilter -username $Username -userid $UserID -Headers $Headers -APIName $APIName
-            } catch {
-                $_.Exception.Message
+                $Result = Revoke-CIPPSessions -tenantFilter $TenantFilter -username $Username -userid $UserID -Headers $Headers -APIName $APIName
+                $JobResults['RevokeSessions'] = $Result
+            }
+            catch {
+                $JobResults['RevokeSessions'] = $_.Exception.Message
             }
         }
         { $_.ResetPass -eq $true } {
             try {
-                Set-CIPPResetPassword -tenantFilter $TenantFilter -DisplayName $DisplayName -UserID $username -Headers $Headers -APIName $APIName
-            } catch {
-                $_.Exception.Message
+                $Result = Set-CIPPResetPassword -tenantFilter $TenantFilter -DisplayName $DisplayName -UserID $username -Headers $Headers -APIName $APIName
+                $JobResults['ResetPassword'] = $Result
+            }
+            catch {
+                $JobResults['ResetPassword'] = $_.Exception.Message
             }
         }
         { $_.RemoveGroups -eq $true } {
-            Remove-CIPPGroups -userid $UserID -tenantFilter $TenantFilter -Headers $Headers -APIName $APIName -Username $Username
+            try {
+                $Result = Remove-CIPPGroups -userid $UserID -tenantFilter $TenantFilter -Headers $Headers -APIName $APIName -Username $Username
+                if ($Result -is [array]) {
+                    $JobResults['RemoveGroups'] = $Result | ForEach-Object { [PSCustomObject]@{ Result = $_ } }
+                }
+                elseif ($Result) {
+                    $JobResults['RemoveGroups'] = @([PSCustomObject]@{ Result = $Result })
+                }
+                else {
+                    $JobResults['RemoveGroups'] = "User $Username is not a member of any groups"
+                }
+            }
+            catch {
+                $JobResults['RemoveGroups'] = @([PSCustomObject]@{ Result = $_.Exception.Message })
+            }
         }
         { $_.HideFromGAL -eq $true } {
             try {
-                Set-CIPPHideFromGAL -tenantFilter $TenantFilter -UserID $username -HideFromGAL $true -Headers $Headers -APIName $APIName
-            } catch {
-                $_.Exception.Message
+                $Result = Set-CIPPHideFromGAL -tenantFilter $TenantFilter -UserID $username -HideFromGAL $true -Headers $Headers -APIName $APIName
+                $JobResults['HideFromGAL'] = $Result
+            }
+            catch {
+                $JobResults['HideFromGAL'] = $_.Exception.Message
             }
         }
         { $_.DisableSignIn -eq $true } {
             try {
-                Set-CIPPSignInState -TenantFilter $TenantFilter -userid $username -AccountEnabled $false -Headers $Headers -APIName $APIName
-            } catch {
-                $_.Exception.Message
+                $Result = Set-CIPPSignInState -TenantFilter $TenantFilter -userid $username -AccountEnabled $false -Headers $Headers -APIName $APIName
+                $JobResults['DisableSignIn'] = $Result
+            }
+            catch {
+                $JobResults['DisableSignIn'] = $_.Exception.Message
             }
         }
         { $_.OnedriveAccess } {
+            $JobResults['OnedriveAccess'] = [System.Collections.Generic.List[PSCustomObject]]::new()
             $Options.OnedriveAccess | ForEach-Object {
                 try {
-                    Set-CIPPSharePointPerms -tenantFilter $TenantFilter -userid $username -OnedriveAccessUser $_.value -Headers $Headers -APIName $APIName
-                } catch {
-                    $_.Exception.Message
+                    $Result = Set-CIPPSharePointPerms -tenantFilter $TenantFilter -userid $username -OnedriveAccessUser $_.value -Headers $Headers -APIName $APIName
+                    $JobResults['OnedriveAccess'].Add([PSCustomObject]@{ Result = $Result })
+                }
+                catch {
+                    $JobResults['OnedriveAccess'].Add([PSCustomObject]@{ Result = $_.Exception.Message })
                 }
             }
         }
         { $_.AccessNoAutomap } {
+            $JobResults['AccessNoAutomap'] = [System.Collections.Generic.List[PSCustomObject]]::new()
             $Options.AccessNoAutomap | ForEach-Object {
                 try {
-                    Set-CIPPMailboxAccess -tenantFilter $TenantFilter -userid $username -AccessUser $_.value -Automap $false -AccessRights @('FullAccess') -Headers $Headers -APIName $APIName
-                } catch {
-                    $_.Exception.Message
+                    $Result = Set-CIPPMailboxAccess -tenantFilter $TenantFilter -userid $username -AccessUser $_.value -Automap $false -AccessRights @('FullAccess') -Headers $Headers -APIName $APIName
+                    $JobResults['AccessNoAutomap'].Add([PSCustomObject]@{ Result = $Result })
+                }
+                catch {
+                    $JobResults['AccessNoAutomap'].Add([PSCustomObject]@{ Result = $_.Exception.Message })
                 }
             }
         }
         { $_.AccessAutomap } {
+            $JobResults['AccessAutomap'] = [System.Collections.Generic.List[PSCustomObject]]::new()
             $Options.AccessAutomap | ForEach-Object {
                 try {
-                    Set-CIPPMailboxAccess -tenantFilter $TenantFilter -userid $username -AccessUser $_.value -Automap $true -AccessRights @('FullAccess') -Headers $Headers -APIName $APIName
-                } catch {
-                    $_.Exception.Message
+                    $Result = Set-CIPPMailboxAccess -tenantFilter $TenantFilter -userid $username -AccessUser $_.value -Automap $true -AccessRights @('FullAccess') -Headers $Headers -APIName $APIName
+                    $JobResults['AccessAutomap'].Add([PSCustomObject]@{ Result = $Result })
+                }
+                catch {
+                    $JobResults['AccessAutomap'].Add([PSCustomObject]@{ Result = $_.Exception.Message })
                 }
             }
         }
         { $_.OOO } {
             try {
-                Set-CIPPOutOfOffice -tenantFilter $TenantFilter -UserID $username -InternalMessage $Options.OOO -ExternalMessage $Options.OOO -Headers $Headers -APIName $APIName -state 'Enabled'
-            } catch {
-                $_.Exception.Message
+                $Result = Set-CIPPOutOfOffice -tenantFilter $TenantFilter -UserID $username -InternalMessage $Options.OOO -ExternalMessage $Options.OOO -Headers $Headers -APIName $APIName -state 'Enabled'
+                $JobResults['SetOutOfOffice'] = $Result
+            }
+            catch {
+                $JobResults['SetOutOfOffice'] = $_.Exception.Message
             }
         }
         { $_.forward } {
             if (!$Options.KeepCopy) {
                 try {
-                    Set-CIPPForwarding -userid $userid -username $username -tenantFilter $TenantFilter -Forward $Options.forward.value -Headers $Headers -APIName $APIName
-                } catch {
-                    $_.Exception.Message
+                    $Result = Set-CIPPForwarding -userid $userid -username $username -tenantFilter $TenantFilter -Forward $Options.forward.value -Headers $Headers -APIName $APIName
+                    $JobResults['SetForwarding'] = $Result
                 }
-            } else {
+                catch {
+                    $JobResults['SetForwarding'] = $_.Exception.Message
+                }
+            }
+            else {
                 $KeepCopy = [boolean]$Options.KeepCopy
                 try {
-                    Set-CIPPForwarding -userid $userid -username $username -tenantFilter $TenantFilter -Forward $Options.forward.value -KeepCopy $KeepCopy -Headers $Headers -APIName $APIName
-                } catch {
-                    $_.Exception.Message
+                    $Result = Set-CIPPForwarding -userid $userid -username $username -tenantFilter $TenantFilter -Forward $Options.forward.value -KeepCopy $KeepCopy -Headers $Headers -APIName $APIName
+                    $JobResults['SetForwarding'] = $Result
+                }
+                catch {
+                    $JobResults['SetForwarding'] = $_.Exception.Message
                 }
             }
         }
         { $_.disableForwarding } {
             try {
-                Set-CIPPForwarding -userid $userid -username $username -tenantFilter $TenantFilter -Disable $true -Headers $Headers -APIName $APIName
-            } catch {
-                $_.Exception.Message
+                $Result = Set-CIPPForwarding -userid $userid -username $username -tenantFilter $TenantFilter -Disable $true -Headers $Headers -APIName $APIName
+                $JobResults['DisableForwarding'] = $Result
+            }
+            catch {
+                $JobResults['DisableForwarding'] = $_.Exception.Message
             }
         }
         { $_.RemoveTeamsPhoneDID } {
             try {
-                Remove-CIPPUserTeamsPhoneDIDs -userid $userid -username $username -tenantFilter $TenantFilter -Headers $Headers -APIName $APIName
-            } catch {
-                $_.Exception.Message
+                $Result = Remove-CIPPUserTeamsPhoneDIDs -userid $userid -username $username -tenantFilter $TenantFilter -Headers $Headers -APIName $APIName
+                $JobResults['RemoveTeamsPhoneDID'] = $Result
+            }
+            catch {
+                $JobResults['RemoveTeamsPhoneDID'] = $_.Exception.Message
             }
         }
         { $_.RemoveLicenses -eq $true } {
-            Remove-CIPPLicense -userid $userid -username $Username -tenantFilter $TenantFilter -Headers $Headers -APIName $APIName -Schedule
+            try {
+                $Result = Remove-CIPPLicense -userid $userid -username $Username -tenantFilter $TenantFilter -Headers $Headers -APIName $APIName -Schedule
+                if ($Result -is [array]) {
+                    $JobResults['RemoveLicenses'] = $Result | ForEach-Object { [PSCustomObject]@{ Result = $_ } }
+                }
+                elseif ($Result) {
+                    $JobResults['RemoveLicenses'] = @([PSCustomObject]@{ Result = $Result })
+                }
+                else {
+                    $JobResults['RemoveLicenses'] = "No licenses found to remove for $Username"
+                }
+            }
+            catch {
+                $JobResults['RemoveLicenses'] = @([PSCustomObject]@{ Result = $_.Exception.Message })
+            }
         }
         { $_.DeleteUser -eq $true } {
             try {
-                Remove-CIPPUser -UserID $userid -Username $Username -TenantFilter $TenantFilter -Headers $Headers -APIName $APIName
-            } catch {
-                $_.Exception.Message
+                $Result = Remove-CIPPUser -UserID $userid -Username $Username -TenantFilter $TenantFilter -Headers $Headers -APIName $APIName
+                $JobResults['DeleteUser'] = $Result
+            }
+            catch {
+                $JobResults['DeleteUser'] = $_.Exception.Message
             }
         }
         { $_.RemoveRules -eq $true } {
             Write-Host "Removing rules for $username"
             try {
-                Remove-CIPPMailboxRule -userid $userid -username $Username -tenantFilter $TenantFilter -Headers $Headers -APIName $APIName -RemoveAllRules
-            } catch {
-                $_.Exception.Message
+                $Result = Remove-CIPPMailboxRule -userid $userid -username $Username -tenantFilter $TenantFilter -Headers $Headers -APIName $APIName -RemoveAllRules
+                $JobResults['RemoveRules'] = $Result
+            }
+            catch {
+                $JobResults['RemoveRules'] = $_.Exception.Message
             }
         }
         { $_.RemoveMobile -eq $true } {
             try {
-                Remove-CIPPMobileDevice -userid $userid -username $Username -tenantFilter $TenantFilter -Headers $Headers -APIName $APIName
-            } catch {
-                $_.Exception.Message
+                $Result = Remove-CIPPMobileDevice -userid $userid -username $Username -tenantFilter $TenantFilter -Headers $Headers -APIName $APIName
+                $JobResults['RemoveMobile'] = $Result
+            }
+            catch {
+                $JobResults['RemoveMobile'] = $_.Exception.Message
             }
         }
         { $_.removeCalendarInvites -eq $true } {
             try {
-                Remove-CIPPCalendarInvites -UserID $userid -Username $Username -TenantFilter $TenantFilter -Headers $Headers -APIName $APIName
-            } catch {
-                $_.Exception.Message
+                $Result = Remove-CIPPCalendarInvites -UserID $userid -Username $Username -TenantFilter $TenantFilter -Headers $Headers -APIName $APIName
+                $JobResults['RemoveCalendarInvites'] = $Result
+            }
+            catch {
+                $JobResults['RemoveCalendarInvites'] = $_.Exception.Message
             }
         }
         { $_.removePermissions } {
             if ($RunScheduled) {
-                Remove-CIPPMailboxPermissions -PermissionsLevel @('FullAccess', 'SendAs', 'SendOnBehalf') -userid 'AllUsers' -AccessUser $UserName -TenantFilter $TenantFilter -APIName $APINAME -Headers $Headers
+                try {
+                    $Result = Remove-CIPPMailboxPermissions -PermissionsLevel @('FullAccess', 'SendAs', 'SendOnBehalf') -userid 'AllUsers' -AccessUser $UserName -TenantFilter $TenantFilter -APIName $APINAME -Headers $Headers
+                    if ($Result -is [array]) {
+                        $JobResults['RemovePermissions'] = $Result | ForEach-Object { [PSCustomObject]@{ Result = $_ } }
+                    }
+                    elseif ($Result) {
+                        $JobResults['RemovePermissions'] = @([PSCustomObject]@{ Result = $Result })
+                    }
+                    else {
+                        $JobResults['RemovePermissions'] = "No mailbox permissions found to remove for $UserName"
+                    }
+                }
+                catch {
+                    if ($_.Exception.Message -is [array]) {
+                        $JobResults['RemovePermissions'] = $_.Exception.Message | ForEach-Object { [PSCustomObject]@{ Result = $_ } }
+                    }
+                    else {
+                        $JobResults['RemovePermissions'] = @([PSCustomObject]@{ Result = $_.Exception.Message })
+                    }
+                }
 
-            } else {
+            }
+            else {
                 $Queue = New-CippQueueEntry -Name "Offboarding - Mailbox Permissions: $Username" -TotalTasks 1
                 $InputObject = [PSCustomObject]@{
                     Batch            = @(
@@ -171,27 +259,33 @@ function Invoke-CIPPOffboardingJob {
                     SkipLog          = $true
                 }
                 $null = Start-NewOrchestration -FunctionName CIPPOrchestrator -InputObject ($InputObject | ConvertTo-Json -Depth 10)
-                "Removal of permissions queued. This task will run in the background and send it's results to the logbook."
+                $Result = "Removal of permissions queued. This task will run in the background and send it's results to the logbook."
+                $JobResults['RemovePermissions'] = $Result
             }
         }
         { $_.RemoveMFADevices -eq $true } {
             try {
-                Remove-CIPPUserMFA -UserPrincipalName $Username -TenantFilter $TenantFilter -Headers $Headers
-            } catch {
-                $_.Exception.Message
+                $Result = Remove-CIPPUserMFA -UserPrincipalName $Username -TenantFilter $TenantFilter -Headers $Headers
+                $JobResults['RemoveMFADevices'] = $Result
+            }
+            catch {
+                $JobResults['RemoveMFADevices'] = $_.Exception.Message
             }
         }
         { $_.ClearImmutableId -eq $true } {
             if ($User.onPremisesSyncEnabled -ne $true -and ![string]::IsNullOrEmpty($User.onPremisesImmutableId)) {
                 Write-LogMessage -Message "User $Username has an ImmutableID set but is not synced from on-premises. Proceeding to clear the ImmutableID." -TenantFilter $TenantFilter -Severity 'Warning' -APIName $APIName -Headers $Headers
                 try {
-                    Clear-CIPPImmutableID -UserID $userid -TenantFilter $TenantFilter -Headers $Headers -APIName $APIName
-                } catch {
-                    $_.Exception.Message
+                    $Result = Clear-CIPPImmutableID -UserID $userid -TenantFilter $TenantFilter -Headers $Headers -APIName $APIName
+                    $JobResults['ClearImmutableId'] = $Result
                 }
-            } elseif ($User.onPremisesSyncEnabled -eq $true -and ![string]::IsNullOrEmpty($User.onPremisesImmutableId)) {
+                catch {
+                    $JobResults['ClearImmutableId'] = $_.Exception.Message
+                }
+            }
+            elseif ($User.onPremisesSyncEnabled -eq $true -and ![string]::IsNullOrEmpty($User.onPremisesImmutableId)) {
                 Write-LogMessage -Message "User $Username is synced from on-premises. Scheduling an Immutable ID clear for when the user account has been soft deleted." -TenantFilter $TenantFilter -Severity 'Error' -APIName $APIName -Headers $Headers
-                'Scheduling Immutable ID clear task for when the user account is no longer synced in the on-premises directory.'
+                $Result = 'Scheduling Immutable ID clear task for when the user account is no longer synced in the on-premises directory.'
                 $ScheduledTask = @{
                     TenantFilter  = $TenantFilter
                     Name          = "Clear Immutable ID: $Username"
@@ -221,9 +315,11 @@ function Invoke-CIPPOffboardingJob {
                     }
                 }
                 Add-CIPPScheduledTask -Task $ScheduledTask -hidden $false
+                $JobResults['ClearImmutableId'] = $Result
             }
         }
     }
-    return $Return
+    return [PSCustomObject]$JobResults
 
 }
+```
