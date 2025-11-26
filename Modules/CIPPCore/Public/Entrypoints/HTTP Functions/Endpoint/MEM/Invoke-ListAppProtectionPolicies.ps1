316793583c6fd735d41fb4b15168429ac614e427
Kristian Kjærgård <kris6673@hotmail.com>
2025-11-26 20:28:22 +0100

    Feat: Enhance Invoke-ListAppProtectionPolicies and Invoke-ListIntunePolicy to support group assignments and bulk requests

--- Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Endpoint/MEM/Invoke-ListAppProtectionPolicies.ps1
+++ Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Endpoint/MEM/Invoke-ListAppProtectionPolicies.ps1
@@ -15,27 +15,68 @@
     $TenantFilter = $Request.Query.tenantFilter
 
     try {
-        # Use bulk requests to get both managed app policies and mobile app configurations
+        # Use bulk requests to get groups, managed app policies and mobile app configurations
         $BulkRequests = @(
+            @{
+                id     = 'Groups'
+                method = 'GET'
+                url    = '/groups?$top=999&$select=id,displayName'
+            }
             @{
                 id     = 'ManagedAppPolicies'
                 method = 'GET'
-                url    = '/deviceAppManagement/managedAppPolicies?$orderby=displayName'
+                url    = '/deviceAppManagement/managedAppPolicies?$expand=assignments&$orderby=displayName'
             }
             @{
                 id     = 'MobileAppConfigurations'
                 method = 'GET'
-                url    = '/deviceAppManagement/mobileAppConfigurations?$orderby=displayName'
+                url    = '/deviceAppManagement/mobileAppConfigurations?$expand=assignments&$orderby=displayName'
             }
         )
 
         $BulkResults = New-GraphBulkRequest -Requests $BulkRequests -tenantid $TenantFilter
 
+        # Extract groups for resolving assignment names
+        $Groups = ($BulkResults | Where-Object { $_.id -eq 'Groups' }).body.value
+
         $GraphRequest = [System.Collections.Generic.List[object]]::new()
 
-        # Process Managed App Policies
+        # Process Managed App Policies - these need separate assignment lookups
         $ManagedAppPolicies = ($BulkResults | Where-Object { $_.id -eq 'ManagedAppPolicies' }).body.value
         if ($ManagedAppPolicies) {
+            # Build bulk requests for assignments of policies that support them
+            $AssignmentRequests = [System.Collections.Generic.List[object]]::new()
+            foreach ($Policy in $ManagedAppPolicies) {
+                # Only certain policy types support assignments endpoint
+                $odataType = $Policy.'@odata.type'
+                if ($odataType -match 'androidManagedAppProtection|iosManagedAppProtection|windowsManagedAppProtection|targetedManagedAppConfiguration') {
+                    $urlSegment = switch -Wildcard ($odataType) {
+                        '*androidManagedAppProtection*' { 'androidManagedAppProtections' }
+                        '*iosManagedAppProtection*' { 'iosManagedAppProtections' }
+                        '*windowsManagedAppProtection*' { 'windowsManagedAppProtections' }
+                        '*targetedManagedAppConfiguration*' { 'targetedManagedAppConfigurations' }
+                    }
+                    if ($urlSegment) {
+                        $AssignmentRequests.Add(@{
+                                id     = $Policy.id
+                                method = 'GET'
+                                url    = "/deviceAppManagement/$urlSegment('$($Policy.id)')/assignments"
+                            })
+                    }
+                }
+            }
+
+            # Fetch assignments in bulk if we have any
+            $AssignmentResults = @{}
+            if ($AssignmentRequests.Count -gt 0) {
+                $AssignmentBulkResults = New-GraphBulkRequest -Requests $AssignmentRequests -tenantid $TenantFilter
+                foreach ($result in $AssignmentBulkResults) {
+                    if ($result.body.value) {
+                        $AssignmentResults[$result.id] = $result.body.value
+                    }
+                }
+            }
+
             foreach ($Policy in $ManagedAppPolicies) {
                 $policyType = switch -Wildcard ($Policy.'@odata.type') {
                     '*androidManagedAppProtection*' { 'Android App Protection' }
@@ -47,14 +88,39 @@
                     '*defaultManagedAppProtection*' { 'Default App Protection' }
                     default { 'App Protection Policy' }
                 }
+
+                # Process assignments
+                $PolicyAssignment = [System.Collections.Generic.List[string]]::new()
+                $PolicyExclude = [System.Collections.Generic.List[string]]::new()
+                $Assignments = $AssignmentResults[$Policy.id]
+                if ($Assignments) {
+                    foreach ($Assignment in $Assignments) {
+                        $target = $Assignment.target
+                        switch ($target.'@odata.type') {
+                            '#microsoft.graph.allDevicesAssignmentTarget' { $PolicyAssignment.Add('All Devices') }
+                            '#microsoft.graph.allLicensedUsersAssignmentTarget' { $PolicyAssignment.Add('All Licensed Users') }
+                            '#microsoft.graph.groupAssignmentTarget' {
+                                $groupName = ($Groups | Where-Object { $_.id -eq $target.groupId }).displayName
+                                if ($groupName) { $PolicyAssignment.Add($groupName) }
+                            }
+                            '#microsoft.graph.exclusionGroupAssignmentTarget' {
+                                $groupName = ($Groups | Where-Object { $_.id -eq $target.groupId }).displayName
+                                if ($groupName) { $PolicyExclude.Add($groupName) }
+                            }
+                        }
+                    }
+                }
+
                 $Policy | Add-Member -NotePropertyName 'PolicyTypeName' -NotePropertyValue $policyType -Force
                 $Policy | Add-Member -NotePropertyName 'URLName' -NotePropertyValue 'managedAppPolicies' -Force
                 $Policy | Add-Member -NotePropertyName 'PolicySource' -NotePropertyValue 'AppProtection' -Force
+                $Policy | Add-Member -NotePropertyName 'PolicyAssignment' -NotePropertyValue ($PolicyAssignment -join ', ') -Force
+                $Policy | Add-Member -NotePropertyName 'PolicyExclude' -NotePropertyValue ($PolicyExclude -join ', ') -Force
                 $GraphRequest.Add($Policy)
             }
         }
 
-        # Process Mobile App Configurations
+        # Process Mobile App Configurations - assignments are already expanded
         $MobileAppConfigs = ($BulkResults | Where-Object { $_.id -eq 'MobileAppConfigurations' }).body.value
         if ($MobileAppConfigs) {
             foreach ($Config in $MobileAppConfigs) {
@@ -64,9 +130,33 @@
                     '*iosMobileAppConfiguration*' { 'iOS App Configuration' }
                     default { 'App Configuration Policy' }
                 }
+
+                # Process assignments
+                $PolicyAssignment = [System.Collections.Generic.List[string]]::new()
+                $PolicyExclude = [System.Collections.Generic.List[string]]::new()
+                if ($Config.assignments) {
+                    foreach ($Assignment in $Config.assignments) {
+                        $target = $Assignment.target
+                        switch ($target.'@odata.type') {
+                            '#microsoft.graph.allDevicesAssignmentTarget' { $PolicyAssignment.Add('All Devices') }
+                            '#microsoft.graph.allLicensedUsersAssignmentTarget' { $PolicyAssignment.Add('All Licensed Users') }
+                            '#microsoft.graph.groupAssignmentTarget' {
+                                $groupName = ($Groups | Where-Object { $_.id -eq $target.groupId }).displayName
+                                if ($groupName) { $PolicyAssignment.Add($groupName) }
+                            }
+                            '#microsoft.graph.exclusionGroupAssignmentTarget' {
+                                $groupName = ($Groups | Where-Object { $_.id -eq $target.groupId }).displayName
+                                if ($groupName) { $PolicyExclude.Add($groupName) }
+                            }
+                        }
+                    }
+                }
+
                 $Config | Add-Member -NotePropertyName 'PolicyTypeName' -NotePropertyValue $policyType -Force
                 $Config | Add-Member -NotePropertyName 'URLName' -NotePropertyValue 'mobileAppConfigurations' -Force
                 $Config | Add-Member -NotePropertyName 'PolicySource' -NotePropertyValue 'AppConfiguration' -Force
+                $Config | Add-Member -NotePropertyName 'PolicyAssignment' -NotePropertyValue ($PolicyAssignment -join ', ') -Force
+                $Config | Add-Member -NotePropertyName 'PolicyExclude' -NotePropertyValue ($PolicyExclude -join ', ') -Force
 
                 # Ensure isAssigned property exists for consistency
                 if (-not $Config.PSObject.Properties['isAssigned']) {
