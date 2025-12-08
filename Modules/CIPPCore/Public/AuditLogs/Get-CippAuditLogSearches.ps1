cd1f2da41847410668954b24ab8ff6c9113949ee
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2025-12-08 12:29:07 +0100

    profiling audit logs

--- Modules/CIPPCore/Public/AuditLogs/Get-CippAuditLogSearches.ps1
+++ Modules/CIPPCore/Public/AuditLogs/Get-CippAuditLogSearches.ps1
@@ -13,30 +13,47 @@ function Get-CippAuditLogSearches {
         [Parameter()]
         [switch]$ReadyToProcess
     )
-    $AuditLogSearchesTable = Get-CippTable -TableName 'AuditLogSearches'
-    if ($ReadyToProcess.IsPresent) {
-        $15MinutesAgo = (Get-Date).AddMinutes(-15).ToUniversalTime().ToString('yyyy-MM-ddTHH:mm:ssZ')
-        $1DayAgo = (Get-Date).AddDays(-1).ToUniversalTime().ToString('yyyy-MM-ddTHH:mm:ssZ')
-        $PendingQueries = Get-CIPPAzDataTableEntity @AuditLogSearchesTable -Filter "PartitionKey eq 'Search' and Tenant eq '$TenantFilter' and (CippStatus eq 'Pending' or (CippStatus eq 'Processing' and Timestamp le datetime'$15MinutesAgo')) and Timestamp ge datetime'$1DayAgo'" | Sort-Object Timestamp
-    } else {
-        $7DaysAgo = (Get-Date).AddDays(-7).ToUniversalTime().ToString('yyyy-MM-ddTHH:mm:ssZ')
-        $PendingQueries = Get-CIPPAzDataTableEntity @AuditLogSearchesTable -Filter "Tenant eq '$TenantFilter' and Timestamp ge datetime'$7DaysAgo'"
-    }
 
-    $BulkRequests = foreach ($PendingQuery in $PendingQueries) {
-        @{
-            id     = $PendingQuery.RowKey
-            url    = 'security/auditLog/queries/' + $PendingQuery.RowKey
-            method = 'GET'
+    Measure-CippTask -TaskName 'GetAuditLogSearches' -EventName 'CIPP.AuditLogsProfile' -Script {
+        $AuditLogSearchesTable = Get-CippTable -TableName 'AuditLogSearches'
+
+        if ($ReadyToProcess.IsPresent) {
+            Measure-CippTask -TaskName 'QueryReadyToProcess' -EventName 'CIPP.AuditLogsProfile' -Script {
+                $15MinutesAgo = (Get-Date).AddMinutes(-15).ToUniversalTime().ToString('yyyy-MM-ddTHH:mm:ssZ')
+                $1DayAgo = (Get-Date).AddDays(-1).ToUniversalTime().ToString('yyyy-MM-ddTHH:mm:ssZ')
+                Get-CIPPAzDataTableEntity @AuditLogSearchesTable -Filter "PartitionKey eq 'Search' and Tenant eq '$TenantFilter' and (CippStatus eq 'Pending' or (CippStatus eq 'Processing' and Timestamp le datetime'$15MinutesAgo')) and Timestamp ge datetime'$1DayAgo'" | Sort-Object Timestamp
+            }
+        } else {
+            Measure-CippTask -TaskName 'QueryAllSearches' -EventName 'CIPP.AuditLogsProfile' -Script {
+                $7DaysAgo = (Get-Date).AddDays(-7).ToUniversalTime().ToString('yyyy-MM-ddTHH:mm:ssZ')
+                Get-CIPPAzDataTableEntity @AuditLogSearchesTable -Filter "Tenant eq '$TenantFilter' and Timestamp ge datetime'$7DaysAgo'"
+            }
         }
     }
+
+    Measure-CippTask -TaskName 'BuildBulkRequests' -EventName 'CIPP.AuditLogsProfile' -Script {
+        $BulkRequests = foreach ($PendingQuery in $PendingQueries) {
+            @{
+                id     = $PendingQuery.RowKey
+                url    = 'security/auditLog/queries/' + $PendingQuery.RowKey
+                method = 'GET'
+            }
+        }
+        $BulkRequests
+    }
+
     if ($BulkRequests.Count -eq 0) {
         return @()
     }
-    $Queries = New-GraphBulkRequest -Requests @($BulkRequests) -AsApp $true -TenantId $TenantFilter | Select-Object -ExpandProperty body
+
+    $Queries = Measure-CippTask -TaskName 'ExecuteBulkGraphRequests' -EventName 'CIPP.AuditLogsProfile' -Script {
+        New-GraphBulkRequest -Requests @($BulkRequests) -AsApp $true -TenantId $TenantFilter | Select-Object -ExpandProperty body
+    }
 
     if ($ReadyToProcess.IsPresent) {
-        $Queries = $Queries | Where-Object { $PendingQueries.RowKey -contains $_.id -and $_.status -eq 'succeeded' }
+        $Queries = Measure-CippTask -TaskName 'FilterSucceededQueries' -EventName 'CIPP.AuditLogsProfile' -Script {
+            $Queries | Where-Object { $PendingQueries.RowKey -contains $_.id -and $_.status -eq 'succeeded' }
+        }
     }
 
     return $Queries
