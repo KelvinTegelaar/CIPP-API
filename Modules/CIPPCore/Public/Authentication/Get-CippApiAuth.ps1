3f6104d72827a703b06badea71708a32c7f114f9
John Duprey <jwd@johnwduprey.com>
2025-12-12 15:22:12 -0500

    Use WEBSITE_AUTH_V2_CONFIG_JSON for auth settings if available
    The function now checks for the WEBSITE_AUTH_V2_CONFIG_JSON environment variable and uses it to obtain authentication settings if present, reducing the need for an additional REST call. Falls back to the previous method if the variable is not set.

--- Modules/CIPPCore/Public/Authentication/Get-CippApiAuth.ps1
+++ Modules/CIPPCore/Public/Authentication/Get-CippApiAuth.ps1
@@ -4,18 +4,24 @@ function Get-CippApiAuth {
         [string]$FunctionAppName
     )
 
-    if ($env:MSI_SECRET) {
-        Disable-AzContextAutosave -Scope Process | Out-Null
-        $null = Connect-AzAccount -Identity
-        $SubscriptionId = $env:WEBSITE_OWNER_NAME -split '\+' | Select-Object -First 1
-        $Context = Set-AzContext -SubscriptionId $SubscriptionId
-    } else {
-        $Context = Get-AzContext
-        $SubscriptionId = $Context.Subscription.Id
+    if ($env:WEBSITE_AUTH_V2_CONFIG_JSON) {
+        $AuthSettings = $env:WEBSITE_AUTH_V2_CONFIG_JSON | ConvertFrom-Json -ErrorAction SilentlyContinue
     }
 
-    # Get auth settings
-    $AuthSettings = Invoke-AzRestMethod -Uri "https://management.azure.com/subscriptions/$SubscriptionId/resourceGroups/$RGName/providers/Microsoft.Web/sites/$($FunctionAppName)/config/authsettingsV2/list?api-version=2020-06-01" -ErrorAction Stop | Select-Object -ExpandProperty Content | ConvertFrom-Json
+    if (-not $AuthSettings) {
+        if ($env:MSI_SECRET) {
+            Disable-AzContextAutosave -Scope Process | Out-Null
+            $null = Connect-AzAccount -Identity
+            $SubscriptionId = $env:WEBSITE_OWNER_NAME -split '\+' | Select-Object -First 1
+            $Context = Set-AzContext -SubscriptionId $SubscriptionId
+        } else {
+            $Context = Get-AzContext
+            $SubscriptionId = $Context.Subscription.Id
+        }
+
+        # Get auth settings
+        $AuthSettings = Invoke-AzRestMethod -Uri "https://management.azure.com/subscriptions/$SubscriptionId/resourceGroups/$RGName/providers/Microsoft.Web/sites/$($FunctionAppName)/config/authsettingsV2/list?api-version=2020-06-01" -ErrorAction Stop | Select-Object -ExpandProperty Content | ConvertFrom-Json
+    }
 
     if ($AuthSettings.properties) {
         [PSCustomObject]@{
