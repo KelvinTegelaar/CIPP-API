4e6385b1e0d7d17d45b98107f57a5ae09ea4d1ec
John Duprey <jwd@johnwduprey.com>
2025-12-16 14:42:05 -0500

    Update Get-CippApiAuth.ps1

--- Modules/CIPPCore/Public/Authentication/Get-CippApiAuth.ps1
+++ Modules/CIPPCore/Public/Authentication/Get-CippApiAuth.ps1
@@ -6,10 +6,14 @@ function Get-CippApiAuth {
 
     $SubscriptionId = Get-CIPPAzFunctionAppSubId
 
-    # Get auth settings via REST
-    $uri = "https://management.azure.com/subscriptions/$SubscriptionId/resourceGroups/$RGName/providers/Microsoft.Web/sites/$($FunctionAppName)/config/authsettingsV2/list?api-version=2020-06-01"
-    $response = New-CIPPAzRestRequest -Uri $uri -Method POST -ErrorAction Stop
-    $AuthSettings = $response.properties
+    try {
+        # Get auth settings via REST
+        $uri = "https://management.azure.com/subscriptions/$SubscriptionId/resourceGroups/$RGName/providers/Microsoft.Web/sites/$($FunctionAppName)/config/authsettingsV2/list?api-version=2020-06-01"
+        $response = New-CIPPAzRestRequest -Uri $uri -Method POST -ErrorAction Stop
+        $AuthSettings = $response.properties
+    } catch {
+        Write-Warning "Failed to get auth settings via REST: $($_.Exception.Message)"
+    }
 
     if (!$AuthSettings -and $env:WEBSITE_AUTH_V2_CONFIG_JSON) {
         $AuthSettings = $env:WEBSITE_AUTH_V2_CONFIG_JSON | ConvertFrom-Json -ErrorAction SilentlyContinue
