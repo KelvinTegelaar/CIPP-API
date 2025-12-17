41895aae79e04a924abc512b443dddf0ff384f50
John Duprey <jwd@johnwduprey.com>
2025-12-16 23:09:30 -0500

    Add tag support to console logging messages
    Log messages now include tags if provided, formatted as a comma-separated list in square brackets before the message. This enhances log clarity by associating tags directly with their messages.

--- Modules/CIPPCore/Public/Tools/Enable-CippConsoleLogging.ps1
+++ Modules/CIPPCore/Public/Tools/Enable-CippConsoleLogging.ps1
@@ -57,7 +57,13 @@ function Enable-CippConsoleLogging {
         if ($PSBoundParameters.ContainsKey('MessageData') -and $MessageData) {
             # Send to telemetry
             if (-not [string]::IsNullOrWhiteSpace(($MessageData | Out-String).Trim())) {
-                Send-CippConsoleLog -Message ($MessageData | Out-String).Trim() -Level 'Information'
+                # If tag is supplied, include it in the log message
+                $LogMessage = if ($Tags -and $Tags.Count -gt 0) {
+                    '[{0}] {1}' -f ($Tags -join ','), ($MessageData | Out-String).Trim()
+                } else {
+                    ($MessageData | Out-String).Trim()
+                }
+                Send-CippConsoleLog -Message $LogMessage -Level 'Information'
             }
 
             # Call original function
