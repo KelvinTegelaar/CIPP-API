2ad0dcfcc60b172c0c88ae81df1e02f4fda892c9
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2025-12-22 00:29:21 +0100

    db request add type

--- Modules/CIPPCore/Public/New-CIPPDbRequest.ps1
+++ Modules/CIPPCore/Public/New-CIPPDbRequest.ps1
@@ -9,24 +9,38 @@ function New-CIPPDbRequest {
     .PARAMETER TenantFilter
         The tenant domain or GUID to filter by (used as partition key)
 
+    .PARAMETER Type
+        Optional. The data type to filter by (e.g., Users, Groups, Devices)
+
     .EXAMPLE
         New-CIPPDbRequest -TenantFilter 'contoso.onmicrosoft.com'
+
+    .EXAMPLE
+        New-CIPPDbRequest -TenantFilter 'contoso.onmicrosoft.com' -Type 'Users'
     #>
     [CmdletBinding()]
     param(
         [Parameter(Mandatory = $true)]
-        [string]$TenantFilter
+        [string]$TenantFilter,
+
+        [Parameter(Mandatory = $false)]
+        [string]$Type
     )
 
     try {
         $Table = Get-CippTable -tablename 'CippReportingDB'
-        $Filter = "PartitionKey eq '{0}'" -f $TenantFilter
+
+        if ($Type) {
+            $Filter = "PartitionKey eq '{0}' and RowKey ge '{1}-' and RowKey lt '{1}.'" -f $TenantFilter, $Type
+        } else {
+            $Filter = "PartitionKey eq '{0}'" -f $TenantFilter
+        }
+
         $Results = Get-CIPPAzDataTableEntity @Table -Filter $Filter
 
-        return $Results
+        return ($Results.Data | ConvertFrom-Json -ErrorAction SilentlyContinue)
     } catch {
-        Write-LogMessage -API 'CIPPDbRequest' -tenant $TenantFilter `
-            -message "Failed to query database: $($_.Exception.Message)" -sev Error
+        Write-LogMessage -API 'CIPPDbRequest' -tenant $TenantFilter -message "Failed to query database: $($_.Exception.Message)" -sev Error
         throw
     }
 }
