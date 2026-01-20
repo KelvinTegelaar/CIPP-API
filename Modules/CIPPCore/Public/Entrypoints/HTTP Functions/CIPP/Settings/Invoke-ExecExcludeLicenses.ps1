b768b5ed7dfdb180d7cc1090d59eb8d650a08d18
Kristian Kjærgård <kris6673@hotmail.com>
2026-01-20 23:06:20 +0100

    feat: refactor license exclusion management
    - Refactor Invoke-ExecExcludeLicenses function to only manage excluded licenses.
    - Introduce action for restoring default exclusions.
    - Split list functionality into Invoke-ListExcludedLicenses function.
    - Implement Initialize-CIPPExcludedLicenses function to populate excluded licenses from config instead of hardcoded variable.

--- Modules/CIPPCore/Public/Entrypoints/HTTP Functions/CIPP/Settings/Invoke-ExecExcludeLicenses.ps1
+++ Modules/CIPPCore/Public/Entrypoints/HTTP Functions/CIPP/Settings/Invoke-ExecExcludeLicenses.ps1
@@ -11,65 +11,59 @@ function Invoke-ExecExcludeLicenses {
     $APIName = $Request.Params.CIPPEndpoint
     $Headers = $Request.Headers
     $Table = Get-CIPPTable -TableName ExcludedLicenses
+
+    # Interact with query parameters or the body of the request.
     try {
-
-        if ($Request.Query.List) {
-            $Rows = Get-CIPPAzDataTableEntity @Table
-
-            # If no excluded licenses exist, initialize from config file
-            if ($Rows.Count -lt 1) {
-                Write-Information "Excluded licenses count is low ($($Rows.Count)). Initializing from config file."
-                $CIPPCoreModuleRoot = Get-Module -Name CIPPCore | Select-Object -ExpandProperty ModuleBase
-                $CIPPRoot = (Get-Item $CIPPCoreModuleRoot).Parent.Parent
-                $TableBaseData = Get-Content -Path (Join-Path $CIPPRoot 'Config\ExcludeSkuList.JSON') -Raw | ConvertFrom-Json -AsHashtable -Depth 10
-                $null = foreach ($Row in $TableBaseData) {
-                    $Row.PartitionKey = 'License'
-                    $Row.RowKey = $Row.GUID
-
-                    Add-CIPPAzDataTableEntity @Table -Entity ([pscustomobject]$Row) -Force | Out-Null
-                }
-
-                $Rows = Get-CIPPAzDataTableEntity @Table
-
-                Write-LogMessage -API $APIName -headers $Headers -message "Initialized $($TableBaseData.Count) excluded licenses from config file" -Sev 'Info'
-            }
-            $body = @($Rows)
-        }
-
-        # Interact with query parameters or the body of the request.
+        $Action = $Request.Body.Action
         $GUID = $Request.Body.GUID
         $DisplayName = $Request.Body.SKUName
-        if ($Request.Query.AddExclusion) {
-            $AddObject = @{
-                PartitionKey           = 'License'
-                RowKey                 = $GUID
-                'GUID'                 = $GUID
-                'Product_Display_Name' = $DisplayName
+
+        switch ($Action) {
+            'AddExclusion' {
+                $AddObject = @{
+                    PartitionKey           = 'License'
+                    RowKey                 = $GUID
+                    'GUID'                 = $GUID
+                    'Product_Display_Name' = $DisplayName
+                }
+                Add-CIPPAzDataTableEntity @Table -Entity $AddObject -Force
+                $Result = "Success. Added $DisplayName($GUID) to the excluded licenses list."
+                Write-LogMessage -API $APIName -headers $Headers -message $Result -Sev 'Info'
+
             }
-            Add-CIPPAzDataTableEntity @Table -Entity $AddObject -Force
+            'RemoveExclusion' {
+                $Filter = "RowKey eq '{0}' and PartitionKey eq 'License'" -f $GUID
+                $Entity = Get-CIPPAzDataTableEntity @Table -Filter $Filter -Property PartitionKey, RowKey
+                Remove-AzDataTableEntity -Force @Table -Entity $Entity
+                $Result = "Success. Removed $DisplayName($GUID) from the excluded licenses list."
+                Write-LogMessage -API $APIName -headers $Headers -message $Result -Sev 'Info'
 
-            Write-LogMessage -API $APIName -headers $Headers -message "Added exclusion $DisplayName" -Sev 'Info'
-            $body = [pscustomobject]@{'Results' = "Success. We've added $DisplayName to the excluded list." }
+            }
+            'RestoreDefaults' {
+                $FullReset = [bool]$Request.Body.FullReset
+                if ($FullReset) {
+                    $InitResult = Initialize-CIPPExcludedLicenses -Force -Headers $Headers -APIName $APIName
+                } else {
+                    $InitResult = Initialize-CIPPExcludedLicenses -Headers $Headers -APIName $APIName
+                }
+                $Result = $InitResult.Message
+
+            }
+            default {
+                $StatusCode = [HttpStatusCode]::BadRequest
+                $Result = "Invalid action specified: $Action"
+            }
         }
 
-        if ($Request.Query.RemoveExclusion) {
-            $Filter = "RowKey eq '{0}' and PartitionKey eq 'License'" -f $GUID
-            $Entity = Get-CIPPAzDataTableEntity @Table -Filter $Filter -Property PartitionKey, RowKey
-            Remove-AzDataTableEntity -Force @Table -Entity $Entity
-            Write-LogMessage -API $APIName -headers $Headers -message "Removed exclusion $GUID" -Sev 'Info'
-            $body = [pscustomobject]@{'Results' = "Success. We've removed $GUID from the excluded list." }
-        }
     } catch {
         $ErrorMessage = Get-CippException -Exception $_
         $StatusCode = [HttpStatusCode]::InternalServerError
         $Result = "Failed to process exclusion request. $($ErrorMessage.NormalizedError)"
         Write-LogMessage -API $APIName -headers $Headers -message $Result -Sev 'Error' -LogData $ErrorMessage
-        $body = [pscustomobject]@{'Results' = $Result }
     }
 
     return ([HttpResponseContext]@{
             StatusCode = $StatusCode ?? [HttpStatusCode]::OK
-            Body       = $body
+            Body       = [pscustomobject]@{ 'Results' = $Result }
         })
-
 }
