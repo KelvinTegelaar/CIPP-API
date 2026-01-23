dc6c7e7ffe0e7e1e80255173049c211aff237a21
rvdwegen <rvdwegen@vdwegen.app>
2026-01-23 01:20:58 +0100

    Make version better available on azure function app interaction

--- profile.ps1
+++ profile.ps1
@@ -97,6 +97,7 @@ $CurrentVersion = (Get-Content -Path (Join-Path $PSScriptRoot 'version_latest.tx
 $Table = Get-CippTable -tablename 'Version'
 Write-Information "Function App: $($env:WEBSITE_SITE_NAME) | API Version: $CurrentVersion | PS Version: $($PSVersionTable.PSVersion)"
 $global:CippVersion = $CurrentVersion
+$ENV:CurrentVersion = $CurrentVersion
 
 $LastStartup = Get-CIPPAzDataTableEntity @Table -Filter "PartitionKey eq 'Version' and RowKey eq '$($env:WEBSITE_SITE_NAME)'"
 if (!$LastStartup -or $CurrentVersion -ne $LastStartup.Version) {
