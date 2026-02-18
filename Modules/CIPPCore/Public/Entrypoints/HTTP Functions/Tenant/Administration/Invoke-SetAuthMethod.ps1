bc49356ef6557a8a2decd66f1e5ffecf97e17d8e
Integrated Solutions <159874617+isgq-github01@users.noreply.github.com>
2026-02-18 15:16:21 +1000

    Update Invoke-SetAuthMethod.ps1 - added functionality for deploying to groups

--- Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Tenant/Administration/Invoke-SetAuthMethod.ps1
+++ Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Tenant/Administration/Invoke-SetAuthMethod.ps1
@@ -15,34 +15,7 @@ function Invoke-SetAuthMethod {
     $State = if ($Request.Body.state -eq 'enabled') { $true } else { $false }
     $TenantFilter = $Request.Body.tenantFilter
     $AuthenticationMethodId = $Request.Body.Id
-    $GroupIdsRaw = $Request.Body.GroupIds
-
-    function Get-StandardizedList {
-        param($InputObject)
-
-        if ($null -eq $InputObject) { return @() }
-
-        if ($InputObject -is [string]) {
-            return @(
-                $InputObject -split ',' |
-                    ForEach-Object { $_.Trim() } |
-                    Where-Object { -not [string]::IsNullOrWhiteSpace($_) }
-            )
-        }
-
-        if ($InputObject -is [array] -or $InputObject -is [System.Collections.IEnumerable]) {
-            return @(
-                $InputObject |
-                    ForEach-Object { "$_".Trim() } |
-                    Where-Object { -not [string]::IsNullOrWhiteSpace($_) }
-            )
-        }
-
-        return @("$InputObject".Trim()) | Where-Object { -not [string]::IsNullOrWhiteSpace($_) }
-    }
-
-    $GroupIds = Get-StandardizedList -InputObject $GroupIdsRaw
-
+    $GroupIds = $Request.Body.GroupIds
 
     try {
         $Params = @{
@@ -52,7 +25,7 @@ function Invoke-SetAuthMethod {
             Enabled                = $State
             Headers                = $Headers
         }
-        if (@($GroupIds).Count -gt 0) {
+        if ($GroupIds) {
             $Params.GroupIds = @($GroupIds)
         }
         $Result = Set-CIPPAuthenticationPolicy @Params
