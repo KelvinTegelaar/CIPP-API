67ee365b4d7a9bb59b93b5020be15e1e451daf89
John Duprey <jwd@johnwduprey.com>
2026-03-07 22:31:43 -0500

    feat: enhance scheduled task management by adding duplicate name prevention and improved removal feedback

--- Modules/CIPPCore/Public/Entrypoints/HTTP Functions/CIPP/Core/Invoke-ExecSetCIPPAutoBackup.ps1
+++ Modules/CIPPCore/Public/Entrypoints/HTTP Functions/CIPP/Core/Invoke-ExecSetCIPPAutoBackup.ps1
@@ -28,8 +28,18 @@ function Invoke-ExecSetCIPPAutoBackup {
             ScheduledTime = $unixtime
             Recurrence    = '1d'
         }
-        Add-CIPPScheduledTask -Task $TaskBody -hidden $false
+        Add-CIPPScheduledTask -Task $TaskBody -hidden $false -DisallowDuplicateName $true
         $Result = @{ 'Results' = 'Scheduled Task Successfully created' }
+    } elseif ($Request.Body.Enabled -eq $false) {
+        $Table = Get-CIPPTable -TableName 'ScheduledTasks'
+        $AutomatedCIPPBackupTask = Get-AzDataTableEntity @table -Filter "Name eq 'Automated CIPP Backup'" -Property RowKey, PartitionKey, ETag
+        if ($AutomatedCIPPBackupTask) {
+            Remove-AzDataTableEntity -Force @Table -Entity $AutomatedCIPPBackupTask | Out-Null
+            $Result = @{ 'Results' = 'Scheduled Task Successfully removed' }
+        } else {
+            $Result = @{ 'Results' = 'No existing scheduled task found to remove' }
+        }
+
     }
     Write-LogMessage -headers $Request.Headers -API $Request.Params.CIPPEndpoint -message 'Scheduled automatic CIPP backups' -Sev 'Info'
     return ([HttpResponseContext]@{
