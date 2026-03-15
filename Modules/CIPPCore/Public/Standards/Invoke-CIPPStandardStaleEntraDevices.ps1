d4b200231944187a717e45ea7de25868925584ec
Zacgoose <107489668+Zacgoose@users.noreply.github.com>
2026-03-15 18:59:09 +0800

    Add remediate option to Stale Entra Devices

--- Modules/CIPPCore/Public/Standards/Invoke-CIPPStandardStaleEntraDevices.ps1
+++ Modules/CIPPCore/Public/Standards/Invoke-CIPPStandardStaleEntraDevices.ps1
@@ -7,8 +7,8 @@ function Invoke-CIPPStandardStaleEntraDevices {
     .SYNOPSIS
         (Label) Cleanup stale Entra devices
     .DESCRIPTION
-        (Helptext) Remediate is currently not available. Cleans up Entra devices that have not connected/signed in for the specified number of days.
-        (DocsDescription) Remediate is currently not available. Cleans up Entra devices that have not connected/signed in for the specified number of days. First disables and later deletes the devices. More info can be found in the [Microsoft documentation](https://learn.microsoft.com/en-us/entra/identity/devices/manage-stale-devices)
+        (Helptext) Cleans up Entra devices that have not connected/signed in for the specified number of days. Remediation first disables stale enabled devices and later deletes stale already-disabled devices.
+        (DocsDescription) Cleans up Entra devices that have not connected/signed in for the specified number of days. Remediation first disables stale enabled devices and later deletes stale already-disabled devices. More info can be found in the [Microsoft documentation](https://learn.microsoft.com/en-us/entra/identity/devices/manage-stale-devices)
     .NOTES
         CAT
             Entra (AAD) Standards
@@ -19,7 +19,8 @@ function Invoke-CIPPStandardStaleEntraDevices {
         EXECUTIVETEXT
             Automatically identifies and removes inactive devices that haven't connected to company systems for a specified period, reducing security risks from abandoned or lost devices. This maintains a clean device inventory and prevents potential unauthorized access through dormant device registrations.
         ADDEDCOMPONENT
-            {"type":"number","name":"standards.StaleEntraDevices.deviceAgeThreshold","label":"Days before stale(Do not set below 30)"}
+            {"type":"number","name":"standards.StaleEntraDevices.deviceAgeThreshold","label":"Days before stale (Disables the device after this many days of inactivity)"}
+            {"type":"number","name":"standards.StaleEntraDevices.deviceDeleteThreshold","label":"Days before stale devices are deleted (0 means devices will NOT be deleted)"}
         DISABLEDFEATURES
             {"report":false,"warn":false,"remediate":false}
         IMPACT
@@ -36,16 +37,9 @@ function Invoke-CIPPStandardStaleEntraDevices {
     #>
 
     param($Tenant, $Settings)
-    $TestResult = Test-CIPPStandardLicense -StandardName 'StaleEntraDevices' -TenantFilter $Tenant -RequiredCapabilities @('INTUNE_A', 'MDM_Services', 'EMS', 'SCCM', 'MICROSOFTINTUNEPLAN1')
-
-    # Get all Entra devices
-
-    if ($TestResult -eq $false) {
-        return $true
-    } #we're done.
 
     try {
-        $AllDevices = New-GraphGetRequest -uri 'https://graph.microsoft.com/beta/devices?$select=id,displayName,approximateLastSignInDateTime,accountEnabled,enrollmentProfileName,operatingSystem,managementType,profileType' -tenantid $Tenant | Where-Object { $null -ne $_.approximateLastSignInDateTime }
+        $AllDevices = New-GraphGetRequest -uri 'https://graph.microsoft.com/beta/devices?$select=id,displayName,approximateLastSignInDateTime,accountEnabled,enrollmentProfileName,operatingSystem,managementType,profileType,onPremisesSyncEnabled,isManaged,isCompliant' -tenantid $Tenant | Where-Object { $null -ne $_.approximateLastSignInDateTime }
     } catch {
         $ErrorMessage = Get-NormalizedError -Message $_.Exception.Message
         Write-LogMessage -API 'Standards' -Tenant $Tenant -Message "Could not get the StaleEntraDevices state for $Tenant. Error: $ErrorMessage" -Sev Error
@@ -53,18 +47,123 @@ function Invoke-CIPPStandardStaleEntraDevices {
     }
 
     $Date = (Get-Date).AddDays( - [int]$Settings.deviceAgeThreshold)
+    $DeleteDate = (Get-Date).AddDays( - [int]$Settings.deviceDeleteThreshold)
     $StaleDevices = $AllDevices | Where-Object { $_.approximateLastSignInDateTime -lt $Date }
+    $DeleteDevices = $AllDevices | Where-Object { $_.approximateLastSignInDateTime -lt $DeleteDate }
+
+    $RefreshRemediationEvaluation = {
+        $RemediationSkippedStaleDevices = @($StaleDevices | Where-Object { $_.onPremisesSyncEnabled -eq $true -or $_.isManaged -eq $true -or $_.isCompliant -eq $true })
+        $RemediationEligibleStaleDevices = @($StaleDevices | Where-Object { $_.onPremisesSyncEnabled -ne $true -and $_.isManaged -ne $true -and $_.isCompliant -ne $true })
+        $DeleteEligibleDevices = @($DeleteDevices | Where-Object { $_.onPremisesSyncEnabled -ne $true -and $_.isManaged -ne $true -and $_.isCompliant -ne $true })
+
+        $DevicesToDisable = @($RemediationEligibleStaleDevices | Where-Object { $_.accountEnabled -eq $true })
+        if ([int]$Settings.deviceDeleteThreshold -eq 0) {
+            $DevicesToDelete = @()
+        } else {
+            $DevicesToDelete = @($DeleteEligibleDevices | Where-Object { $_.accountEnabled -ne $true })
+        }
+    }
+
+    & $RefreshRemediationEvaluation
 
     if ($Settings.remediate -eq $true) {
-        # TODO: Implement remediation. For others in the future that want to try this:
-        # Good MS guide on what to watch out for https://learn.microsoft.com/en-us/entra/identity/devices/manage-stale-devices#clean-up-stale-devices
-        # https://learn.microsoft.com/en-us/graph/api/device-list?view=graph-rest-beta&tabs=http
-        # Properties to look at:
-        # approximateLastSignInDateTime: For knowing when the device last signed in
-        # enrollmentProfileName and operatingSystem: For knowing if it's an AutoPilot device
-        # managementType or isManaged: For knowing if it's an Intune managed device. If it is, should be removed from Intune also. Stale intune standard could possibly be used for this.
-        # profileType: For knowing if it's only registered or also managed
-        # accountEnabled: For knowing if the device is disabled or not
+        $DeletedDeviceIds = [System.Collections.Generic.List[string]]::new()
+        $SkippedCount = $RemediationSkippedStaleDevices.Count
+        $DisabledCount = 0
+        $DeletedCount = 0
+        $FailedCount = 0
+
+        if ($DevicesToDisable.Count -gt 0) {
+            $DisableRequests = [System.Collections.Generic.List[hashtable]]::new()
+            $DisableMap = @{}
+            $RequestId = 0
+
+            foreach ($Device in $DevicesToDisable) {
+                $CurrentId = $RequestId++
+                $DisableMap[$CurrentId] = $Device
+                $DisableRequests.Add(@{
+                        id        = $CurrentId
+                        method    = 'PATCH'
+                        url       = "devices/$($Device.id)"
+                        body      = @{ accountEnabled = $false }
+                        headers   = @{
+                            'Content-Type' = 'application/json'
+                        }
+                    })
+            }
+
+            try {
+                $DisableResults = New-GraphBulkRequest -tenantid $Tenant -Version 'v1.0' -Requests @($DisableRequests)
+                foreach ($Result in $DisableResults) {
+                    $Device = $DisableMap[[int]$Result.id]
+                    if ($null -eq $Device) {
+                        continue
+                    }
+
+                    if ($Result.status -eq 200 -or $Result.status -eq 204) {
+                        $DisabledCount++
+                        $Device.accountEnabled = $false
+                    } else {
+                        $FailedCount++
+                        $ErrorMessage = if ($Result.body.error.message) { $Result.body.error.message } else { "Unknown error (Status: $($Result.status))" }
+                        Write-LogMessage -API 'Standards' -Tenant $Tenant -Message "Could not disable stale device $($Device.displayName) ($($Device.id)). Error: $ErrorMessage" -Sev Error
+                    }
+                }
+            } catch {
+                $ErrorMessage = Get-NormalizedError -Message $_.Exception.Message
+                $FailedCount += $DevicesToDisable.Count
+                Write-LogMessage -API 'Standards' -Tenant $Tenant -Message "Failed to process bulk disable stale devices request. Error: $ErrorMessage" -Sev Error
+            }
+        }
+
+        if ($DevicesToDelete.Count -gt 0) {
+            $DeleteRequests = [System.Collections.Generic.List[hashtable]]::new()
+            $DeleteMap = @{}
+            $RequestId = 0
+
+            foreach ($Device in $DevicesToDelete) {
+                $CurrentId = $RequestId++
+                $DeleteMap[$CurrentId] = $Device
+                $DeleteRequests.Add(@{
+                        id      = $CurrentId
+                        method  = 'DELETE'
+                        url     = "devices/$($Device.id)"
+                    })
+            }
+
+            try {
+                $DeleteResults = New-GraphBulkRequest -tenantid $Tenant -Version 'v1.0' -Requests @($DeleteRequests)
+                foreach ($Result in $DeleteResults) {
+                    $Device = $DeleteMap[[int]$Result.id]
+                    if ($null -eq $Device) {
+                        continue
+                    }
+
+                    if ($Result.status -eq 200 -or $Result.status -eq 204) {
+                        $DeletedCount++
+                        $null = $DeletedDeviceIds.Add([string]$Device.id)
+                    } else {
+                        $FailedCount++
+                        $ErrorMessage = if ($Result.body.error.message) { $Result.body.error.message } else { "Unknown error (Status: $($Result.status))" }
+                        Write-LogMessage -API 'Standards' -Tenant $Tenant -Message "Could not delete stale disabled device $($Device.displayName) ($($Device.id)). Error: $ErrorMessage" -Sev Error
+                    }
+                }
+            } catch {
+                $ErrorMessage = Get-NormalizedError -Message $_.Exception.Message
+                $FailedCount += $DevicesToDelete.Count
+                Write-LogMessage -API 'Standards' -Tenant $Tenant -Message "Failed to process bulk delete stale devices request. Error: $ErrorMessage" -Sev Error
+            }
+        }
+
+        if ($DeletedDeviceIds.Count -gt 0) {
+            $StaleDevices = @($StaleDevices | Where-Object { $_.id -notin $DeletedDeviceIds })
+            $DeleteDevices = @($DeleteDevices | Where-Object { $_.id -notin $DeletedDeviceIds })
+            & $RefreshRemediationEvaluation
+        }
+
+        if ($DisabledCount -gt 0 -or $DeletedCount -gt 0 -or $FailedCount -gt 0 -or $SkippedCount -gt 0) {
+            Write-LogMessage -API 'Standards' -Tenant $Tenant -Message "StaleEntraDevices remediation completed. Disabled: $DisabledCount. Deleted: $DeletedCount. Failed: $FailedCount. Skipped: $SkippedCount (onPremisesSyncEnabled/isManaged/isCompliant)." -Sev Info
+        }
 
     }
 
@@ -72,8 +171,9 @@ function Invoke-CIPPStandardStaleEntraDevices {
     if ($Settings.alert -eq $true) {
 
         if ($StaleDevices.Count -gt 0) {
-            Write-StandardsAlert -message "$($StaleDevices.Count) Stale devices found" -object $StaleDevices -tenant $Tenant -standardName 'StaleEntraDevices' -standardId $Settings.standardId
-            Write-LogMessage -API 'Standards' -tenant $Tenant -message "$($StaleDevices.Count) Stale devices found" -sev Info
+            $AlertMessage = "$($StaleDevices.Count) stale devices found. Eligible for remediation: $($RemediationEligibleStaleDevices.Count) (disable: $($DevicesToDisable.Count), delete: $($DevicesToDelete.Count)). Skipped by safety checks: $($RemediationSkippedStaleDevices.Count)."
+            Write-StandardsAlert -message $AlertMessage -object $StaleDevices -tenant $Tenant -standardName 'StaleEntraDevices' -standardId $Settings.standardId
+            Write-LogMessage -API 'Standards' -tenant $Tenant -message $AlertMessage -sev Info
         } else {
             Write-LogMessage -API 'Standards' -tenant $Tenant -message 'No stale devices found' -sev Info
         }
@@ -96,12 +196,10 @@ function Invoke-CIPPStandardStaleEntraDevices {
         $CurrentValue = @{
             StaleDevicesCount  = $StaleDevices.Count
             StaleDevices       = ($FieldValue ? @($FieldValue) :@())
-            DeviceAgeThreshold = [int]$Settings.deviceAgeThreshold
         }
         $ExpectedValue = @{
             StaleDevicesCount  = 0
             StaleDevices       = @()
-            DeviceAgeThreshold = [int]$Settings.deviceAgeThreshold
         }
         Set-CIPPStandardsCompareField -FieldName 'standards.StaleEntraDevices' -CurrentValue $CurrentValue -ExpectedValue $ExpectedValue -Tenant $Tenant
     }
