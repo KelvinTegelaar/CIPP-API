e8c0d3258c1363de8087ce4ad6c9790e0aad3500
John Duprey <jwd@johnwduprey.com>
2026-03-25 13:08:49 -0400

    refactor: Defender deployment into separate helper scripts - Implement Set-CIPPDefenderASRPolicy to manage Attack Surface Reduction settings. - Implement Set-CIPPDefenderAVPolicy for configuring Antivirus policy settings. - Implement Set-CIPPDefenderCompliancePolicy to manage compliance settings for devices. - Implement Set-CIPPDefenderEDRPolicy for configuring Endpoint Detection and Response settings. - Implement Set-CIPPDefenderExclusionPolicy to manage exclusions for Antivirus.

---
+++ Modules/CIPPCore/Public/Set-CIPPDefenderExclusionPolicy.ps1
@@ -0,0 +1,93 @@
+function Set-CIPPDefenderExclusionPolicy {
+    <#
+    .FUNCTIONALITY
+        Internal
+    #>
+    [CmdletBinding()]
+    param(
+        [string]$TenantFilter,
+        $DefenderExclusions,
+        $Headers,
+        [string]$APIName
+    )
+
+    $ExclusionAssignTo = $DefenderExclusions.AssignTo
+    if ($DefenderExclusions.excludedExtensions) {
+        $ExcludedExtensions = $DefenderExclusions.excludedExtensions | Where-Object { $_ -and $_.Trim() } | ForEach-Object {
+            @{ '@odata.type' = '#microsoft.graph.deviceManagementConfigurationStringSettingValue'; value = $_ }
+        }
+    }
+    if ($DefenderExclusions.excludedPaths) {
+        $ExcludedPaths = $DefenderExclusions.excludedPaths | Where-Object { $_ -and $_.Trim() } | ForEach-Object {
+            @{ '@odata.type' = '#microsoft.graph.deviceManagementConfigurationStringSettingValue'; value = $_ }
+        }
+    }
+    if ($DefenderExclusions.excludedProcesses) {
+        $ExcludedProcesses = $DefenderExclusions.excludedProcesses | Where-Object { $_ -and $_.Trim() } | ForEach-Object {
+            @{ '@odata.type' = '#microsoft.graph.deviceManagementConfigurationStringSettingValue'; value = $_ }
+        }
+    }
+
+    $ExclusionSettings = [System.Collections.Generic.List[System.Object]]::new()
+    if ($ExcludedExtensions.Count -gt 0) {
+        $ExclusionSettings.Add(@{
+                id              = '2'
+                settingInstance = @{
+                    '@odata.type'                    = '#microsoft.graph.deviceManagementConfigurationSimpleSettingCollectionInstance'
+                    settingDefinitionId              = 'device_vendor_msft_policy_config_defender_excludedextensions'
+                    settingInstanceTemplateReference = @{ settingInstanceTemplateId = 'c203725b-17dc-427b-9470-673a2ce9cd5e' }
+                    simpleSettingCollectionValue     = @($ExcludedExtensions)
+                }
+            })
+    }
+    if ($ExcludedPaths.Count -gt 0) {
+        $ExclusionSettings.Add(@{
+                id              = '1'
+                settingInstance = @{
+                    '@odata.type'                    = '#microsoft.graph.deviceManagementConfigurationSimpleSettingCollectionInstance'
+                    settingDefinitionId              = 'device_vendor_msft_policy_config_defender_excludedpaths'
+                    settingInstanceTemplateReference = @{ settingInstanceTemplateId = 'aaf04adc-c639-464f-b4a7-152e784092e8' }
+                    simpleSettingCollectionValue     = @($ExcludedPaths)
+                }
+            })
+    }
+    if ($ExcludedProcesses.Count -gt 0) {
+        $ExclusionSettings.Add(@{
+                id              = '0'
+                settingInstance = @{
+                    '@odata.type'                    = '#microsoft.graph.deviceManagementConfigurationSimpleSettingCollectionInstance'
+                    settingDefinitionId              = 'device_vendor_msft_policy_config_defender_excludedprocesses'
+                    settingInstanceTemplateReference = @{ settingInstanceTemplateId = '96b046ed-f138-4250-9ae0-b0772a93d16f' }
+                    simpleSettingCollectionValue     = @($ExcludedProcesses)
+                }
+            })
+    }
+
+    if ($ExclusionSettings.Count -gt 0) {
+        $ExclusionBody = ConvertTo-Json -Depth 15 -Compress -InputObject @{
+            name              = 'Default AV Exclusion Policy'
+            displayName       = 'Default AV Exclusion Policy'
+            settings          = @($ExclusionSettings)
+            platforms         = 'windows10'
+            technologies      = 'mdm,microsoftSense'
+            templateReference = @{
+                templateId             = '45fea5e9-280d-4da1-9792-fb5736da0ca9_1'
+                templateFamily         = 'endpointSecurityAntivirus'
+                templateDisplayName    = 'Microsoft Defender Antivirus exclusions'
+                templateDisplayVersion = 'Version 1'
+            }
+        }
+        $CheckExistingExclusion = New-GraphGetRequest -uri 'https://graph.microsoft.com/beta/deviceManagement/configurationPolicies' -tenantid $TenantFilter
+        if ('Default AV Exclusion Policy' -in $CheckExistingExclusion.Name) {
+            "$($TenantFilter): Exclusion Policy already exists. Skipping"
+        } else {
+            $ExclusionRequest = New-GraphPOSTRequest -uri 'https://graph.microsoft.com/beta/deviceManagement/configurationPolicies' -tenantid $TenantFilter -type POST -body $ExclusionBody
+            if ($ExclusionAssignTo -and $ExclusionAssignTo -ne 'none') {
+                $AssignBody = if ($ExclusionAssignTo -ne 'AllDevicesAndUsers') { '{"assignments":[{"id":"","target":{"@odata.type":"#microsoft.graph.' + $($ExclusionAssignTo) + 'AssignmentTarget"}}]}' } else { '{"assignments":[{"id":"","target":{"@odata.type":"#microsoft.graph.allDevicesAssignmentTarget"}},{"id":"","target":{"@odata.type":"#microsoft.graph.allLicensedUsersAssignmentTarget"}}]}' }
+                $null = New-GraphPOSTRequest -uri "https://graph.microsoft.com/beta/deviceManagement/configurationPolicies('$($ExclusionRequest.id)')/assign" -tenantid $TenantFilter -type POST -body $AssignBody
+                Write-LogMessage -headers $Headers -API $APIName -tenant $TenantFilter -message "Assigned Exclusion policy to $($ExclusionAssignTo)" -Sev 'Info'
+            }
+            "$($TenantFilter): Successfully set Default AV Exclusion Policy settings"
+        }
+    }
+}
