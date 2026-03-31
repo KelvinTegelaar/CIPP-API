b184de2163a034045c033d6cfebdadd98c98889f
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2026-03-31 13:57:26 +0200

    add the option for db blocks

--- Modules/CIPPCore/Public/Tools/Push-ExecGenerateReportBuilderReport.ps1
+++ Modules/CIPPCore/Public/Tools/Push-ExecGenerateReportBuilderReport.ps1
@@ -49,9 +49,11 @@ function Push-ExecGenerateReportBuilderReport {
             $TestResults = @(Get-CIPPAzDataTableEntity @TestTable -Filter $TestFilter)
         }
 
-        # Build enriched blocks with fresh content for live test blocks
+        # Build enriched blocks with fresh content
         $EnrichedBlocks = @($ParsedBlocks | ForEach-Object {
                 $Block = $_
+
+                # Enrich live test blocks
                 if ($Block.type -eq 'test' -and $Block.static -ne $true -and $TestResults) {
                     $TestResult = $TestResults | Where-Object { $_.TestId -eq $Block.testId -or $_.RowKey -eq $Block.testId } | Select-Object -First 1
                     if ($TestResult) {
@@ -63,6 +65,65 @@ function Push-ExecGenerateReportBuilderReport {
                         }
                     }
                 }
+
+                # Enrich database blocks
+                if ($Block.type -eq 'database' -and $Block.dbType) {
+                    try {
+                        $DbData = New-CIPPDbRequest -TenantFilter $TenantFilter -Type $Block.dbType
+                        if ($null -ne $DbData) {
+                            $FirstItem = if ($DbData -is [array]) { $DbData[0] } else { $DbData }
+                            $SelectedHeaders = @($Block.selectedHeaders)
+                            if ($SelectedHeaders.Count -eq 0) {
+                                $ExcludedHeaders = @('id', 'rowkey', 'partitionkey', 'etag', 'timestamp')
+                                $AllHeaders = @($FirstItem.PSObject.Properties.Name | Where-Object { $_.ToLower() -notin $ExcludedHeaders }) | Sort-Object
+                                $SelectedHeaders = $AllHeaders
+                            }
+                            $Format = if ($Block.format) { $Block.format } else { 'text' }
+                            $FilteredData = @(@($DbData) | ForEach-Object {
+                                    $Row = $_
+                                    $Obj = [ordered]@{}
+                                    foreach ($Header in $SelectedHeaders) {
+                                        $Val = $Row.$Header
+                                        $Obj[$Header] = if ($null -ne $Val) { $Val } else { '' }
+                                    }
+                                    [PSCustomObject]$Obj
+                                })
+
+                            $BlockContent = switch ($Format) {
+                                'json' {
+                                    ConvertTo-Json -InputObject @($FilteredData) -Depth 10 -Compress
+                                }
+                                'csv' {
+                                    ($FilteredData | ConvertTo-Csv -NoTypeInformation) -join "`n"
+                                }
+                                default {
+                                    $HeaderLine = '| ' + ($SelectedHeaders -join ' | ') + ' |'
+                                    $SeparatorLine = '| ' + (($SelectedHeaders | ForEach-Object { '---' }) -join ' | ') + ' |'
+                                    $DataLines = @($FilteredData | ForEach-Object {
+                                            $DataRow = $_
+                                            $Cells = $SelectedHeaders | ForEach-Object {
+                                                $Val = $DataRow.$_
+                                                if ($null -eq $Val) { '' }
+                                                elseif ($Val -is [PSCustomObject] -or $Val -is [hashtable]) { ConvertTo-Json -InputObject $Val -Depth 5 -Compress }
+                                                else { "$Val" -replace '\|', '\|' -replace "`n", ' ' }
+                                            }
+                                            '| ' + ($Cells -join ' | ') + ' |'
+                                        })
+                                    (@($HeaderLine, $SeparatorLine) + $DataLines) -join "`n"
+                                }
+                            }
+                            $Block | Add-Member -NotePropertyName 'content' -NotePropertyValue $BlockContent -Force
+                            $Block | Add-Member -NotePropertyName 'static' -NotePropertyValue $true -Force
+                        } else {
+                            $Block | Add-Member -NotePropertyName 'content' -NotePropertyValue 'No data available for this data source.' -Force
+                        }
+                    } catch {
+                        $DbError = Get-CippException -Exception $_
+                        Write-LogMessage -API 'ReportBuilder' -tenant $TenantFilter -message "Failed to fetch database data for type $($Block.dbType): $($DbError.NormalizedError)" -Sev 'Warning' -LogData $DbError
+                        $Block | Add-Member -NotePropertyName 'content' -NotePropertyValue "Error fetching data: $($DbError.NormalizedError)" -Force
+                    }
+                }
+
                 $Block
             })
 
