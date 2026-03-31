4a3f3939a6e4b6322881ce996f838436196c0734
Bobby <31723128+kris6673@users.noreply.github.com>
2026-03-31 23:01:46 +0200

    feat: More pages gets to have alltenant support

---
+++ Modules/CIPPCore/Public/Entrypoints/Activity Triggers/Push-ListAntiPhishingFiltersAllTenants.ps1
@@ -0,0 +1,50 @@
+﻿function Push-ListAntiPhishingFiltersAllTenants {
+    <#
+    .FUNCTIONALITY
+        Entrypoint
+    #>
+    param($Item)
+
+    $Tenant = Get-Tenants -TenantFilter $Item.customerId
+    $DomainName = $Tenant.defaultDomainName
+    $Table = Get-CIPPTable -TableName CacheAntiPhishingFilters
+
+    try {
+        $Policies = New-ExoRequest -tenantid $DomainName -cmdlet 'Get-AntiPhishPolicy' | Select-Object -Property *
+        $Rules = New-ExoRequest -tenantid $DomainName -cmdlet 'Get-AntiPhishRule' | Select-Object -Property *
+
+        $Output = $Policies | Select-Object -Property *,
+        @{ Name = 'RuleName'; Expression = { foreach ($item in $Rules) { if ($item.AntiPhishPolicy -eq $_.Name) { $item.Name } } } },
+        @{ Name = 'Priority'; Expression = { foreach ($item in $Rules) { if ($item.AntiPhishPolicy -eq $_.Name) { $item.Priority } } } },
+        @{ Name = 'RecipientDomainIs'; Expression = { foreach ($item in $Rules) { if ($item.AntiPhishPolicy -eq $_.Name) { $item.RecipientDomainIs } } } },
+        @{ Name = 'State'; Expression = { foreach ($item in $Rules) { if ($item.AntiPhishPolicy -eq $_.Name) { $item.State } } } }
+
+        foreach ($Item in $Output) {
+            $GUID = (New-Guid).Guid
+            $PolicyData = $Item | Select-Object *, @{n = 'Tenant'; e = { $DomainName } }
+            $Entity = @{
+                Policy       = [string]($PolicyData | ConvertTo-Json -Depth 10 -Compress)
+                RowKey       = [string]$GUID
+                PartitionKey = 'AntiPhishingFilter'
+                Tenant       = [string]$DomainName
+            }
+            Add-CIPPAzDataTableEntity @Table -Entity $Entity -Force | Out-Null
+        }
+
+    } catch {
+        $GUID = (New-Guid).Guid
+        $ErrorPolicy = ConvertTo-Json -InputObject @{
+            Tenant      = $DomainName
+            RuleName    = "Could not connect to Tenant: $($_.Exception.Message)"
+            Name        = 'Error'
+            id          = 'Error'
+        } -Compress
+        $Entity = @{
+            Policy       = [string]$ErrorPolicy
+            RowKey       = [string]$GUID
+            PartitionKey = 'AntiPhishingFilter'
+            Tenant       = [string]$DomainName
+        }
+        Add-CIPPAzDataTableEntity @Table -Entity $Entity -Force | Out-Null
+    }
+}
---
+++ Modules/CIPPCore/Public/Entrypoints/Activity Triggers/Push-ListRoomListsAllTenants.ps1
@@ -0,0 +1,56 @@
+﻿function Push-ListRoomListsAllTenants {
+    <#
+    .FUNCTIONALITY
+        Entrypoint
+    #>
+    param($Item)
+
+    $Tenant = Get-Tenants -TenantFilter $Item.customerId
+    $DomainName = $Tenant.defaultDomainName
+    $Table = Get-CIPPTable -TableName CacheRoomLists
+
+    try {
+        $RoomLists = New-ExoRequest -tenantid $DomainName -cmdlet 'Get-DistributionGroup' -cmdParams @{
+            RecipientTypeDetails = 'RoomList'
+            ResultSize           = 'Unlimited'
+        } | Select-Object Guid, DisplayName, PrimarySmtpAddress, Alias, Phone, Identity, Notes, Description, Id -ExcludeProperty *data.type*
+
+        foreach ($Item in $RoomLists) {
+            $GUID = (New-Guid).Guid
+            $PolicyData = [PSCustomObject]@{
+                Guid               = $Item.Guid
+                DisplayName        = $Item.DisplayName
+                PrimarySmtpAddress = $Item.PrimarySmtpAddress
+                Alias              = $Item.Alias
+                Phone              = $Item.Phone
+                Identity           = $Item.Identity
+                Notes              = $Item.Notes
+                Description        = $Item.Description
+                MailNickname       = $Item.Alias
+                Tenant             = $DomainName
+            }
+            $Entity = @{
+                Policy       = [string]($PolicyData | ConvertTo-Json -Depth 10 -Compress)
+                RowKey       = [string]$GUID
+                PartitionKey = 'RoomList'
+                Tenant       = [string]$DomainName
+            }
+            Add-CIPPAzDataTableEntity @Table -Entity $Entity -Force | Out-Null
+        }
+
+    } catch {
+        $GUID = (New-Guid).Guid
+        $ErrorPolicy = ConvertTo-Json -InputObject @{
+            Tenant      = $DomainName
+            DisplayName = "Could not connect to Tenant: $($_.Exception.Message)"
+            id          = 'Error'
+        } -Compress
+        $Entity = @{
+            Policy       = [string]$ErrorPolicy
+            RowKey       = [string]$GUID
+            PartitionKey = 'RoomList'
+            Tenant       = [string]$DomainName
+        }
+        Add-CIPPAzDataTableEntity @Table -Entity $Entity -Force | Out-Null
+    }
+}
