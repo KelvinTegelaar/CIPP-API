e2f71c27fe19d4475c5db20bd9531ddb1a9c3180
John Duprey <jwd@johnwduprey.com>
2026-04-28 17:30:26 -0400

    fix: group types in edit user

--- Modules/CIPPHTTP/Public/Entrypoints/HTTP Functions/Identity/Administration/Users/Invoke-EditUser.ps1
+++ Modules/CIPPHTTP/Public/Entrypoints/HTTP Functions/Identity/Administration/Users/Invoke-EditUser.ps1
@@ -190,7 +190,7 @@ function Invoke-EditUser {
             Write-Host "About to add $($UserObj.userPrincipalName) to $GroupName. Group ID is: $GroupID and type is: $GroupType"
 
             try {
-                if ($GroupType -eq 'Distribution list' -or $GroupType -eq 'Mail-Enabled Security') {
+                if ($GroupType -eq 'distributionList' -or $GroupType -eq 'security') {
                     Write-Host 'Adding to group via Add-DistributionGroupMember'
                     $Params = @{ Identity = $GroupID; Member = $UserObj.id; BypassSecurityGroupManagerCheck = $true }
                     $null = New-ExoRequest -tenantid $UserObj.tenantFilter -cmdlet 'Add-DistributionGroupMember' -cmdParams $params -UseSystemMailbox $true
@@ -222,7 +222,7 @@ function Invoke-EditUser {
             Write-Host "About to remove $($UserObj.userPrincipalName) from $GroupName. Group ID is: $GroupID and type is: $GroupType"
 
             try {
-                if ($GroupType -eq 'Distribution list' -or $GroupType -eq 'Mail-Enabled Security') {
+                if ($GroupType -eq 'distributionList' -or $GroupType -eq 'security') {
                     Write-Host 'Removing From group via Remove-DistributionGroupMember'
                     $Params = @{ Identity = $GroupID; Member = $UserObj.id; BypassSecurityGroupManagerCheck = $true }
                     $null = New-ExoRequest -tenantid $UserObj.tenantFilter -cmdlet 'Remove-DistributionGroupMember' -cmdParams $params -UseSystemMailbox $true
