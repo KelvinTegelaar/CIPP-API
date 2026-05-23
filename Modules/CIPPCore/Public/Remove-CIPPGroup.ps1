a89c2b9333ba7a1bca50fda53145894835de8f80
Luis Mengel <luis.mengel@united-gameserver.de>
2026-05-23 15:20:47 +0200

    Add Group-Based Licensing support

--- Modules/CIPPCore/Public/Remove-CIPPGroup.ps1
+++ Modules/CIPPCore/Public/Remove-CIPPGroup.ps1
@@ -16,6 +16,14 @@ function Remove-CIPPGroup {
             return "Successfully Deleted $($GroupType) group $($DisplayName)"
 
         } elseif ($GroupType -eq 'Microsoft 365' -or $GroupType -eq 'Security') {
+            # Remove any assigned licenses
+            if ($GroupType -eq 'Security') {
+                $Group = New-GraphGetRequest -uri "https://graph.microsoft.com/v1.0/groups/$($ID)?`$select=assignedLicenses" -tenantid $TenantFilter
+                $CurrentSkus = @($Group.assignedLicenses.skuId | Where-Object { $_ })
+                if ($CurrentSkus.Count -gt 0) {
+                    $null = Set-CIPPGroupLicense -GroupId $ID -GroupName $DisplayName -RemoveLicenses $CurrentSkus -TenantFilter $TenantFilter -Headers $Headers -APIName $APIName
+                }
+            }
             $null = New-GraphPostRequest -uri "https://graph.microsoft.com/v1.0/groups/$($ID)" -tenantid $TenantFilter -type Delete -verbose
             Write-LogMessage -headers $Headers -API $APINAME -tenant $($TenantFilter) -message "$($DisplayName) Deleted" -Sev 'Info'
             return "Successfully Deleted $($GroupType) group $($DisplayName)"
