7ae8fcdbda8f6fa30058cd704c6667f1f6e3739c
DamienMatthys <damien@pcunplug.com>
2026-06-10 20:14:35 +1000

    Update Push-NinjaOneQueue.ps1
    Remove old sync schedule
    
    Signed-off-by: DamienMatthys <damien@pcunplug.com>

--- Modules/CippExtensions/Public/NinjaOne/Push-NinjaOneQueue.ps1
+++ Modules/CippExtensions/Public/NinjaOne/Push-NinjaOneQueue.ps1
@@ -10,7 +10,6 @@ function Push-NinjaOneQueue {
         'AutoMapTenant'    { Invoke-NinjaOneOrgMappingTenant -QueueItem $Item }
         'SyncTenant'       { Invoke-NinjaOneTenantSync -QueueItem $Item }
         'SyncTenants'      { Invoke-NinjaOneSync }
-        'CveSyncTenant'    { Invoke-NinjaOneCveSyncTenant -QueueItem $Item }
     }
     return $true
 }
