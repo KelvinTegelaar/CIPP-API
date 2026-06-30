6205e97c2cbf4dfb58f20878dd3b76b1b4169b69
Bobby <31723128+kris6673@users.noreply.github.com>
2026-06-30 13:46:30 +0200

    fix: modernize and improve logging

--- Modules/CIPPHTTP/Public/Entrypoints/HTTP Functions/Tenant/GDAP/Invoke-ExecDeleteGDAPRelationship.ps1
+++ Modules/CIPPHTTP/Public/Entrypoints/HTTP Functions/Tenant/GDAP/Invoke-ExecDeleteGDAPRelationship.ps1
@@ -1,4 +1,4 @@
-Function Invoke-ExecDeleteGDAPRelationship {
+function Invoke-ExecDeleteGDAPRelationship {
     <#
     .FUNCTIONALITY
         Entrypoint,AnyTenant
@@ -13,19 +13,23 @@ Function Invoke-ExecDeleteGDAPRelationship {
 
 
     # Interact with query parameters or the body of the request.
-    $GDAPID = $Request.Query.GDAPId ?? $Request.Body.GDAPId
+    $GDAPId = $Request.Query.GDAPId ?? $Request.Body.GDAPId
     try {
-        $DELETE = New-GraphPostRequest -NoAuthCheck $True -uri "https://graph.microsoft.com/beta/tenantRelationships/delegatedAdminRelationships/$($GDAPID)/requests" -type POST -body '{"action":"terminate"}' -tenantid $env:TenantID
-        $Results = [pscustomobject]@{'Results' = "Success. GDAP relationship for $($GDAPID) been revoked" }
-        Write-LogMessage -headers $Headers -API $APIName -message "Success. GDAP relationship for $($GDAPID) been revoked" -Sev 'Info'
+        $null = New-GraphPostRequest -NoAuthCheck $True -uri "https://graph.microsoft.com/beta/tenantRelationships/delegatedAdminRelationships/$($GDAPId)/requests" -type POST -body '{"action":"terminate"}' -tenantid $env:TenantID
+        $Result = "Success. GDAP relationship for $($GDAPId) been revoked"
+        Write-LogMessage -headers $Headers -API $APIName -message $Result -Sev 'Info'
+        $StatusCode = [HttpStatusCode]::OK
 
     } catch {
-        $Results = [pscustomobject]@{'Results' = "Failed. $($_.Exception.Message)" }
+        $ErrorMessage = Get-CippException -Exception $_
+        $Result = "Failed to revoke GDAP relationship for $($GDAPId). Error: $($ErrorMessage.NormalizedError)"
+        $StatusCode = [HttpStatusCode]::InternalServerError
+        Write-LogMessage -headers $Headers -API $APIName -message $Result -Sev 'Error' -LogData $ErrorMessage
     }
 
     return ([HttpResponseContext]@{
-            StatusCode = [HttpStatusCode]::OK
-            Body       = $Results
+            StatusCode = $StatusCode
+            Body       = @{ 'Results' = $Result }
         })
 
 }
